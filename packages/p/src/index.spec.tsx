e834f91ddb30bbd346342641bee63ea195a8496d
Bu Kinoshita <6929565+bukinoshita@users.noreply.github.com>
2022-09-23 02:40:34 -0300

    fix(p): Render email dependency (#60)

--- packages/p/src/index.spec.tsx
+++ packages/p/src/index.spec.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { P } from './index';
-import { render } from '../../render/src/index';
+import { render } from '@react-email/render';
 
 describe('render', () => {
   beforeEach(() => {
