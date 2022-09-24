40397efa3a3d8cdf309cb572488c7ece605b4bdd
Bu Kinoshita <6929565+bukinoshita@users.noreply.github.com>
2022-09-24 13:22:00 -0300

    fix(ul): Test (#86)

--- packages/ul/src/ul.tsx
+++ packages/ul/src/ul.tsx
@@ -1,20 +1,14 @@
-import * as React from "react";
+import * as React from 'react';
 
-type UlElement = React.ElementRef<"ul">;
-type RootProps = React.ComponentPropsWithoutRef<"ul">;
+type UlElement = React.ElementRef<'ul'>;
+type RootProps = React.ComponentPropsWithoutRef<'ul'>;
 
 export interface UlProps extends RootProps {}
 
 export const Ul = React.forwardRef<UlElement, Readonly<UlProps>>(
   ({ style, ...props }, forwardedRef) => (
-    <ul
-      ref={forwardedRef}
-      style={{        
-        ...style,
-      }}
-      {...props}
-    />
-  )
+    <ul ref={forwardedRef} style={style} {...props} />
+  ),
 );
 
-Ul.displayName = "Ul";
+Ul.displayName = 'Ul';
