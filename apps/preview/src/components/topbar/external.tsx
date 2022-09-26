df1d2bd8f951a5d35e84c9296981155968fc03b7
Zeno Rocha <zno.rocha@gmail.com>
2022-09-25 20:34:55 -0700

    feat(design-system): Add tooltip component #25

--- apps/preview/src/components/topbar/external.tsx
+++ apps/preview/src/components/topbar/external.tsx
@@ -1,29 +1,37 @@
 import * as React from 'react';
 import launchIcon from '../../helpers/launch-icon.json';
 import Lottie, { LottieRefCurrentProps } from 'lottie-react';
+import { Tooltip } from 'design-system';
 
 export default function External() {
   const launchIconRef = React.useRef<LottieRefCurrentProps>(null);
 
   return (
-    <a
-      href="/api/preview"
-      target="_blank"
-      className="text-current transition duration-300 ease-in-out hover:opacity-60"
-      onMouseEnter={() => {
-        launchIconRef.current?.play();
-      }}
-      onMouseLeave={() => {
-        launchIconRef.current?.stop();
-      }}
-    >
-      <Lottie
-        lottieRef={launchIconRef}
-        className="mr-1 w-5 h-5"
-        animationData={launchIcon}
-        loop={false}
-        autoplay={false}
-      />
-    </a>
+    <Tooltip>
+      <Tooltip.Trigger>
+        <a
+          href="/api/preview"
+          target="_blank"
+          className="text-current transition duration-300 ease-in-out hover:opacity-60"
+          onMouseEnter={() => {
+            launchIconRef.current?.play();
+          }}
+          onMouseLeave={() => {
+            launchIconRef.current?.stop();
+          }}
+        >
+          <Lottie
+            lottieRef={launchIconRef}
+            className="mr-1 w-5 h-5"
+            animationData={launchIcon}
+            loop={false}
+            autoplay={false}
+          />
+        </a>
+      </Tooltip.Trigger>
+      <Tooltip.Content>
+        Open in new tab
+      </Tooltip.Content>
+    </Tooltip>
   );
 }
--- apps/preview/src/pages/_app.tsx
+++ apps/preview/src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Tooltip } from 'design-system';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Tooltip.Provider>
+      <Component {...pageProps} />
+    </Tooltip.Provider>
+  );
 }
