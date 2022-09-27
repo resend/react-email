88e6941ce8928c8eba2ee121d02dc8f584a8e962
Bu Kinoshita <6929565+bukinoshita@users.noreply.github.com>
2022-09-27 18:37:37 -0300

    feat(utils): Add `pxToPt` util

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,3 +1,4 @@
 export * from './as';
 export * from './copy-text-to-clipboard';
+export * from './px-to-pt';
 export * from './style-to-string';
---
+++ packages/utils/src/px-to-pt.ts
@@ -0,0 +1,1 @@
+export const pxToPt = (px: string): number => (parseInt(px, 10) * 3) / 4;
