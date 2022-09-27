57d79737e9afb89422d349c7e2b517b5bd640c23
Bu Kinoshita <6929565+bukinoshita@users.noreply.github.com>
2022-09-27 19:26:57 -0300

    Update packages/design-system/src/icons/icon-download.tsx

--- packages/design-system/src/icons/icon-download.tsx
+++ packages/design-system/src/icons/icon-download.tsx
@@ -6,7 +6,7 @@ export const IconDownload = React.forwardRef<IconElement, Readonly<IconProps>>(
     <IconBase ref={forwardedRef} {...props}>
       <path
         d="M4.75 14.75v1.5a3 3 0 0 0 3 3h8.5a3 3 0 0 0 3-3v-1.5M12 14.25v-9.5M8.75 10.75l3.25 3.5 3.25-3.5"
-        stroke="#A09FA6"
+        stroke="currentColor"
         strokeWidth={1.5}
         strokeLinecap="round"
         strokeLinejoin="round"
