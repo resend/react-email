99aa61d694e405aa0a69de0eee7aee822a79c590
Bu Kinoshita <6929565+bukinoshita@users.noreply.github.com>
2022-09-28 17:27:24 -0300

    fix(button): Outlook support

--- apps/preview/src/components/vercel-invite-user.tsx
+++ apps/preview/src/components/vercel-invite-user.tsx
@@ -122,10 +122,12 @@ export const VercelInviteUser: React.FC<
                     <td align="center">
                       <div>
                         <Button
+                          pX={20}
+                          pY={12}
                           style={btn}
                           href="https://vercel.com/teams/invite/foo"
                         >
-                          JOIN TEAM
+                          Join the team
                         </Button>
                       </div>
                     </td>
@@ -239,7 +241,6 @@ const btn = {
   lineHeight: '50px',
   textDecoration: 'none',
   textAlign: 'center' as const,
-  width: '200px',
 };
 
 const spacing = {
