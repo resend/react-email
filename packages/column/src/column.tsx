e54636d3ef437ae7dfc08920297d92ebd5245dae
cassiorsfreitas <cassio.rsfreitas@gmail.com>
2022-11-02 11:08:21 +0000

    fix: run prettier write to formart file

--- packages/column/src/column.tsx
+++ packages/column/src/column.tsx
@@ -12,22 +12,21 @@ export const Column = React.forwardRef<DivElement, Readonly<ColumnProps>>(
     const styleDefault = {
       display: 'inline-block',
       width: '100%',
-      fontSize:'0px',
+      fontSize: '0px',
       verticalAlign: 'top',
-      ...style
-    } 
+      ...style,
+    };
 
-    return(
+    return (
       <div
         ref={forwardedRef}
         style={styleDefault}
         role="presentation"
-        {...props}>
-          <tbody>
-            {children}
-          </tbody>
+        {...props}
+      >
+        <tbody>{children}</tbody>
       </div>
-    )
+    );
   },
 );
 
