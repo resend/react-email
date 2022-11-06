25d30576ec5cb79e61762d89d0dab5b0265df187
Bu Kinoshita <6929565+bukinoshita@users.noreply.github.com>
2022-11-06 14:12:26 -0300

    feat(website): Add analytics

--- apps/website/src/pages/_app.tsx
+++ apps/website/src/pages/_app.tsx
@@ -1,7 +1,13 @@
 import '../styles/globals.css';
 import 'design-system/styles.css';
 import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
