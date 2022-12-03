5a38ed41d83b1b93605118bc191b41df7e62f937
Zeno Rocha <zno.rocha@gmail.com>
2022-12-03 10:25:52 -0300

    feat(create-email): Display success message

--- packages/create-email/src/index.js
+++ packages/create-email/src/index.js
@@ -1,11 +1,17 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --no-warnings
 
-const { Command } = require('commander');
-const path = require('path');
-const fse = require('fs-extra');
-const packageJson = require('../package.json');
+import { Command } from 'commander';
+import { fileURLToPath } from 'node:url';
+import fse from 'fs-extra';
+import logSymbols from 'log-symbols';
+import ora from 'ora';
+import path from 'node:path';
+import tree from 'tree-node-cli';
+import packageJson from '../package.json' assert { type: "json" };
 
 const init = (name) => {
+  const spinner = ora('Preparing files...\n').start();
+
   let projectPath = name;
 
   if (!projectPath) {
@@ -16,10 +22,24 @@ const init = (name) => {
     projectPath = projectPath.trim()
   }
 
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
   const templatePath = path.resolve(__dirname, '../template');
   const resolvedProjectPath = path.resolve(projectPath);
 
   fse.copySync(templatePath, resolvedProjectPath, { recursive: true });
+
+  const fileTree = tree(projectPath, {
+    allFiles: true,
+    exclude: [/node_modules/],
+    maxDepth: 4,
+  });
+
+  console.log(fileTree);
+
+  spinner.stopAndPersist({
+    symbol: logSymbols.success,
+    text: 'React Email Starter files ready',
+  });
 }
 
 const program = new Command()
