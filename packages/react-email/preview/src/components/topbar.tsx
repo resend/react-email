e8d3c90ea3ee3eaea9bd3e306e5cf93973b5a150
Zeno Rocha <zno.rocha@gmail.com>
2022-12-21 20:27:24 -0300

    feat(react-email): Add ability to send test email (#264)
    * feat(react-email): Add ability to send test email
    
    * feat(react-email): Error handling for limit of test email sends per day

--- packages/react-email/preview/src/components/topbar.tsx
+++ packages/react-email/preview/src/components/topbar.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import classnames from 'classnames';
 import { Heading } from './heading';
 import { Text } from './text';
+import { Send } from './send';
 import * as ToggleGroup from '@radix-ui/react-toggle-group';
 
 type TopbarElement = React.ElementRef<'header'>;
@@ -10,11 +11,14 @@ type RootProps = React.ComponentPropsWithoutRef<'header'>;
 interface TopbarProps extends RootProps {
   title: string;
   viewMode?: string;
+  markup?: string;
   setViewMode?: (viewMode: string) => void;
 }
 
 export const Topbar = React.forwardRef<TopbarElement, Readonly<TopbarProps>>(
-  ({ className, title, viewMode, setViewMode, ...props }, forwardedRef) => {
+  ({ className, title, markup, viewMode, setViewMode, ...props }, forwardedRef) => {
+    const columnWidth = 'w-[200px]';
+
     return (
       <header
         ref={forwardedRef}
@@ -24,70 +28,57 @@ export const Topbar = React.forwardRef<TopbarElement, Readonly<TopbarProps>>(
         )}
         {...props}
       >
-        <div className="flex items-center">
-          <Text size="2" color="gray" weight="medium">
-            All emails
-          </Text>
-          <svg
-            className="text-slate-11"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M10.75 8.75L14.25 12L10.75 15.25"
-              stroke="currentColor"
-              strokeOpacity="0.615"
-              strokeWidth="1.5"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-
-          <Heading as="h2" size="2" weight="medium">
+        <div className={`flex items-center overflow-hidden ${columnWidth}`}>
+          <Heading as="h2" size="2" weight="medium" className="truncate">
             {title}
           </Heading>
         </div>
 
-        {setViewMode && (
-          <ToggleGroup.Root
-            className="items-center bg-slate-2 p-1.5 flex gap-1 border border-slate-6 rounded-md absolute top-4 right-4"
-            type="single"
-            value={viewMode}
-            aria-label="View mode"
-            onValueChange={(value) => {
-              if (!value) {
-                return setViewMode('desktop');
-              }
-              setViewMode(value);
-            }}
-          >
-            <ToggleGroup.Item
-              className={classnames(
-                'text-sm text-slate-11 rounded px-1.5 py-0.5',
-                {
-                  'text-slate-12 bg-slate-3 font-medium':
-                    viewMode === 'desktop',
-                },
-              )}
-              value="desktop"
+        <div className={`${columnWidth}`}>
+          {setViewMode && (
+            <ToggleGroup.Root
+              className="w-[145px] m-auto items-center bg-slate-2 p-1.5 flex gap-1 border border-slate-6 rounded-md"
+              type="single"
+              value={viewMode}
+              aria-label="View mode"
+              onValueChange={(value) => {
+                if (!value) {
+                  return setViewMode('desktop');
+                }
+                setViewMode(value);
+              }}
             >
-              Desktop
-            </ToggleGroup.Item>
-            <ToggleGroup.Item
-              className={classnames(
-                'text-sm text-slate-11 rounded px-1.5 py-0.5',
-                {
-                  'text-slate-12 bg-slate-3 font-medium': viewMode === 'source',
-                },
-              )}
-              value="source"
-            >
-              Source
-            </ToggleGroup.Item>
-          </ToggleGroup.Root>
+              <ToggleGroup.Item
+                className={classnames(
+                  'text-sm text-slate-11 rounded px-1.5 py-0.5',
+                  {
+                    'text-slate-12 bg-slate-3 font-medium':
+                      viewMode === 'desktop',
+                  },
+                )}
+                value="desktop"
+              >
+                Desktop
+              </ToggleGroup.Item>
+              <ToggleGroup.Item
+                className={classnames(
+                  'text-sm text-slate-11 rounded px-1.5 py-0.5',
+                  {
+                    'text-slate-12 bg-slate-3 font-medium': viewMode === 'source',
+                  },
+                )}
+                value="source"
+              >
+                Source
+              </ToggleGroup.Item>
+            </ToggleGroup.Root>
+          )}
+        </div>
+
+        {markup && (
+          <div className={`flex justify-end ${columnWidth}`}>
+            <Send markup={markup} />
+          </div>
         )}
       </header>
     );
