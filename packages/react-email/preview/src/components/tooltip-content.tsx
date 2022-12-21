1b86bf20f5e8d726ed731c138591f32125f8b6fa
Zeno Rocha <zno.rocha@gmail.com>
2022-12-21 20:28:38 -0300

    chore(react-email): Improve tooltip styling

--- packages/react-email/preview/src/components/tooltip-content.tsx
+++ packages/react-email/preview/src/components/tooltip-content.tsx
@@ -19,7 +19,7 @@ export const TooltipContent = React.forwardRef<
       {...props}
       ref={forwardedRef}
       className={classnames(
-        'bg-slate-6 border border-slate-6 z-20 px-3 py-2 rounded-md text-xs',
+        'bg-black border border-slate-6 z-20 px-3 py-2 rounded-md text-xs',
         `${inter.variable} font-sans`,
       )}
       sideOffset={sideOffset}
