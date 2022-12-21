7f5041e5152e35588839432cde884c75c092c1ce
Bu Kinoshita <bukinoshita@gmail.com>
2022-12-21 12:09:41 -0300

    feat: Add components directory

--- packages/react-email/preview/src/pages/preview/[slug].tsx
+++ packages/react-email/preview/src/pages/preview/[slug].tsx
@@ -15,7 +15,9 @@ export const CONTENT_DIR = 'emails';
 const getEmails = async () => {
   const emailsDirectory = path.join(process.cwd(), CONTENT_DIR);
   const filenames = await fs.readdir(emailsDirectory);
-  const emails = filenames.map((file) => file.replace(/\.(jsx|tsx)$/g, ''));
+  const emails = filenames
+    .map((file) => file.replace(/\.(jsx|tsx)$/g, ''))
+    .filter((file) => file !== 'components');
   return { emails, filenames };
 };
 
