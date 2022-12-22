bc0343aec6fc03ad3584573e67cb88f5c66ec823
cassiorsfreitas <cassio.rsfreitas@gmail.com>
2022-12-22 15:55:16 +0000

    test: ensure <Section> renders with <td> wrapper if <Column> is provided

--- packages/section/src/section.spec.tsx
+++ packages/section/src/section.spec.tsx
@@ -20,4 +20,13 @@ describe('render', () => {
     );
     expect(actualOutput).toContain("<td>");
   });
+
+  it('renders the <Section> with <td> wrapper if <Column> is provided', () => {
+    const actualOutput = render(
+      <Section>
+        <td>Lorem ipsum</td>
+      </Section>
+    );
+    expect(actualOutput).toContain("<td>");
+  });
 });
