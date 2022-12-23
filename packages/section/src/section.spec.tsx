755cf89c21f90526e055605dfccb7e78071bce90
Cássio Freitas <cassio.rsfreitas@gmail.com>
2022-12-23 16:03:12 +0000

    feat(section): `Section` component provides td tag (#268)
    * feat: add method to handle the type of children
    
    * test: update snapshot
    
    * test: ensure <Section> renders with <td> wrapper if no <Column> is provided
    
    * test: ensure <Section> renders with <td> wrapper if <Column> is provided

--- packages/section/src/section.spec.tsx
+++ packages/section/src/section.spec.tsx
@@ -11,4 +11,22 @@ describe('render', () => {
     const actualOutput = render(<Section>Lorem ipsum</Section>);
     expect(actualOutput).toMatchSnapshot();
   });
+
+  it('renders the <Section> with <td> wrapper if no <Column> is provided', () => {
+    const actualOutput = render(
+      <Section>
+        <div>Lorem ipsum</div>
+      </Section>
+    );
+    expect(actualOutput).toContain("<td>");
+  });
+
+  it('renders the <Section> with <td> wrapper if <Column> is provided', () => {
+    const actualOutput = render(
+      <Section>
+        <td>Lorem ipsum</td>
+      </Section>
+    );
+    expect(actualOutput).toContain("<td>");
+  });
 });
