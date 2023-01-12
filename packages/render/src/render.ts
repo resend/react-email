515b6b9853caef671ed9e3b07d201deae4b367d1
Alec <alecharmon@outlook.com>
2023-01-12 10:31:16 -0500

    feat(react-email, render): Add Support for Plain Text (#320)

--- packages/render/src/render.ts
+++ packages/render/src/render.ts
@@ -1,11 +1,16 @@
 import * as ReactDomServer from 'react-dom/server';
+import { convert } from 'html-to-text';
 import pretty from 'pretty';
 
 interface Options {
   pretty?: boolean;
+  plainText?: boolean;
 }
 
 export const render = (component: React.ReactElement, options?: Options) => {
+  if (options?.plainText) {
+    return renderAsPlainText(component, options);
+  }
   const doctype =
     '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">';
   const markup = ReactDomServer.renderToStaticMarkup(component);
@@ -16,4 +21,13 @@ export const render = (component: React.ReactElement, options?: Options) => {
   }
 
   return document;
+};
+
+const renderAsPlainText = (
+  component: React.ReactElement,
+  _options?: Options,
+) => {
+  return convert(ReactDomServer.renderToStaticMarkup(component), {
+    selectors: [{ selector: 'img', format: 'skip' }],
+  });
 };
--- packages/render/src/utils/template.tsx
+++ packages/render/src/utils/template.tsx
@@ -7,6 +7,7 @@ interface TemplateProps {
 export const Template: React.VFC<Readonly<TemplateProps>> = ({ firstName }) => (
   <>
     <h1>Welcome, {firstName}!</h1>
+    <img src="img/test.png" alt="test" />
     <p>Thanks for trying our product. We're thrilled to have you on board!</p>
   </>
 );
