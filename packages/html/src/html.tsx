8c47b6539bf4f3c8307ab2307fbb615904c3ec10
Leonardo Pinheiro <leonardo.pinheiro.dev@gmail.com>
2023-01-18 03:47:21 -0300

    fix(container, html): Missing props (#349)

--- packages/html/src/html.tsx
+++ packages/html/src/html.tsx
@@ -1,17 +1,16 @@
 import * as React from 'react';
 
+type HtmlElement = React.ElementRef<'html'>;
 type RootProps = React.ComponentPropsWithoutRef<'html'>;
 
 export interface HtmlProps extends RootProps {}
 
-export const Html: React.FC<Readonly<HtmlProps>> = ({
-  children,
-  lang = 'en',
-  ...props
-}) => (
-  <html lang={lang} {...props}>
-    {children}
-  </html>
+export const Html = React.forwardRef<HtmlElement, Readonly<HtmlProps>>(
+  ({ children, lang = 'en', ...props }, forwardedRef) => (
+    <html ref={forwardedRef} lang={lang} {...props}>
+      {children}
+    </html>
+  ),
 );
 
 Html.displayName = 'Html';
