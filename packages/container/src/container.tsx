9d11e47de60d140672fdcab9b0a3dc3b938de900
Bu Kinoshita <6929565+bukinoshita@users.noreply.github.com>
2023-01-27 22:31:43 -0300

    refactor(container): API component (#398)

--- packages/container/src/container.tsx
+++ packages/container/src/container.tsx
@@ -1,40 +1,30 @@
 import * as React from 'react';
-import { styleToString } from '../../utils';
 
-type ContainerElement = React.ElementRef<'div'>;
-type RootProps = React.ComponentPropsWithoutRef<'div'>;
+type ContainerElement = React.ElementRef<'table'>;
+type RootProps = React.ComponentPropsWithoutRef<'table'>;
 
-export interface ContainerProps extends RootProps {
-  style?: React.CSSProperties;
-}
+export interface ContainerProps extends RootProps {}
 
 export const Container = React.forwardRef<
   ContainerElement,
   Readonly<ContainerProps>
 >(({ children, style, ...props }, forwardedRef) => {
-  const styles = { maxWidth: '37.5em', ...style };
-  const inlineStyle = styleToString(styles);
-
   return (
-    <>
-      <div
-        dangerouslySetInnerHTML={{
-          __html: `<!--[if mso | IE]>
-            <table role="presentation" width="100%" align="center" style="${inlineStyle}"><tr><td></td><td style="width:37.5em;background:#ffffff">
-          <![endif]-->`,
-        }}
-      />
-      <div ref={forwardedRef} style={styles} {...props}>
-        {children}
-      </div>
-      <div
-        dangerouslySetInnerHTML={{
-          __html: `<!--[if mso | IE]>
-          </td><td></td></tr></table>
-          <![endif]-->`,
-        }}
-      />
-    </>
+    <table
+      {...props}
+      ref={forwardedRef}
+      align="center"
+      role="presentation"
+      cellSpacing="0"
+      cellPadding="0"
+      border={0}
+      width="100%"
+      style={{ maxWidth: '37.5em', ...style }}
+    >
+      <tr style={{ width: '100%' }}>
+        <td>{children}</td>
+      </tr>
+    </table>
   );
 });
 
