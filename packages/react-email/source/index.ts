51d3a8673591021bdd166f046bd299bf0782da65
Zeno Rocha <zno.rocha@gmail.com>
2023-01-27 17:55:06 -0800

    feat(docs): Document `plainText` CLI flag

--- packages/react-email/source/index.ts
+++ packages/react-email/source/index.ts
@@ -20,7 +20,7 @@ program
   .description('Build the templates to the `out` directory')
   .option('--outDir <path>', 'Output directory', 'out')
   .option('-p, --pretty', 'Pretty print the output', false)
-  .option('-t, --plainText', 'Set output format as plain Text', false)
+  .option('-t, --plainText', 'Set output format as plain text', false)
   .action(({ outDir, pretty, plainText }) =>
     exportTemplates(outDir, { pretty, plainText }),
   );
