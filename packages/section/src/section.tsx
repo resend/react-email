09b72a83bdcaf1540b3529ce9b0c1713fd4df25e
Bu Kinoshita <6929565+bukinoshita@users.noreply.github.com>
2023-02-09 00:35:34 -0300

    refactor(section): Component (#456)

--- packages/section/src/section.tsx
+++ packages/section/src/section.tsx
@@ -3,53 +3,32 @@ import * as React from 'react';
 type SectionElement = React.ElementRef<'table'>;
 type RootProps = React.ComponentPropsWithoutRef<'table'>;
 
-export interface SectionProps extends RootProps {
-  style?: React.CSSProperties;
-}
+export interface SectionProps extends RootProps {}
 
 type ReactEmailChild = {
   type?: {
     displayName?: string;
-  }
+  };
 } & React.ReactNode;
 
 export const Section = React.forwardRef<SectionElement, Readonly<SectionProps>>(
   ({ children, style, ...props }, forwardedRef) => {
-    const styleDefaultTable = {
-      width: '100%',
-      ...style,
-    };
-
-    const styleDefaultTr = {
-      display: 'grid',
-      gridAutoColumns: 'minmax(0, 1fr)',
-      gridAutoFlow: 'column',
-    };
-
-    const arrayChildren = React.Children.toArray(children);
-
-    const hasTdElement = (child: ReactEmailChild) => {
-      return React.isValidElement(child) && (child.type === "td" || child.type.displayName === "Column");
-    };
-  
-    const finalChildren = arrayChildren.map((child: ReactEmailChild, index) => {
-      return hasTdElement(child) ? child : <td key={index}>{child}</td>;
-    });
-
-
     return (
       <table
+        {...props}
         ref={forwardedRef}
-        style={styleDefaultTable}
+        style={style}
         align="center"
         border={0}
-        cellPadding={0}
-        cellSpacing={0}
+        cellPadding="0"
+        cellSpacing="0"
         role="presentation"
         {...props}
       >
         <tbody>
-          <tr style={styleDefaultTr}>{finalChildren}</tr>
+          <tr>
+            <td>{children}</td>
+          </tr>
         </tbody>
       </table>
     );
