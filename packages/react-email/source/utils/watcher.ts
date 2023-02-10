2fa27e572bf81ea57f42e05b0709077e94d7a76e
bastiaanv <bastiankpn7800@gmail.com>
2023-02-10 08:52:41 +0100

    🐛 fix(#447): Replaced cpy with shelljs.cp

--- packages/react-email/source/utils/watcher.ts
+++ packages/react-email/source/utils/watcher.ts
@@ -7,7 +7,7 @@ import {
 } from './constants';
 import fs from 'fs';
 import path from 'path';
-import copy from 'cpy';
+import shell from 'shelljs';
 
 export const createWatcherInstance = (watchDir: string) =>
   chokidar.watch(watchDir, {
@@ -37,12 +37,28 @@ export const watcher = (watcherInstance: FSWatcher, watchDir: string) => {
 
     if (file[1] === 'static' && file[2]) {
       const srcPath = path.join(watchDir, 'static', file[2]);
-      await copy(srcPath, `${REACT_EMAIL_ROOT}/public/static`);
+      const result = shell.cp(
+        '-r',
+        srcPath,
+        path.join(REACT_EMAIL_ROOT, 'public', 'static'),
+      );
+      if (result.code > 0) {
+        throw new Error(
+          `Something went wrong while copying the file to ${PACKAGE_EMAILS_PATH}, ${result.cat()}`,
+        );
+      }
       return;
     }
-    await copy(
+
+    const result = shell.cp(
+      '-r',
       path.join(watchDir, ...file.slice(1)),
       path.join(PACKAGE_EMAILS_PATH, ...file.slice(1, -1)),
     );
+    if (result.code > 0) {
+      throw new Error(
+        `Something went wrong while copying the file to ${PACKAGE_EMAILS_PATH}, ${result.cat()}`,
+      );
+    }
   });
 };
