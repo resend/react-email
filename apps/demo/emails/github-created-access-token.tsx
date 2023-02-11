b4adb818af0fac12eb64dd49b37dfb2bb5bc7f4f
bruno88cabral@gmail.com <bruno88cabral@gmail.com>
2023-02-11 17:50:36 -0300

    feat: add new example - github

---
+++ apps/demo/emails/github-created-access-token.tsx
@@ -0,0 +1,121 @@
+import { Body } from '@react-email/body';
+import { Button } from '@react-email/button';
+import { Container } from '@react-email/container';
+import { Head } from '@react-email/head';
+import { Hr } from '@react-email/hr';
+import { Html } from '@react-email/html';
+import { Img } from '@react-email/img';
+import { Link } from '@react-email/link';
+import { Preview } from '@react-email/preview';
+import { Section } from '@react-email/section';
+import { Text } from '@react-email/text';
+import * as React from 'react';
+
+interface EmailProps {
+  userFirstname: string;
+}
+
+const baseUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : '';
+
+export default function Email({ userFirstname = 'Zeno' }: EmailProps) {
+  return (
+    <Html>
+      <Head />
+      <Preview>
+        A fine-grained personal access token has been added to your account
+      </Preview>
+      <Body style={main}>
+        <Container style={container}>
+          <Img
+            src={`${baseUrl}/static/github.png`}
+            width="32"
+            height="32"
+            alt="Github"
+          />
+
+          <Text style={title}>
+            <strong>@zenorocha</strong>, a personal access was created on your
+            account.
+          </Text>
+
+          <Section style={section}>
+            <Text style={text}>
+              Hey <strong>zenorocha</strong>!
+            </Text>
+            <Text style={text}>
+              A fine-grained personal access token (<Link>resend</Link>) was
+              recently added to your account.
+            </Text>
+
+            <Button style={button}>View your token</Button>
+          </Section>
+          <Text style={links}>
+            <Link style={link}>Your security audit log</Link> ・{' '}
+            <Link style={link}>Contact support</Link>
+          </Text>
+
+          <Text style={footer}>
+            GitHub, Inc. ・88 Colin P Kelly Jr Street ・San Francisco, CA 94107
+          </Text>
+        </Container>
+      </Body>
+    </Html>
+  );
+}
+
+const main = {
+  backgroundColor: '#ffffff',
+  color: '#24292e',
+  fontFamily:
+    '-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji"',
+};
+
+const container = {
+  width: '480px',
+  margin: '0 auto',
+  padding: '20px 0 48px',
+};
+
+const title = {
+  fontSize: '24px',
+  lineHeight: 1.25,
+};
+
+const section = {
+  padding: '24px',
+  border: 'solid 1px #dedede',
+  borderRadius: '5px',
+  textAlign: 'center' as const,
+};
+
+const text = {
+  margin: '0 0 10px 0',
+  textAlign: 'left' as const,
+};
+
+const button = {
+  fontSize: '14px',
+  backgroundColor: '#28a745',
+  color: '#fff',
+  lineHeight: 1.5,
+  borderRadius: '0.5em',
+  padding: '0.75em 1.5em',
+};
+
+const links = {
+  textAlign: 'center' as const,
+};
+
+const link = {
+  color: '#0366d6',
+  fontSize: '12px',
+};
+
+const footer = {
+  color: '#6a737d',
+  fontSize: '12px',
+  textAlign: 'center' as const,
+  marginTop: '60px',
+};
