6d22418e37da170e486df29b5cf67d0c207eb1c7
Abdullah Mzaien <xa300600@hotmail.com>
2023-02-13 18:10:26 +0300

    feat(html): add direction prop to head component

--- packages/html/src/html.tsx
+++ packages/html/src/html.tsx
@@ -6,8 +6,8 @@ type RootProps = React.ComponentPropsWithoutRef<'html'>;
 export interface HtmlProps extends RootProps {}
 
 export const Html = React.forwardRef<HtmlElement, Readonly<HtmlProps>>(
-  ({ children, lang = 'en', ...props }, forwardedRef) => (
-    <html {...props} id="__react-email" ref={forwardedRef} lang={lang}>
+  ({ children, lang = 'en', dir = "ltr", ...props }, forwardedRef) => (
+    <html {...props} id="__react-email" ref={forwardedRef} lang={lang} dir={dir}>
       {children}
     </html>
   ),
