20b30c1fd1e376b350475a540251158c9112d8cb
Vinicius de Moraes <vinicoder@gmail.com>
2023-02-26 17:08:43 -0300

    chore(tailwind): Add support for responsive classes and custom nested components (#502)

--- packages/tailwind/src/tailwind.tsx
+++ packages/tailwind/src/tailwind.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { renderToStaticMarkup } from 'react-dom/server';
+import htmlParser, { attributesToProps, Element } from 'html-react-parser';
 import { tailwindToCSS, TailwindConfig } from 'tw-to-css';
 
 export interface TailwindProps {
@@ -6,32 +8,116 @@ export interface TailwindProps {
   config?: TailwindConfig;
 }
 
-export const Tailwind: React.FC<Readonly<TailwindProps>> = ({
-  children,
-  config,
-}) => {
-  const { twj } = tailwindToCSS({ config });
+export const Tailwind: React.FC<TailwindProps> = ({ children, config }) => {
+  const { twi } = tailwindToCSS({
+    config,
+  });
 
-  const replaceTailwindStyles = (child: React.ReactNode): React.ReactNode => {
-    if (!React.isValidElement(child)) {
-      return child;
-    }
+  const newChildren = React.Children.toArray(children);
 
-    const { className, children: childChildren, style, ...rest } = child.props;
+  const fullHTML = renderToStaticMarkup(<>{newChildren}</>);
 
-    return React.cloneElement(child, {
-      ...rest,
-      children: React.Children.map(childChildren, replaceTailwindStyles),
-      style: { ...style, ...(className ? twj(className) : {}) },
-    });
-  };
-
-  const tailwindStylesToCSS = React.Children.map(
-    children,
-    replaceTailwindStyles,
+  const headStyle = getMediaQueryCSS(
+    twi(fullHTML, {
+      merge: false,
+      ignoreMediaQueries: false,
+    }),
   );
 
-  return <>{tailwindStylesToCSS}</>;
+  const hasResponsiveStyles = /@media[^{]+\{(?<content>[\s\S]+?)\}\s*\}/gm.test(
+    headStyle,
+  );
+  const hasHTML = /<html[^>]*>/gm.test(fullHTML);
+  const hasHead = /<head[^>]*>/gm.test(fullHTML);
+
+  if (hasResponsiveStyles && !hasHTML && !hasHead) {
+    throw new Error(
+      'Tailwind: To use responsive styles you must have a <html> and <head> element in your template.',
+    );
+  }
+
+  const reactHTML = React.Children.map(newChildren, (child) => {
+    if (!React.isValidElement(child)) return child;
+
+    const html = renderToStaticMarkup(child);
+
+    const parsedHTML = htmlParser(html, {
+      replace: (domNode) => {
+        if (domNode instanceof Element) {
+          if (hasResponsiveStyles && hasHead && domNode.name === 'head') {
+            let newDomNode: JSX.Element | null = null;
+
+            if (domNode.children) {
+              const style = domNode.children.find(
+                (child) => child instanceof Element && child.name === 'style',
+              );
+
+              const props = attributesToProps(domNode.attribs);
+
+              newDomNode = (
+                <head {...props}>
+                  {style && style instanceof Element ? (
+                    <style>{`${style.children} ${headStyle}`}</style>
+                  ) : (
+                    <style>{headStyle}</style>
+                  )}
+                </head>
+              );
+            }
+
+            return newDomNode;
+          }
+
+          if (domNode.attribs?.class) {
+            if (hasResponsiveStyles) {
+              domNode.attribs.class = domNode.attribs.class.replace(
+                /[:#\!\-[\]]+/g,
+                '_',
+              );
+            } else {
+              domNode.attribs.style = `${twi(domNode.attribs.class)} ${
+                domNode.attribs.style || ''
+              }`;
+              delete domNode.attribs.class;
+            }
+          }
+        }
+      },
+    });
+
+    return parsedHTML;
+  });
+
+  return <>{reactHTML}</>;
 };
 
 Tailwind.displayName = 'Tailwind';
+
+function getMediaQueryCSS(css: string) {
+  const mediaQueryRegex = /@media[^{]+\{(?<content>[\s\S]+?)\}\s*\}/gm;
+
+  let newCss = css
+    .replace(mediaQueryRegex, (m) => {
+      return m.replace(
+        /([^{]+\{)([\s\S]+?)(\}\s*\})/gm,
+        (_, start, content, end) => {
+          const newcontent = (content as string).replace(
+            /(?:[\s\r\n]*)?(?<prop>[\w-]+)\s*:\s*(?<value>[^;\r\n]+)/gm,
+            (_, prop, value) => {
+              return `${prop}: ${value} !important`;
+            },
+          );
+
+          return `${start}${newcontent}${end}`;
+        },
+      );
+    })
+    .replace(/[.\!\#\w\d\\:\-\[\]]+\s*?{/g, (m) => {
+      return m.replace(/[:#\!\-[\\\]]+/g, '_');
+    })
+    .replace(/font-family(?<value>[^;\r\n]+)/g, (m, value) => {
+      return `font-family${value.replace(/['"]+/g, '')}`;
+    });
+
+  return newCss;
+}
