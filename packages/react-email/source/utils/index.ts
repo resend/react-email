70777682117557618d24c6c9ed1197bf67573e9e
Cody Miller <hello@looskie.com>
2023-03-06 13:30:15 -0500

    refactor: move convert-to-absolute-path into its own utility

--- packages/react-email/source/utils/index.ts
+++ packages/react-email/source/utils/index.ts
@@ -2,5 +2,6 @@ export * from './check-directory-exist';
 export * from './check-empty-directory';
 export * from './check-is-up-to-date';
 export * from './constants';
+export * from './convert-to-absolute-path';
 export * from './create-directory';
 export * from './watcher';
