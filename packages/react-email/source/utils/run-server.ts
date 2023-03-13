869ab57f56aae11416b9c60d133bdcd037c92268
Cody Miller <50378828+Looskie@users.noreply.github.com>
2023-03-13 00:31:02 -0400

    refactor + feat: allow building and starting for setupServer + refactor name to setupServer

--- packages/react-email/source/utils/run-server.ts
+++ packages/react-email/source/utils/run-server.ts
@@ -9,21 +9,50 @@ import {
   PackageManager,
   syncPkg,
   generateEmailsPreview,
+  buildProdServer,
+  startProdServer,
+  REACT_EMAIL_ROOT,
 } from '.';
+import path from 'path';
+import shell from 'shelljs';
 
-export const runServer = async (dir: string, port: string) => {
+/**
+ * Utility function to run init/sync for the server in dev, build or start mode.
+ *
+ * @param type dev | build | start
+ * @param dir Directory in which the emails are located, only for dev and build, unused for start.
+ * @param port The port on which the server will run, only for dev and start, unused for build.
+ */
+export const setupServer = async (
+  type: 'dev' | 'build' | 'start',
+  dir: string,
+  port: string,
+) => {
   const cwd = await findRoot(CURRENT_PATH).catch(() => ({
     rootDir: CURRENT_PATH,
   }));
   const emailDir = convertToAbsolutePath(dir);
-  const watcherInstance = createWatcherInstance(emailDir);
   const packageManager: PackageManager = await detectPackageManager({
     cwd: cwd.rootDir,
   }).catch(() => 'npm');
 
-  await generateEmailsPreview(emailDir);
-  await syncPkg();
-  await installDependencies(packageManager);
-  startDevServer(packageManager, port);
-  watcher(watcherInstance, emailDir);
+  // when starting, we dont need to worry about these because it should've already happened during the build stage.
+  if (type !== 'start') {
+    await generateEmailsPreview(emailDir);
+    await syncPkg();
+    await installDependencies(packageManager);
+  }
+
+  if (type === 'dev') {
+    const watcherInstance = createWatcherInstance(emailDir);
+
+    startDevServer(packageManager, port);
+    watcher(watcherInstance, emailDir);
+  } else if (type === 'build') {
+    buildProdServer(packageManager);
+  } else {
+    shell.cd(path.join(REACT_EMAIL_ROOT));
+
+    startProdServer(packageManager, port);
+  }
 };
