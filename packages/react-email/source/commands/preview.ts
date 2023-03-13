af194029b9f0aadf91b49107992ed1b9bf80e64e
Cody Miller <50378828+Looskie@users.noreply.github.com>
2023-03-13 00:30:01 -0400

    refactor: use new setupServer utility

--- packages/react-email/source/commands/preview.ts
+++ packages/react-email/source/commands/preview.ts
@@ -1,33 +1,24 @@
-import { findRoot } from '@manypkg/find-root';
-import { detect as detectPackageManager } from 'detect-package-manager';
-import path from 'path';
+import fs from 'fs';
 import shell from 'shelljs';
-import {
-  convertToAbsolutePath,
-  CURRENT_PATH,
-  REACT_EMAIL_ROOT,
-} from '../utils';
-import { initOrSync } from './dev';
+import { downloadClient, REACT_EMAIL_ROOT } from '../utils';
+import { setupServer } from '../utils/run-server';
 
 interface BuildPreviewArgs {
   dir: string;
 }
 
 export const buildPreview = async ({ dir }: BuildPreviewArgs) => {
-  const emailDir = convertToAbsolutePath(dir);
-
   try {
-    const cwd = await findRoot(CURRENT_PATH).catch(() => ({
-      rootDir: CURRENT_PATH,
-    }));
-    const packageManager = await detectPackageManager({
-      cwd: cwd.rootDir,
-    }).catch(() => 'npm' as const);
+    if (fs.existsSync(REACT_EMAIL_ROOT)) {
+      await setupServer('build', dir, '');
+      return;
+    }
 
-    await initOrSync(packageManager, emailDir);
+    await downloadClient();
 
-    buildProductionServer(packageManager);
+    await setupServer('build', dir, '');
   } catch (error) {
+    console.log(error);
     shell.exit(1);
   }
 };
@@ -38,32 +29,16 @@ interface StartPreviewArgs {
 
 export const startPreview = async ({ port }: StartPreviewArgs) => {
   try {
-    const cwd = await findRoot(CURRENT_PATH).catch(() => ({
-      rootDir: CURRENT_PATH,
-    }));
-    const packageManager = await detectPackageManager({
-      cwd: cwd.rootDir,
-    }).catch(() => 'npm' as const);
+    if (fs.existsSync(REACT_EMAIL_ROOT)) {
+      await setupServer('start', '', port);
+      return;
+    }
 
-    shell.cd(path.join(REACT_EMAIL_ROOT));
+    await downloadClient();
 
-    startProductionServer(packageManager, port);
+    await setupServer('start', '', port);
   } catch (error) {
+    console.log(error);
     shell.exit(1);
   }
-};
-
-const startProductionServer = (packageManager: string, port: string) => {
-  shell.exec(`${packageManager} run start -p ${port}`, {
-    async: true,
-  });
-};
-
-const buildProductionServer = (packageManager: string) => {
-  const process = shell.exec(`${packageManager} run build`, { async: true });
-
-  // if build fails for whatever reason, make sure the shell actually exits
-  process.on('close', (code) => {
-    shell.exit(code ?? undefined);
-  });
 };
