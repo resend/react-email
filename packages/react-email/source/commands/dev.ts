88c2bceb9c54d874e0bc41d99f59b02e5633a622
Bu Kinoshita <6929565+bukinoshita@users.noreply.github.com>
2023-03-21 17:44:57 -0300

    fix(react-email): Handle error when missing `emails` folder (#595)

--- packages/react-email/source/commands/dev.ts
+++ packages/react-email/source/commands/dev.ts
@@ -10,14 +10,18 @@ interface Args {
 
 export const dev = async ({ dir, port }: Args) => {
   try {
+    if (!fs.existsSync(dir)) {
+      throw new Error(`Missing ${dir} folder`);
+    }
+
     if (fs.existsSync(REACT_EMAIL_ROOT)) {
-      await setupServer("dev", dir, port);
+      await setupServer('dev', dir, port);
       return;
     }
 
     await downloadClient();
 
-    await setupServer("dev", dir, port);
+    await setupServer('dev', dir, port);
   } catch (error) {
     console.log(error);
     shell.exit(1);
