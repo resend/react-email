a109444ef6390d496704fded78084c71ad97a779
Niaz Morshed <77217706+NiazMorshed2007@users.noreply.github.com>
2023-03-21 21:27:55 +0600

    fix(web): `@next/font` warning (#588)
    Co-authored-by: Bu Kinoshita <6929565+bukinoshita@users.noreply.github.com>

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import '../styles/globals.css';
-import { Inter } from '@next/font/google';
+import { Inter } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/react';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
