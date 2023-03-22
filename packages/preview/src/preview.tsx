fc405f495bf9b16cd91c3a9e6c2cd3dc48a7db3d
Will O'Beirne <wbobeirne@gmail.com>
2023-03-22 16:29:52 -0500

    fix: truncate text over 150

--- packages/preview/src/preview.tsx
+++ packages/preview/src/preview.tsx
@@ -7,36 +7,37 @@ export interface PreviewProps extends RootProps {
   children: string | string[];
 }
 
-export const Preview = React.forwardRef<PreviewElement, Readonly<PreviewProps>>(
-  ({ children = "", ...props }, forwardedRef) => (
-    <div
-      ref={forwardedRef}
-      id="__react-email-preview"
-      style={{
-        display: "none",
-        overflow: "hidden",
-        lineHeight: "1px",
-        opacity: 0,
-        maxHeight: 0,
-        maxWidth: 0,
-      }}
-      {...props}
-    >
-      {children}
-      {renderWhiteSpace(children)}
-    </div>
-  )
-);
-
 const PREVIEW_MAX_LENGTH = 150;
 
-const renderWhiteSpace = (children: string | string[]) => {
-  const text = Array.isArray(children) ? children.join("") : children;
+export const Preview = React.forwardRef<PreviewElement, Readonly<PreviewProps>>(
+  ({ children = "", ...props }, forwardedRef) => {
+    let text = Array.isArray(children) ? children.join("") : children;
+    text = text.substr(0, PREVIEW_MAX_LENGTH);
+    return (
+      <div
+        ref={forwardedRef}
+        id="__react-email-preview"
+        style={{
+          display: "none",
+          overflow: "hidden",
+          lineHeight: "1px",
+          opacity: 0,
+          maxHeight: 0,
+          maxWidth: 0,
+        }}
+        {...props}
+      >
+        {text}
+        {renderWhiteSpace(text)}
+      </div>
+    );
+  }
+);
 
-  if (text.length > PREVIEW_MAX_LENGTH) {
+const renderWhiteSpace = (text: string) => {
+  if (text.length >= PREVIEW_MAX_LENGTH) {
     return null;
   }
-
   const whiteSpaceCodes = "\xa0\u200C\u200B\u200D\u200E\u200F\uFEFF";
   return <div>{whiteSpaceCodes.repeat(PREVIEW_MAX_LENGTH - text.length)}</div>;
 };
