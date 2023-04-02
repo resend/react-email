bf1abce27bf10e60bdc318d86819f3820efc6dcb
Mathis König <mathis@simpletechs.net>
2023-04-02 12:49:35 +0200

    support importing files which are outside of the emails/ folder

--- client/next.config.js
+++ client/next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
   swcMinify: true,
   experimental: {
     appDir: true,
+    externalDir: true // compile files that are located next to the .react-email directory
   },
 };
 
--- packages/react-email/source/utils/generate-email-preview.ts
+++ packages/react-email/source/utils/generate-email-preview.ts
@@ -1,10 +1,11 @@
 import logSymbols from 'log-symbols';
-import { PACKAGE_EMAILS_PATH, PACKAGE_PUBLIC_PATH } from './constants';
+import { DEFAULT_EMAILS_DIRECTORY, PACKAGE_EMAILS_PATH, PACKAGE_PUBLIC_PATH } from './constants';
 import fs from 'fs';
 import ora from 'ora';
 import shell from 'shelljs';
 import path from 'path';
 import fse from 'fs-extra';
+import glob from 'glob';
 
 export const generateEmailsPreview = async (emailDir: string) => {
   try {
@@ -29,12 +30,28 @@ const createEmailPreviews = async (emailDir: string) => {
     await fs.promises.rm(PACKAGE_EMAILS_PATH, { recursive: true });
   }
 
-  const result = shell.cp('-r', emailDir, PACKAGE_EMAILS_PATH);
+  const list = glob.sync(path.join(emailDir, '**/*.{jsx,tsx}'), {
+    absolute: true,
+  })
 
-  if (result.code > 0) {
-    throw new Error(
-      `Something went wrong while copying the file to ${PACKAGE_EMAILS_PATH}, ${result.cat()}`,
-    );
+  /**
+   * instead of copying all files, which would break and js/ts imports,
+   * we create placeholder files which just contain the following code:
+   * 
+   * import Mail from '../../path/to/emails/my-template.tsx`
+   * export default Mail 
+  */
+  for(const absoluteSrcFilePath of list) {
+    const fileName = absoluteSrcFilePath.split('/').pop()!
+    const targetFile = path.join(PACKAGE_EMAILS_PATH, absoluteSrcFilePath.replace(emailDir, ''))
+    const importPath = path.relative(path.dirname(targetFile),path.dirname(absoluteSrcFilePath))
+    
+    let importFile = path.join(importPath, fileName)
+    importFile = importFile.replace(path.extname(importFile), '')
+
+    const sourceCode = `import Mail from '${importFile}';export default Mail;`.replace(';', ';\n')
+    await fse.ensureDir(path.dirname(targetFile))
+    await fse.writeFile(targetFile, sourceCode)
   }
 };
 
