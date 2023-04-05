5de844837cd4927566d5984caad3141d3e466d28
Bu Kinoshita <6929565+bukinoshita@users.noreply.github.com>
2023-04-05 04:30:58 -0300

    fix(create-email): Required node (#623)

--- packages/create-email/src/index.js
+++ packages/create-email/src/index.js
@@ -1,29 +1,28 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander';
-import { fileURLToPath } from 'node:url';
-import fse from 'fs-extra';
-import logSymbols from 'log-symbols';
-import ora from 'ora';
-import path from 'node:path';
-import tree from 'tree-node-cli';
-import packageJson from '../package.json' assert { type: "json" };
+import { Command } from "commander";
+import { fileURLToPath } from "node:url";
+import fse from "fs-extra";
+import logSymbols from "log-symbols";
+import ora from "ora";
+import path from "node:path";
+import tree from "tree-node-cli";
 
 const init = (name) => {
-  const spinner = ora('Preparing files...\n').start();
+  const spinner = ora("Preparing files...\n").start();
 
   let projectPath = name;
 
   if (!projectPath) {
-    projectPath = path.join(process.cwd(), 'react-email-starter');
+    projectPath = path.join(process.cwd(), "react-email-starter");
   }
-    
-  if (typeof projectPath === 'string') {
-    projectPath = projectPath.trim()
+
+  if (typeof projectPath === "string") {
+    projectPath = projectPath.trim();
   }
 
   const __dirname = path.dirname(fileURLToPath(import.meta.url));
-  const templatePath = path.resolve(__dirname, '../template');
+  const templatePath = path.resolve(__dirname, "../template");
   const resolvedProjectPath = path.resolve(projectPath);
 
   fse.copySync(templatePath, resolvedProjectPath, { recursive: true });
@@ -38,14 +37,14 @@ const init = (name) => {
 
   spinner.stopAndPersist({
     symbol: logSymbols.success,
-    text: 'React Email Starter files ready',
+    text: "React Email Starter files ready",
   });
-}
+};
 
 const program = new Command()
-  .name(packageJson.name)
-  .version(packageJson.version)
-  .description(packageJson.description)
-  .arguments('[dir]', 'path to initiliaze the project')
+  .name("create-email")
+  .version("0.0.16")
+  .description("The easiest way to get started with React Email")
+  .arguments("[dir]", "path to initiliaze the project")
   .action(init)
   .parse(process.argv);
