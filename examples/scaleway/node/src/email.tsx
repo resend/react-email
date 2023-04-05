320b57957c9d641f41f413c6f3b6d387bc0e37e9
Nathanael Demacon <nathanael.dmc@outlook.fr>
2023-04-05 08:13:43 +0200

    feat(examples): Add Scaleway integration (#605)
    Co-authored-by: Alexandre Philibeaux <aphilibeaux@scaleway.com>

---
+++ examples/scaleway/node/src/email.tsx
@@ -0,0 +1,60 @@
+import type { FC } from "react";
+import {
+  Body,
+  Container,
+  Head,
+  Link,
+  Preview,
+  Heading,
+  Html,
+  Section,
+  Img,
+  Text,
+} from "@react-email/components";
+
+interface EmailProps {
+  url: string;
+  username: string;
+  invitedByUsername: string;
+  teamName: string;
+  invitedByEmail: string;
+}
+
+export const Email: FC<Readonly<EmailProps>> = ({
+  invitedByUsername,
+  username,
+  teamName,
+  invitedByEmail,
+}) => {
+  const previewText = `Production INCIDENT ${invitedByUsername} on Scaleway`;
+  return (
+    <Html lang="en">
+      <Head />
+      <Preview>{previewText}</Preview>
+      <Body>
+        <Container>
+          <Section>
+            <Img
+              src="https://www-uploads.scaleway.com/Logos_Image_4ce23fe78a.webp"
+              width="400"
+              height="200"
+              alt="Scaleway"
+            />
+          </Section>
+          <Heading>
+            Join <strong>{teamName}</strong> on <strong>Scaleway</strong>
+          </Heading>
+          <Text >
+            Hello {username},
+          </Text>
+          <Text >
+            <strong>{invitedByUsername}</strong> (
+            <Link href={`mailto:${invitedByEmail}`}>{invitedByEmail}</Link>) has
+            invited you to the <strong>{teamName}</strong> team on{" "}
+            <strong>Scaleway</strong>.
+          </Text>
+        </Container>
+      </Body>
+    </Html>
+  );
+};
