32f0ed5e945f314f2f82113345777eb74286cc7e
Bu Kinoshita <6929565+bukinoshita@users.noreply.github.com>
2023-04-25 09:05:25 -0300

    chore(react-email): Bump to 1.9.3

--- packages/react-email/source/utils/download-client.ts
+++ packages/react-email/source/utils/download-client.ts
@@ -9,7 +9,7 @@ export const downloadClient = async () => {
   const downloadRes = await octokit.repos.downloadTarballArchive({
     owner: 'resendlabs',
     repo: 'react-email',
-    ref: 'v0.0.13',
+    ref: 'v0.0.14',
   });
   fs.mkdirSync('.react-email-temp');
   const TAR_PATH = path.join('.react-email-temp', 'react-email.tar.gz');
