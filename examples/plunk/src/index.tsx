0eb2f56e15fd4b014dacaf90dc6fe72093005211
Dries Augustyns <inbox@driaug.com>
2023-05-21 11:52:43 +0200

    Added Plunk as React Email compatible integration

---
+++ examples/plunk/src/index.tsx
@@ -0,0 +1,15 @@
+import { render } from '@react-email/render';
+import Plunk from '@plunk/node';
+import * as React from 'react';
+import { Email } from './email';
+
+// eslint-disable-next-line turbo/no-undeclared-env-vars
+const plunk = new Plunk(process.env.PLUNK_API_KEY || '');
+
+const emailHtml = render(<Email url="https://example.com" />);
+
+plunk.emails.send({
+  to: "hello@useplunk.com",
+  subject: "Hello world",
+  body: emailHtml,
+});
