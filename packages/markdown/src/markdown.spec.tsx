bd8bd84e9d19287ca696be2e611f44c5e3c68ff9
lordelogos <paulehiks@gmail.com>
2023-07-15 03:19:01 +0100

    chore: updated , updated tests

--- packages/markdown/src/markdown.spec.tsx
+++ packages/markdown/src/markdown.spec.tsx
@@ -17,8 +17,8 @@ describe("ReactEmailMarkdown component renders correctly", () => {
     );
     expect(actualOutput).toMatchInlineSnapshot(`
       "<!DOCTYPE html PUBLIC \\"-//W3C//DTD XHTML 1.0 Transitional//EN\\" \\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\\"><div data-id=\\"react-email-markdown\\">
-      <h1 data-id=\\"react-email-heading\\" style=\\"font-weight:500;padding-top:20;font-size:2.5rem\\">This is a <del style=\\"\\">chair</del> Table</h1>
-             <table style=\\"\\"><thead style=\\"\\"><tr style=\\"\\"><th style=\\"\\" align=\\"center\\">Heading 1</th><th style=\\"\\" align=\\"center\\">Heading 2</th></tr></thead><tbody style=\\"\\"><tr style=\\"\\"><td style=\\"\\" align=\\"center\\">Cell 1</td><td style=\\"\\" align=\\"center\\">Cell 2</td></tr><tr style=\\"\\"><td style=\\"\\" align=\\"center\\">Cell 3</td><td style=\\"\\" align=\\"center\\">Cell 4</td></tr></tbody></table>       </div>"
+      <h1 data-id=\\"react-email-heading\\" style=\\"font-weight:500;padding-top:20;font-size:2.5rem\\">This is a <del>chair</del> Table</h1>
+             <table><thead><tr><th align=\\"center\\">Heading 1</th><th align=\\"center\\">Heading 2</th></tr></thead><tbody><tr><td align=\\"center\\">Cell 1</td><td align=\\"center\\">Cell 2</td></tr><tr><td align=\\"center\\">Cell 3</td><td align=\\"center\\">Cell 4</td></tr></tbody></table>       </div>"
     `);
   });
 
@@ -39,10 +39,10 @@ describe("ReactEmailMarkdown component renders correctly", () => {
       "<!DOCTYPE html PUBLIC \\"-//W3C//DTD XHTML 1.0 Transitional//EN\\" \\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\\"><div data-id=\\"react-email-markdown\\">
       <h1 data-id=\\"react-email-heading\\" style=\\"font-weight:500;padding-top:20;font-size:2.5rem\\">Heading 1!</h1>
       <h2 data-id=\\"react-email-heading\\" style=\\"font-weight:500;padding-top:20;font-size:2rem\\">Heading 2!</h2>
-      <h3 style=\\"font-weight:500;padding-top:20;font-size:1.75rem\\" data-id=\\"react-email-heading\\">Heading 3!</h3>
-      <h4 style=\\"font-weight:500;padding-top:20;font-size:1.5rem\\" data-id=\\"react-email-heading\\">Heading 4!</h4>
-      <h5 style=\\"font-weight:500;padding-top:20;font-size:1.25rem\\" data-id=\\"react-email-heading\\">Heading 5!</h5>
-      <h6 style=\\"font-weight:500;padding-top:20;font-size:1rem\\" data-id=\\"react-email-heading\\">Heading 6!</h6>
+      <h3 data-id=\\"react-email-heading\\" style=\\"font-weight:500;padding-top:20;font-size:1.75rem\\">Heading 3!</h3>
+      <h4 data-id=\\"react-email-heading\\" style=\\"font-weight:500;padding-top:20;font-size:1.5rem\\">Heading 4!</h4>
+      <h5 data-id=\\"react-email-heading\\" style=\\"font-weight:500;padding-top:20;font-size:1.25rem\\">Heading 5!</h5>
+      <h6 data-id=\\"react-email-heading\\" style=\\"font-weight:500;padding-top:20;font-size:1rem\\">Heading 6!</h6>
              </div>"
     `);
   });
@@ -52,7 +52,7 @@ describe("ReactEmailMarkdown component renders correctly", () => {
       <Markdown>{`**This is sample bold text in markdown** and *this is italic text*`}</Markdown>
     );
     expect(actualOutput).toMatchInlineSnapshot(
-      `"<!DOCTYPE html PUBLIC \\"-//W3C//DTD XHTML 1.0 Transitional//EN\\" \\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\\"><div data-id=\\"react-email-markdown\\"><p style=\\"font-weight:bold\\" data-id=\\"react-email-text\\">This is sample bold text in markdown</p> and <p style=\\"font-style:italic\\" data-id=\\"react-email-text\\">this is italic text</p></div>"`
+      `"<!DOCTYPE html PUBLIC \\"-//W3C//DTD XHTML 1.0 Transitional//EN\\" \\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\\"><div data-id=\\"react-email-markdown\\"><strong data-id=\\"react-email-text\\" style=\\"font-weight:bold\\">This is sample bold text in markdown</strong> and <em data-id=\\"react-email-text\\" style=\\"font-style:italic\\">this is italic text</em></div>"`
     );
   });
 
@@ -61,7 +61,7 @@ describe("ReactEmailMarkdown component renders correctly", () => {
       <Markdown>{`Link to [React-email](https://react.email)`}</Markdown>
     );
     expect(actualOutput).toMatchInlineSnapshot(
-      `"<!DOCTYPE html PUBLIC \\"-//W3C//DTD XHTML 1.0 Transitional//EN\\" \\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\\"><div data-id=\\"react-email-markdown\\">Link to <a href=\\"https://react.email\\" style=\\"color:#007bff;text-decoration:underline;background-color:transparent\\" data-id=\\"react-email-link\\" target=\\"_blank\\">React-email</a></div>"`
+      `"<!DOCTYPE html PUBLIC \\"-//W3C//DTD XHTML 1.0 Transitional//EN\\" \\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\\"><div data-id=\\"react-email-markdown\\"><p data-id=\\"react-email-text\\">Link to <a href=\\"https://react.email\\" style=\\"color:#007bff;text-decoration:underline;background-color:transparent\\" data-id=\\"react-email-link\\" target=\\"_blank\\">React-email</a></p></div>"`
     );
   });
 
@@ -80,9 +80,9 @@ describe("ReactEmailMarkdown component renders correctly", () => {
     expect(actualOutput).toMatchInlineSnapshot(`
       "<!DOCTYPE html PUBLIC \\"-//W3C//DTD XHTML 1.0 Transitional//EN\\" \\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\\"><div data-id=\\"react-email-markdown\\">
       <h1 data-id=\\"react-email-heading\\" style=\\"font-weight:500;padding-top:20;font-size:2.5rem\\">Below is a list </h1>
-      <ul style=\\"\\"><li style=\\"\\">Item One</li>
-      <li style=\\"\\">Item Two</li>
-      <li style=\\"\\">Item Three</li></ul>
+      <ul><li>Item One</li>
+      <li>Item Two</li>
+      <li>Item Three</li></ul>
              </div>"
     `);
   });
