5b839ef53e721ced568aaba18d517c8d9d2c69ac
Kim Jisena <73499939+kimjisena@users.noreply.github.com>
2023-07-28 13:39:50 +0300

    misc: add paths to the returned emails object
    Exporting paths allows us to fix a module resolution error in `client/src/app/preview/[slug]/page.tsx`

--- client/src/utils/get-emails.ts
+++ client/src/utils/get-emails.ts
@@ -6,8 +6,13 @@ export const CONTENT_DIR = 'emails';
 export const getEmails = async () => {
   const emailsDirectory = path.join(process.cwd(), CONTENT_DIR);
   const filenames = await fs.readdir(emailsDirectory);
+  const paths = filenames.map((filename) => {
+    return path.join(emailsDirectory, filename);
+  });
+
   const emails = filenames
     .map((file) => file.replace(/\.(jsx|tsx)$/g, ''))
     .filter((file) => file !== 'components');
-  return { emails, filenames };
+
+  return { emails, filenames, paths };
 };
