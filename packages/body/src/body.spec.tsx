743259756000b2b4082aaefcbe2bb257be1ec172
lordelogos <paulehiks@gmail.com>
2023-09-22 00:53:04 +0100

    wip: remove data-id from body

--- packages/body/src/body.spec.tsx
+++ packages/body/src/body.spec.tsx
@@ -18,11 +18,6 @@ describe("Body component", () => {
     expect(html).toContain('style="background-color:red"');
     expect(html).toContain('data-testid="body-test"');
   });
-
-  it("has correct data-id attribute", () => {
-    const html = render(<Body>Test</Body>);
-    expect(html).toContain('data-id="__react-email-body"');
-  });
 });
 
 describe("render", () => {
@@ -34,7 +29,7 @@ describe("render", () => {
   it("renders the <Body> component", () => {
     const actualOutput = render(<Body>Lorem ipsum</Body>);
     expect(actualOutput).toMatchInlineSnapshot(
-      `"<!DOCTYPE html PUBLIC \\"-//W3C//DTD XHTML 1.0 Transitional//EN\\" \\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\\"><body data-id=\\"__react-email-body\\">Lorem ipsum</body>"`,
+      `"<!DOCTYPE html PUBLIC \\"-//W3C//DTD XHTML 1.0 Transitional//EN\\" \\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\\"><body>Lorem ipsum</body>"`,
     );
   });
 });
