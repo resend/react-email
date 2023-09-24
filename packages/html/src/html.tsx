3ba4bb636cb756b2c525c37dc0f8cd15d17b2db5
lordelogos <paulehiks@gmail.com>
2023-09-24 23:27:17 +0100

    wip: html component

--- packages/html/src/html.tsx
+++ packages/html/src/html.tsx
@@ -7,13 +7,7 @@ export interface HtmlProps extends RootProps {}
 
 export const Html = React.forwardRef<HtmlElement, Readonly<HtmlProps>>(
   ({ children, lang = "en", dir = "ltr", ...props }, forwardedRef) => (
-    <html
-      {...props}
-      id="__react-email"
-      ref={forwardedRef}
-      lang={lang}
-      dir={dir}
-    >
+    <html {...props} ref={forwardedRef} lang={lang} dir={dir}>
       {children}
     </html>
   ),
