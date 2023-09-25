cc88c25978e9fd14f768f6fa62b0ef53c7c43d67
lordelogos <paulehiks@gmail.com>
2023-09-25 23:45:50 +0100

    wip: add html tests

--- packages/html/src/html.spec.tsx
+++ packages/html/src/html.spec.tsx
@@ -7,28 +7,23 @@ describe("render", () => {
     jest.resetModules();
   });
 
+  it("renders children correctly", () => {
+    const testMessage = "Test message";
+    const html = render(<Html>{testMessage}</Html>);
+    expect(html).toContain(testMessage);
+  });
+
+  it("passes props correctly", () => {
+    const html = render(<Html lang="fr" dir="rtl" data-testid="html-test" />);
+    expect(html).toContain('lang="fr"');
+    expect(html).toContain('dir="rtl"');
+    expect(html).toContain('data-testid="html-test"');
+  });
+
   it("renders the <Html> component", () => {
     const actualOutput = render(<Html />);
     expect(actualOutput).toMatchInlineSnapshot(
       `"<!DOCTYPE html PUBLIC \\"-//W3C//DTD XHTML 1.0 Transitional//EN\\" \\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\\"><html lang=\\"en\\" dir=\\"ltr\\"></html>"`,
-    );
-  });
-
-  it("renders the <Html> correctly with children", () => {
-    const actualOutput = render(
-      <Html>
-        <p>Hello World!</p>
-      </Html>,
-    );
-    expect(actualOutput).toMatchInlineSnapshot(
-      `"<!DOCTYPE html PUBLIC \\"-//W3C//DTD XHTML 1.0 Transitional//EN\\" \\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\\"><html lang=\\"en\\" dir=\\"ltr\\"><p>Hello World!</p></html>"`,
-    );
-  });
-
-  it("renders the <Html> component with correct props", () => {
-    const actualOutput = render(<Html lang="fr" dir="rtl" />);
-    expect(actualOutput).toMatchInlineSnapshot(
-      `"<!DOCTYPE html PUBLIC \\"-//W3C//DTD XHTML 1.0 Transitional//EN\\" \\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\\"><html lang=\\"fr\\" dir=\\"rtl\\"></html>"`,
     );
   });
 });
