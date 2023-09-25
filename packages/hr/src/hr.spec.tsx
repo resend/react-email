a4cfde092cab73c3ba89a53cd9bb649fd41714ba
lordelogos <paulehiks@gmail.com>
2023-09-25 23:18:31 +0100

    wip: add hr tests

--- packages/hr/src/hr.spec.tsx
+++ packages/hr/src/hr.spec.tsx
@@ -10,7 +10,21 @@ describe("render", () => {
   it("renders the <Hr> component", () => {
     const actualOutput = render(<Hr />);
     expect(actualOutput).toMatchInlineSnapshot(
-      `"<!DOCTYPE html PUBLIC \\"-//W3C//DTD XHTML 1.0 Transitional//EN\\" \\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\\"><hr data-id=\\"react-email-hr\\" style=\\"width:100%;border:none;border-top:1px solid #eaeaea\\"/>"`,
+      `"<!DOCTYPE html PUBLIC \\"-//W3C//DTD XHTML 1.0 Transitional//EN\\" \\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\\"><hr style=\\"width:100%;border:none;border-top:1px solid #eaeaea\\"/>"`,
+    );
+  });
+
+  it("renders the <Hr> correct styles", () => {
+    const actualOutput = render(
+      <Hr
+        style={{
+          width: "50%",
+          borderColor: "black",
+        }}
+      />,
+    );
+    expect(actualOutput).toMatchInlineSnapshot(
+      `"<!DOCTYPE html PUBLIC \\"-//W3C//DTD XHTML 1.0 Transitional//EN\\" \\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\\"><hr style=\\"width:50%;border:none;border-top:1px solid #eaeaea;border-color:black\\"/>"`,
     );
   });
 });
