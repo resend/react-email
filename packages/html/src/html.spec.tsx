00dc3907545b23bb0e203e39c91843c68c02088a
lordelogos <paulehiks@gmail.com>
2023-09-25 23:29:54 +0100

    wip: add html tests

--- packages/html/src/html.spec.tsx
+++ packages/html/src/html.spec.tsx
@@ -10,7 +10,25 @@ describe("render", () => {
   it("renders the <Html> component", () => {
     const actualOutput = render(<Html />);
     expect(actualOutput).toMatchInlineSnapshot(
-      `"<!DOCTYPE html PUBLIC \\"-//W3C//DTD XHTML 1.0 Transitional//EN\\" \\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\\"><html id=\\"__react-email\\" lang=\\"en\\" dir=\\"ltr\\"></html>"`,
+      `"<!DOCTYPE html PUBLIC \\"-//W3C//DTD XHTML 1.0 Transitional//EN\\" \\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\\"><html lang=\\"en\\" dir=\\"ltr\\"></html>"`,
+    );
+  });
+
+  it("renders the <Html> correctly with children", () => {
+    const actualOutput = render(
+      <Html>
+        <p>Hello World!</p>
+      </Html>,
+    );
+    expect(actualOutput).toMatchInlineSnapshot(
+      `"<!DOCTYPE html PUBLIC \\"-//W3C//DTD XHTML 1.0 Transitional//EN\\" \\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\\"><html lang=\\"en\\" dir=\\"ltr\\"><p>Hello World!</p></html>"`,
+    );
+  });
+
+  it("renders the <Html> component with correct props", () => {
+    const actualOutput = render(<Html lang="fr" dir="rtl" />);
+    expect(actualOutput).toMatchInlineSnapshot(
+      `"<!DOCTYPE html PUBLIC \\"-//W3C//DTD XHTML 1.0 Transitional//EN\\" \\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\\"><html lang=\\"fr\\" dir=\\"rtl\\"></html>"`,
     );
   });
 });
