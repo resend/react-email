afadb0daffd0ac05a6c63f2f887af4a9307d9a6a
lordelogos <paulehiks@gmail.com>
2023-09-25 23:12:13 +0100

    wip: add head tests

--- packages/head/src/head.spec.tsx
+++ packages/head/src/head.spec.tsx
@@ -10,7 +10,7 @@ describe("render", () => {
   it("renders the <Head> component", () => {
     const actualOutput = render(<Head />);
     expect(actualOutput).toMatchInlineSnapshot(
-      `"<!DOCTYPE html PUBLIC \\"-//W3C//DTD XHTML 1.0 Transitional//EN\\" \\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\\"><head data-id=\\"__react-email-head\\"><meta http-equiv=\\"Content-Type\\" content=\\"text/html; charset=UTF-8\\"/></head>"`,
+      `"<!DOCTYPE html PUBLIC \\"-//W3C//DTD XHTML 1.0 Transitional//EN\\" \\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\\"><head><meta http-equiv=\\"Content-Type\\" content=\\"text/html; charset=UTF-8\\"/></head>"`,
     );
   });
 
@@ -21,7 +21,24 @@ describe("render", () => {
       </Head>,
     );
     expect(actualOutput).toMatchInlineSnapshot(
-      `"<!DOCTYPE html PUBLIC \\"-//W3C//DTD XHTML 1.0 Transitional//EN\\" \\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\\"><head data-id=\\"__react-email-head\\"><meta http-equiv=\\"Content-Type\\" content=\\"text/html; charset=UTF-8\\"/><title>My email title</title></head>"`,
+      `"<!DOCTYPE html PUBLIC \\"-//W3C//DTD XHTML 1.0 Transitional//EN\\" \\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\\"><head><meta http-equiv=\\"Content-Type\\" content=\\"text/html; charset=UTF-8\\"/><title>My email title</title></head>"`,
     );
+  });
+
+  it("renders style tags", () => {
+    const actualOutput = render(
+      <Head>
+        <style>
+          {`body{
+            color: red;
+          }`}
+        </style>
+      </Head>,
+    );
+    expect(actualOutput).toMatchInlineSnapshot(`
+"<!DOCTYPE html PUBLIC \\"-//W3C//DTD XHTML 1.0 Transitional//EN\\" \\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\\"><head><meta http-equiv=\\"Content-Type\\" content=\\"text/html; charset=UTF-8\\"/><style>body{
+            color: red;
+          }</style></head>"
+`);
   });
 });
