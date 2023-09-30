fda383331969e3f14e241a259a4ece621e631039
gabriel miranda <gabrielmfern@outlook.com>
2023-09-30 09:24:11 -0300

    feature (react-email): make the closeOraOnSIGNIT use the same event for all spinners to avoid warnings

--- packages/react-email/source/utils/close-ora-on-sigint.ts
+++ packages/react-email/source/utils/close-ora-on-sigint.ts
@@ -1,7 +1,16 @@
 import { Ora } from 'ora';
 
+const spinenrsToStop: Ora[] = [];
+let hasSetSigintEvent: boolean = false;
+
+/**
+  * @description Stops spinner when pressing "Ctrl + C" or "CMD + C"
+  */
 export const closeOraOnSIGNIT = (spinner: Ora) => {
-  process.on('SIGINT', function () {
-    spinner.stop();
-  });
+  spinenrsToStop.push(spinner);
+  if (!hasSetSigintEvent) {
+    process.on('SIGINT', () => {
+      spinenrsToStop.forEach(spinner => spinner.stopAndPersist());
+    });
+  }
 };
