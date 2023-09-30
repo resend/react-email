a8876a38e2a890ee7379250a5a454a88e6c71bf2
gabriel miranda <gabrielmfern@outlook.com>
2023-09-30 13:49:46 -0300

    feature (react-email): improve the code for the run-server util

--- packages/react-email/source/utils/run-server.ts
+++ packages/react-email/source/utils/run-server.ts
@@ -1,27 +1,28 @@
-import { createWatcherInstance, watcher } from './watcher';
+import { createEmailsWatcherInstance, emailPreviewGeneratorWatcher } from './watcher';
 import {
   convertToAbsolutePath,
   generateEmailsPreview,
-  startDevServer,
+  startPreviewServer,
 } from '.';
 
 /**
  * Utility function to run init/sync for the server in dev, build or start mode.
  *
- * @param type dev | build | start
- * @param dir Directory in which the emails are located, only for dev and build, unused for start.
+ * @param emailsDir Directory in which the emails are located, only for dev and build, unused for start.
  * @param port The port on which the server will run, only for dev and start, unused for build.
  */
 export const setupServer = async (
-  _type: 'dev' | 'build' | 'start',
-  dir: string,
+  emailsDir: string,
   port: string
 ) => {
-  const emailDir = convertToAbsolutePath(dir);
+  const absoluteEmailsDir = convertToAbsolutePath(emailsDir);
 
-  const watcherInstance = createWatcherInstance(emailDir);
+  const watcherInstance = createEmailsWatcherInstance(absoluteEmailsDir);
 
-  generateEmailsPreview(emailDir);
-  startDevServer(emailDir, port);
-  watcher(watcherInstance, emailDir);
+  console.clear();
+  await generateEmailsPreview(absoluteEmailsDir);
+  console.info('\n');
+  startPreviewServer(absoluteEmailsDir, port);
+
+  emailPreviewGeneratorWatcher(watcherInstance, absoluteEmailsDir);
 };
