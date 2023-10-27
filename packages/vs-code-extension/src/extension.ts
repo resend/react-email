0355f668123d4096c79f4e0e6e480e9b1d61d92b
gabriel miranda <gabrielmfern@outlook.com>
2023-10-27 06:07:00 -0300

    feat (vs-code-ext): basic implementation for the vs code preview extension

--- packages/vs-code-extension/src/extension.ts
+++ packages/vs-code-extension/src/extension.ts
@@ -1,26 +1,125 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+import { basename, dirname, join, normalize } from "path";
+import * as esbuild from "esbuild";
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "react-email-preview" is now active!');
+import { render } from "@react-email/render";
+import { unlink } from "fs/promises";
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('react-email-preview.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from react-email preview!');
-	});
-
-	context.subscriptions.push(disposable);
+interface BuiltEmail {
+  filename: string;
+  html: string;
+  text: string;
 }
 
-// This method is called when your extension is deactivated
+async function getHTMLAndTextForEmailOfEditor<
+  ActiveEditor extends vscode.TextEditor | undefined,
+>(
+  activeEditor: ActiveEditor,
+): Promise<ActiveEditor extends undefined ? undefined : BuiltEmail> {
+  if (activeEditor) {
+    const currentlyOpenTabfilePath = activeEditor.document.fileName; // actually a path not the name of the file
+    const emailsDirectory = join(currentlyOpenTabfilePath, "..");
+
+    const previewDirectory = join(emailsDirectory, ".preview");
+    await esbuild.build({
+      bundle: true,
+      entryPoints: [currentlyOpenTabfilePath],
+      platform: "node",
+      write: true,
+      outdir: previewDirectory,
+    });
+
+    const filename = basename(currentlyOpenTabfilePath);
+    const templatePath = `${previewDirectory}/${filename.replace(
+      ".tsx",
+      "",
+    )}.js`;
+
+    delete require.cache[templatePath];
+    // we need to use require since it has a way to programatically invalidate its cache
+    const email = require(templatePath);
+
+    const comp = email.default(email.default.PreviewProps ?? {}); // this may come without a defualt which might cause problems
+    const emailAsText = render(comp, {
+      plainText: true,
+      pretty: true,
+    });
+    const emailAsHTML = render(comp, { pretty: true });
+    await unlink(templatePath);
+
+    return {
+      filename,
+      html: emailAsHTML,
+      text: emailAsText,
+    } as unknown as ActiveEditor extends undefined ? undefined : BuiltEmail;
+  }
+
+  return undefined as unknown as ActiveEditor extends undefined
+    ? undefined
+    : BuiltEmail;
+}
+
+function convertAllEmailAssetSourcesIntoWebviewURIs(
+  html: string,
+  dirnameOfEmail: vscode.Uri,
+  panel: vscode.WebviewPanel,
+): string {
+  return html.replace(
+    /src\s*=\s*"(\/static\/[^"]*)"/,
+    (srcProperty, /* the first capturing group */ uri) => {
+      const newSource = panel.webview.asWebviewUri(
+        vscode.Uri.joinPath(dirnameOfEmail, uri),
+      );
+
+      return `src="${newSource}"`;
+    },
+  );
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  let previewPanel: vscode.WebviewPanel | undefined = undefined;
+
+  let disposable = vscode.commands.registerCommand(
+    "react-email-preview.preview",
+    () => {
+      previewPanel = vscode.window.createWebviewPanel(
+        "react-email preview",
+        "react-email preview",
+        vscode.ViewColumn.Two,
+        { enableScripts: true },
+      );
+
+      const updatePreview = async () => {
+        if (vscode.window.activeTextEditor && previewPanel) {
+          let builtEmail = await getHTMLAndTextForEmailOfEditor(
+            vscode.window.activeTextEditor,
+          );
+          previewPanel.title = `react-email preview for ${builtEmail.filename}`;
+          previewPanel.webview.html =
+            convertAllEmailAssetSourcesIntoWebviewURIs(
+              builtEmail.html, // temporarily
+              vscode.Uri.joinPath(vscode.window.activeTextEditor.document.uri, '..'),
+              previewPanel,
+            );
+        }
+      };
+
+      vscode.workspace.onDidSaveTextDocument((ev) => {
+        if (ev.fileName === vscode.window.activeTextEditor?.document.fileName) {
+          updatePreview();
+        }
+      });
+
+      vscode.window.onDidChangeActiveTextEditor(() => {
+        updatePreview();
+      });
+    },
+  );
+
+  context.subscriptions.push(disposable);
+}
+
 export function deactivate() {}
