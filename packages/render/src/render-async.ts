42080d48508b1694ba7f4fdfb5520f41f30d769b
Bu Kinoshita <bukinoshita@gmail.com>
2023-11-01 11:14:43 -0300

    chore(render): Format

--- packages/render/src/render-async.ts
+++ packages/render/src/render-async.ts
@@ -3,7 +3,7 @@ import { convert } from "html-to-text";
 import pretty from "pretty";
 import type { ReactDOMServerReadableStream } from "react-dom/server";
 
-const readStream = async(readableStream: ReactDOMServerReadableStream) => {
+const readStream = async (readableStream: ReactDOMServerReadableStream) => {
   const reader = readableStream.getReader();
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const chunks: any[] = [];
@@ -11,7 +11,7 @@ const readStream = async(readableStream: ReactDOMServerReadableStream) => {
   // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition, no-constant-condition
   while (true) {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, no-await-in-loop
-    const {value, done} = await reader.read();
+    const { value, done } = await reader.read();
     if (done) {
       break;
     }
@@ -19,8 +19,8 @@ const readStream = async(readableStream: ReactDOMServerReadableStream) => {
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-  return chunks.map((chunk) => new TextDecoder('utf-8').decode(chunk)).join('');
-}
+  return chunks.map((chunk) => new TextDecoder("utf-8").decode(chunk)).join("");
+};
 
 export const renderAsync = async (
   component: React.ReactElement,
@@ -29,7 +29,7 @@ export const renderAsync = async (
     plainText?: boolean;
   },
 ) => {
-  const reactDOMServer = (await import('react-dom/server')).default;
+  const reactDOMServer = (await import("react-dom/server")).default;
   const renderToStream =
     reactDOMServer.renderToReadableStream ||
     reactDOMServer.renderToString ||
@@ -39,7 +39,10 @@ export const renderAsync = async (
     '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">';
 
   const readableStream = await renderToStream(component);
-  const html = typeof readableStream === 'string' ? readableStream : await readStream(readableStream);
+  const html =
+    typeof readableStream === "string"
+      ? readableStream
+      : await readStream(readableStream);
 
   if (options?.plainText) {
     return convert(html, {
