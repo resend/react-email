c7d9dfe51775f19298df40025bd6969a3a32c461
gabriel miranda <gabrielmfern@outlook.com>
2023-11-07 09:30:14 -0300

    fix (tailwind): get back to the same way of setting up tailwind internally

--- packages/tailwind/src/utils/get-css-for-classes.ts
+++ packages/tailwind/src/utils/get-css-for-classes.ts
@@ -1,13 +1,10 @@
-/// <reference path="../tailwind.d.ts" />
 import postcss from "postcss";
-import postcssVariables from "postcss-css-variables";
+import postcssCssVaraibles from "postcss-css-variables";
 import processTailwindFeatures from "tailwindcss/src/processTailwindFeatures.js";
 import resolveConfig from "tailwindcss/src/public/resolve-config.js";
-
 import type { Config as TailwindConfig } from "tailwindcss";
-import { RequiredConfig } from "tailwindcss/types/config";
-import { createContext } from "tailwindcss/src/lib/setupContextUtils.js";
-import { contextMap } from "tailwindcss/src/lib/sharedState.js";
+import type { RequiredConfig } from "tailwindcss/types/config";
+
 
 declare global {
   var __OXIDE__: undefined;
@@ -27,21 +24,21 @@ export const getCSSForMarkup = (markup: string, config: Omit<TailwindConfig, 'co
     }
   } as unknown as RequiredConfig);
 
-  const tailwindcssPlugin: postcss.AcceptedPlugin = (root, result) => {
-    processTailwindFeatures(() => () => { 
-      const context = createContext(tailwindConfig, [{ content: markup }], root);
-
-      return context;
-    })(root, result);
-  }
-  const result = postcss([tailwindcssPlugin, postcssVariables()])
+  const tailwindcssPlugin = processTailwindFeatures(
+    ({ createContext }) => () => createContext(tailwindConfig, [{ content: markup }])
+  );
+  const processor = postcss([
+    tailwindcssPlugin, 
+    postcssCssVaraibles(),
+  ]);
+  const result = processor
     .process(
       String.raw`
         @tailwind base;
         @tailwind components;
         @tailwind utilities;
       `,
-      { from: 'fake' } // no need to use from since the `content` context is sent into tailwind
+      { from: undefined } // no need to use from since the `content` context is sent into tailwind
     );
   return result.css;
 };
