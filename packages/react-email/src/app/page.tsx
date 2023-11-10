09e66ad9b4d7253cd8a40284c875c9c2afa80312
Vitor Capretz <capretzvitor@gmail.com>
2023-11-10 11:51:26 -0300

    better setup

--- packages/react-email/src/app/page.tsx
+++ packages/react-email/src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col  items-center justify-between p-24">
       <h1>React Email</h1>
     </main>
   );
--- packages/react-email/tailwind.config.ts
+++ packages/react-email/tailwind.config.ts
@@ -1,20 +1,14 @@
-import type { Config } from 'tailwindcss'
+import { type Config } from 'tailwindcss';
+import { fontFamily } from 'tailwindcss/defaultTheme';
 
-const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+export default {
+  content: ['./src/**/*.tsx'],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: ['var(--font-sans)', ...fontFamily.sans],
       },
     },
   },
   plugins: [],
-}
-export default config
+} satisfies Config;
