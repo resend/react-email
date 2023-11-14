e902ca569b6d0837ef4280c23e6c7c14e987fda5
josh <josh@mjbrowns.com>
2023-11-14 11:58:44 -0600

    fix: remove deprecated app dir experiemental flag from next config

--- client/next.config.js
+++ client/next.config.js
@@ -3,7 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
-    appDir: true,
     externalDir: true // compile files that are located next to the .react-email directory
   },
 };
