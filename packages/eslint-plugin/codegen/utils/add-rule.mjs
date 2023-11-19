6b8e922a3c056ca32963e1db54bf4eb1d40907c1
gabriel miranda <gabrielmfern@outlook.com>
2023-11-19 05:29:50 -0300

    feat (eslint-plugin): add jsdocs to the addRule function for codegen

--- packages/eslint-plugin/codegen/utils/add-rule.mjs
+++ packages/eslint-plugin/codegen/utils/add-rule.mjs
@@ -1,20 +1,27 @@
 // @ts-check
 
-import { readFileSync, writeFileSync } from "fs"
+import { existsSync, readFileSync, writeFileSync } from "fs"
 
 import { camelize } from "./camelize.mjs";
 
 /**
+  * @description Inserts the {@link content} into a new file at `src/rules/no-${feature}` ({@link feature})
+  * and then adds an import and export of this new rule to the file at `src/rules/index.ts`.
+  *
   * @param content {string} The contents of the typescript file associated with the rule.
   * Must export the created rule as default so that the exports work properly.
   *
-  * @param feature {string}
+  * @param feature {string} The **kebab-cased** name of the feature that this rule is meant to avoid
   */
 export const addRule = (content, feature) => {
-  console.warn(`WARNING: The file for the ${feature} seems to already exist, this is going to overwrite that file`);
-
   const ruleBasename = `no-${feature}`;
-  writeFileSync(`./src/rules/${ruleBasename}.ts`, content);
+  const newRuleFilename = `./src/rules/${ruleBasename}.ts`;
+
+  if (existsSync(newRuleFilename)) {
+    console.warn(`WARNING: The file for the ${feature} seems to already exist, overwriting that file now`);
+  }
+
+  writeFileSync(newRuleFilename, content);
   const camelCasedRule = camelize(feature);
   const currentIndexContents = readFileSync('./src/rules/index.ts', 'utf-8');
 
