064d1e6505a4298348d4ccdd961fe8990fc1c46b
gabriel miranda <gabrielmfern@outlook.com>
2023-11-20 08:33:18 -0300

    feat (eslint-plugin): use an abstract value for supported environments with partial values on unknown and partial support

--- packages/eslint-plugin/codegen/utils/compute-feature-support-percentage.mjs
+++ packages/eslint-plugin/codegen/utils/compute-feature-support-percentage.mjs
@@ -4,18 +4,24 @@
   * @param feature {import('zod').z.infer<typeof import('./get-features-data-with-prefix.mjs').featureSchema>}
   */
 export const computeFeatureSupportPercentage = (feature) => {
-  let supportedEnvironments = 0;
+  let abstractSupportedEnvironments = 0;
   let allEnvironments = 0;
   for (const versionSupportPerOs of Object.values(feature.stats)) {
     for (const supportPerVersion of Object.values(versionSupportPerOs)) {
       for (const supported of Object.values(supportPerVersion)) {
         allEnvironments++;
-        if (supported === 'y') {
-          supportedEnvironments++;
+        // using startsWith here because supported generally has references on it
+        // such as `a #6 #7`
+        if (supported.startsWith('y')) {
+          abstractSupportedEnvironments++;
+        } else if (supported.startsWith('u')) {
+          abstractSupportedEnvironments += 0.25;
+        } else if (supported.startsWith('a')) {
+          abstractSupportedEnvironments += 0.5;
         }
       }
     }
   }
 
-  return supportedEnvironments / allEnvironments;
+  return abstractSupportedEnvironments / allEnvironments;
 };
