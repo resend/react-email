5f83e6ce2f49b902a7684b0dbc54eee284a99231
gabriel miranda <gabrielmfern@outlook.com>
2023-11-20 04:46:42 -0300

    feat (eslint-plugin): update exports and package name

--- packages/eslint-plugin/rollup.config.js
+++ packages/eslint-plugin/rollup.config.js
@@ -5,9 +5,11 @@ import commonjs from '@rollup/plugin-commonjs'
 import esbuild from 'rollup-plugin-esbuild'
 
 const pkg = JSON.parse(fs.readFileSync(
-  new URL('./package.json', import.meta.url), 
+  new URL('./package.json', import.meta.url),
   'utf-8'
 ))
+
+let ruleNames = [];
 
 export default defineConfig({
   input: 'src/index.ts',
@@ -22,6 +24,9 @@ export default defineConfig({
           return 'configs'
         if (id.includes('rules')) {
           const name = basename(id, '.ts');
+          if (name !== 'index') {
+            ruleNames.push(name);
+          }
           return name
         }
       },
@@ -31,6 +36,25 @@ export default defineConfig({
   plugins: [
     commonjs(),
     esbuild(),
+    {
+      writeBundle() {
+        const packageJsonExportsChunk = {
+          exports: {
+            ".": {
+              "require": "./dist/index.js",
+              "default": "./dist/index.js"
+            },
+          }
+        };
+        for (const name of ruleNames) {
+          packageJsonExportsChunk.exports[`./rules/${name}`] = `./dist/${name}.js`;
+        }
+        console.info(
+          `Paste this on the exports section of the "package.json" for the rules to work properly: \n`, 
+          JSON.stringify(packageJsonExportsChunk, null, 2)
+        );
+      }
+    }
   ],
   external: [
     ...Object.keys(pkg.dependencies || []),
