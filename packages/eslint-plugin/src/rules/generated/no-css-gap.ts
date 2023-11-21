2979c43997f8ef8f5f84f11c128d119c018ae4c7
gabriel miranda <gabrielmfern@outlook.com>
2023-11-21 13:34:54 -0300

    fix (eslint-plugin): column-gap and row-gap not being diallowed by the no-css-gap rule

--- packages/eslint-plugin/src/rules/generated/no-css-gap.ts
+++ packages/eslint-plugin/src/rules/generated/no-css-gap.ts
@@ -1,7 +1,7 @@
 import { createNoStylePropertyRule } from "../../utils/create-no-style-property-rule";
 
 export default createNoStylePropertyRule(
-  'gap', 
+  ['gap', 'row-gap', 'column-gap'], 
   48.75, 
   'https://www.caniemail.com/features/css-gap/'
 );
