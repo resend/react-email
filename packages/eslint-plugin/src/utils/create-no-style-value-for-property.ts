f0531d18e664f50f8ce954069ea836754a73daac
gabriel miranda <gabrielmfern@outlook.com>
2023-11-21 11:37:56 -0300

    fix (eslint-plugin): createNoStyleValueForProperty not working with flex-direction and other properties

--- packages/eslint-plugin/src/utils/create-no-style-value-for-property.ts
+++ packages/eslint-plugin/src/utils/create-no-style-value-for-property.ts
@@ -2,7 +2,7 @@ import { AST_NODE_TYPES } from "@typescript-eslint/utils";
 import { createRule } from "./create-rule";
 
 export const createNoStyleValueForProperty = (
-  value: string,
+  valueToDisallow: string,
   property: string,
   supportPercetange: number,
   caniemailLink: string,
@@ -12,30 +12,36 @@ export const createNoStyleValueForProperty = (
       type: 'suggestion',
       schema: [],
       messages: {
-        'not-supported-on-most-emails-clients': `The style ${property}: ${value} is supported only ${supportPercetange.toFixed(2)}% of email clients, see ${caniemailLink}`
+        'not-supported-on-most-emails-clients': `The style ${property}: ${valueToDisallow} is supported only ${supportPercetange.toFixed(2)}% of email clients, see ${caniemailLink}`
       }
     },
     create(context) {
       return {
         Property(node) {
-          const camelCasedProperty = property.replace(/-([a-z])/g, (g) => g[1].toUpperCase());
-
-          const [attributeName] = context.sourceCode.getText(node.key)
-            .trim()
-            .match(/\w+/g) ?? ['']; // only select the word
-
           if (
             node.parent.type === AST_NODE_TYPES.ObjectExpression &&
             node.parent.parent.type === AST_NODE_TYPES.JSXExpressionContainer &&
-            node.parent.parent.parent.type === AST_NODE_TYPES.JSXAttribute &&
-            context.sourceCode.getText(node.parent.parent.parent.name) === "style" &&
-            context.sourceCode.getText(node.value).trim() === value &&
-            camelCasedProperty === attributeName
+            node.parent.parent.parent.type === AST_NODE_TYPES.JSXAttribute
           ) {
-            context.report({
-              node,
-              messageId: 'not-supported-on-most-emails-clients'
-            });
+            const camelCasedProperty = property.replace(/-([a-z])/g, (g) => g[1].toUpperCase());
+
+            const [attributeName] = context.sourceCode.getText(node.key)
+              .trim()
+              .match(/\w+/g) ?? ['']; // only select the word
+            const [value] = context.sourceCode.getText(node.value)
+              .trim()
+              .match(/[\w\s-]+/g) ?? [''];
+
+            if (
+              context.sourceCode.getText(node.parent.parent.parent.name) === "style" &&
+              value.trim() === valueToDisallow &&
+              camelCasedProperty === attributeName
+            ) {
+              context.report({
+                node,
+                messageId: 'not-supported-on-most-emails-clients'
+              });
+            }
           }
         }
       }
