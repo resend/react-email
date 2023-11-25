fd8483fd79fae9056033f23df5074bbc7fda9d0c
gabriel miranda <gabrielmfern@outlook.com>
2023-11-25 06:55:52 -0300

    chore (tailwind): lint and format

--- packages/tailwind/src/utils/get-css-for-markup.ts
+++ packages/tailwind/src/utils/get-css-for-markup.ts
@@ -1,9 +1,11 @@
-// this is to avoid Server Action problems
-const postcss = require("postcss");
 import tailwindcss from "tailwindcss";
 import type { CorePluginsConfig } from "tailwindcss/types/config";
 import postcssCssVariables from "postcss-css-variables";
 import type { TailwindConfig } from "../tailwind";
+// this is to avoid Server Action problems
+ 
+// eslint-disable-next-line @typescript-eslint/consistent-type-imports, @typescript-eslint/no-var-requires
+const postcss = require("postcss") as import('postcss').Postcss;
 
 declare global {
   // eslint-disable-next-line no-var
@@ -44,5 +46,5 @@ export const getCssForMarkup = (
       `,
     { from: undefined }, // no need to use from since the `content` context is sent into tailwind
   );
-  return result.css as string;
+  return result.css;
 };
