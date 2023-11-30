60fb0ab033ecb5a77bea57e27557738d3580de7b
gabriel miranda <gabrielmfern@outlook.com>
2023-11-30 10:17:02 -0300

    feat (tailwind): remove polyfills

--- packages/tailwind/vite.config.ts
+++ packages/tailwind/vite.config.ts
@@ -1,18 +1,10 @@
 import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig({
   plugins: [
-    dts({ include: ["src"], outDir: "dist" }),
-    nodePolyfills({
-      include: ["path", "fs", "tty", "crypto", "os", "process"],
-      overrides: {
-        fs: "memfs",
-        process: "process",
-      },
-    }),
+    dts({ include: ["src"], outDir: "dist" })
   ],
   build: {
     rollupOptions: {
