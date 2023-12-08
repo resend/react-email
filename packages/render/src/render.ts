f96305abfe99e26507423a2505c394adf6a23957
Marcus Stenbeck <marcus.stenbeck@gmail.com>
2023-12-08 11:44:14 +0100

    feat: remove brackets surrounding anchor tags in plain text rendering

--- packages/render/src/render.ts
+++ packages/render/src/render.ts
@@ -32,6 +32,10 @@ const renderAsPlainText = (
     selectors: [
       { selector: "img", format: "skip" },
       { selector: "#__react-email-preview", format: "skip" },
+      {
+        selector: "a",
+        options: { linkBrackets: false }
+      }
     ],
   });
 };
