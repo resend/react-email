42c644f5549f59d81f00e466124f2646b1589d75
gabriel miranda <gabrielmfern@outlook.com>
2023-12-14 06:25:54 -0300

    feat(render): Unit test for checking if plain text links contain surrounding brackets

--- packages/render/src/render.spec.tsx
+++ packages/render/src/render.spec.tsx
@@ -1,8 +1,7 @@
+import React from "react";
 import { Template } from "./utils/template";
 import { Preview } from "./utils/preview";
 import { render } from "./index";
-
-import React from "react";
 
 describe("render", () => {
   it("converts a React component into HTML", () => {
@@ -10,6 +9,21 @@ describe("render", () => {
     expect(actualOutput).toMatchInlineSnapshot(
       '"<!DOCTYPE html PUBLIC \\"-//W3C//DTD XHTML 1.0 Transitional//EN\\" \\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\\"><h1>Welcome, Jim!</h1><img alt=\\"test\\" src=\\"img/test.png\\"/><p>Thanks for trying our product. We&#x27;re thrilled to have you on board!</p>"',
     );
+  });
+
+  it("should not have brackets around the link with the plain text active", () => {
+    const Email = (
+      <div>
+        <a href="https://github.com/resend/react-email/pull/1087">
+          This is the PR that fixes the issues with this!
+        </a>
+      </div>
+    );
+    const textRendered = render(Email, { plainText: true });
+    expect(textRendered).toMatchInlineSnapshot(`
+      "This is the PR that fixes the issues with this!
+      https://github.com/resend/react-email/pull/1087"
+    `);
   });
 
   it("converts a React component into PlainText", () => {
