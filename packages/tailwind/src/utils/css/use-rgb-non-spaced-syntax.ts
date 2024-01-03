21901c465a55c59bebb592262eba746c878c1500
gabriel miranda <gabrielmfern@outlook.com>
2024-01-03 13:13:12 -0300

    fix(tailwind): useRgbNonSpacedSyntax not using the same regex

--- packages/tailwind/src/utils/css/use-rgb-non-spaced-syntax.ts
+++ packages/tailwind/src/utils/css/use-rgb-non-spaced-syntax.ts
@@ -1,15 +1,15 @@
+// Thanks tw-to-css!
+// from https://github.com/vinicoder/tw-to-css/blob/main/src/util/format-css.ts
+const regex = /rgb\(\s*(\d+)\s*(\d+)\s*(\d+)(?:\s*\/\s*([\d%.]+))?\s*\)/;
+
 /**
  * This is to avoid problems on email clients that don't allow for spaced syntax on
  * rgb.
  *
  * See https://www.caniemail.com/features/css-rgb/
  */
-export const useRgbNonSpacedSyntax = (css: string) => {
-  // Thanks tw-to-css!
-  // from https://github.com/vinicoder/tw-to-css/blob/main/src/util/format-css.ts
-  const regex = /rgb\(\s*(\d+)\s*(\d+)\s*(\d+)(?:\s*\/\s*([\d%.]+))?\s*\)/g;
-
-  return css.replaceAll(regex, (_match, r, g, b, a) => {
+export const useRgbNonSpacedSyntax = (cssDeclarationValue: string) => {
+  return cssDeclarationValue.replace(regex, (_match, r, g, b, a) => {
     const alpha = a === "1" || typeof a === "undefined" ? "" : `,${a}`;
     return `rgb(${r},${g},${b}${alpha})`;
   });
