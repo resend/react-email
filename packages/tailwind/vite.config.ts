d885ef23338814291fb8100f43f17924f374f648
gabriel miranda <gabrielmfern@outlook.com>
2024-01-03 13:01:04 -0300

    chore(tailwind): remove polyfills dependencies and the patch on the process one

--- packages/tailwind/vite.config.ts
+++ packages/tailwind/vite.config.ts
@@ -1,25 +1,17 @@
-import { resolve } from "node:path";
+import path from "node:path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
-// import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig({
   plugins: [
     dts({ include: ["src"], outDir: "dist" }),
-    // nodePolyfills({
-    //   include: ["path", "tty", "fs", "crypto", "process", "os"],
-    //   overrides: {
-    //     fs: "memfs",
-    //     process: "process",
-    //   },
-    // }),
   ],
   build: {
     rollupOptions: {
       external: ["react", "react-dom", "postcss", /react-dom\/.*/],
     },
     lib: {
-      entry: resolve(__dirname, "src/index.ts"),
+      entry: path.resolve(__dirname, "src/index.ts"),
       fileName: "index",
       formats: ["es", "cjs"],
     },
