b4582ab20b0f8f9356740595e6f3e15778e1d213
gabriel miranda <gabrielmfern@outlook.com>
2024-01-03 13:23:05 -0300

    fix(tailwind): Non global regex for rgbs causing issues

--- packages/tailwind/src/utils/css/use-rgb-non-spaced-syntax.ts
+++ packages/tailwind/src/utils/css/use-rgb-non-spaced-syntax.ts
@@ -1,6 +1,3 @@
-// Thanks tw-to-css!
-// from https://github.com/vinicoder/tw-to-css/blob/main/src/util/format-css.ts
-const regex = /rgb\(\s*(\d+)\s*(\d+)\s*(\d+)(?:\s*\/\s*([\d%.]+))?\s*\)/;
 
 /**
  * This is to avoid problems on email clients that don't allow for spaced syntax on
@@ -9,7 +6,11 @@ const regex = /rgb\(\s*(\d+)\s*(\d+)\s*(\d+)(?:\s*\/\s*([\d%.]+))?\s*\)/;
  * See https://www.caniemail.com/features/css-rgb/
  */
 export const useRgbNonSpacedSyntax = (cssDeclarationValue: string) => {
-  return cssDeclarationValue.replace(regex, (_match, r, g, b, a) => {
+  // Thanks tw-to-css!
+  // from https://github.com/vinicoder/tw-to-css/blob/main/src/util/format-css.ts
+  const regex = /rgb\(\s*(\d+)\s*(\d+)\s*(\d+)(?:\s*\/\s*([\d%.]+))?\s*\)/g;
+
+  return cssDeclarationValue.replaceAll(regex, (_match, r, g, b, a) => {
     const alpha = a === "1" || typeof a === "undefined" ? "" : `,${a}`;
     return `rgb(${r},${g},${b}${alpha})`;
   });
