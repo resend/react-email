c49591d53410bacfb530307e817149a66d2b723a
gabriel miranda <gabrielmfern@outlook.com>
2024-01-05 14:51:30 -0300

    chore(react-email): format

--- packages/react-email/src/utils/unreachable.ts
+++ packages/react-email/src/utils/unreachable.ts
@@ -1,7 +1,7 @@
 export const unreachable = (
   condition: string | Record<string, unknown>,
   message = `Entered unreachable code. Received '${
-    typeof condition === "string" ? condition : JSON.stringify(condition)
+    typeof condition === 'string' ? condition : JSON.stringify(condition)
   }'.`,
 ): never => {
   throw new TypeError(message);
