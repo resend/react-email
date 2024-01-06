a5058be6914bc1ff3e4c8bcfd14e6c5c10502186
gabriel miranda <gabrielmfern@outlook.com>
2024-01-06 16:17:23 -0300

    fix(react-email): sidebar horizontal scroll

--- packages/react-email/src/components/sidebar/sidebar.tsx
+++ packages/react-email/src/components/sidebar/sidebar.tsx
@@ -14,9 +14,9 @@ export const Sidebar = React.forwardRef<SidebarElement, Readonly<SidebarProps>>(
   ({ className, emailsDirectoryMetadata, currentEmailOpenSlug, ...props }, forwardedRef) => {
     return (
       <aside className={className} ref={forwardedRef} {...props}>
-        <nav className="p-6 w-screen md:w-full md:min-w-[275px] md:max-w-[275px] overflow-x-auto flex flex-col gap-4 border-r border-slate-6">
+        <nav className="p-6 w-screen md:w-full md:min-w-[275px] md:max-w-[275px] flex flex-col gap-4 border-r border-slate-6">
           <SidebarDirectory
-            className="w-fit"
+            className="w-fit overflow-x-auto"
             currentEmailOpenSlug={currentEmailOpenSlug}
             emailsDirectoryMetadata={emailsDirectoryMetadata}
           />
