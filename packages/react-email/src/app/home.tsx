c4d2eae4244514a68c502688b7ae51534350c154
gabriel miranda <gabrielmfern@outlook.com>
2024-01-09 05:55:47 -0300

    fix(react-email): Centering for "Welcome" card on Home

--- packages/react-email/src/app/home.tsx
+++ packages/react-email/src/app/home.tsx
@@ -8,15 +8,15 @@ import { Shell } from '../components/shell';
 export const Home = (props: { emailsDirectoryMetadata: EmailsDirectory }) => {
   return (
     <Shell emailsDirectoryMetadata={props.emailsDirectoryMetadata}>
-      <div className="mx-auto">
+      <div className="mx-auto w-fit">
         <div className="max-w-md border border-slate-6 mx-6 mt-56 rounded-md p-8">
           <Heading as="h2" weight="medium">
             Welcome to the React Email preview!
           </Heading>
           <Text as="p" className="mt-2 mb-4">
             To start developing your next email template, you can create a{' '}
-            <code>.jsx</code> or <code>.tsx</code> file under the
-            &quot;emails&quot; folder.
+            <code>.jsx</code> or <code>.tsx</code> file under your
+            &quot;{props.emailsDirectoryMetadata.directoryName}&quot; folder.
           </Text>
 
           <Button asChild>
