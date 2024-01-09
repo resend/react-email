267aeb185dfee16f6eb08b587d33da906a4882b5
gabriel miranda <gabrielmfern@outlook.com>
2024-01-09 06:13:59 -0300

    feat(react-email): Send watcher changes to the useHotreload hook

--- packages/react-email/src/cli/utils/preview/setup-hot-reloading.ts
+++ packages/react-email/src/cli/utils/preview/setup-hot-reloading.ts
@@ -2,6 +2,8 @@ import type http from 'node:http';
 import path from 'node:path';
 import { Server as SocketServer, type Socket } from 'socket.io';
 import { watch } from 'chokidar';
+import debounce from 'debounce';
+import type { HotReloadChange } from '@/utils/types/hot-reload-change';
 
 export const setupHotreloading = (
   devServer: http.Server,
@@ -18,13 +20,6 @@ export const setupHotreloading = (
     });
   });
 
-  const reload = () => {
-    // we detect these using the useHotreload hook on the Next app
-    clients.forEach((client) => {
-      client.emit('reload');
-    });
-  };
-
   const watcher = watch(emailDirRelativePath, {
     ignoreInitial: true,
     cwd: path.resolve(process.cwd()),
@@ -38,12 +33,29 @@ export const setupHotreloading = (
   process.on('SIGINT', exit);
   process.on('uncaughtException', exit);
 
-  watcher.on('all', (_event, filename) => {
+  // used to keep track of all changes
+  // and send them at once to the preview app through the web socket
+  let changes = [] as HotReloadChange[];
+
+  const reload = debounce(() => {
+    // we detect these using the useHotreload hook on the Next app
+    clients.forEach((client) => {
+      client.emit('reload', changes);
+    });
+
+    changes = [];
+  }, 150);
+
+  watcher.on('all', (event, filename) => {
     const file = filename.split(path.sep);
     if (file.length === 0) {
       return;
     }
 
+    changes.push({
+      event,
+      filename,
+    });
     reload();
   });
 
--- packages/react-email/src/utils/hooks/use-hot-reload.ts
+++ packages/react-email/src/utils/hooks/use-hot-reload.ts
@@ -1,9 +1,10 @@
 'use client';
 import { useEffect, useRef } from 'react';
 import { type Socket, io } from 'socket.io-client';
+import type { HotReloadChange } from '../types/hot-reload-change';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const useHotreload = (onShouldReload: () => any) => {
+export const useHotreload = (onShouldReload: (changes: HotReloadChange[]) => any) => {
   const socketRef = useRef<Socket | null>(null);
 
   useEffect(() => {
@@ -16,9 +17,9 @@ export const useHotreload = (onShouldReload: () => any) => {
     }
     const socket = socketRef.current;
 
-    socket.on('reload', () => {
+    socket.on('reload', (changes: HotReloadChange[]) => {
       console.debug('Reloading...');
-      void onShouldReload();
+      void onShouldReload(changes);
     });
 
     return () => {
