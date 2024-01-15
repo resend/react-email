7a169ec9e8ba9a01a33f38d15c006fa0c8650682
gabriel miranda <gabrielmfern@outlook.com>
2024-01-15 13:35:57 -0300

    fix(react-email): directories not uncollapsing if the preview of one of its emails is open

--- packages/react-email/src/components/sidebar/sidebar-directory.tsx
+++ packages/react-email/src/components/sidebar/sidebar-directory.tsx
@@ -2,7 +2,7 @@
 import * as Collapsible from '@radix-ui/react-collapsible';
 import * as React from 'react';
 import { cn } from '../../utils';
-import { emailsDirectoryAbsolutePath } from '../../utils/emails-directory-absolute-path';
+import { emailsDirectoryAbsolutePath, pathSeparator } from '../../utils/emails-directory-absolute-path';
 import { type EmailsDirectory } from '../../actions/get-emails-directory-metadata';
 import { Heading } from '../heading';
 import { IconFolder } from '../icons/icon-folder';
@@ -24,6 +24,7 @@ export const SidebarDirectory = ({
     emailsDirectoryMetadata.absolutePath === emailsDirectoryAbsolutePath;
   const directoryPathRelativeToEmailsDirectory =
     emailsDirectoryMetadata.absolutePath
+      .replace(`${emailsDirectoryAbsolutePath}${pathSeparator}`, '')
       .replace(emailsDirectoryAbsolutePath, '')
       .trim();
   const doesFolderContainCurrentEmailOpen = currentEmailOpenSlug
