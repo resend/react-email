cc8285c0bb75e44b8eced8c2b34e322a560128c0
gabriel miranda <gabrielmfern@outlook.com>
2024-01-22 08:38:15 -0300

    fix(react-email): Missing preloaded node modules causing issues

--- packages/react-email/src/utils/static-node-modules-for-vm.ts
+++ packages/react-email/src/utils/static-node-modules-for-vm.ts
@@ -1,13 +1,16 @@
 import zlib from 'node:zlib';
 import vm from 'node:vm';
+import v8 from 'node:v8';
 import util from 'node:util';
 import url from 'node:url';
+import tty from 'node:tty';
 import tls from 'node:tls';
 import timers from 'node:timers';
 import stringDecoder from 'node:string_decoder';
 import stream from 'node:stream';
 import readline from 'node:readline';
 import querystring from 'node:querystring';
+import punycode from 'node:punycode';
 import path from 'node:path';
 import os from 'node:os';
 import net from 'node:net';
@@ -15,10 +18,12 @@ import https from 'node:https';
 import http from 'node:http';
 import fs from 'node:fs';
 import events from 'node:events';
+import domain from 'node:domain';
 import dns from 'node:dns';
 import dgram from 'node:dgram';
 import crypto from 'node:crypto';
 import cluster from 'node:cluster';
+import childProcess from 'node:child_process';
 import buffer from 'node:buffer';
 import assert from 'node:assert';
 
@@ -29,49 +34,59 @@ import assert from 'node:assert';
  */
 export const staticNodeModulesForVM = {
   zlib,
-  'node:zlib': zlib,
+  "node:zlib": zlib,
   vm,
-  'node:vm': vm,
+  "node:vm": vm,
+  v8,
+  "node:v8": v8,
   util,
-  'node:util': util,
+  "node:util": util,
   url,
-  'node:url': url,
+  "node:url": url,
+  tty,
+  "node:tty": tty,
   tls,
-  'node:tls': tls,
+  "node:tls": tls,
   timers,
-  'node:timers': timers,
-  'string_decoder': stringDecoder,
-  'node:string_decoder': stringDecoder,
+  "node:timers": timers,
+  "string_decoder": stringDecoder,
+  "node:string_decoder": stringDecoder,
   stream,
-  'node:stream': stream,
+  "node:stream": stream,
   readline,
-  'node:readline': readline,
+  "node:readline": readline,
   querystring,
-  'node:querystring': querystring,
+  "node:querystring": querystring,
+  punycode,
+  "node:punycode": punycode,
   path,
-  'node:path': path,
+  "node:path": path,
   os,
-  'node:os': os,
+  "node:os": os,
   net,
-  'node:net': net,
+  "node:net": net,
   https,
-  'node:https': https,
+  "node:https": https,
   http,
-  'node:http': http,
+  "node:http": http,
   fs,
-  'node:fs': fs,
+  "node:fs": fs,
   events,
-  'node:events': events,
+  "node:events": events,
+  domain,
+  "node:domain": domain,
   dns,
-  'node:dns': dns,
+  "node:dns": dns,
   dgram,
-  'node:dgram': dgram,
+  "node:dgram": dgram,
   crypto,
-  'node:crypto': crypto,
+  "node:crypto": crypto,
   cluster,
-  'node:cluster': cluster,
+  "node:cluster": cluster,
+  "child_process": childProcess,
+  "node:child_process": childProcess,
   buffer,
-  'node:buffer': buffer,
+  "node:buffer": buffer,
   assert,
-  'node:assert': assert,
+  "node:assert": assert,
 };
