4eeda7f0b9aed6d3f2d5a1028c4023665361f3be
gabriel miranda <gabrielmfern@outlook.com>
2024-01-23 09:58:01 -0300

    fix(react-email): Hot reloading not working with custom emails directory

--- packages/react-email/src/contexts/emails.tsx
+++ packages/react-email/src/contexts/emails.tsx
@@ -6,7 +6,9 @@ import {
 } from '../actions/get-emails-directory-metadata';
 import { useHotreload } from '../hooks/use-hot-reload';
 import {
+  emailsDirRelativePath,
   emailsDirectoryAbsolutePath,
+  normalizePath,
   pathSeparator,
 } from '../utils/emails-directory-absolute-path';
 import {
@@ -16,15 +18,15 @@ import {
 
 const EmailsContext = createContext<
   | {
-      emailsDirectoryMetadata: EmailsDirectory;
-      /**
-       * Uses the hot reloaded bundled build and rendering email result
-       */
-      useEmailRenderingResult: (
-        slug: string,
-        serverEmailRenderedResult: EmailRenderingResult,
-      ) => EmailRenderingResult;
-    }
+    emailsDirectoryMetadata: EmailsDirectory;
+    /**
+     * Uses the hot reloaded bundled build and rendering email result
+     */
+    useEmailRenderingResult: (
+      slug: string,
+      serverEmailRenderedResult: EmailRenderingResult,
+    ) => EmailRenderingResult;
+  }
   | undefined
 >(undefined);
 
@@ -70,11 +72,16 @@ export const EmailsProvider = (props: {
         });
 
       for (const change of changes) {
+        const normalizedEmailsDirRelativePath = normalizePath(
+          emailsDirRelativePath,
+        );
         const slugForChangedEmail =
           // filename ex: emails/apple-receipt.tsx
           // so we need to remove the "emails/" because it isn't used
           // on the slug parameter for the preview page
-          change.filename.split(pathSeparator).slice(1).join('/');
+          change.filename
+            .replace(`${normalizedEmailsDirRelativePath}${pathSeparator}`, '')
+            .replace(normalizedEmailsDirRelativePath, '');
 
         const lastResult = renderingResultPerEmailSlug[slugForChangedEmail];
 
