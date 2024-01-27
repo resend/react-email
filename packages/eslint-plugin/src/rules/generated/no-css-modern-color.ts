e19c3e1095fd3ca6857cd7431ff44033d369aad5
gabriel miranda <gabrielmfern@outlook.com>
2024-01-27 03:55:06 -0300

    fix(eslint-plugin): modern colors being treated as units instead of functions

--- packages/eslint-plugin/src/rules/generated/no-css-modern-color.ts
+++ packages/eslint-plugin/src/rules/generated/no-css-modern-color.ts
@@ -1,7 +1,7 @@
 import { createNoStyleValueKeywordRule } from "../../utils/create-no-style-value-keyword-rule";
 
 export default createNoStyleValueKeywordRule(
-  [/[0-9]+lch/, /[0-9]+oklch/, /[0-9]+lab/, /[0-9]+oklab/],
+  [/lch\([^)]*\)/, /oklch\([^)]*\)/, /lab\([^)]*\)/, /oklab\([^)]*\)/],
   27.027027027027028,
   "https://www.caniemail.com/features/css-modern-color/",
 );
