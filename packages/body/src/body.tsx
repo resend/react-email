4fc8ffe0f448511135b48b71e10eba009664be03
gabriel miranda <gabrielmfern@outlook.com>
2024-02-08 10:58:23 -0300

    chore(body): Add forwardRef

--- packages/body/src/body.tsx
+++ packages/body/src/body.tsx
@@ -1,19 +1,19 @@
 import * as React from "react";
 
+type BodyElement = React.ElementRef<"body">;
 type RootProps = React.ComponentPropsWithoutRef<"body">;
 
 export type BodyProps = RootProps;
 
-export const Body: React.FC<Readonly<BodyProps>> = ({
-  children,
-  style,
-  ...props
-}) => {
+export const Body: React.FC<Readonly<BodyProps>> = React.forwardRef<
+  BodyElement,
+  Readonly<BodyProps>
+>(({ children, style, ...props }, ref) => {
   return (
-    <body {...props} style={style}>
+    <body {...props} ref={ref} style={style}>
       {children}
     </body>
   );
-};
+});
 
 Body.displayName = "Body";
