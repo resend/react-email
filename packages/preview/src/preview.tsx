26fd6ea696d71e40d7896ad773b859be4f536840
gabriel miranda <gabrielmfern@outlook.com>
2024-02-08 11:16:02 -0300

    chore(preview): Add forward ref

--- packages/preview/src/preview.tsx
+++ packages/preview/src/preview.tsx
@@ -8,29 +8,31 @@ export interface PreviewProps extends RootProps {
 
 const PREVIEW_MAX_LENGTH = 150;
 
-export const Preview: React.FC<Readonly<PreviewProps>> = ({
-  children = "",
-  ...props
-}) => {
-  let text = Array.isArray(children) ? children.join("") : children;
-  text = text.substr(0, PREVIEW_MAX_LENGTH);
-  return (
-    <div
-      style={{
-        display: "none",
-        overflow: "hidden",
-        lineHeight: "1px",
-        opacity: 0,
-        maxHeight: 0,
-        maxWidth: 0,
-      }}
-      {...props}
-    >
-      {text}
-      {renderWhiteSpace(text)}
-    </div>
-  );
-};
+export const Preview = React.forwardRef<React.ElementRef<"div">, Readonly<PreviewProps>>(
+  ({ children = "", ...props }, ref) => {
+    let text = Array.isArray(children) ? children.join("") : children;
+    text = text.substr(0, PREVIEW_MAX_LENGTH);
+    return (
+      <div
+        ref={ref}
+        style={{
+          display: "none",
+          overflow: "hidden",
+          lineHeight: "1px",
+          opacity: 0,
+          maxHeight: 0,
+          maxWidth: 0,
+        }}
+        {...props}
+      >
+        {text}
+        {renderWhiteSpace(text)}
+      </div>
+    );
+  },
+);
+
+Preview.displayName = "Preview";
 
 export const renderWhiteSpace = (text: string) => {
   if (text.length >= PREVIEW_MAX_LENGTH) {
