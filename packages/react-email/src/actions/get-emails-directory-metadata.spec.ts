ddd65d1057a9edf6f579736169f2b650f2edcad1
Gabriel Miranda <gabrielmfern@outlook.com>
2024-02-16 14:31:03 -0300

    chore(demo): Move emails into directories of their own (#1298)
    Co-authored-by: Zeno Rocha <zno.rocha@gmail.com>

--- packages/react-email/src/actions/get-emails-directory-metadata.spec.ts
+++ packages/react-email/src/actions/get-emails-directory-metadata.spec.ts
@@ -2,36 +2,72 @@ import path from 'node:path';
 import { getEmailsDirectoryMetadata } from './get-emails-directory-metadata';
 
 test('getEmailsDirectoryMetadata on demo emails', async () => {
-  expect(
-    await getEmailsDirectoryMetadata(
-      path.resolve(__dirname, '../../../../apps/demo/emails/'),
-    ),
-  ).toEqual({
-    absolutePath: path.resolve(__dirname, '../../../../apps/demo/emails/'),
+  const emailsDirectoryPath = path.resolve(
+    __dirname,
+    '../../../../apps/demo/emails/',
+  );
+  expect(await getEmailsDirectoryMetadata(emailsDirectoryPath)).toEqual({
+    absolutePath: emailsDirectoryPath,
     directoryName: 'emails',
-    emailFilenames: [
-      'airbnb-review',
-      'amazon-review',
-      'apple-receipt',
-      'aws-verify-email',
-      'codepen-challengers',
-      'dropbox-reset-password',
-      'github-access-token',
-      'google-play-policy-update',
-      'koala-welcome',
-      'linear-login-code',
-      'netlify-welcome',
-      'nike-receipt',
-      'notion-magic-link',
-      'plaid-verify-identity',
-      'raycast-magic-link',
-      'slack-confirm',
-      'stack-overflow-tips',
-      'stripe-welcome',
-      'twitch-reset-password',
-      'vercel-invite-user',
-      'yelp-recent-login',
+    emailFilenames: [],
+    subDirectories: [
+      {
+        absolutePath: `${emailsDirectoryPath}/magic-links`,
+        directoryName: 'magic-links',
+        emailFilenames: [
+          'aws-verify-email',
+          'linear-login-code',
+          'notion-magic-link',
+          'plaid-verify-identity',
+          'raycast-magic-link',
+          'slack-confirm',
+        ],
+        subDirectories: [],
+      },
+      {
+        absolutePath: `${emailsDirectoryPath}/newsletters`,
+        directoryName: 'newsletters',
+        emailFilenames: [
+          'codepen-challengers',
+          'google-play-policy-update',
+          'stack-overflow-tips',
+        ],
+        subDirectories: [],
+      },
+      {
+        absolutePath: `${emailsDirectoryPath}/notifications`,
+        directoryName: 'notifications',
+        emailFilenames: [
+          'github-access-token',
+          'vercel-invite-user',
+          'yelp-recent-login',
+        ],
+        subDirectories: [],
+      },
+      {
+        absolutePath: `${emailsDirectoryPath}/receipts`,
+        directoryName: 'receipts',
+        emailFilenames: ['apple-receipt', 'nike-receipt'],
+        subDirectories: [],
+      },
+      {
+        absolutePath: `${emailsDirectoryPath}/reset-password`,
+        directoryName: 'reset-password',
+        emailFilenames: ['dropbox-reset-password', 'twitch-reset-password'],
+        subDirectories: [],
+      },
+      {
+        absolutePath: `${emailsDirectoryPath}/reviews`,
+        directoryName: 'reviews',
+        emailFilenames: ['airbnb-review', 'amazon-review'],
+        subDirectories: [],
+      },
+      {
+        absolutePath: `${emailsDirectoryPath}/welcome`,
+        directoryName: 'welcome',
+        emailFilenames: ['koala-welcome', 'netlify-welcome', 'stripe-welcome'],
+        subDirectories: [],
+      },
     ],
-    subDirectories: [],
   });
 });
