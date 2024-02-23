ae5f6973b9b51b65fd677087c7a6e23816a1ec9c
gabriel miranda <gabrielmfern@outlook.com>
2024-02-23 11:29:00 -0300

    feat(tailwind): Integration test for building a Next app using the Tailwind component by installing automatically through yalc

---
+++ packages/tailwind/src/automated-test-next-app/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { renderAsync } from '@react-email/components';
+import { VercelInviteUserEmail } from '../../emails/vercel-invite-user';
+
+export default async function Home() {
+  const emailHTML = await renderAsync(<VercelInviteUserEmail/>);
+  return (
+    <div dangerouslySetInnerHTML={{ __html: emailHTML }} />
+  );
+}
