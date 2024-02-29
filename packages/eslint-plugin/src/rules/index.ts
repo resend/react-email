046dac23f5ba3e0422d35d7b2693450b4f6ccc60
gabriel miranda <gabrielmfern@outlook.com>
2024-02-29 11:02:04 -0300

    fix(eslint-plugin): Missing semicolon on no unsupported rule imports in the index

--- packages/eslint-plugin/src/rules/index.ts
+++ packages/eslint-plugin/src/rules/index.ts
@@ -1,195 +1,195 @@
-import noUnsupportedOnAppleMailMacos from "./no-unsupported-on-apple-mail-macos"
+import noUnsupportedOnAppleMailMacos from "./no-unsupported-on-apple-mail-macos";
 import noPartiallySupportedOnAppleMailMacos from "./no-partially-supported-on-apple-mail-macos";
-import noUnsupportedOnAppleMailIos from "./no-unsupported-on-apple-mail-ios"
+import noUnsupportedOnAppleMailIos from "./no-unsupported-on-apple-mail-ios";
 import noPartiallySupportedOnAppleMailIos from "./no-partially-supported-on-apple-mail-ios";
-import noUnsupportedOnGmailDesktopWebmail from "./no-unsupported-on-gmail-desktop-webmail"
+import noUnsupportedOnGmailDesktopWebmail from "./no-unsupported-on-gmail-desktop-webmail";
 import noPartiallySupportedOnGmailDesktopWebmail from "./no-partially-supported-on-gmail-desktop-webmail";
-import noUnsupportedOnGmailIos from "./no-unsupported-on-gmail-ios"
+import noUnsupportedOnGmailIos from "./no-unsupported-on-gmail-ios";
 import noPartiallySupportedOnGmailIos from "./no-partially-supported-on-gmail-ios";
-import noUnsupportedOnGmailAndroid from "./no-unsupported-on-gmail-android"
+import noUnsupportedOnGmailAndroid from "./no-unsupported-on-gmail-android";
 import noPartiallySupportedOnGmailAndroid from "./no-partially-supported-on-gmail-android";
-import noUnsupportedOnGmailMobileWebmail from "./no-unsupported-on-gmail-mobile-webmail"
+import noUnsupportedOnGmailMobileWebmail from "./no-unsupported-on-gmail-mobile-webmail";
 import noPartiallySupportedOnGmailMobileWebmail from "./no-partially-supported-on-gmail-mobile-webmail";
-import noUnsupportedOnOrangeDesktopWebmail from "./no-unsupported-on-orange-desktop-webmail"
+import noUnsupportedOnOrangeDesktopWebmail from "./no-unsupported-on-orange-desktop-webmail";
 import noPartiallySupportedOnOrangeDesktopWebmail from "./no-partially-supported-on-orange-desktop-webmail";
-import noUnsupportedOnOrangeIos from "./no-unsupported-on-orange-ios"
+import noUnsupportedOnOrangeIos from "./no-unsupported-on-orange-ios";
 import noPartiallySupportedOnOrangeIos from "./no-partially-supported-on-orange-ios";
-import noUnsupportedOnOrangeAndroid from "./no-unsupported-on-orange-android"
+import noUnsupportedOnOrangeAndroid from "./no-unsupported-on-orange-android";
 import noPartiallySupportedOnOrangeAndroid from "./no-partially-supported-on-orange-android";
-import noUnsupportedOnOutlookWindows from "./no-unsupported-on-outlook-windows"
+import noUnsupportedOnOutlookWindows from "./no-unsupported-on-outlook-windows";
 import noPartiallySupportedOnOutlookWindows from "./no-partially-supported-on-outlook-windows";
-import noUnsupportedOnOutlookWindowsMail from "./no-unsupported-on-outlook-windows-mail"
+import noUnsupportedOnOutlookWindowsMail from "./no-unsupported-on-outlook-windows-mail";
 import noPartiallySupportedOnOutlookWindowsMail from "./no-partially-supported-on-outlook-windows-mail";
-import noUnsupportedOnOutlookMacos from "./no-unsupported-on-outlook-macos"
+import noUnsupportedOnOutlookMacos from "./no-unsupported-on-outlook-macos";
 import noPartiallySupportedOnOutlookMacos from "./no-partially-supported-on-outlook-macos";
-import noUnsupportedOnOutlookOutlookCom from "./no-unsupported-on-outlook-outlook-com"
+import noUnsupportedOnOutlookOutlookCom from "./no-unsupported-on-outlook-outlook-com";
 import noPartiallySupportedOnOutlookOutlookCom from "./no-partially-supported-on-outlook-outlook-com";
-import noUnsupportedOnOutlookIos from "./no-unsupported-on-outlook-ios"
+import noUnsupportedOnOutlookIos from "./no-unsupported-on-outlook-ios";
 import noPartiallySupportedOnOutlookIos from "./no-partially-supported-on-outlook-ios";
-import noUnsupportedOnOutlookAndroid from "./no-unsupported-on-outlook-android"
+import noUnsupportedOnOutlookAndroid from "./no-unsupported-on-outlook-android";
 import noPartiallySupportedOnOutlookAndroid from "./no-partially-supported-on-outlook-android";
-import noUnsupportedOnYahooDesktopWebmail from "./no-unsupported-on-yahoo-desktop-webmail"
+import noUnsupportedOnYahooDesktopWebmail from "./no-unsupported-on-yahoo-desktop-webmail";
 import noPartiallySupportedOnYahooDesktopWebmail from "./no-partially-supported-on-yahoo-desktop-webmail";
-import noUnsupportedOnYahooIos from "./no-unsupported-on-yahoo-ios"
+import noUnsupportedOnYahooIos from "./no-unsupported-on-yahoo-ios";
 import noPartiallySupportedOnYahooIos from "./no-partially-supported-on-yahoo-ios";
-import noUnsupportedOnYahooAndroid from "./no-unsupported-on-yahoo-android"
+import noUnsupportedOnYahooAndroid from "./no-unsupported-on-yahoo-android";
 import noPartiallySupportedOnYahooAndroid from "./no-partially-supported-on-yahoo-android";
-import noUnsupportedOnAolDesktopWebmail from "./no-unsupported-on-aol-desktop-webmail"
+import noUnsupportedOnAolDesktopWebmail from "./no-unsupported-on-aol-desktop-webmail";
 import noPartiallySupportedOnAolDesktopWebmail from "./no-partially-supported-on-aol-desktop-webmail";
-import noUnsupportedOnAolIos from "./no-unsupported-on-aol-ios"
+import noUnsupportedOnAolIos from "./no-unsupported-on-aol-ios";
 import noPartiallySupportedOnAolIos from "./no-partially-supported-on-aol-ios";
-import noUnsupportedOnAolAndroid from "./no-unsupported-on-aol-android"
+import noUnsupportedOnAolAndroid from "./no-unsupported-on-aol-android";
 import noPartiallySupportedOnAolAndroid from "./no-partially-supported-on-aol-android";
-import noUnsupportedOnSamsungEmailAndroid from "./no-unsupported-on-samsung-email-android"
+import noUnsupportedOnSamsungEmailAndroid from "./no-unsupported-on-samsung-email-android";
 import noPartiallySupportedOnSamsungEmailAndroid from "./no-partially-supported-on-samsung-email-android";
-import noUnsupportedOnSfrDesktopWebmail from "./no-unsupported-on-sfr-desktop-webmail"
+import noUnsupportedOnSfrDesktopWebmail from "./no-unsupported-on-sfr-desktop-webmail";
 import noPartiallySupportedOnSfrDesktopWebmail from "./no-partially-supported-on-sfr-desktop-webmail";
-import noUnsupportedOnSfrIos from "./no-unsupported-on-sfr-ios"
+import noUnsupportedOnSfrIos from "./no-unsupported-on-sfr-ios";
 import noPartiallySupportedOnSfrIos from "./no-partially-supported-on-sfr-ios";
-import noUnsupportedOnSfrAndroid from "./no-unsupported-on-sfr-android"
+import noUnsupportedOnSfrAndroid from "./no-unsupported-on-sfr-android";
 import noPartiallySupportedOnSfrAndroid from "./no-partially-supported-on-sfr-android";
-import noUnsupportedOnThunderbirdMacos from "./no-unsupported-on-thunderbird-macos"
+import noUnsupportedOnThunderbirdMacos from "./no-unsupported-on-thunderbird-macos";
 import noPartiallySupportedOnThunderbirdMacos from "./no-partially-supported-on-thunderbird-macos";
-import noUnsupportedOnThunderbirdWindows from "./no-unsupported-on-thunderbird-windows"
+import noUnsupportedOnThunderbirdWindows from "./no-unsupported-on-thunderbird-windows";
 import noPartiallySupportedOnThunderbirdWindows from "./no-partially-supported-on-thunderbird-windows";
-import noUnsupportedOnProtonmailDesktopWebmail from "./no-unsupported-on-protonmail-desktop-webmail"
+import noUnsupportedOnProtonmailDesktopWebmail from "./no-unsupported-on-protonmail-desktop-webmail";
 import noPartiallySupportedOnProtonmailDesktopWebmail from "./no-partially-supported-on-protonmail-desktop-webmail";
-import noUnsupportedOnProtonmailIos from "./no-unsupported-on-protonmail-ios"
+import noUnsupportedOnProtonmailIos from "./no-unsupported-on-protonmail-ios";
 import noPartiallySupportedOnProtonmailIos from "./no-partially-supported-on-protonmail-ios";
-import noUnsupportedOnProtonmailAndroid from "./no-unsupported-on-protonmail-android"
+import noUnsupportedOnProtonmailAndroid from "./no-unsupported-on-protonmail-android";
 import noPartiallySupportedOnProtonmailAndroid from "./no-partially-supported-on-protonmail-android";
-import noUnsupportedOnHeyDesktopWebmail from "./no-unsupported-on-hey-desktop-webmail"
+import noUnsupportedOnHeyDesktopWebmail from "./no-unsupported-on-hey-desktop-webmail";
 import noPartiallySupportedOnHeyDesktopWebmail from "./no-partially-supported-on-hey-desktop-webmail";
-import noUnsupportedOnMailRuDesktopWebmail from "./no-unsupported-on-mail-ru-desktop-webmail"
+import noUnsupportedOnMailRuDesktopWebmail from "./no-unsupported-on-mail-ru-desktop-webmail";
 import noPartiallySupportedOnMailRuDesktopWebmail from "./no-partially-supported-on-mail-ru-desktop-webmail";
-import noUnsupportedOnFastmailDesktopWebmail from "./no-unsupported-on-fastmail-desktop-webmail"
+import noUnsupportedOnFastmailDesktopWebmail from "./no-unsupported-on-fastmail-desktop-webmail";
 import noPartiallySupportedOnFastmailDesktopWebmail from "./no-partially-supported-on-fastmail-desktop-webmail";
-import noUnsupportedOnLaposteDesktopWebmail from "./no-unsupported-on-laposte-desktop-webmail"
+import noUnsupportedOnLaposteDesktopWebmail from "./no-unsupported-on-laposte-desktop-webmail";
 import noPartiallySupportedOnLaposteDesktopWebmail from "./no-partially-supported-on-laposte-desktop-webmail";
-import noUnsupportedOnLaposteIos from "./no-unsupported-on-laposte-ios"
+import noUnsupportedOnLaposteIos from "./no-unsupported-on-laposte-ios";
 import noPartiallySupportedOnLaposteIos from "./no-partially-supported-on-laposte-ios";
-import noUnsupportedOnLaposteAndroid from "./no-unsupported-on-laposte-android"
+import noUnsupportedOnLaposteAndroid from "./no-unsupported-on-laposte-android";
 import noPartiallySupportedOnLaposteAndroid from "./no-partially-supported-on-laposte-android";
-import noUnsupportedOnFreeFrDesktopWebmail from "./no-unsupported-on-free-fr-desktop-webmail"
+import noUnsupportedOnFreeFrDesktopWebmail from "./no-unsupported-on-free-fr-desktop-webmail";
 import noPartiallySupportedOnFreeFrDesktopWebmail from "./no-partially-supported-on-free-fr-desktop-webmail";
-import noUnsupportedOnTOnlineDeDesktopWebmail from "./no-unsupported-on-t-online-de-desktop-webmail"
+import noUnsupportedOnTOnlineDeDesktopWebmail from "./no-unsupported-on-t-online-de-desktop-webmail";
 import noPartiallySupportedOnTOnlineDeDesktopWebmail from "./no-partially-supported-on-t-online-de-desktop-webmail";
-import noUnsupportedOnGmxDesktopWebmail from "./no-unsupported-on-gmx-desktop-webmail"
+import noUnsupportedOnGmxDesktopWebmail from "./no-unsupported-on-gmx-desktop-webmail";
 import noPartiallySupportedOnGmxDesktopWebmail from "./no-partially-supported-on-gmx-desktop-webmail";
-import noUnsupportedOnGmxIos from "./no-unsupported-on-gmx-ios"
+import noUnsupportedOnGmxIos from "./no-unsupported-on-gmx-ios";
 import noPartiallySupportedOnGmxIos from "./no-partially-supported-on-gmx-ios";
-import noUnsupportedOnGmxAndroid from "./no-unsupported-on-gmx-android"
+import noUnsupportedOnGmxAndroid from "./no-unsupported-on-gmx-android";
 import noPartiallySupportedOnGmxAndroid from "./no-partially-supported-on-gmx-android";
-import noUnsupportedOnWebDeDesktopWebmail from "./no-unsupported-on-web-de-desktop-webmail"
+import noUnsupportedOnWebDeDesktopWebmail from "./no-unsupported-on-web-de-desktop-webmail";
 import noPartiallySupportedOnWebDeDesktopWebmail from "./no-partially-supported-on-web-de-desktop-webmail";
-import noUnsupportedOnWebDeIos from "./no-unsupported-on-web-de-ios"
+import noUnsupportedOnWebDeIos from "./no-unsupported-on-web-de-ios";
 import noPartiallySupportedOnWebDeIos from "./no-partially-supported-on-web-de-ios";
-import noUnsupportedOnWebDeAndroid from "./no-unsupported-on-web-de-android"
+import noUnsupportedOnWebDeAndroid from "./no-unsupported-on-web-de-android";
 import noPartiallySupportedOnWebDeAndroid from "./no-partially-supported-on-web-de-android";
-import noUnsupportedOnIonos1and1DesktopWebmail from "./no-unsupported-on-ionos-1and1-desktop-webmail"
+import noUnsupportedOnIonos1and1DesktopWebmail from "./no-unsupported-on-ionos-1and1-desktop-webmail";
 import noPartiallySupportedOnIonos1and1DesktopWebmail from "./no-partially-supported-on-ionos-1and1-desktop-webmail";
-import noUnsupportedOnIonos1and1Android from "./no-unsupported-on-ionos-1and1-android"
+import noUnsupportedOnIonos1and1Android from "./no-unsupported-on-ionos-1and1-android";
 import noPartiallySupportedOnIonos1and1Android from "./no-partially-supported-on-ionos-1and1-android";
-import noUnsupportedOnWpPlDesktopWebmail from "./no-unsupported-on-wp-pl-desktop-webmail"
+import noUnsupportedOnWpPlDesktopWebmail from "./no-unsupported-on-wp-pl-desktop-webmail";
 import noPartiallySupportedOnWpPlDesktopWebmail from "./no-partially-supported-on-wp-pl-desktop-webmail";
-import noUnsupportedOnRainloopDesktopWebmail from "./no-unsupported-on-rainloop-desktop-webmail"
+import noUnsupportedOnRainloopDesktopWebmail from "./no-unsupported-on-rainloop-desktop-webmail";
 import noPartiallySupportedOnRainloopDesktopWebmail from "./no-partially-supported-on-rainloop-desktop-webmail";
 
 export {
-  noUnsupportedOnAppleMailMacos
+  noUnsupportedOnAppleMailMacos,
   noPartiallySupportedOnAppleMailMacos,
-  noUnsupportedOnAppleMailIos
+  noUnsupportedOnAppleMailIos,
   noPartiallySupportedOnAppleMailIos,
-  noUnsupportedOnGmailDesktopWebmail
+  noUnsupportedOnGmailDesktopWebmail,
   noPartiallySupportedOnGmailDesktopWebmail,
-  noUnsupportedOnGmailIos
+  noUnsupportedOnGmailIos,
   noPartiallySupportedOnGmailIos,
-  noUnsupportedOnGmailAndroid
+  noUnsupportedOnGmailAndroid,
   noPartiallySupportedOnGmailAndroid,
-  noUnsupportedOnGmailMobileWebmail
+  noUnsupportedOnGmailMobileWebmail,
   noPartiallySupportedOnGmailMobileWebmail,
-  noUnsupportedOnOrangeDesktopWebmail
+  noUnsupportedOnOrangeDesktopWebmail,
   noPartiallySupportedOnOrangeDesktopWebmail,
-  noUnsupportedOnOrangeIos
+  noUnsupportedOnOrangeIos,
   noPartiallySupportedOnOrangeIos,
-  noUnsupportedOnOrangeAndroid
+  noUnsupportedOnOrangeAndroid,
   noPartiallySupportedOnOrangeAndroid,
-  noUnsupportedOnOutlookWindows
+  noUnsupportedOnOutlookWindows,
   noPartiallySupportedOnOutlookWindows,
-  noUnsupportedOnOutlookWindowsMail
+  noUnsupportedOnOutlookWindowsMail,
   noPartiallySupportedOnOutlookWindowsMail,
-  noUnsupportedOnOutlookMacos
+  noUnsupportedOnOutlookMacos,
   noPartiallySupportedOnOutlookMacos,
-  noUnsupportedOnOutlookOutlookCom
+  noUnsupportedOnOutlookOutlookCom,
   noPartiallySupportedOnOutlookOutlookCom,
-  noUnsupportedOnOutlookIos
+  noUnsupportedOnOutlookIos,
   noPartiallySupportedOnOutlookIos,
-  noUnsupportedOnOutlookAndroid
+  noUnsupportedOnOutlookAndroid,
   noPartiallySupportedOnOutlookAndroid,
-  noUnsupportedOnYahooDesktopWebmail
+  noUnsupportedOnYahooDesktopWebmail,
   noPartiallySupportedOnYahooDesktopWebmail,
-  noUnsupportedOnYahooIos
+  noUnsupportedOnYahooIos,
   noPartiallySupportedOnYahooIos,
-  noUnsupportedOnYahooAndroid
+  noUnsupportedOnYahooAndroid,
   noPartiallySupportedOnYahooAndroid,
-  noUnsupportedOnAolDesktopWebmail
+  noUnsupportedOnAolDesktopWebmail,
   noPartiallySupportedOnAolDesktopWebmail,
-  noUnsupportedOnAolIos
+  noUnsupportedOnAolIos,
   noPartiallySupportedOnAolIos,
-  noUnsupportedOnAolAndroid
+  noUnsupportedOnAolAndroid,
   noPartiallySupportedOnAolAndroid,
-  noUnsupportedOnSamsungEmailAndroid
+  noUnsupportedOnSamsungEmailAndroid,
   noPartiallySupportedOnSamsungEmailAndroid,
-  noUnsupportedOnSfrDesktopWebmail
+  noUnsupportedOnSfrDesktopWebmail,
   noPartiallySupportedOnSfrDesktopWebmail,
-  noUnsupportedOnSfrIos
+  noUnsupportedOnSfrIos,
   noPartiallySupportedOnSfrIos,
-  noUnsupportedOnSfrAndroid
+  noUnsupportedOnSfrAndroid,
   noPartiallySupportedOnSfrAndroid,
-  noUnsupportedOnThunderbirdMacos
+  noUnsupportedOnThunderbirdMacos,
   noPartiallySupportedOnThunderbirdMacos,
-  noUnsupportedOnThunderbirdWindows
+  noUnsupportedOnThunderbirdWindows,
   noPartiallySupportedOnThunderbirdWindows,
-  noUnsupportedOnProtonmailDesktopWebmail
+  noUnsupportedOnProtonmailDesktopWebmail,
   noPartiallySupportedOnProtonmailDesktopWebmail,
-  noUnsupportedOnProtonmailIos
+  noUnsupportedOnProtonmailIos,
   noPartiallySupportedOnProtonmailIos,
-  noUnsupportedOnProtonmailAndroid
+  noUnsupportedOnProtonmailAndroid,
   noPartiallySupportedOnProtonmailAndroid,
-  noUnsupportedOnHeyDesktopWebmail
+  noUnsupportedOnHeyDesktopWebmail,
   noPartiallySupportedOnHeyDesktopWebmail,
-  noUnsupportedOnMailRuDesktopWebmail
+  noUnsupportedOnMailRuDesktopWebmail,
   noPartiallySupportedOnMailRuDesktopWebmail,
-  noUnsupportedOnFastmailDesktopWebmail
+  noUnsupportedOnFastmailDesktopWebmail,
   noPartiallySupportedOnFastmailDesktopWebmail,
-  noUnsupportedOnLaposteDesktopWebmail
+  noUnsupportedOnLaposteDesktopWebmail,
   noPartiallySupportedOnLaposteDesktopWebmail,
-  noUnsupportedOnLaposteIos
+  noUnsupportedOnLaposteIos,
   noPartiallySupportedOnLaposteIos,
-  noUnsupportedOnLaposteAndroid
+  noUnsupportedOnLaposteAndroid,
   noPartiallySupportedOnLaposteAndroid,
-  noUnsupportedOnFreeFrDesktopWebmail
+  noUnsupportedOnFreeFrDesktopWebmail,
   noPartiallySupportedOnFreeFrDesktopWebmail,
-  noUnsupportedOnTOnlineDeDesktopWebmail
+  noUnsupportedOnTOnlineDeDesktopWebmail,
   noPartiallySupportedOnTOnlineDeDesktopWebmail,
-  noUnsupportedOnGmxDesktopWebmail
+  noUnsupportedOnGmxDesktopWebmail,
   noPartiallySupportedOnGmxDesktopWebmail,
-  noUnsupportedOnGmxIos
+  noUnsupportedOnGmxIos,
   noPartiallySupportedOnGmxIos,
-  noUnsupportedOnGmxAndroid
+  noUnsupportedOnGmxAndroid,
   noPartiallySupportedOnGmxAndroid,
-  noUnsupportedOnWebDeDesktopWebmail
+  noUnsupportedOnWebDeDesktopWebmail,
   noPartiallySupportedOnWebDeDesktopWebmail,
-  noUnsupportedOnWebDeIos
+  noUnsupportedOnWebDeIos,
   noPartiallySupportedOnWebDeIos,
-  noUnsupportedOnWebDeAndroid
+  noUnsupportedOnWebDeAndroid,
   noPartiallySupportedOnWebDeAndroid,
-  noUnsupportedOnIonos1and1DesktopWebmail
+  noUnsupportedOnIonos1and1DesktopWebmail,
   noPartiallySupportedOnIonos1and1DesktopWebmail,
-  noUnsupportedOnIonos1and1Android
+  noUnsupportedOnIonos1and1Android,
   noPartiallySupportedOnIonos1and1Android,
-  noUnsupportedOnWpPlDesktopWebmail
+  noUnsupportedOnWpPlDesktopWebmail,
   noPartiallySupportedOnWpPlDesktopWebmail,
-  noUnsupportedOnRainloopDesktopWebmail
+  noUnsupportedOnRainloopDesktopWebmail,
   noPartiallySupportedOnRainloopDesktopWebmail,
 };
