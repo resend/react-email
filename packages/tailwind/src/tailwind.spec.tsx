623382c65d7826dab41402dcf803390fdf945e6b
Ben Gracewood <ben@gracewood.nz>
2024-04-04 01:46:13 +1300

    fix(tailwind): React.forwardRef components not working (#1335)

--- packages/tailwind/src/tailwind.spec.tsx
+++ packages/tailwind/src/tailwind.spec.tsx
@@ -1,11 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { renderToStaticMarkup as render } from "react-dom/server";
-import React from "react";
 import { Hr } from "@react-email/hr";
 import { Html } from "@react-email/html";
 import { Head } from "@react-email/head";
 import { Button } from "@react-email/button";
+import React from "react";
 import { Tailwind } from ".";
 import type { TailwindConfig } from ".";
 
@@ -83,6 +83,40 @@ describe("Tailwind component", () => {
         </div>
       );
     };
+
+    const EmailTemplate = () => {
+      return (
+        <Wrapper>
+          <div className="text-[50px] leading-[1] mt-[100px]">Hello world</div>
+          <Brand />
+        </Wrapper>
+      );
+    };
+
+    const actualOutput = render(EmailTemplate());
+
+    expect(actualOutput).toMatchInlineSnapshot(
+      `"<div style=\\"font-size:50px;line-height:1;margin-top:100px\\">Hello world</div><div style=\\"padding:20px\\"><p style=\\"font-weight:700;font-size:50px\\">React Email</p></div>"`,
+    );
+  });
+
+  it("should work with components that use React.forwardRef", () => {
+    const Wrapper = (props: { children: React.ReactNode }) => {
+      return <Tailwind>{props.children}</Tailwind>;
+    };
+
+    const Brand = React.forwardRef<HTMLDivElement>((ref, props) => {
+      return (
+        <div
+          className="p-[20px]"
+          ref={ref as React.LegacyRef<HTMLDivElement>}
+          {...props}
+        >
+          <p className="font-bold text-[50px]">React Email</p>
+        </div>
+      );
+    });
+    Brand.displayName = "Brand";
 
     const EmailTemplate = () => {
       return (
