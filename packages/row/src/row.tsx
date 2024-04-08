c63476fc46a2a466eb12f42b9fac9f4731c6e238
Ross Reicks <rreicks@tractorzoom.com>
2024-04-08 09:45:28 -0500

    chore(row): format

--- packages/row/src/row.tsx
+++ packages/row/src/row.tsx
@@ -5,24 +5,24 @@ type RootProps = React.ComponentPropsWithoutRef<"table">;
 export type RowProps = React.PropsWithChildren<RootProps>
 
 export const Row: React.FC<Readonly<RowProps>> = ({
-  children,
-  style,
-  ...props
+    children,
+    style,
+    ...props
 }) => {
-  return (
-    <table
-      align="center"
-      width="100%"
-      {...props}
-      border={0}
-      cellPadding="0"
-      cellSpacing="0"
-      role="presentation"
-      style={style}
-    >
-      <tbody style={{ width: "100%" }}>
-        <tr style={{ width: "100%" }}>{children}</tr>
-      </tbody>
-    </table>
-  );
+    return (
+        <table
+            align="center"
+            width="100%"
+            {...props}
+            border={0}
+            cellPadding="0"
+            cellSpacing="0"
+            role="presentation"
+            style={style}
+        >
+            <tbody style={{ width: "100%" }}>
+                <tr style={{ width: "100%" }}>{children}</tr>
+            </tbody>
+        </table>
+    );
 };
