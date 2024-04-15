ba6f385a15728e0989658203d271c1c5b300fccf
gabriel miranda <gabrielmfern@outlook.com>
2024-04-15 12:37:01 -0300

    fix(python): build importing quickjs polyfills from wrong path

--- packages/rendering-packages/sdks/python/__init__.py
+++ packages/rendering-packages/sdks/python/__init__.py
@@ -11,7 +11,10 @@ plugin = extism.Plugin(manifest, wasi=True)
 
 def render(template_path: str, props: Any):
     # using #this while https://github.com/bellard/quickjs/issues/261 doesn't get fixed
-    template_code = subprocess.run(["node", urllib.parse.urljoin(__file__, "./build.mjs"), template_path], capture_output=True).stdout.decode()
+    template_code = subprocess.run(
+        ["node", urllib.parse.urljoin(__file__, "./build.mjs"), template_path], 
+        capture_output=True
+    ).stdout.decode()
 
     return plugin.call(
         "render",
--- packages/rendering-packages/sdks/python/build.mjs
+++ packages/rendering-packages/sdks/python/build.mjs
@@ -1,9 +1,5 @@
-import path from "path";
-import { promises as fs } from "fs";
 import { build } from "esbuild";
-import url from "url";
-import { polyfillCodeForQuickJS } from "../quickjs-polyfills/polyfill-code-for-quickjs.mjs";
-const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+import { polyfillCodeForQuickJS } from "../../quickjs-polyfills/polyfill-code-for-quickjs.mjs";
 
 const result = await build({
   entryPoints: [process.argv[2]],
@@ -15,9 +11,4 @@ const result = await build({
 });
 
 const bundledFile = await polyfillCodeForQuickJS(result.outputFiles[0].text);
-await fs.writeFile(
-  path.resolve(__dirname, "../compiled-template.js"),
-  bundledFile,
-  "utf8",
-);
 process.stdout.write(bundledFile);
