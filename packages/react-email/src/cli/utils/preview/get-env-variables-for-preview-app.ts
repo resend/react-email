34633822a5fafe5485c93097a7f4aeb956100901
gabriel miranda <gabrielmfern@outlook.com>
2024-04-18 13:14:44 -0300

    fix(react-email): wrong env name for EMAILS_DIR_RELATIVE_PATh

--- packages/react-email/src/cli/utils/preview/get-env-variables-for-preview-app.ts
+++ packages/react-email/src/cli/utils/preview/get-env-variables-for-preview-app.ts
@@ -5,7 +5,7 @@ export const getEnvVariablesForPreviewApp = (
   cwd: string,
 ) => {
   return {
-    DIR_RELATIVE_PATH: relativePathToEmailsDirectory,
+    EMAILS_DIR_RELATIVE_PATH: relativePathToEmailsDirectory,
     EMAILS_DIR_ABSOLUTE_PATH: path.resolve(cwd, relativePathToEmailsDirectory),
     USER_PROJECT_LOCATION: cwd,
   } as const;
