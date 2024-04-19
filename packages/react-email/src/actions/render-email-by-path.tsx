65ecc42bffbcf0bd98ca29dd3b658a4282f89f22
gabriel miranda <gabrielmfern@outlook.com>
2024-04-19 10:30:30 -0300

    feat(react-email): Ora spinner for email rendering process

--- packages/react-email/src/actions/render-email-by-path.tsx
+++ packages/react-email/src/actions/render-email-by-path.tsx
@@ -1,8 +1,13 @@
 'use server';
 import fs from 'node:fs';
+import path from 'node:path';
+import ora from 'ora';
+import logSymbols from 'log-symbols';
+import chalk from 'chalk';
 import { getEmailComponent } from '../utils/get-email-component';
 import type { ErrorObject } from '../utils/types/error-object';
 import { improveErrorWithSourceMap } from '../utils/improve-error-with-sourcemap';
+import { closeOraOnSIGNIT } from '../cli/utils/close-ora-on-sigint';
 
 export interface RenderedEmailMetadata {
   markup: string;
@@ -13,15 +18,29 @@ export interface RenderedEmailMetadata {
 export type EmailRenderingResult =
   | RenderedEmailMetadata
   | {
-      error: ErrorObject;
-    };
+    error: ErrorObject;
+  };
 
 export const renderEmailByPath = async (
   emailPath: string,
 ): Promise<EmailRenderingResult> => {
+  const timeBeforeEmailRendered = performance.now();
+
+  const emailFilename = path.basename(emailPath);
+  const spinner = ora({
+    text: `Rendering email template ${emailFilename}\n`,
+    prefixText: ' ',
+  }).start();
+
+  closeOraOnSIGNIT(spinner);
+
   const result = await getEmailComponent(emailPath);
 
   if ('error' in result) {
+    spinner.stopAndPersist({
+      symbol: logSymbols.error,
+      text: `Failed while rendering ${emailFilename}`
+    });
     return { error: result.error };
   }
 
@@ -50,6 +69,20 @@ export const renderEmailByPath = async (
 
     const reactMarkup = await fs.promises.readFile(emailPath, 'utf-8');
 
+    const milisecondsToRendered = performance.now() - timeBeforeEmailRendered;
+    let timeForConsole = `${milisecondsToRendered.toFixed(0)}ms`;
+    if (milisecondsToRendered <= 450) {
+      timeForConsole = chalk.green(timeForConsole);
+    } else if (milisecondsToRendered <= 1000) {
+      timeForConsole = chalk.yellow(timeForConsole);
+    } else {
+      timeForConsole = chalk.red(timeForConsole);
+    }
+    spinner.stopAndPersist({
+      symbol: logSymbols.success,
+      text: `Successfully rendered ${emailFilename} in ${timeForConsole}`
+    });
+
     return {
       markup,
       plainText,
@@ -57,6 +90,11 @@ export const renderEmailByPath = async (
     };
   } catch (exception) {
     const error = exception as Error;
+
+    spinner.stopAndPersist({
+      symbol: logSymbols.error,
+      text: `Failed while rendering ${emailFilename}`
+    });
 
     return {
       error: improveErrorWithSourceMap(
