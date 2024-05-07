8f0ca7fbda2afc48dfb18e813a55c5c0ced488b9
Uğur Demirel <ugurdemirel1905@gmail.com>
2024-05-07 15:14:13 +0300

    fix(react-email): Missing Headers on the global context for email templates (#1446)
    Co-authored-by: gabriel miranda <gabrielmfern@outlook.com>

--- packages/react-email/src/utils/get-email-component.ts
+++ packages/react-email/src/utils/get-email-component.ts
@@ -63,6 +63,7 @@ export const getEmailComponent = async (
     ReadableStream,
     URL,
     URLSearchParams,
+    Headers,
     module: { exports: { default: undefined as unknown } },
     __filanem: emailPath,
     __dirname: path.dirname(emailPath),
