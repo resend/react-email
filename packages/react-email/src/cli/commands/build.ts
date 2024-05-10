1f217e7666ee6240cb9b91b40c33de0af5b9a662
gabriel miranda <gabrielmfern@outlook.com>
2024-05-10 13:49:32 -0300

    change the name in the `package.json` while building
    this is to avoid issues with the preview server and the CLI having the
    same name in our workspace, should not affect any users

--- packages/react-email/src/cli/commands/build.ts
+++ packages/react-email/src/cli/commands/build.ts
@@ -158,12 +158,14 @@ const updatePackageJson = async (builtPreviewAppPath: string) => {
   const packageJson = JSON.parse(
     await fs.promises.readFile(packageJsonPath, 'utf8'),
   ) as {
+    name: string;
     scripts: Record<string, string>;
     dependencies: Record<string, string>;
   };
   packageJson.scripts.build = 'next build';
   packageJson.scripts.start = 'next start';
 
+  packageJson.name = "preview-server";
   packageJson.dependencies.sharp = '0.33.2';
   await fs.promises.writeFile(
     packageJsonPath,
