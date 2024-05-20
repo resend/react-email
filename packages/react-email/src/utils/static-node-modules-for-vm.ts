eb5020a2487425f6be480312597463e7fed109df
gabriel miranda <gabrielmfern@outlook.com>
2024-05-20 16:41:34 -0300

    add missing node modules for react-email's preview server VM

--- packages/react-email/src/utils/static-node-modules-for-vm.ts
+++ packages/react-email/src/utils/static-node-modules-for-vm.ts
@@ -1,32 +1,44 @@
-import zlib from 'node:zlib';
-import vm from 'node:vm';
-import v8 from 'node:v8';
-import util from 'node:util';
-import url from 'node:url';
-import tty from 'node:tty';
-import tls from 'node:tls';
-import timers from 'node:timers';
-import stringDecoder from 'node:string_decoder';
-import stream from 'node:stream';
-import readline from 'node:readline';
-import querystring from 'node:querystring';
-import punycode from 'node:punycode';
-import path from 'node:path';
-import os from 'node:os';
-import net from 'node:net';
-import https from 'node:https';
-import http from 'node:http';
-import fs from 'node:fs';
-import events from 'node:events';
-import domain from 'node:domain';
-import dns from 'node:dns';
-import dgram from 'node:dgram';
-import crypto from 'node:crypto';
-import cluster from 'node:cluster';
-import childProcess from 'node:child_process';
-import buffer from 'node:buffer';
 import assert from 'node:assert';
 import asyncHooks from 'node:async_hooks';
+import buffer from 'node:buffer';
+import childProcess from 'node:child_process';
+import cluster from 'node:cluster';
+import console from 'node:console';
+import constants from 'node:constants';
+import crypto from 'node:crypto';
+import dgram from 'node:dgram';
+import diagnosticsChannel from 'node:diagnostics_channel';
+import dns from 'node:dns';
+import domain from 'node:domain';
+import events from 'node:events';
+import fs from 'node:fs';
+import fsPromises from 'node:fs/promises';
+import http from 'node:http';
+import http2 from 'node:http2';
+import https from 'node:https';
+import inspector from 'node:inspector';
+import module from 'node:module';
+import net from 'node:net';
+import os from 'node:os';
+import path from 'node:path';
+import perfHooks from 'node:perf_hooks';
+import process from 'node:process';
+import punycode from 'node:punycode';
+import querystring from 'node:querystring';
+import readline from 'node:readline';
+import repl from 'node:repl';
+import stream from 'node:stream';
+import stringDecoder from 'node:string_decoder';
+import timers from 'node:timers';
+import tls from 'node:tls';
+import traceEvents from 'node:trace_events';
+import tty from 'node:tty';
+import url from 'node:url';
+import util from 'node:util';
+import v8 from 'node:v8';
+import vm from 'node:vm';
+import workerThreads from 'node:worker_threads';
+import zlib from 'node:zlib';
 
 /**
  * A map of the name of the modules (including `node:` prefixed ones)
@@ -34,64 +46,45 @@ import asyncHooks from 'node:async_hooks';
  * will not be resolved properly
  */
 export const staticNodeModulesForVM = {
-  zlib,
-  'node:zlib': zlib,
-  vm,
-  'node:vm': vm,
-  v8,
-  'node:v8': v8,
-  util,
-  'node:util': util,
-  url,
-  'node:url': url,
-  tty,
-  'node:tty': tty,
-  tls,
-  'node:tls': tls,
-  timers,
-  'node:timers': timers,
-  'string_decoder': stringDecoder,
-  'node:string_decoder': stringDecoder,
-  stream,
-  'node:stream': stream,
-  readline,
-  'node:readline': readline,
-  querystring,
-  'node:querystring': querystring,
-  punycode,
-  'node:punycode': punycode,
-  path,
-  'node:path': path,
-  os,
-  'node:os': os,
-  net,
-  'node:net': net,
-  https,
-  'node:https': https,
-  http,
-  'node:http': http,
-  fs,
-  'node:fs': fs,
-  'fs/promises': fs.promises,
-  'node:fs/promises': fs.promises,
-  events,
-  'node:events': events,
-  domain,
-  'node:domain': domain,
-  dns,
-  'node:dns': dns,
-  dgram,
-  'node:dgram': dgram,
-  crypto,
-  'node:crypto': crypto,
-  cluster,
-  'node:cluster': cluster,
-  'child_process': childProcess,
-  'node:child_process': childProcess,
-  buffer,
-  'node:buffer': buffer,
   assert,
-  'node:assert': assert,
-  'async_hooks': asyncHooks,
-  'node:async_hooks': asyncHooks,
+  async_hooks: asyncHooks,
+  buffer,
+  child_process: childProcess,
+  cluster,
+  console,
+  constants,
+  crypto,
+  dgram,
+  diagnostics_channel: diagnosticsChannel,
+  dns,
+  domain,
+  events,
+  fs,
+  'fs/promises': fsPromises,
+  http,
+  http2,
+  https,
+  inspector,
+  module,
+  net,
+  os,
+  path,
+  perf_hooks: perfHooks,
+  process,
+  punycode,
+  querystring,
+  readline,
+  repl,
+  stream,
+  string_decoder: stringDecoder,
+  timers,
+  tls,
+  trace_events: traceEvents,
+  tty,
+  url,
+  util,
+  v8,
+  vm,
+  worker_threads: workerThreads,
+  zlib,
 };
