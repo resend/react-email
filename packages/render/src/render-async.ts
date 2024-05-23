cc9ee38bca1025ca9346f8dac2125fb26c971225
gabriel miranda <gabrielmfern@outlook.com>
2024-05-23 14:57:03 -0300

    remove any extra doctype from renderAsync's result

--- packages/render/src/render-async.ts
+++ packages/render/src/render-async.ts
@@ -84,7 +84,7 @@ export const renderAsync = async (
   const doctype =
     '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">';
 
-  const document = `${doctype}${html}`;
+  const document = `${doctype}${html.replace(/<!DOCTYPE.*?>/, '')}`;
 
   if (options?.pretty) {
     return pretty(document);
