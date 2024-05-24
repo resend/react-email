503e6788ae5a1f969add3f809af0c94ea8c5d7c4
gabriel miranda <gabrielmfern@outlook.com>
2024-05-24 13:30:11 -0300

    use the types from react-reconciler for fibers

--- packages/render/src/custom-render.ts
+++ packages/render/src/custom-render.ts
@@ -1,32 +1,26 @@
 import { ConcurrentRoot } from "react-reconciler/constants";
+import { Fiber } from "react-reconciler";
+import { convert } from "html-to-text";
 import { renderer } from "./renderer";
 import { Root } from "./renderer/nodes/root";
 import { Options } from "./options";
-import { convert } from "html-to-text";
 import { plainTextSelectors } from "./plain-text-selectors";
 import { pretty } from "./utils/pretty";
 
-interface IncompleteFiber {
-  tag: number;
-  memoizedState: Record<string, unknown> | null;
-  sibling: IncompleteFiber | null;
-  child: IncompleteFiber | null;
-}
-
 /**
-  * Checks recursively through React's fibers to make sure that all suspense
-  * boundaries are complete. It uses the fact that, once they are complete,
-  * they will have a `memoizedState` of null. It also checks for an element being
-  * a Suspense boundary by looking for an internal React field in the fibers.
-  *
-  * There is currently no other way that can be used to wait for these 
-  * Suspense boundaries to be done except for checking the fibers themselves.
-  *
-  * NOTE: this code is very fragile and should be tested very carefully
-  * with each `react-reconciler` update. This is because React might change
-  * these internal structures and cause this code to break.
-  */
-function checkIfAllSuspensesAreDone(fiber: IncompleteFiber) {
+ * Checks recursively through React's fibers to make sure that all suspense
+ * boundaries are complete. It uses the fact that, once they are complete,
+ * they will have a `memoizedState` of null. It also checks for an element being
+ * a Suspense boundary by looking for an internal React field in the fibers.
+ *
+ * There is currently no other way that can be used to wait for these
+ * Suspense boundaries to be done except for checking the fibers themselves.
+ *
+ * NOTE: this code is very fragile and should be tested very carefully
+ * with each `react-reconciler` update. This is because React might change
+ * these internal structures and cause this code to break.
+ */
+function checkIfAllSuspensesAreDone(fiber: Fiber) {
   if (fiber.tag === 13 && fiber.memoizedState !== null) {
     // Suspense boundary is still pending
     return false;
@@ -57,7 +51,7 @@ const renderToMarkup = (element: React.ReactElement) => {
       },
       {},
     ) as {
-      current: IncompleteFiber;
+      current: Fiber;
     };
 
     renderer.updateContainer(element, container, null, () => {
@@ -74,12 +68,15 @@ const renderToMarkup = (element: React.ReactElement) => {
       checkCompletion();
     });
   });
-}
+};
 
 const doctype =
   '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">';
 
-export const render = async (element: React.ReactElement, options?: Options) => {
+export const render = async (
+  element: React.ReactElement,
+  options?: Options,
+) => {
   const markup = await renderToMarkup(element);
 
   const document = `${doctype}${markup}`;
