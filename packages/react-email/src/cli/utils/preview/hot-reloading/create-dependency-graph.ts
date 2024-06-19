c966389e50edf6ac370605ab0ba90075d48ed869
gabriel miranda <gabrielmfern@outlook.com>
2024-06-19 18:14:14 -0300

    undo removal of stale module of the dependency graph

--- packages/react-email/src/cli/utils/preview/hot-reloading/create-dependency-graph.ts
+++ packages/react-email/src/cli/utils/preview/hot-reloading/create-dependency-graph.ts
@@ -193,15 +193,6 @@ export const createDependencyGraph = async (directory: string) => {
           dependencyModule.dependentPaths.filter(
             (dependentPath) => dependentPath !== moduleFilePath,
           );
-        if (
-          dependencyModule.dependentPaths.length === 0 &&
-          // This means that this module is higher up than the original directory we are looking at,
-          // therefore we must remove this module as its sole purpose was to have this module as a
-          // dependent and it is not needed anymore.
-          path.relative(directory, dependencyModule.path).startsWith('..')
-        ) {
-          removeModuleFromGraph(dependencyModule.path);
-        }
       }
     }
 
