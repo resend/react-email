f2f77430bea733173947857b27b1ba59da044e0f
gabriel miranda <gabrielmfern@outlook.com>
2024-06-26 18:18:58 -0300

    fix tests failing for the CLI

--- packages/react-email/src/cli/commands/testing/export.spec.ts
+++ packages/react-email/src/cli/commands/testing/export.spec.ts
@@ -2,12 +2,8 @@ import path from 'node:path';
 import fs from 'node:fs';
 import { exportTemplates } from '../export';
 
-// This test is failing while we are using React 18 in the preview server because
-// the components will use React 19, while the email template will use React 18
-// so the different structures of elements will be used interchangeably which will fail
-// when rendering.
-test.fails('email export', async () => {
-  const pathToEmailsDirectory = path.resolve(__dirname, './emails');
+test('email export', async () => {
+  const pathToEmailsDirectory = path.resolve(__dirname, '../../../../../../apps/demo/emails');
   const pathToDumpMarkup = path.resolve(__dirname, './out');
   await exportTemplates(pathToDumpMarkup, pathToEmailsDirectory, {
     pretty: true,
@@ -17,7 +13,7 @@ test.fails('email export', async () => {
   expect(fs.existsSync(pathToDumpMarkup)).toBe(true);
   expect(
     await fs.promises.readFile(
-      path.resolve(pathToDumpMarkup, './vercel-invite-user.html'),
+      path.resolve(pathToDumpMarkup, './notifications/vercel-invite-user.html'),
       'utf8',
     ),
   ).toMatchSnapshot();
