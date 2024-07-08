d1ce8dafde08a75b975cd387e8bf705c1561d2fe
gabriel miranda <gabrielmfern@outlook.com>
2024-07-08 16:05:57 -0300

    remove tailwind usage from patterns

--- apps/web/src/components/patterns/headers/with-social-icons.tsx
+++ apps/web/src/components/patterns/headers/with-social-icons.tsx
@@ -4,47 +4,46 @@ import {
   Link,
   Row,
   Section,
-  Tailwind,
 } from "@react-email/components";
+import type { ComponentPropsWithoutRef } from "react";
 
 export const title = "With social icons";
 
 const SocialLink = ({ logo, link }: { logo: string; link: string }) => {
   return (
     <Link href={link}>
-      <Img className="mx-1" height="36" src={logo} width="36" />
+      <Img className="mx-[4px]" height="36" src={logo} width="36" />
     </Link>
   );
 };
 
-export const HeaderWithSocialIcons = () => {
+export const HeaderWithSocialIcons = (
+  props: ComponentPropsWithoutRef<typeof Section>,
+) => {
   return (
-    <Tailwind>
-      <Section className="py-[40px] px-[32px] rounded border border-solid border-gray-200 bg-white">
-        <Row>
-          <Column className="w-[80%]">
-            <Img
-              alt="company-logo"
-              src="/static/company-logo.png"
-              height="42"
-            />
-          </Column>
-          <Column align="right">
-            <Row align="right">
-              <Column>
-                <SocialLink link="#" logo="/static/facebook-logo.png" />
-              </Column>
-              <Column>
-                <SocialLink link="#" logo="/static/twitter-logo.png" />
-              </Column>
-              <Column>
-                <SocialLink link="#" logo="/static/instagram-logo.png" />
-              </Column>
-            </Row>
-          </Column>
-        </Row>
-      </Section>
-    </Tailwind>
+    <Section
+      {...props}
+      className="py-[40px] px-[32px] rounded border border-solid border-gray-200 bg-white"
+    >
+      <Row>
+        <Column className="w-[80%]">
+          <Img alt="company-logo" height="42" src="/static/company-logo.png" />
+        </Column>
+        <Column align="right">
+          <Row align="right">
+            <Column>
+              <SocialLink link="#" logo="/static/facebook-logo.png" />
+            </Column>
+            <Column>
+              <SocialLink link="#" logo="/static/twitter-logo.png" />
+            </Column>
+            <Column>
+              <SocialLink link="#" logo="/static/instagram-logo.png" />
+            </Column>
+          </Row>
+        </Column>
+      </Row>
+    </Section>
   );
 };
 
