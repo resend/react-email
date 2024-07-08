151999fc13d3750596e04858b49c624335c667b2
gabriel miranda <gabrielmfern@outlook.com>
2024-07-08 12:33:00 -0300

    add initial patterns page

---
+++ apps/web/src/app/patterns/page.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+
+const title = "Patterns — React Email";
+const description =
+  "Open-source copy-paste patterns to use as building blocks with React Email";
+export const metadata: Metadata = {
+  title,
+  description,
+  openGraph: {
+    title,
+    description,
+  },
+};
+
+const Patterns = () => {
+  return <main>
+  </main>;
+};
+
+export default Patterns;
