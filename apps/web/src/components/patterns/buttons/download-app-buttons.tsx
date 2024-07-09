773350f1432ed61572154ec4fe0a248c5cea1a68
gabriel miranda <gabrielmfern@outlook.com>
2024-07-09 16:38:11 -0300

    use images instead of manually written html for the app store badges

--- apps/web/src/components/patterns/buttons/download-app-buttons.tsx
+++ apps/web/src/components/patterns/buttons/download-app-buttons.tsx
@@ -21,63 +21,16 @@ export const downloadAppButtons = (
           <table>
             <tr>
               <td className="pr-4">
-                <Button
-                  className="w-[170px] box-border bg-black px-[8px] py-[6px] rounded-[12px]"
-                  href="#"
-                >
-                  <Row className="bg-black">
-                    <Column className="pr-[12px]">
-                      <Img
-                        alt="Google Play Logo"
-                        src="/static/google-play-logo.png"
-                        width="32"
-                      />
-                    </Column>
-                    <Column>
-                      <table>
-                        <tr>
-                          <Text className="m-0 font-bold leading-[12px] text-[10px] text-white">
-                            GET IT ON
-                          </Text>
-                        </tr>
-                        <tr>
-                          <Text className="m-0 font-bold text-[20px] text-white">
-                            Google Play
-                          </Text>
-                        </tr>
-                      </table>
-                    </Column>
-                  </Row>
+                <Button href="#">
+                  <Img height={54} src="/static/get-it-on-google-play.png" />
                 </Button>
               </td>
               <td className="pl-4">
-                <Button
-                  className="w-[170px] box-border bg-black px-[8px] py-[6px] rounded-[12px]"
-                  href="#"
-                >
-                  <Row className="bg-black">
-                    <Column className="pr-[12px]">
-                      <Img
-                        alt="Apple Logo"
-                        src="/static/apple-logo.png"
-                        width="32"
-                      />
-                    </Column>
-                    <Column>
-                      <table>
-                        <tr>
-                          <Text className="m-0 font-bold text-white leading-[12px] text-[13px]">
-                            Download on the
-                          </Text>
-                        </tr>
-                        <tr>
-                          <Text className="m-0 font-bold text-white text-[24px]">
-                            App Store
-                          </Text>
-                        </tr>
-                      </table>
-                    </Column>
-                  </Row>
+                <Button href="#">
+                  <Img
+                    height={54}
+                    src="/static/download-on-the-app-store.png"
+                  />
                 </Button>
               </td>
             </tr>
