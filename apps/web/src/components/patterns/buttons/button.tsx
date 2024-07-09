57df6d16db7b9d9b8572247cd55ff5071be682a8
gabriel miranda <gabrielmfern@outlook.com>
2024-07-09 13:54:19 -0300

    fix BaseButton import as

--- apps/web/src/components/patterns/buttons/button.tsx
+++ apps/web/src/components/patterns/buttons/button.tsx
@@ -1,15 +1,15 @@
-import { Button as BaseButton } from "@react-email/components";
+import { Button } from "@react-email/components";
 
 export const title = "One button";
 
 export const button = (
   /* start pattern code */
-  <BaseButton
+  <Button
     className="w-full rounded-[8px] text-center bg-indigo-600 py-[12px] font-semibold text-white"
     href="#"
   >
     Example button
-  </BaseButton>
+  </Button>
   /* end pattern code */
 );
 
