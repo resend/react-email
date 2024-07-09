4917df99c407860dd356d86f8c813de1153131be
gabriel miranda <gabrielmfern@outlook.com>
2024-07-09 14:14:29 -0300

    use a Row instead of a plain table

--- apps/web/src/components/patterns/buttons/two-buttons.tsx
+++ apps/web/src/components/patterns/buttons/two-buttons.tsx
@@ -6,26 +6,24 @@ export const twoButtons = (
   /* start pattern code */
   <Row>
     <Column align="center">
-      <table className="w-full">
-        <tr>
-          <td className="pr-4 w-1/2" colSpan={1}>
-            <Button
-              className="w-full py-[12px] rounded-[8px] bg-indigo-600 text-center font-semibold text-white"
-              href="#"
-            >
-              Explore
-            </Button>
-          </td>
-          <td className="pl-4 w-1/2" colSpan={1}>
-            <Button
-              className="w-full py-[12px] rounded-[8px] border border-solid text-center border-gray-200 bg-white font-semibold text-gray-900"
-              href="#"
-            >
-              Buy
-            </Button>
-          </td>
-        </tr>
-      </table>
+      <Row>
+        <td className="pr-4 w-1/2" colSpan={1}>
+          <Button
+            className="w-full py-[12px] rounded-[8px] bg-indigo-600 text-center font-semibold text-white"
+            href="#"
+          >
+            Explore
+          </Button>
+        </td>
+        <td className="pl-4 w-1/2" colSpan={1}>
+          <Button
+            className="w-full py-[12px] rounded-[8px] border border-solid text-center border-gray-200 bg-white font-semibold text-gray-900"
+            href="#"
+          >
+            Buy
+          </Button>
+        </td>
+      </Row>
     </Column>
   </Row>
   /* end pattern code */
