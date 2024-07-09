45283707980e1ab0a22dae81dfa047a5528d4056
gabriel miranda <gabrielmfern@outlook.com>
2024-07-09 13:54:12 -0300

    add code for two buttons pattern

--- apps/web/src/components/patterns/buttons/two-buttons.tsx
+++ apps/web/src/components/patterns/buttons/two-buttons.tsx
@@ -1,15 +1,33 @@
-import { Button as BaseButton } from "@react-email/components";
+import { Button, Column, Row } from "@react-email/components";
 
 export const title = "Two buttons";
 
 export const twoButtons = (
   /* start pattern code */
-  <BaseButton
-    className="w-full rounded-[8px] text-center bg-indigo-600 py-[12px] font-semibold text-white"
-    href="#"
-  >
-    Example button
-  </BaseButton>
+  <Row>
+    <Column align="center">
+      <table className="w-full">
+        <tr>
+          <td className="pr-2 w-1/2" colSpan={1}>
+            <Button
+              className="w-full py-[12px] rounded-[8px] bg-indigo-600 text-center font-semibold text-white"
+              href="#"
+            >
+              Explore
+            </Button>
+          </td>
+          <td className="pl-2 w-1/2" colSpan={1}>
+            <Button
+              className="w-full py-[12px] rounded-[8px] border border-solid text-center border-gray-200 bg-white font-semibold text-gray-900"
+              href="#"
+            >
+              Buy
+            </Button>
+          </td>
+        </tr>
+      </table>
+    </Column>
+  </Row>
   /* end pattern code */
 );
 
