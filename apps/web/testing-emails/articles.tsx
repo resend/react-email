d18b46ff8cd01907ab22687b2a5e7ef723730df9
gabriel miranda <gabrielmfern@outlook.com>
2024-07-11 16:15:18 -0300

    add a font to all of the testing emails

--- apps/web/testing-emails/articles.tsx
+++ apps/web/testing-emails/articles.tsx
@@ -1,4 +1,4 @@
-import { Body, Container, Head, Html, Tailwind } from "@react-email/components";
+import { Body, Container, Font, Head, Html, Tailwind } from "@react-email/components";
 import tailwindConfig from "./tailwind.config";
 import { articleWithTwoCards } from "./patterns/articles/article-with-two-cards";
 import { articleWithImageAsBackground } from "./patterns/articles/article-with-image-as-background";
@@ -6,7 +6,18 @@ import { articleWithImageAsBackground } from "./patterns/articles/article-with-i
 const Articles = () => {
   return (
     <Html>
-      <Head />
+      <Head>
+        <Font
+          fallbackFontFamily="Helvetica"
+          fontFamily="Inter"
+          fontStyle="normal"
+          fontWeight={400}
+          webFont={{
+            url: "https://fonts.googleapis.com/css2?family=Inter&display=swap",
+            format: "woff2",
+          }}
+        />
+      </Head>
       <Tailwind config={tailwindConfig}>
         <Body className="bg-white font-sans antialiased">
           <Container>
--- apps/web/testing-emails/headers.tsx
+++ apps/web/testing-emails/headers.tsx
@@ -1,23 +1,43 @@
-import { Body, Container, Head, Html, Tailwind } from '@react-email/components';
-import { headerWithSocialIcons } from './patterns/headers/with-social-icons';
-import { headerWithCenteredMenu } from './patterns/headers/with-centered-menu';
-import { headerWithMenu } from './patterns/headers/with-menu';
-import tailwindConfig from './tailwind.config';
+import {
+  Body,
+  Container,
+  Font,
+  Head,
+  Html,
+  Tailwind,
+} from "@react-email/components";
+import { headerWithSocialIcons } from "./patterns/headers/with-social-icons";
+import { headerWithCenteredMenu } from "./patterns/headers/with-centered-menu";
+import { headerWithMenu } from "./patterns/headers/with-menu";
+import tailwindConfig from "./tailwind.config";
 
 const Headers = () => {
-  return <Html>
-    <Head/>
+  return (
+    <Html>
+      <Head>
+        <Font
+          fallbackFontFamily="Helvetica"
+          fontFamily="Inter"
+          fontStyle="normal"
+          fontWeight={400}
+          webFont={{
+            url: "https://fonts.googleapis.com/css2?family=Inter&display=swap",
+            format: "woff2",
+          }}
+        />
+      </Head>
 
-    <Tailwind config={tailwindConfig}>
-      <Body>
-        <Container>
-          {headerWithSocialIcons}
-          {headerWithCenteredMenu}
-          {headerWithMenu}
-        </Container>
-      </Body>
-    </Tailwind>
-  </Html>;
-}
+      <Tailwind config={tailwindConfig}>
+        <Body>
+          <Container>
+            {headerWithSocialIcons}
+            {headerWithCenteredMenu}
+            {headerWithMenu}
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
+};
 
 export default Headers;
