931f257ce5eaca697c0a6ac7ab67026a27a6c7ee
gabriel miranda <gabrielmfern@outlook.com>
2024-07-11 16:15:18 -0300

    add a font to all of the testing emails

--- apps/web/testing-emails/buttons.tsx
+++ apps/web/testing-emails/buttons.tsx
@@ -1,6 +1,7 @@
 import {
   Body,
   Container,
+  Font,
   Head,
   Html,
   Section,
@@ -14,19 +15,24 @@ import tailwindConfig from "./tailwind.config";
 const Buttons = () => {
   return (
     <Html>
-      <Head />
+      <Head>
+        <Font
+          fallbackFontFamily="Helvetica"
+          fontFamily="Inter"
+          fontStyle="normal"
+          fontWeight={400}
+          webFont={{
+            url: "https://fonts.googleapis.com/css2?family=Inter&display=swap",
+            format: "woff2",
+          }}
+        />
+      </Head>
       <Tailwind config={tailwindConfig}>
         <Body className="bg-white font-sans antialiased">
           <Container className="my-[40px] rounded-[4px]">
-            <Section>
-              {button}
-            </Section>
-            <Section>
-              {twoButtons}
-            </Section>
-            <Section>
-              {downloadAppButtons}
-            </Section>
+            <Section>{button}</Section>
+            <Section>{twoButtons}</Section>
+            <Section>{downloadAppButtons}</Section>
           </Container>
         </Body>
       </Tailwind>
