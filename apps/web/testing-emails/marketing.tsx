7e8a9fc9af9152f438ba99603f198c2a3f90ffda
gabriel miranda <gabrielmfern@outlook.com>
2024-07-11 15:46:19 -0300

    add checkout pattern

--- apps/web/testing-emails/marketing.tsx
+++ apps/web/testing-emails/marketing.tsx
@@ -1,15 +1,28 @@
-import { Body, Container, Head, Html, Tailwind } from "@react-email/components";
+import { Body, Container, Font, Head, Html, Tailwind } from "@react-email/components";
 import { survey } from "./patterns/marketing/survey";
+import { checkout } from "./patterns/marketing/checkout";
 import tailwindConfig from "./tailwind.config";
 
 const Marketing = () => {
   return (
     <Html>
-      <Head />
+      <Head>
+        <Font
+          fallbackFontFamily="Helvetica"
+          fontFamily="Inter"
+          fontStyle="normal"
+          fontWeight={400}
+          webFont={{
+            url: "https://fonts.googleapis.com/css2?family=Inter&display=swap",
+            format: "woff2",
+          }}
+        />
+      </Head>
 
       <Tailwind config={tailwindConfig}>
         <Body>
           <Container>
+            {checkout}
             {survey}
           </Container>
         </Body>
