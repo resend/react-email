a79b7fd8362aba4eae1f1d19f28fabd4d27fcac6
gabriel miranda <gabrielmfern@outlook.com>
2024-07-17 13:37:37 -0300

    make it so that all "workspace:" prefixes are removed from template dependencies

--- packages/create-email/src/index.js
+++ packages/create-email/src/index.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
-import { Command } from "commander";
 import { fileURLToPath } from "node:url";
+import path from "node:path";
+import { Command } from "commander";
 import fse from "fs-extra";
 import logSymbols from "log-symbols";
-import { tree } from "./tree.js";
 import ora from "ora";
-import path from "node:path";
+import { tree } from "./tree.js";
 
 const init = async (name) => {
   const spinner = ora("Preparing files...\n").start();
@@ -26,12 +26,32 @@ const init = async (name) => {
   const resolvedProjectPath = path.resolve(projectPath);
 
   fse.copySync(templatePath, resolvedProjectPath, { recursive: true });
+  const templatePackageJsonPath = path.resolve(
+    resolvedProjectPath,
+    "./template/package.json",
+  );
+  const templatePackageJson = JSON.parse(
+    fse.readFileSync(templatePackageJsonPath, 'utf8'),
+  );
+  for (const key in templatePackageJson.dependencies) {
+    // We remove any workspace prefix that might have been added for the purposes
+    // of being used locally
+    templatePackageJson.dependencies[key] = templatePackageJson.dependencies[
+      key
+    ].replace("workspace:", "");
+  }
+  fse.writeFileSync(
+    templatePackageJsonPath,
+    JSON.stringify(templatePackageJson, null, 2),
+    'utf8'
+  );
 
   spinner.stopAndPersist({
     symbol: logSymbols.success,
     text: "React Email Starter files ready",
   });
 
+  // eslint-disable-next-line no-console
   console.log(await tree("./react-email-starter", 4));
 };
 
