8d006f82659c6d6b91c526dfcba74ddbb61dedb4
gabriel miranda <gabrielmfern@outlook.com>
2024-07-22 17:00:53 -0300

    add link patterns

---
+++ apps/web/patterns/link/link-inline-with-text.tsx
@@ -0,0 +1,18 @@
+import { Link, Text } from "@react-email/components";
+import { Layout } from "../_components/layout";
+
+export const title = "Link inline with text";
+
+export const LinkInlineWithText = () => {
+  return (
+    <Layout>
+      {/* start pattern code */}
+      <Text>
+        This is <Link href="https://react.email">React Email</Link>
+      </Text>
+      {/* end pattern code */}
+    </Layout>
+  );
+};
+
+export default LinkInlineWithText;
