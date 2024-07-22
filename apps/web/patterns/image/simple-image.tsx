862701aa65a8bb2b47d80f3bc8fd2e0a948f9909
gabriel miranda <gabrielmfern@outlook.com>
2024-07-22 16:54:47 -0300

    add image patterns

---
+++ apps/web/patterns/image/simple-image.tsx
@@ -0,0 +1,20 @@
+import { Img } from "@react-email/components";
+import { Layout } from "../_components/layout";
+
+export const title = "Single Image";
+
+export const SingleImage = () => {
+  return (
+    <Layout>
+      {/* start pattern code */}
+      <Img
+        alt="Picture of part of a camera with other things around it, all unfocused except the camera piece."
+        height={288}
+        src="https://images.unsplash.com/photo-1617005082133-548c4dd27f35?q=80&w=2864&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+      {/* end pattern code */}
+    </Layout>
+  );
+};
+
+export default SingleImage;
