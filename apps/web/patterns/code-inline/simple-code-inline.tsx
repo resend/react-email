9fa3ad78c18955609419ed044b4f3dcd5a099e70
gabriel miranda <gabrielmfern@outlook.com>
2024-07-22 17:45:37 -0300

    add code inline patterns

---
+++ apps/web/patterns/code-inline/simple-code-inline.tsx
@@ -0,0 +1,22 @@
+import { CodeInline, Text } from "@react-email/components";
+import { Layout } from "../_components/layout";
+
+export const title = "Simple code inline";
+
+export const SimpleCodeInline = () => {
+  return (
+    <Layout>
+      {/* start pattern code */}
+      <Text>
+        Install the{" "}
+        <CodeInline className="bg-gray-300 rounded-md px-1 py-0.5">
+          @react-email/components
+        </CodeInline>{" "}
+        package
+      </Text>
+      {/* end pattern code */}
+    </Layout>
+  );
+};
+
+export default SimpleCodeInline;
