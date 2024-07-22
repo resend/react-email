19c8c6b8cf7187f2f55235c368ab752f0f87da52
gabriel miranda <gabrielmfern@outlook.com>
2024-07-22 15:34:26 -0300

    add heading patterns

---
+++ apps/web/patterns/heading/multiple-headings.tsx
@@ -0,0 +1,21 @@
+import { Heading } from "@react-email/components";
+import { Layout } from "../_components/layout";
+
+export const title = "Multiple heading";
+
+export const MultipleHeadings = () => {
+  return (
+    <Layout>
+      {/* start pattern code */}
+      <Heading as="h1">Header</Heading>
+      <Heading as="h2">Header</Heading>
+      <Heading as="h3">Header</Heading>
+      <Heading as="h4">Header</Heading>
+      <Heading as="h5">Header</Heading>
+      <Heading as="h6">Header</Heading>
+      {/* end pattern code */}
+    </Layout>
+  );
+};
+
+export default MultipleHeadings;
