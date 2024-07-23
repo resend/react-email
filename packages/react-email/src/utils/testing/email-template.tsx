9c38d32a98c12c95b5a6a653a470215ea53ede4c
Gabriel Miranda <gabrielmfern@outlook.com>
2024-07-23 16:40:24 -0300

    chore: Add direct support for React 19 (#1474)

--- packages/react-email/src/utils/testing/email-template.tsx
+++
@@ -1,9 +0,0 @@
-const TestingEmailTemplate = (props: { name: string }) => {
-  return <div>Hello {props.name}!</div>;
-};
-
-TestingEmailTemplate.PreviewProps = {
-  name: 'Gabriel',
-};
-
-export default TestingEmailTemplate;
