08971fe68ce97ccd8244e056e78f1ccd295338b8
gabriel miranda <gabrielmfern@outlook.com>
2024-07-23 19:45:40 -0300

    update snapshots

--- packages/tailwind/src/tailwind.spec.tsx
+++ packages/tailwind/src/tailwind.spec.tsx
@@ -192,7 +192,7 @@ describe("Tailwind component", () => {
     );
   });
 
-  it("should override inline styles with Tailwind styles", () => {
+  it("should not override inline styles with Tailwind styles", () => {
     const actualOutput = render(
       <Tailwind>
         <div
@@ -202,9 +202,7 @@ describe("Tailwind component", () => {
       </Tailwind>,
     );
 
-    expect(actualOutput).toMatchInlineSnapshot(
-      '"<div style=\\"background-color:rgb(0,0,0);font-size:16px\\"></div>"',
-    );
+    expect(actualOutput).toMatchSnapshot();
   });
 
   it("should override component styles with Tailwind styles", () => {
