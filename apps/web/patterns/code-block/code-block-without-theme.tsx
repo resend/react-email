dcc2c5868aa006b360126544ebe0f74bf0f1f16a
gabriel miranda <gabrielmfern@outlook.com>
2024-07-23 14:04:32 -0300

    add code-block patterns

---
+++ apps/web/patterns/code-block/code-block-without-theme.tsx
@@ -0,0 +1,25 @@
+import { CodeBlock } from "@react-email/components";
+import { Layout } from "../_components/layout";
+
+export const title = "Code Block without theme";
+
+export const CodeBlockWithoutTheme = () => {
+  return (
+    <Layout>
+      {/* start pattern code */}
+      <CodeBlock
+        code={`await resend.emails.send({
+  from: 'you@example.com',
+  to: 'user@gmail.com',
+  subject: 'hello world',
+  html: '<strong>it works!</strong>',
+});`}
+        language="javascript"
+        theme={{}}
+      />
+      {/* end pattern code */}
+    </Layout>
+  );
+};
+
+export default CodeBlockWithoutTheme;
