f80d309338144668306a910a8221302b05ea2f94
gabriel miranda <gabrielmfern@outlook.com>
2024-07-25 20:29:08 -0300

    adjust use of auto margins to center things

--- apps/web/patterns/articles/article-with-image-as-background.tsx
+++ apps/web/patterns/articles/article-with-image-as-background.tsx
@@ -11,33 +11,45 @@ export const ArticleWithImageAsBackground = () => {
   return (
     <Layout>
       {/* start pattern code */}
-      <Section
+      <table
+        align="center"
+        border={0}
+        cellPadding="0"
+        cellSpacing="0"
         className="h-[424px] my-4 rounded-xl bg-blue-600"
+        role="presentation"
         style={{
-          // This image must be in quotes for Yahoo
+          // This url must be in quotes for Yahoo
           backgroundImage: "url('/static/my-image.png')",
           backgroundSize: "100% 100%",
         }}
+        width="100%"
       >
-        <div className="bottom-0 left-0 right-0 top-[15%] mx-auto my-auto p-10 text-center">
-          <Text className="m-0 font-semibold text-gray-200">New article</Text>
-          <Heading as="h1" className="m-0 mt-1 font-bold text-white">
-            Artful Accents
-          </Heading>
-          <Text className="m-0 mt-2 text-base font-medium text-white">
-            Uncover the power of accent furniture in transforming your space
-            with subtle touches of style, personality, and functionality, as we
-            explore the art of curating captivating accents for a truly curated
-            home
-          </Text>
-          <Button
-            className="mt-6 rounded-lg border border-solid border-gray-200 bg-white px-10 py-3 font-semibold text-gray-900"
-            href="#"
-          >
-            Read more
-          </Button>
-        </div>
-      </Section>
+        <tbody>
+          <tr>
+            <td align="center" className="p-10 text-center">
+              <Text className="m-0 font-semibold text-gray-200">
+                New article
+              </Text>
+              <Heading as="h1" className="m-0 mt-1 font-bold text-white">
+                Artful Accents
+              </Heading>
+              <Text className="m-0 mt-2 text-base text-white">
+                Uncover the power of accent furniture in transforming your space
+                with subtle touches of style, personality, and functionality, as
+                we explore the art of curating captivating accents for a truly
+                curated home
+              </Text>
+              <Button
+                className="mt-6 rounded-lg border border-solid border-gray-200 bg-white px-10 py-3 font-semibold text-gray-900"
+                href="#"
+              >
+                Read more
+              </Button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
       {/* end pattern code */}
     </Layout>
   );
