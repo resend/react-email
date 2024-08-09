9c112038ee3748b29cedc531ed3b4c42940b380d
Vitor Capretz <capretzvitor@gmail.com>
2024-08-09 14:05:16 -0300

    forward ref for heading

--- packages/heading/src/heading.tsx
+++ packages/heading/src/heading.tsx
@@ -6,25 +6,24 @@ import { withMargin } from "./utils/spaces";
 export type HeadingAs = As<"h1", "h2", "h3", "h4", "h5", "h6">;
 export type HeadingProps = HeadingAs & Margin;
 
-export const Heading: React.FC<Readonly<HeadingProps>> = ({
-  as: Tag = "h1",
-  children,
-  style,
-  m,
-  mx,
-  my,
-  mt,
-  mr,
-  mb,
-  ml,
-  ...props
-}) => {
-  return (
-    <Tag
-      {...props}
-      style={{ ...withMargin({ m, mx, my, mt, mr, mb, ml }), ...style }}
-    >
-      {children}
-    </Tag>
-  );
-};
+export const Heading = React.forwardRef<
+  HTMLHeadingElement,
+  Readonly<HeadingProps>
+>(
+  (
+    { as: Tag = "h1", children, style, m, mx, my, mt, mr, mb, ml, ...props },
+    ref,
+  ) => {
+    return (
+      <Tag
+        {...props}
+        ref={ref}
+        style={{ ...withMargin({ m, mx, my, mt, mr, mb, ml }), ...style }}
+      >
+        {children}
+      </Tag>
+    );
+  },
+);
+
+Heading.displayName = "Heading";
