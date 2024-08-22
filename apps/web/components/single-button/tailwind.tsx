a0cf8b8bd3f6ee34a891ed062dfb9973dc28359f
Zeno Rocha <zno.rocha@gmail.com>
2024-08-22 16:49:15 -0700

    feat(web): Review Button content (#1599)

--- apps/web/components/single-button/tailwind.tsx
+++ apps/web/components/single-button/tailwind.tsx
@@ -3,19 +3,10 @@ import { Layout } from "../_components/layout";
 
 export const component = (
   <BaseButton
+    className="box-border w-full rounded-[8px] bg-indigo-600 px-[12px] py-[12px] text-center font-semibold text-white"
     href="https://react.email"
-    style={{
-      width: "100%",
-      boxSizing: "border-box",
-      padding: 12,
-      fontWeight: 600,
-      borderRadius: 8,
-      textAlign: "center",
-      backgroundColor: "rgb(79,70,229)",
-      color: "rgb(255,255,255)",
-    }}
   >
-    Example button
+    Get started
   </BaseButton>
 );
 
--- apps/web/components/structure.ts
+++ apps/web/components/structure.ts
@@ -40,9 +40,9 @@ export const componentsStructure: Category[] = [
     description:
       "A collection of button components to use in various parts of your application.",
     components: [
-      { slug: "button", title: "Button" },
-      { slug: "download-app-buttons", title: "Download app buttons" },
+      { slug: "single-button", title: "Single button" },
       { slug: "two-buttons", title: "Two buttons" },
+      { slug: "download-buttons", title: "Download buttons" },
     ],
   },
   {
