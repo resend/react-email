037c3887ed2ac4b7efa9fb94c8bd22a3f75a7786
Zeno Rocha <zno.rocha@gmail.com>
2024-08-22 16:10:05 -0700

    feat: Review Button content

--- apps/web/components/download-buttons/inline-styles.tsx
+++ apps/web/components/download-buttons/inline-styles.tsx
@@ -6,19 +6,29 @@ export const component = (
   <Row>
     <Column align="center">
       <Row>
-        <Text className="text-[18px] font-bold leading-[28px] text-indigo-500">
-          What's new
+        <Text
+          style={{
+            color: "rgb(99,102,241)",
+            fontWeight: 700,
+            fontSize: 18,
+            lineHeight: "28px",
+          }}
+        >
+          Try now
         </Text>
-        <Text className="text-gray-900">
-          Browse, Customize, and Shop an Extensive Range of Furniture Anytime,
-          Anywhere with our app
+        <Text
+          style={{
+            color: "rgb(17,24,39)",
+          }}
+        >
+          The app all cheese enthusiasts have been waiting for
         </Text>
       </Row>
       <Row>
         <td align="center">
           <table>
             <tr>
-              <td className="pr-[16px]">
+              <td style={{ paddingRight: 16 }}>
                 <Button href="https://react.email">
                   <Img
                     alt="Get it on Google Play button"
@@ -27,7 +37,7 @@ export const component = (
                   />
                 </Button>
               </td>
-              <td className="pl-[16px]">
+              <td style={{ paddingLeft: 16 }}>
                 <Button href="https://react.email">
                   <Img
                     alt="Download on the App Store button"
--- apps/web/components/two-buttons/tailwind.tsx
+++ apps/web/components/two-buttons/tailwind.tsx
@@ -10,7 +10,7 @@ export const component = (
             className="box-border w-full rounded-[8px] bg-indigo-600 px-[20px] py-[12px] text-center font-semibold text-white"
             href="https://react.email"
           >
-            Explore
+            Login
           </Button>
         </td>
         <td align="center" className="w-1/2 pl-[16px]" colSpan={1}>
@@ -18,7 +18,7 @@ export const component = (
             className="box-border w-full rounded-[8px] border border-solid border-gray-200 bg-white px-[20px] py-[12px] text-center font-semibold text-gray-900"
             href="https://react.email"
           >
-            Buy
+            Sign up
           </Button>
         </td>
       </Row>
