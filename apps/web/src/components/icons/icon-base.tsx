a8814f2bd59572c8022255d2193c6517d7dcb4e3
gabriel miranda <gabrielmfern@outlook.com>
2024-08-22 18:06:25 -0300

    remove some extra exports

--- apps/web/src/components/icons/icon-base.tsx
+++ apps/web/src/components/icons/icon-base.tsx
@@ -1,13 +1,10 @@
 import * as React from "react";
 
-export type IconElement = React.ElementRef<"svg">;
-export type RootProps = React.ComponentPropsWithoutRef<"svg">;
-
-export interface IconProps extends RootProps {
+export interface IconProps extends React.ComponentPropsWithoutRef<"svg"> {
   size?: number;
 }
 
-export const IconBase = React.forwardRef<IconElement, Readonly<IconProps>>(
+export const IconBase = React.forwardRef<SVGSVGElement, Readonly<IconProps>>(
   ({ size = 20, ...props }, forwardedRef) => (
     <svg
       fill="none"
