488a26d0d4ceaba38a692fc384249f35b9eb5dbe
gabriel miranda <gabrielmfern@outlook.com>
2024-08-22 15:16:41 -0300

    format

--- apps/web/src/components/component-view.tsx
+++ apps/web/src/components/component-view.tsx
@@ -95,7 +95,7 @@ export const ComponentView: React.FC<ComponentViewProps> = ({
   const code =
     typeof component.code === "string"
       ? component.code
-      : (component.code[selectedCodeVariant] ?? "");
+      : component.code[selectedCodeVariant] ?? "";
 
   const onCopy = () => {
     void navigator.clipboard.writeText(code);
@@ -154,13 +154,13 @@ export const ComponentView: React.FC<ComponentViewProps> = ({
           </Tabs.List>
           <div className="absolute bottom-0 right-0 h-px w-[100dvw] bg-slate-4" />
         </div>
-        <div className="relative w-full transition-all duration-300 ease-[cubic-bezier(.36,.66,.6,1)] [transition-behavior:allow-discrete] [height:calc-size(auto)]">
+        <div className="relative w-full transition-all duration-300 ease-[cubic-bezier(.36,.66,.6,1)] [height:calc-size(auto)] [transition-behavior:allow-discrete]">
           <TabContent additionalClasses="bg-zinc-200" value="desktop">
-            <div className="absolute inset-0 bg-transparent bg-[radial-gradient(#091A21_.0313rem,transparent_.0313rem),_radial-gradient(#091A21_.0313rem,transparent_.0313rem)] opacity-30 transition-all duration-300 ease-[cubic-bezier(.36,.66,.6,1)] [background-position:0_0,.625rem_.625rem] [background-size:1.25rem_1.25rem] [transition-behavior:allow-discrete] [height:calc-size(auto)]" />
+            <div className="absolute inset-0 bg-transparent bg-[radial-gradient(#091A21_.0313rem,transparent_.0313rem),_radial-gradient(#091A21_.0313rem,transparent_.0313rem)] opacity-30 transition-all duration-300 ease-[cubic-bezier(.36,.66,.6,1)] [background-position:0_0,.625rem_.625rem] [background-size:1.25rem_1.25rem] [height:calc-size(auto)] [transition-behavior:allow-discrete]" />
             <ComponentPreview activeView="desktop" html={component.html} />
           </TabContent>
           <TabContent additionalClasses="bg-zinc-200" value="mobile">
-            <div className="absolute inset-0 bg-transparent bg-[radial-gradient(#091A21_.0313rem,transparent_.0313rem),_radial-gradient(#091A21_.0313rem,transparent_.0313rem)] opacity-30 transition-all duration-300 ease-[cubic-bezier(.36,.66,.6,1)] [background-position:0_0,.625rem_.625rem] [background-size:1.25rem_1.25rem] [transition-behavior:allow-discrete] [height:calc-size(auto)]" />
+            <div className="absolute inset-0 bg-transparent bg-[radial-gradient(#091A21_.0313rem,transparent_.0313rem),_radial-gradient(#091A21_.0313rem,transparent_.0313rem)] opacity-30 transition-all duration-300 ease-[cubic-bezier(.36,.66,.6,1)] [background-position:0_0,.625rem_.625rem] [background-size:1.25rem_1.25rem] [height:calc-size(auto)] [transition-behavior:allow-discrete]" />
             <ComponentPreview activeView="mobile" html={component.html} />
           </TabContent>
           <TabContent value="code">
