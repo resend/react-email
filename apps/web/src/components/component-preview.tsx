1bba5963dd2e18e0814f7885f6e87753276ee82c
Zeh Fernandes <ozehfernandes@gmail.com>
2024-08-23 01:00:05 -0300

    feat(web): Iframe always get the full height of the content avoid scroll trap (#1607)

--- apps/web/src/components/component-preview.tsx
+++ apps/web/src/components/component-preview.tsx
@@ -1,3 +1,4 @@
+import * as React from "react";
 import classNames from "classnames";
 
 interface ComponentPreviewProps {
@@ -11,13 +12,47 @@ export const ComponentPreview = ({
   className,
   html,
 }: ComponentPreviewProps) => {
+  const iframeRef = React.useRef<HTMLIFrameElement>(null);
+
+  React.useEffect(() => {
+    const handleResize = () => {
+      if (iframeRef.current) {
+        const iframeDocument = iframeRef.current.contentDocument;
+        if (iframeDocument) {
+          const body = iframeDocument.body;
+          const htmlFrame = iframeDocument.documentElement;
+          const height = Math.max(
+            body.scrollHeight,
+            body.offsetHeight,
+            htmlFrame.clientHeight,
+            htmlFrame.scrollHeight,
+            htmlFrame.offsetHeight,
+          );
+          iframeRef.current.style.height = `${height + 20}px`;
+        }
+      }
+    };
+
+    const iframe = iframeRef.current;
+    if (iframe) {
+      iframe.addEventListener("load", handleResize);
+
+      handleResize();
+
+      return () => {
+        iframe.removeEventListener("load", handleResize);
+      };
+    }
+  }, []);
+
   return (
     <iframe
       className={classNames(
-        "relative z-[2] m-auto -mt-4 flex h-full rounded-md bg-zinc-200 transition-none duration-300 ease-[cubic-bezier(.36,.66,.6,1)] [transition-behavior:allow-discrete]",
+        "relative z-[2] m-auto flex h-fit overflow-y-hidden rounded-md bg-zinc-200 transition-none duration-300 ease-[cubic-bezier(.36,.66,.6,1)] [transition-behavior:allow-discrete]",
         activeView === "mobile" ? "w-[360px]" : "w-full",
         className,
       )}
+      ref={iframeRef}
       srcDoc={html}
       title="Component preview"
     />
