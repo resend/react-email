0b6840cf4f79d80f1cf722cc24836a4d501d58a6
Zeno Rocha <zno.rocha@gmail.com>
2024-08-22 19:53:35 -0700

    feat: Review Grid Content (#1605)

--- apps/web/components/one-row-two-columns/tailwind.tsx
+++ apps/web/components/one-row-two-columns/tailwind.tsx
@@ -5,18 +5,18 @@ export const component = (
   <>
     <Row>
       <Column align="center" className="h-[40px] w-1/2 bg-emerald-400/60">
-        Half
+        1/2
       </Column>
       <Column align="center" className="h-[40px] w-1/2 bg-cyan-400/60">
-        Half
+        1/2
       </Column>
     </Row>
     <Row>
       <Column align="center" className="h-[40px] w-1/3 bg-pink-400/60">
-        A third
+        1/3
       </Column>
       <Column align="center" className="h-[40px] w-2/3 bg-purple-400/60">
-        Two thirds
+        2/3
       </Column>
     </Row>
   </>
