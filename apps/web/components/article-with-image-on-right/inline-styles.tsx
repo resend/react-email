11bb4708b64d49b95de8a0afb20c10def923835d
gabriel miranda <gabrielmfern@outlook.com>
2024-08-23 10:22:49 -0300

    center the article with image on the right when on mobile

--- apps/web/components/article-with-image-on-right/inline-styles.tsx
+++ apps/web/components/article-with-image-on-right/inline-styles.tsx
@@ -3,81 +3,101 @@ import { Img, Link, Section, Text } from "@react-email/components";
 import { Layout } from "../_components/layout";
 
 export const component = (
-  <Section style={{ marginTop: "16px", marginBottom: "16px" }}>
-    <Section
-      align="left"
-      style={{
-        display: "inline-block",
-        textAlign: "left",
-        width: "100%",
-        maxWidth: "250px",
-        verticalAlign: "top",
-      }}
-    >
-      <Text
-        style={{
-          margin: 0,
-          fontSize: 16,
-          lineHeight: "24px",
-          fontWeight: 600,
-          color: "rgb(79,70,229)",
-        }}
-      >
-        What's new
-      </Text>
-      <Text
-        style={{
-          margin: 0,
-          marginTop: "8px",
-          fontSize: 20,
-          lineHeight: "28px",
-          fontWeight: 600,
-          color: "rgb(17,24,39)",
-        }}
-      >
-        Versatile Comfort
-      </Text>
-      <Text
-        style={{
-          marginTop: 8,
-          fontSize: 16,
-          lineHeight: "24px",
-          color: "rgb(107,114,128)",
-        }}
-      >
-        Experience ultimate comfort and versatility with our furniture
-        collection, designed to adapt to your ever-changing needs.
-      </Text>
-      <Link
-        href="https://react.email"
-        style={{ color: "rgb(79 70 229)", textDecorationLine: "underline" }}
-      >
-        Read more
-      </Link>
-    </Section>
-    <Section
-      align="right"
-      style={{
-        display: "inline-block",
-        marginTop: 8,
-        marginBottom: 8,
-        width: "100%",
-        maxWidth: 220,
-        verticalAlign: "top",
-      }}
-    >
-      <Img
-        alt="An aesthetic picture taken of an Iphone, flowers, glasses and a card that reads 'Gucci, bloom' coming out of a leathered bag with a ziper"
-        height={220}
-        src="/static/versatile-comfort.jpg"
-        style={{
-          borderRadius: 8,
-          objectFit: "cover",
-        }}
-        width={220}
-      />
-    </Section>
-  </Section>
+  <table
+    align="center"
+    border={0}
+    cellPadding="0"
+    cellSpacing="0"
+    role="presentation"
+    style={{
+      marginTop: 16,
+      marginBottom: 16,
+    }}
+    width="100%"
+  >
+    <tbody>
+      <tr>
+        <td align="center">
+          <Section
+            align="center"
+            style={{
+              display: "inline-block",
+              textAlign: "left",
+              width: "100%",
+              maxWidth: "250px",
+              verticalAlign: "top",
+            }}
+          >
+            <Text
+              style={{
+                margin: 0,
+                fontSize: 16,
+                lineHeight: "24px",
+                fontWeight: 600,
+                color: "rgb(79,70,229)",
+              }}
+            >
+              What's new
+            </Text>
+            <Text
+              style={{
+                margin: 0,
+                marginTop: "8px",
+                fontSize: 20,
+                lineHeight: "28px",
+                fontWeight: 600,
+                color: "rgb(17,24,39)",
+              }}
+            >
+              Versatile Comfort
+            </Text>
+            <Text
+              style={{
+                marginTop: 8,
+                fontSize: 16,
+                lineHeight: "24px",
+                color: "rgb(107,114,128)",
+              }}
+            >
+              Experience ultimate comfort and versatility with our furniture
+              collection, designed to adapt to your ever-changing needs.
+            </Text>
+            <Link
+              href="https://react.email"
+              style={{
+                color: "rgb(79 70 229)",
+                textDecorationLine: "underline",
+              }}
+            >
+              Read more
+            </Link>
+          </Section>
+          <Section
+            align="center"
+            style={{
+              display: "inline-block",
+              marginTop: 8,
+              marginBottom: 8,
+              width: "100%",
+              maxWidth: 220,
+              verticalAlign: "top",
+            }}
+          >
+            <Img
+              alt="An aesthetic picture taken of an Iphone, flowers, glasses and a card that reads 'Gucci, bloom' coming out of a leathered bag with a ziper"
+              height={220}
+              src="/static/versatile-comfort.jpg"
+              style={{
+                borderRadius: 8,
+                objectFit: "cover",
+              }}
+              width={220}
+            />
+          </Section>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 );
 
 export default () => {
