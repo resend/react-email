ffaef988456d28accc42d8a906ca93b5c0fd1089
Lucas de França <luxon4uta@gmail.com>
2024-08-23 10:39:03 -0300

    🧩 fix: Improve metadata (#1619)

---
+++ apps/web/src/app/robots.ts
@@ -0,0 +1,14 @@
+const Robots = () => {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+    sitemap: "https://react.email/sitemap.xml",
+    host: "https://react.email",
+  };
+};
+
+export default Robots;
