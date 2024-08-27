710fd515ca17c940bc14e561ea10827fb2648186
gabriel miranda <gabrielmfern@outlook.com>
2024-08-27 14:21:00 -0300

    add Suspense wrapping around the element to be rendered

--- packages/render/src/node/render-async.tsx
+++ packages/render/src/node/render-async.tsx
@@ -7,6 +7,7 @@ import type {
 import { pretty } from "../shared/utils/pretty";
 import { plainTextSelectors } from "../shared/plain-text-selectors";
 import type { Options } from "../shared/options";
+import { Suspense } from "react";
 
 const decoder = new TextDecoder("utf-8");
 
@@ -44,20 +45,24 @@ const readStream = async (
   return result;
 };
 
-export const render = async (
-  component: React.ReactElement,
+/**
+ * @deprecated use `render`
+ */
+export const renderAsync = async (
+  element: React.ReactElement,
   options?: Options,
 ) => {
+  const suspendedElement = <Suspense>{element}</Suspense>;
   const { default: reactDOMServer } = await import("react-dom/server");
 
   let html!: string;
   if (Object.hasOwn(reactDOMServer, "renderToReadableStream")) {
     html = await readStream(
-      await reactDOMServer.renderToReadableStream(component),
+      await reactDOMServer.renderToReadableStream(suspendedElement),
     );
   } else {
     await new Promise<void>((resolve, reject) => {
-      const stream = reactDOMServer.renderToPipeableStream(component, {
+      const stream = reactDOMServer.renderToPipeableStream(suspendedElement, {
         async onAllReady() {
           html = await readStream(stream);
           resolve();
