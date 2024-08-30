64892543e9088279aa9052710de77446b4491d12
Gabriel Miranda <gabrielmfern@outlook.com>
2024-08-30 11:46:59 -0300

    chore(all): Upgrade testing snapshots and vitest (#1644)

--- packages/font/src/font.spec.tsx
+++ packages/font/src/font.spec.tsx
@@ -44,20 +44,6 @@ describe("<Font> component", () => {
     const actualOutput = await render(
       <Font fallbackFontFamily="Verdana" fontFamily="Roboto" />,
     );
-    expect(actualOutput).toMatchInlineSnapshot(`
-      "<!DOCTYPE html PUBLIC \\"-//W3C//DTD XHTML 1.0 Transitional//EN\\" \\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\\"><!--$--><style>
-          @font-face {
-            font-family: 'Roboto';
-            font-style: normal;
-            font-weight: 400;
-            mso-font-alt: 'Verdana';
-            
-          }
-
-          * {
-            font-family: 'Roboto', Verdana;
-          }
-        </style><!--/$-->"
-    `);
+    expect(actualOutput).toMatchSnapshot();
   });
 });
