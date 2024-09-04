c6d63f7304c0ebc963b7452e73078e533eff199c
gabriel miranda <gabrielmfern@outlook.com>
2024-09-04 13:32:08 -0300

    use a modified AST to properly compare the HTML of components

--- apps/web/components/ensure-matching-variants.spec.tsx
+++ apps/web/components/ensure-matching-variants.spec.tsx
@@ -1,9 +1,43 @@
 import path from "node:path";
 import { existsSync } from "node:fs";
 import { render } from "@react-email/components";
+import { parse } from "html-to-ast";
 import { getComponentElement } from "../src/app/components/get-components";
 import { componentsStructure, getComponentPathFromSlug } from "./structure";
 import { Layout } from "./_components/layout";
+
+type Doc = Omit<ReturnType<typeof parse>[number], "style">;
+type DocWithSetStyle = Omit<Doc, "children"> & {
+  style?: Record<string, string>;
+  children?: DocWithSetStyle[];
+};
+
+const walkAst = <T extends Doc>(ast: T[], callback: (doc: T) => void) => {
+  for (const doc of ast) {
+    callback(doc);
+    if (doc.children) {
+      walkAst(doc.children as T[], callback);
+    }
+  }
+};
+
+const getComparableAstFrom = (html: string): DocWithSetStyle[] => {
+  const ast = parse(html) as Doc[];
+  walkAst(ast, (doc) => {
+    if (doc.attrs && "style" in doc.attrs) {
+      const style = doc.attrs.style as string;
+      (doc as DocWithSetStyle).style = {};
+      for (const line of style.split(";")) {
+        const prop = line.split(":")[0];
+        const value = line.split(":").slice(1).join(":");
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        (doc as DocWithSetStyle).style![prop] = value;
+      }
+      delete doc.attrs.style;
+    }
+  });
+  return ast as DocWithSetStyle[];
+};
 
 describe("copy-paste components", () => {
   const components = componentsStructure.flatMap(
@@ -26,14 +60,15 @@ describe("copy-paste components", () => {
         const inlineStylesElement = await getComponentElement(
           inlineStylesVariantPath,
         );
-        const tailwindHtml = await render(<Layout>{tailwindElement}</Layout>);
-        const inlineStylesHtml = await render(
-          <Layout withTailwind={false}>{inlineStylesElement}</Layout>,
+        const tailwindAst = getComparableAstFrom(
+          await render(<Layout>{tailwindElement}</Layout>),
         );
-        expect(
-          tailwindHtml,
-          `Expected Tailwind and Inline Styles to match the HTML on the component ${component.slug}`,
-        ).toBe(inlineStylesHtml);
+        const inlineStylesAst = getComparableAstFrom(
+          await render(
+            <Layout withTailwind={false}>{inlineStylesElement}</Layout>,
+          ),
+        );
+        expect(tailwindAst).toEqual(inlineStylesAst);
       }
     });
   }
