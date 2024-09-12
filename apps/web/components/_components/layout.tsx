1d97305d492c1e108c2c08646171d1e7874dfc50
gabriel miranda <gabrielmfern@outlook.com>
2024-09-12 14:29:05 -0300

    fix(web): Font component usage in copy-paste components layout

--- apps/web/components/_components/layout.tsx
+++ apps/web/components/_components/layout.tsx
@@ -24,7 +24,27 @@ export const Layout = ({
           fontStyle="normal"
           fontWeight={400}
           webFont={{
-            url: "https://fonts.googleapis.com/css2?family=Inter&display=swap",
+            url: "https://fonts.gstatic.com/s/inter/v18/UcCO3FwrK3iLTeHuS_nVMrMxCp50SjIw2boKoduKmMEVuLyfAZ9hiA.woff2",
+            format: "woff2",
+          }}
+        />
+        <Font
+          fallbackFontFamily="Helvetica"
+          fontFamily="Inter"
+          fontStyle="normal"
+          fontWeight={600}
+          webFont={{
+            url: "https://fonts.gstatic.com/s/inter/v18/UcC73FwrK3iLTeHuS_fjbvMwCp50PDca1ZL7.woff2",
+            format: "woff2",
+          }}
+        />
+        <Font
+          fallbackFontFamily="Helvetica"
+          fontFamily="Inter"
+          fontStyle="normal"
+          fontWeight={700}
+          webFont={{
+            url: "https://fonts.gstatic.com/s/inter/v18/UcC73FwrK3iLTeHuS_fjbvMwCp50BTca1ZL7.woff2",
             format: "woff2",
           }}
         />
