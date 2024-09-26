1044098a4c24908d39b40504d3b8b9310d27efb2
gabriel miranda <gabrielmfern@outlook.com>
2024-09-26 14:30:17 -0300

    add fix for possible unfinished characters in byte chunks

--- packages/render/src/browser/render-async.tsx
+++ packages/render/src/browser/render-async.tsx
@@ -1,41 +1,9 @@
 import { convert } from "html-to-text";
-import type {
-  PipeableStream,
-  ReactDOMServerReadableStream,
-} from "react-dom/server";
+import { Suspense } from "react";
 import { pretty } from "../shared/utils/pretty";
 import { plainTextSelectors } from "../shared/plain-text-selectors";
 import type { Options } from "../shared/options";
-import { Suspense } from "react";
-
-const decoder = new TextDecoder("utf-8");
-
-const readStream = async (
-  stream: PipeableStream | ReactDOMServerReadableStream,
-) => {
-  let result = "";
-
-  if ("pipeTo" in stream) {
-    // means it's a readable stream
-    const writableStream = new WritableStream({
-      write(chunk: BufferSource) {
-        result += decoder.decode(chunk);
-      },
-    });
-    await stream.pipeTo(writableStream);
-  } else {
-    throw new Error(
-      "For some reason, the Node version of `react-dom/server` has been imported instead of the browser one.",
-      {
-        cause: {
-          stream,
-        },
-      },
-    );
-  }
-
-  return result;
-};
+import { readStream } from "./read-stream";
 
 export const renderAsync = async (
   element: React.ReactElement,
