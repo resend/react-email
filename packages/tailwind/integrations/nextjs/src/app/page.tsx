f32468daa8d7470335c9b0aa90e534c8d9b6c059
Gabriel Miranda <gabrielmfern@outlook.com>
2024-10-02 12:13:52 -0300

    Use proper casing for emailHtml variable
    Co-authored-by: Bu Kinoshita <6929565+bukinoshita@users.noreply.github.com>

--- packages/tailwind/integrations/nextjs/src/app/page.tsx
+++ packages/tailwind/integrations/nextjs/src/app/page.tsx
@@ -2,6 +2,6 @@ import { renderAsync } from "@react-email/components";
 import { VercelInviteUserEmail } from "../../emails/vercel-invite-user";
 
 export default async function Home() {
-  const emailHTML = await renderAsync(<VercelInviteUserEmail />);
-  return <div dangerouslySetInnerHTML={{ __html: emailHTML }} />;
+  const emailHtml = await renderAsync(<VercelInviteUserEmail />);
+  return <div dangerouslySetInnerHTML={{ __html: emailHtml }} />;
 }
