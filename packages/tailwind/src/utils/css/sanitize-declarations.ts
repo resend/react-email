500a9af3673bf3cd18f9ad5cbe77f30abc9f65ee
gabriel miranda <gabrielmfern@outlook.com>
2024-10-04 15:23:19 -0300

    improve name for rgb parser regex

--- packages/tailwind/src/utils/css/sanitize-declarations.ts
+++ packages/tailwind/src/utils/css/sanitize-declarations.ts
@@ -1,6 +1,6 @@
 import type { AtRule, Root, Rule } from "postcss";
 
-const regex = /rgb\(\s*(\d+)\s*(\d+)\s*(\d+)(?:\s*\/\s*([\d%.]+))?\s*\)/g;
+const rgbParserRegex = /rgb\(\s*(\d+)\s*(\d+)\s*(\d+)(?:\s*\/\s*([\d%.]+))?\s*\)/g;
 
 /**
  * Meant to do all the things necessary, in a per-declaration basis, to have the best email client
@@ -14,7 +14,7 @@ export const sanitizeDeclarations = (
 ) => {
   nodeContainingDeclarations.walkDecls((declaration) => {
     declaration.value = declaration.value.replaceAll(
-      regex,
+      rgbParserRegex,
       (_match, r, g, b, a) => {
         const alpha = a === "1" || typeof a === "undefined" ? "" : `,${a}`;
         return `rgb(${r},${g},${b}${alpha})`;
@@ -25,6 +25,6 @@ export const sanitizeDeclarations = (
       We reset the regex's last index so that the state of the last time it ran doesn't intefere
       with when it is used another time
     */
-    regex.lastIndex = 0;
+    rgbParserRegex.lastIndex = 0;
   });
 };
