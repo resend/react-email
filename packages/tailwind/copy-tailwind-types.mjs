83c77f0051d6eeb1c187f44e90f1222396041f44
gabriel miranda <gabrielmfern@outlook.com>
2024-10-18 15:46:15 -0300

    fix build on CI failing

--- packages/tailwind/copy-tailwind-types.mjs
+++ packages/tailwind/copy-tailwind-types.mjs
@@ -1,9 +1,12 @@
 import { promises as fs } from "node:fs";
 import path from "node:path";
+import url from "node:url";
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 await fs.cp(
-  path.resolve(import.meta.dirname, "./node_modules/tailwindcss/types"),
-  path.resolve(import.meta.dirname, "./dist/tailwindcss"),
+  path.resolve(__dirname, "./node_modules/tailwindcss/types"),
+  path.resolve(__dirname, "./dist/tailwindcss"),
   {
     recursive: true
   }
