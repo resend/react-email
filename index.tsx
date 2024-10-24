36b5787fb77847a9322cfb2b4d8880590d5f8692
nehhhhha6 <n0370525@gmail.com>
2024-10-24 19:58:37 +0530

    message

---
+++ index.tsx
@@ -0,0 +1,11 @@
+import "web-streams-polyfill/polyfill"; // Ensure this is at the top
+
+const App = () => {
+  return (
+    <div>
+      <h1>Email Rendering Application</h1>
+    </div>
+  );
+};
+
+export default App;
