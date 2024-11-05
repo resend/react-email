725f75e5d96067c19dbc84ac69418589491556a4
gabriel miranda <gabrielmfern@outlook.com>
2024-11-05 12:39:35 -0300

    update internal React to match Next 15.0.2

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -17,10 +17,10 @@ const components = packages
 function readPackage(pkg) {
   if (components.includes(pkg.name)) {
     if ("react" in pkg.peerDependencies) {
-      pkg.peerDependencies.react = "19.0.0-rc-187dd6a7-20240806";
+      pkg.peerDependencies.react = "19.0.0-rc-02c0e824-20241028";
     }
     if ("react-dom" in pkg.peerDependencies) {
-      pkg.peerDependencies["react-dom"] = "19.0.0-rc-187dd6a7-20240806";
+      pkg.peerDependencies["react-dom"] = "19.0.0-rc-02c0e824-20241028";
     }
   }
   return pkg;
