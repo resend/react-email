a152c4546ad351660911ca2b4b721930ff99220b
gabriel miranda <gabrielmfern@outlook.com>
2024-12-02 12:50:39 -0300

    add image warnings

---
+++ packages/react-email/src/utils/email-validation/get-image-warnings.ts
@@ -0,0 +1,32 @@
+import traverse from '@babel/traverse';
+import type { EmailValidationWarning } from '../../actions/get-warnings-for-email';
+import { getLineAndColumnFromIndex } from './get-line-and-column-from-index';
+import type { AST } from '.';
+
+export const getImageWarnings = (ast: AST, code: string) => {
+  const warnings: EmailValidationWarning[] = [];
+
+  traverse(ast, {
+    JSXOpeningElement(path) {
+      if (
+        !path.node.attributes.some(
+          (attrib) =>
+            attrib.type === 'JSXAttribute' && attrib.name.name === 'alt',
+        )
+      ) {
+        const [line, column] = getLineAndColumnFromIndex(
+          code,
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          path.node.start!,
+        );
+        warnings.push({
+          message: "Image doesn't have alt text",
+          line,
+          column,
+        });
+      }
+    },
+  });
+
+  return warnings;
+};
