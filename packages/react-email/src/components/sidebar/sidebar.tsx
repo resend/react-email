b3c24df8e308e931a802a68b8486d29da4a1f6a4
gabriel miranda <gabrielmfern@outlook.com>
2024-12-04 14:41:02 -0300

    initial changes for link checking

--- packages/react-email/src/components/sidebar/sidebar.tsx
+++ packages/react-email/src/components/sidebar/sidebar.tsx
@@ -8,6 +8,7 @@ import { useEmails } from '../../contexts/emails';
 import { cn } from '../../utils';
 import { Logo } from '../logo';
 import { IconEmail } from '../icons/icon-email';
+import { IconLink } from '../icons/icon-link';
 import { SidebarDirectoryChildren } from './sidebar-directory-children';
 
 interface SidebarProps {
@@ -24,7 +25,7 @@ interface SidebarTabTriggerProps {
   activeTabValue: SidebarTab;
 }
 
-type SidebarTab = 'email-templates' | 'validation';
+type SidebarTab = 'email-templates' | 'link-checker';
 
 const SidebarTabTrigger = ({
   children,
@@ -35,7 +36,7 @@ const SidebarTabTrigger = ({
   return (
     <Tabs.Trigger
       className={clsx(
-        'w-[36px] h-[36px] flex items-center justify-center bg-transparent data-[active=true]:bg-slate-6 text-white',
+        'w-[40px] h-[40px] flex items-center justify-center bg-transparent data-[active=true]:bg-slate-6 text-white',
         className,
       )}
       data-active={tabValue === activeTabValue}
@@ -68,52 +69,46 @@ export const Sidebar = ({
         className={cn('border-r flex flex-col border-slate-6', className)}
         style={{ ...style }}
       >
-        <div className="p-4 h-[70px] flex-shrink items-center hidden lg:flex">
-          <Logo />
-        </div>
         <div className="flex border-t border-slate-6 h-[calc(100vh_-_70px)]">
-          <Tabs.List className="flex flex-col w-[36px] h-full border-r border-slate-6">
+          <Tabs.List className="flex flex-col w-[40px] h-full border-r border-slate-6">
             <SidebarTabTrigger
               activeTabValue={activeTabValue}
               tabValue="email-templates"
             >
-              <IconEmail />
+              <IconEmail height="24" width="24" />
             </SidebarTabTrigger>
-            {/*<SidebarTabTrigger
+            <SidebarTabTrigger
               activeTabValue={activeTabValue}
               className="relative"
-              tabValue="validation"
+              tabValue="link-checker"
             >
-              <IconStamp />
-              <span className="absolute w-4 bg-black border border-slate-6 rounded-full h-4 text-xs left-4 top-0">
-                {emailValidationWarnings &&
-                emailValidationWarnings.length > 0 ? (
-                  emailValidationWarnings.length
-                ) : (
-                  <IconCheck />
-                )}
-              </span>
-            </SidebarTabTrigger>*/}
+              <IconLink height="24" width="24" />
+            </SidebarTabTrigger>
           </Tabs.List>
-          {/*activeTabValue === 'validation' && emailValidationWarnings ? (
-            <WarningsView warnings={emailValidationWarnings} />
-          ) : null*/}
-          {activeTabValue === 'email-templates' ? (
-            <div className="flex flex-col w-[calc(100vw-36px)] h-full lg:w-full lg:min-w-[231px] lg:max-w-[231px]">
-              <nav className="p-4 flex-grow lg:pt-0 pl-0 w-full flex flex-col overflow-y-auto">
-                <Collapsible.Root>
-                  <React.Suspense>
-                    <SidebarDirectoryChildren
-                      currentEmailOpenSlug={currentEmailOpenSlug}
-                      emailsDirectoryMetadata={emailsDirectoryMetadata}
-                      isRoot
-                      open
-                    />
-                  </React.Suspense>
-                </Collapsible.Root>
-              </nav>
+          <div>
+            <div className="p-4 h-[70px] flex-shrink items-center hidden lg:flex">
+              <Logo />
             </div>
-          ) : null}
+            {/*activeTabValue === 'link-checker' && emailValidationWarnings ? (
+              <WarningsView warnings={emailValidationWarnings} />
+            ) : null*/}
+            {activeTabValue === 'email-templates' ? (
+              <div className="flex flex-col w-[calc(100vw-36px)] h-full lg:w-full lg:min-w-[231px] lg:max-w-[231px]">
+                <nav className="p-4 flex-grow lg:pt-0 pl-0 w-full flex flex-col overflow-y-auto">
+                  <Collapsible.Root>
+                    <React.Suspense>
+                      <SidebarDirectoryChildren
+                        currentEmailOpenSlug={currentEmailOpenSlug}
+                        emailsDirectoryMetadata={emailsDirectoryMetadata}
+                        isRoot
+                        open
+                      />
+                    </React.Suspense>
+                  </Collapsible.Root>
+                </nav>
+              </div>
+            ) : null}
+          </div>
         </div>
       </aside>
     </Tabs.Root>
