66fd5f9678baa188ed7473837b25b00fda4f3580
Lucas de França <luxon4uta@gmail.com>
2024-12-13 11:09:15 -0300

    ✨ Add animations & improve layout

--- packages/react-email/src/components/sidebar/link-checker.tsx
+++ packages/react-email/src/components/sidebar/link-checker.tsx
@@ -9,6 +9,8 @@ import { emailSlugToPathMap } from '../../app/preview/[...slug]/preview';
 import { IconCircleCheck } from '../icons/icon-circle-check';
 import { IconCircleClose } from '../icons/icon-circle-close';
 import { IconCircleWarning } from '../icons/icon-circle-warning';
+import { Heading } from '../heading';
+import { motion } from 'framer-motion';
 
 const checkingResultsCache = new Map<string, LinkCheckingResult[]>();
 
@@ -40,8 +42,10 @@ export const LinkChecker = ({ currentEmailOpenSlug }: LinkCheckerProps) => {
   };
 
   return (
-    <div className="flex h-full w-[calc(100vw-36px)] flex-col gap-3 p-2 text-center lg:w-full lg:min-w-[231px] lg:max-w-[231px]">
-      <h2 className="text-xl">Link Checker</h2>
+    <div className="mt-4 flex w-full flex-col gap-2 text-pretty p-2 pr-2.5">
+      <Heading as="h2" size="3" weight="medium">
+        Link Checker
+      </Heading>
       {results ? (
         <>
           <ol className="list-none p-0">
@@ -55,10 +59,10 @@ export const LinkChecker = ({ currentEmailOpenSlug }: LinkCheckerProps) => {
         </>
       ) : (
         <>
-          <span className="text-sm text-gray-300">
-            Check if all links are valid and going to the right pages
+          <span className="text-xs">
+            Check if all links are valid and going to the correct pages.
           </span>
-          <Button className="mx-auto w-fit" onClick={handleRun}>
+          <Button className="mt-1.5 w-fit" onClick={handleRun}>
             Run
           </Button>
         </>
@@ -67,55 +71,69 @@ export const LinkChecker = ({ currentEmailOpenSlug }: LinkCheckerProps) => {
   );
 };
 
+const containerVariants = {
+  hidden: { opacity: 0, y: 20 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.5,
+      staggerChildren: 0.15,
+    },
+  },
+};
+
 const LinkCheckingResultView = (props: LinkCheckingResult) => {
   let status: 'success' | 'error' | 'warning' = 'success';
-  if (props.checks.security === 'failed') {
-    status = 'warning';
-  }
-  if (props.checks.syntax === 'failed') {
-    status = 'error';
-  }
+
+  if (props.checks.security === 'failed') status = 'warning';
+  if (props.checks.syntax === 'failed') status = 'error';
   if (
     props.responseStatusCode === undefined ||
     !props.responseStatusCode.toString().startsWith('2')
-  ) {
+  )
     status = 'error';
-  }
+
   return (
-    <li className="group my-4 text-left" data-status={status}>
-      <div className="flex items-center gap-2 text-sm group-data-[status=error]:text-red-400 group-data-[status=success]:text-green-400 group-data-[status=warning]:text-yellow-300">
-        {(() => {
-          if (status === 'success') {
-            return <IconCircleCheck />;
-          }
-          if (status === 'warning') {
-            return <IconCircleWarning />;
-          }
-          return <IconCircleClose />;
-        })()}
+    <motion.li
+      className="group relative my-4"
+      data-status={status}
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+    >
+      <motion.div className="flex items-center gap-2 text-sm group-data-[status=error]:text-red-400 group-data-[status=success]:text-green-400 group-data-[status=warning]:text-yellow-300">
+        <span>
+          {(() => {
+            if (status === 'success') return <IconCircleCheck size={14} />;
+            if (status === 'warning') return <IconCircleWarning size={14} />;
+            return <IconCircleClose size={14} />;
+          })()}
+        </span>
         <a
           className="flex-shrink overflow-hidden text-ellipsis whitespace-nowrap"
           href={props.link}
         >
           {props.link}
         </a>
-      </div>
+      </motion.div>
+      <motion.div className="mt-1 text-xs">
+        {(() => {
+          if (props.checks.syntax === 'failed') {
+            return 'Invalid URL';
+          }
 
-      {(() => {
-        if (props.checks.syntax === 'failed') {
-          return 'Invalid URL';
-        }
+          if (props.responseStatusCode) {
+            return `${props.responseStatusCode} - ${
+              props.checks.security === 'failed' ? 'Insecure' : 'Secure'
+            }`;
+          }
 
-        if (props.responseStatusCode) {
-          return `${props.responseStatusCode} - ${
-            props.checks.security === 'failed' ? 'Insecure' : 'Secure'
-          }`;
-        }
-
-        if (!props.responseStatusCode) {
-          return `URL could not be reached`;
-        }
-      })()}
-    </li>
+          if (!props.responseStatusCode) {
+            return `URL could not be reached`;
+          }
+        })()}
+      </motion.div>
+    </motion.li>
   );
 };
