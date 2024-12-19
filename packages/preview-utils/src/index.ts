b4bb8877a78489f06fc2b0437e1fbcd171ac3f00
gabriel miranda <gabrielmfern@outlook.com>
2024-12-19 15:32:59 -0300

    move isFileAnEmail to the preview-utils

--- packages/preview-utils/src/index.ts
+++ packages/preview-utils/src/index.ts
@@ -2,4 +2,5 @@ export * from './render-email-by-path';
 export * from './get-email-component';
 export * from './utils/email-template';
 export * from './utils/rendering-utilities-exporter';
+export * from './is-file-an-email';
 
--- packages/react-email/src/actions/get-emails-directory-metadata.ts
+++ packages/react-email/src/actions/get-emails-directory-metadata.ts
@@ -2,29 +2,7 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import fs from 'node:fs';
 import path from 'node:path';
-
-const isFileAnEmail = (fullPath: string): boolean => {
-  const stat = fs.statSync(fullPath);
-
-  if (stat.isDirectory()) return false;
-
-  const { ext } = path.parse(fullPath);
-
-  if (!['.js', '.tsx', '.jsx'].includes(ext)) return false;
-
-  // This is to avoid a possible race condition where the file doesn't exist anymore
-  // once we are checking if it is an actual email, this couuld cause issues that
-  // would be very hard to debug and find out the why of it happening.
-  if (!fs.existsSync(fullPath)) {
-    return false;
-  }
-
-  // check with a heuristic to see if the file has at least
-  // a default export
-  const fileContents = fs.readFileSync(fullPath, 'utf8');
-
-  return /\bexport\s+default\b/gm.test(fileContents);
-};
+import { isFileAnEmail } from 'preview-utils';
 
 export interface EmailsDirectory {
   absolutePath: string;
