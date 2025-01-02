a0974f2a92cbec07bc52daddb691235419896c4a
gabriel miranda <gabrielmfern@outlook.com>
2025-01-02 13:06:27 -0300

    write sketch of PreviewPropsControls

--- packages/react-email/src/actions/get-email-controls.ts
+++ packages/react-email/src/actions/get-email-controls.ts
@@ -1,19 +1,20 @@
 'use server';
 
-import type { Controls } from "../package";
-import { getEmailComponent } from "../utils/get-email-component";
-import type { ErrorObject } from "../utils/types/error-object";
+import type { Controls } from '../package';
+import { getEmailComponent } from '../utils/get-email-component';
+import type { ErrorObject } from '../utils/types/error-object';
 
-export const getEmailControls = async (emailPath: string): Promise<{ error: ErrorObject } | { controls: Controls }> => {
+export type ControlsResult = { error: ErrorObject } | { controls: Controls };
+
+export const getEmailControls = async (
+  emailPath: string,
+): Promise<ControlsResult> => {
   const result = await getEmailComponent(emailPath);
   if ('error' in result) {
     return { error: result.error };
   }
 
-  const {
-    emailComponent: Email
-  } = result;
+  const { emailComponent: Email } = result;
 
   return { controls: Email.controls ?? {} };
 };
-
