6d6b1835e672955faea4288b4b65dc5bd24ee9d0
gabriel miranda <gabrielmfern@outlook.com>
2025-01-06 12:11:49 -0300

    add styles for Select control

--- packages/react-email/src/app/preview/[...slug]/preview.tsx
+++ packages/react-email/src/app/preview/[...slug]/preview.tsx
@@ -3,9 +3,7 @@
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import React, { useState } from 'react';
 import { Toaster } from 'sonner';
-import * as Select from '@radix-ui/react-select';
 import { useDebouncedCallback } from 'use-debounce';
-import * as Checkbox from '@radix-ui/react-checkbox';
 import {
   renderEmailByPath,
   type EmailRenderingResult,
@@ -17,11 +15,9 @@ import { useEmailRenderingResult } from '../../../hooks/use-email-rendering-resu
 import { useHotreload } from '../../../hooks/use-hot-reload';
 import { useRenderingMetadata } from '../../../hooks/use-rendering-metadata';
 import type { ControlsResult } from '../../../actions/get-email-controls';
-import type { Controls } from '../../../package';
-import { IconArrowDown } from '../../../components/icons/icon-arrow-down';
-import { IconCheck } from '../../../components/icons/icon-check';
 import { useEmailControls } from '../../../hooks/use-email-controls';
 import { updatePreviewProps } from '../../../actions/update-preview-props';
+import { PreviewPropControls } from '../../../components/preview-prop-controls';
 import { RenderingError } from './rendering-error';
 
 interface PreviewProps {
@@ -137,7 +133,7 @@ const Preview = ({
               debouncedUpdatePreviewProps(newPreviewProps);
 
               renderEmailByPath(emailPath, newPreviewProps, {
-                invalidatingRenderingCache: true
+                invalidatingRenderingCache: true,
               })
                 .then((newRenderingResult) => {
                   setRenderingResult(newRenderingResult);
@@ -204,119 +200,6 @@ const Preview = ({
         <Toaster />
       </div>
     </Shell>
-  );
-};
-
-interface PreviewPropControls {
-  previewProps: Record<string, unknown>;
-  onValueChange: (key: string, newValue: unknown) => void;
-  controls: Controls;
-}
-
-const PreviewPropControls = ({
-  previewProps,
-  onValueChange,
-  controls,
-}: PreviewPropControls) => {
-  return (
-    <div className="fixed px-3 py-2 border-t border-solid border-t-slate-9 left-0 bottom-0 bg-black w-full grid gap-3 grid-cols-1 md:grid-cols-3 lg:grid-cols-4 h-40">
-      {Object.entries(controls).map(([key, control]) => {
-        if (control) {
-          const fieldId = `${key}-${control.type}`;
-          const value = previewProps[key];
-
-          switch (control.type) {
-            case 'text':
-            case 'email':
-            case 'number':
-              return (
-                <div key={fieldId}>
-                  <label
-                    className="text-slate-10 text-sm mb-2 block"
-                    htmlFor={fieldId}
-                  >
-                    {key}
-                  </label>
-                  <input
-                    className="appearance-none rounded-lg px-2 py-1 mb-3 outline-none w-full bg-slate-3 border placeholder-slate-10 border-slate-6 text-slate-12 text-sm focus:ring-1 focus:ring-slate-10 transition duration-300 ease-in-out"
-                    data-1p-ignore
-                    id={fieldId}
-                    onChange={(event) => {
-                      onValueChange(key, event.currentTarget.value);
-                    }}
-                    type={control.type}
-                    value={value as string}
-                  />
-                </div>
-              );
-            case 'select':
-              return (
-                <div key={fieldId}>
-                  <label
-                    className="text-slate-10 text-sm mb-2 block"
-                    htmlFor={fieldId}
-                  >
-                    {key}
-                  </label>
-                  <Select.Root
-                    onValueChange={(newValue) => {
-                      onValueChange(key, newValue);
-                    }}
-                    value={value as string}
-                  >
-                    <Select.Trigger id={fieldId}>
-                      <Select.Value />
-                      <Select.Icon>
-                        <IconArrowDown />
-                      </Select.Icon>
-                    </Select.Trigger>
-                    <Select.Portal>
-                      <Select.Content>
-                        <Select.Viewport>
-                          {control.options.map((option) => (
-                            <Select.Item
-                              key={option.name}
-                              value={option.value as string}
-                            >
-                              <Select.ItemText>{option.name}</Select.ItemText>
-                              <Select.ItemIndicator>
-                                <IconCheck />
-                              </Select.ItemIndicator>
-                            </Select.Item>
-                          ))}
-                        </Select.Viewport>
-                      </Select.Content>
-                    </Select.Portal>
-                  </Select.Root>
-                </div>
-              );
-            case 'checkbox':
-              return (
-                <div key={fieldId}>
-                  <Checkbox.Root
-                    checked={value as boolean}
-                    id={fieldId}
-                    onCheckedChange={(newValue) => {
-                      onValueChange(key, newValue);
-                    }}
-                  >
-                    <Checkbox.Indicator>
-                      <IconCheck />
-                    </Checkbox.Indicator>
-                  </Checkbox.Root>
-                  <label
-                    className="text-slate-10 text-sm mb-2 block"
-                    htmlFor={fieldId}
-                  >
-                    {key}
-                  </label>
-                </div>
-              );
-          }
-        }
-        return null;
-      })}
-    </div>
   );
 };
 
