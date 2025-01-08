027ecdd75334c6e972fc428e5ff1ef477d3da78e
gabriel miranda <gabrielmfern@outlook.com>
2025-01-08 17:26:23 -0300

    optimize build for esbuild

--- packages/react-email/tsup.config.ts
+++ packages/react-email/tsup.config.ts
@@ -1,4 +1,32 @@
+import fs from 'node:fs';
+import path from 'node:path';
 import { defineConfig } from 'tsup';
+
+const componentDirectories = fs
+  .readdirSync('./src/package', { withFileTypes: true })
+  .filter((dirent) => dirent.isDirectory())
+  .map((dirent) => path.join(dirent.parentPath, dirent.name));
+
+const componentEntries: Record<string, string> = {};
+
+for (const componentDirectory of componentDirectories) {
+  const componentName = path.basename(componentDirectory);
+  if (componentName === 'render') {
+    componentEntries[`${componentName}.browser`] = path.resolve(
+      componentDirectory,
+      'browser/index.ts',
+    );
+    componentEntries[`${componentName}.node`] = path.resolve(
+      componentDirectory,
+      'node/index.ts',
+    );
+  } else {
+    componentEntries[componentName] = path.resolve(
+      componentDirectory,
+      'index.ts',
+    );
+  }
+}
 
 export default defineConfig([
   {
@@ -9,7 +37,7 @@ export default defineConfig([
   },
   {
     dts: true,
-    entry: ['./src/package/index.browser.ts'],
+    entry: componentEntries,
     tsconfig: "./src/package/tsconfig.json",
     treeshake: true,
     noExternal: [
@@ -27,27 +55,13 @@ export default defineConfig([
       'camelcase-css',
     ],
     format: ['cjs', 'esm'],
-    outDir: 'dist',
+    outDir: `dist`,
   },
   {
     dts: true,
-    entry: ['./src/package/index.node.ts'],
     tsconfig: "./src/package/tsconfig.json",
-    treeshake: true,
-    noExternal: [
-      /patched-tailwindcss\/.*/,
-      /patched-postcss\/.*/,
-      /postcss\/.*/,
-      'dlv',
-      'didyoumean',
-      '@alloc/quick-lru',
-      'patched-postcss-selector-parser',
-      'postcss-selector-parser',
-      'postcss-js',
-      'postcss-nested',
-      'cssesc',
-      'camelcase-css',
-    ],
+    entry: ['./src/package/index.browser.ts', './src/package/index.node.ts'],
+    bundle: false,
     format: ['cjs', 'esm'],
     outDir: 'dist',
   },
