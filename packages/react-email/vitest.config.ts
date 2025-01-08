774c04f68b08b5eddbfbcdba9e9dfbb86d6380d9
gabriel miranda <gabrielmfern@outlook.com>
2025-01-08 14:32:25 -0300

    fix test being duplicated

--- packages/react-email/vitest.config.ts
+++ packages/react-email/vitest.config.ts
@@ -1,14 +1,21 @@
-import path from "node:path";
-import { defineConfig } from "vitest/config";
+import path from 'node:path';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     globals: true,
-    environment: "happy-dom",
+    environment: 'happy-dom',
+    exclude: [
+      '.yalc',
+      './src/package/tailwind/integrations/nextjs',
+      './src/package/tailwind/integrations/vite',
+    ],
   },
   server: {
     watch: {
-      ignored: [path.resolve(__dirname, "./src/package/tailwind/integrations/**/*")],
+      ignored: [
+        path.resolve(__dirname, './src/package/tailwind/integrations/**/*'),
+      ],
     },
   },
 });
