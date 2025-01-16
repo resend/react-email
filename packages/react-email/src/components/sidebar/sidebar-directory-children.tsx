7b340ad41c8c3cc19a3053aef869666f37f9ada5
Bu Kinoshita <6929565+bukinoshita@users.noreply.github.com>
2025-01-16 15:39:58 +0000

    chore(all): use biome (#1833)
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: gabriel miranda <gabrielmfern@outlook.com>

--- packages/react-email/src/components/sidebar/sidebar-directory-children.tsx
+++ packages/react-email/src/components/sidebar/sidebar-directory-children.tsx
@@ -64,7 +64,7 @@ export const SidebarDirectoryChildren = (props: {
                     };
                     const isCurrentPage = props.currentEmailOpenSlug
                       ? removeExtensionFrom(props.currentEmailOpenSlug) ===
-                      emailSlug
+                        emailSlug
                       : false;
 
                     return (
