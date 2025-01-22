ed2fa33657b4a11a673ab1280b4ea56b41633beb
Gabriel Miranda <gabrielmfern@outlook.com>
2025-01-22 09:46:59 -0300

    fix(render): Prettier breaking if mso comments (#1839)
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Bu Kinoshita <6929565+bukinoshita@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- packages/render/src/shared/utils/pretty.ts
+++ packages/render/src/shared/utils/pretty.ts
@@ -1,11 +1,36 @@
-import type { Options } from 'prettier';
+import type { Options, Plugin } from 'prettier';
 import html from 'prettier/plugins/html';
 import { format } from 'prettier/standalone';
+
+const modifiedHtml = { ...html } as Plugin;
+if (modifiedHtml.printers) {
+  // eslint-disable-next-line @typescript-eslint/unbound-method
+  const previousPrint = modifiedHtml.printers.html.print;
+  modifiedHtml.printers.html.print = (path, options, print, args) => {
+    const node = path.getNode() as {
+      type: string;
+      sourceSpan: {
+        start: { file: unknown[]; offset: number; line: number; col: number };
+        end: { file: unknown[]; offset: number; line: number; col: number };
+        details: null;
+      };
+    };
+
+    if (node.type === 'ieConditionalComment') {
+      return options.originalText.slice(
+        node.sourceSpan.start.offset,
+        node.sourceSpan.end.offset,
+      );
+    }
+    return previousPrint(path, options, print, args);
+  };
+}
 
 const defaults: Options = {
   endOfLine: 'lf',
   tabWidth: 2,
-  plugins: [html],
+  plugins: [modifiedHtml],
+  bracketSameLine: true,
   parser: 'html',
 };
 
