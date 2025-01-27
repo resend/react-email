901f6b1734ba8def1e8c428f8468b38518f33f8c
gabriel miranda <gabrielmfern@outlook.com>
2025-01-27 19:18:47 -0300

    make all the code for the results list agnostic

--- packages/react-email/src/components/sidebar/link-checker.tsx
+++ packages/react-email/src/components/sidebar/link-checker.tsx
@@ -1,174 +1,76 @@
-import * as Collapsible from '@radix-ui/react-collapsible';
-import { clsx } from 'clsx';
-import { AnimatePresence, motion } from 'framer-motion';
 import * as React from 'react';
 import {
   type LinkCheckingResult,
   checkLinks,
 } from '../../actions/email-validation/check-links';
 import { Button } from '../button';
+import { Result, ResultList, type ResultStatus } from './checking-results';
 
-const containerAnimation = {
-  hidden: { opacity: 0, y: 10 },
-  visible: {
-    opacity: 1,
-    y: 0,
-    transition: { duration: 0.6, ease: 'easeOut', staggerChildren: 0.1 },
-  },
-};
+interface LinkCheckerResultsProps {
+  label: string;
+  status: ResultStatus;
+  results: LinkCheckingResult[];
 
-const childAnimation = {
-  hidden: { opacity: 0, y: 5 },
-  visible: {
-    opacity: 1,
-    y: 0,
-    transition: { duration: 0.4, ease: 'easeOut' },
-  },
-};
+  justLoadedIn: boolean;
+}
 
-const statusStyles = {
-  error: 'text-red-600 hover:bg-red-600/10',
-  warning: 'text-yellow-300 hover:bg-yellow-400/10',
-  success: 'text-green-600 hover:bg-green-600/10',
+const LinkCheckerResults = ({
+  label,
+  status,
+  results,
+
+  justLoadedIn,
+}: LinkCheckerResultsProps) => {
+  return (
+    <ResultList
+      label={
+        <>
+          <span>{label}</span>
+          <span>({results.length})</span>
+        </>
+      }
+      defaultOpen={justLoadedIn}
+      status={status}
+      disabled={results.length === 0}
+    >
+      {results.map(({ link, status, checks }) => (
+        <Result key={link} status={status}>
+          <a
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="w-full"
+          >
+            <Result.Title>
+              <span className="block overflow-hidden truncate text-ellipsis whitespace-nowrap">
+                {link}
+              </span>
+            </Result.Title>
+            <Result.StatusDescription>
+              {checks
+                .map((check) => {
+                  if (check.type === 'syntax' && !check.passed)
+                    return 'Invalid URL';
+                  if (check.type === 'fetch_attempt')
+                    return `${check.metadata.fetchStatusCode}`;
+                  if (check.type === 'security')
+                    return check.passed ? 'Secure' : 'Insecure';
+                  return null;
+                })
+                .filter(Boolean)
+                .join(' - ')}
+            </Result.StatusDescription>
+          </a>
+        </Result>
+      ))}
+    </ResultList>
+  );
 };
 
 interface LinkCheckerProps {
   emailSlug: string;
   emailMarkup: string;
 }
-
-type ResultStatus = 'error' | 'warning' | 'success';
-
-interface CollapsibleTriggerProps {
-  count: number;
-  label: string;
-  variant: ResultStatus;
-  disabled?: boolean;
-}
-
-interface ResultSectionProps {
-  status: ResultStatus;
-  label: string;
-  results: LinkCheckingResult[];
-  open: boolean;
-}
-
-const CollapsibleTrigger = ({
-  count,
-  label,
-  variant,
-  disabled,
-}: CollapsibleTriggerProps) => (
-  <Collapsible.Trigger
-    className={clsx(
-      'group flex w-full items-center gap-1 rounded p-2 transition-colors duration-200 ease-[cubic-bezier(.36,.66,.6,1)]',
-      statusStyles[variant],
-      disabled && 'cursor-not-allowed opacity-70',
-    )}
-    disabled={disabled}
-  >
-    <span
-      className={clsx(
-        '-mt-[.125rem] transition-transform duration-200 ease-[cubic-bezier(.36,.66,.6,1)]',
-        'rotate-0 group-data-[state=open]:rotate-90',
-      )}
-    >
-      <svg
-        fill="none"
-        height="15"
-        viewBox="0 0 15 15"
-        width="15"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          clipRule="evenodd"
-          d="M6.1584 3.13508C6.35985 2.94621 6.67627 2.95642 6.86514 3.15788L10.6151 7.15788C10.7954 7.3502 10.7954 7.64949 10.6151 7.84182L6.86514 11.8418C6.67627 12.0433 6.35985 12.0535 6.1584 11.8646C5.95694 11.6757 5.94673 11.3593 6.1356 11.1579L9.565 7.49985L6.1356 3.84182C5.94673 3.64036 5.95694 3.32394 6.1584 3.13508Z"
-          fill="currentColor"
-          fillRule="evenodd"
-        />
-      </svg>
-    </span>
-    <div className="flex flex-1 items-center gap-1 font-bold text-[.625rem] uppercase tracking-wide">
-      <span>{label}</span>
-      <span>({count})</span>
-    </div>
-  </Collapsible.Trigger>
-);
-
-const ResultSection = ({
-  status,
-  label,
-  results,
-  open,
-}: ResultSectionProps) => {
-  const isEmpty = results.length === 0;
-
-  return (
-    <Collapsible.Root className="group" defaultOpen={open && !isEmpty}>
-      <CollapsibleTrigger
-        count={results.length}
-        label={label}
-        variant={status}
-        disabled={isEmpty}
-      />
-      {!isEmpty && (
-        <Collapsible.Content>
-          <ol className="mt-2 mb-1 flex list-none flex-col gap-4">
-            {results.map((result, index) => (
-              <LinkResultView key={index} {...result} />
-            ))}
-          </ol>
-        </Collapsible.Content>
-      )}
-    </Collapsible.Root>
-  );
-};
-
-const LinkResultView = (props: LinkCheckingResult) => (
-  <AnimatePresence mode="wait">
-    <motion.li
-      animate="visible"
-      className="group/item relative w-full rounded-md p-2 pl-4 transition-colors duration-300 ease-out hover:bg-slate-5"
-      data-status={props.status}
-      initial="hidden"
-      layout
-      variants={containerAnimation}
-    >
-      <a
-        href={props.link}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="w-full"
-      >
-        <motion.div
-          className="flex w-full items-center gap-2 text-xs group-data-[status=error]/item:text-red-400 group-data-[status=success]/item:text-green-400 group-data-[status=warning]/item:text-yellow-300"
-          variants={childAnimation}
-        >
-          <span className="block overflow-hidden truncate text-ellipsis whitespace-nowrap">
-            {props.link}
-          </span>
-        </motion.div>
-        <motion.div
-          className="mt-1 font-semibold text-[.625rem] uppercase"
-          variants={childAnimation}
-        >
-          {props.checks
-            .map((check) => {
-              if (check.type === 'syntax' && !check.passed)
-                return 'Invalid URL';
-              if (check.type === 'fetch_attempt')
-                return `${check.metadata.fetchStatusCode}`;
-              if (check.type === 'security')
-                return check.passed ? 'Secure' : 'Insecure';
-              return null;
-            })
-            .filter(Boolean)
-            .join(' - ')}
-        </motion.div>
-      </a>
-    </motion.li>
-  </AnimatePresence>
-);
 
 export const LinkChecker = ({ emailSlug, emailMarkup }: LinkCheckerProps) => {
   const cacheKey = `link-checking-results-${emailSlug.replaceAll('/', '-')}`;
@@ -185,8 +87,20 @@ export const LinkChecker = ({ emailSlug, emailMarkup }: LinkCheckerProps) => {
     }
   }, [cacheKey]);
 
-  const [sectionsOpen, setSectionsOpen] = React.useState(false);
+  const [justLoadedIn, setJustLoadedIn] = React.useState(false);
   const [loading, setLoading] = React.useState(false);
+
+  const handleRun = () => {
+    setLoading(true);
+    checkLinks(emailMarkup)
+      .then((newResults) => {
+        setResults(newResults);
+        setJustLoadedIn(true);
+        localStorage.setItem(cacheKey, JSON.stringify(newResults));
+      })
+      .catch(console.error)
+      .finally(() => setLoading(false));
+  };
 
   const errorResults = React.useMemo(
     () => results?.filter((r) => r.status === 'error') || [],
@@ -201,39 +115,27 @@ export const LinkChecker = ({ emailSlug, emailMarkup }: LinkCheckerProps) => {
     [results],
   );
 
-  const handleRun = () => {
-    setLoading(true);
-    checkLinks(emailMarkup)
-      .then((newResults) => {
-        setResults(newResults);
-        setSectionsOpen(true);
-        localStorage.setItem(cacheKey, JSON.stringify(newResults));
-      })
-      .catch(console.error)
-      .finally(() => setLoading(false));
-  };
-
   return (
     <div className="mt-4 flex w-full flex-col gap-2 text-pretty">
       {results ? (
         <>
-          <ResultSection
+          <LinkCheckerResults
             label="Errors"
             results={errorResults}
+            justLoadedIn={justLoadedIn}
             status="error"
-            open={sectionsOpen}
           />
-          <ResultSection
+          <LinkCheckerResults
             label="Warnings"
             results={warningResults}
+            justLoadedIn={justLoadedIn}
             status="warning"
-            open={sectionsOpen}
           />
-          <ResultSection
+          <LinkCheckerResults
             label="Success"
             results={successResults}
+            justLoadedIn={justLoadedIn}
             status="success"
-            open={sectionsOpen}
           />
         </>
       ) : (
