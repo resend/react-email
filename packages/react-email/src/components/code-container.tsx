8e98b991ee1450d0fb5fbc374dc22e042751c803
Gabriel Miranda <gabrielmfern@outlook.com>
2025-01-27 16:47:09 -0300

    feat(react-email): Link checker for preview server on a toolbar (#1799)
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Bu Kinoshita <6929565+bukinoshita@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Lucas de França <luxon4uta@gmail.com>

--- packages/react-email/src/components/code-container.tsx
+++ packages/react-email/src/components/code-container.tsx
@@ -37,7 +37,7 @@ export const CodeContainer: React.FC<Readonly<CodeContainerProps>> = ({
 
     return (
       <a
-        className="text-slate-11 transition ease-in-out duration-200 hover:text-slate-12"
+        className="text-slate-11 transition duration-200 ease-in-out hover:text-slate-12"
         download={file.name}
         href={url}
       >
@@ -72,7 +72,7 @@ export const CodeContainer: React.FC<Readonly<CodeContainerProps>> = ({
 
   return (
     <div
-      className="border-slate-6 relative w-full items-center whitespace-pre rounded-md border text-sm backdrop-blur-md"
+      className="relative w-full items-center whitespace-pre rounded-md border border-slate-6 text-sm backdrop-blur-md"
       style={{
         lineHeight: '130%',
         background:
@@ -87,7 +87,7 @@ export const CodeContainer: React.FC<Readonly<CodeContainerProps>> = ({
               const isCurrentLang = activeLang === language;
               return (
                 <motion.button
-                  className={`relative py-[8px] px-4 text-sm font-medium font-sans transition ease-in-out duration-200 hover:text-slate-12 ${
+                  className={`relative px-4 py-[8px] font-sans text-sm font-medium transition duration-200 ease-in-out hover:text-slate-12 ${
                     activeLang !== language ? 'text-slate-11' : 'text-slate-12'
                   }`}
                   key={language}
@@ -98,7 +98,7 @@ export const CodeContainer: React.FC<Readonly<CodeContainerProps>> = ({
                   {isCurrentLang ? (
                     <motion.span
                       animate={{ opacity: 1 }}
-                      className="absolute left-0 right-0 top-0 bottom-0 bg-slate-4"
+                      className="absolute bottom-0 left-0 right-0 top-0 bg-slate-4"
                       exit={{ opacity: 0 }}
                       initial={{ opacity: 0 }}
                       layoutId="code"
@@ -114,7 +114,7 @@ export const CodeContainer: React.FC<Readonly<CodeContainerProps>> = ({
         <Tooltip>
           <Tooltip.Trigger
             asChild
-            className="absolute top-2 right-2 hidden md:block"
+            className="absolute right-2 top-2 hidden md:block"
           >
             {renderClipboardIcon()}
           </Tooltip.Trigger>
@@ -123,7 +123,7 @@ export const CodeContainer: React.FC<Readonly<CodeContainerProps>> = ({
         <Tooltip>
           <Tooltip.Trigger
             asChild
-            className="text-gray-11 absolute top-2 right-8 hidden md:block"
+            className="text-gray-11 absolute right-8 top-2 hidden md:block"
           >
             {renderDownloadIcon()}
           </Tooltip.Trigger>
