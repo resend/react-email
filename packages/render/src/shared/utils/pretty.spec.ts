d0de4309e1bb5b2ba7266c0cd2a8ddc919185bf1
gabriel miranda <gabrielmfern@outlook.com>
2025-01-27 15:27:39 -0300

    fix(render): `pretty` option including extra `<` in the `Button`

--- packages/render/src/shared/utils/pretty.spec.ts
+++ packages/render/src/shared/utils/pretty.spec.ts
@@ -15,7 +15,7 @@ describe('pretty', () => {
   test('if mso syntax does not wrap', async () => {
     expect(
       await pretty(
-        `<!--[if mso]><i style="mso-font-width:100%;mso-text-raise:12" hidden>&#8202;&#8202;</i><![endif]-->`,
+        `<span><!--[if mso]><i style="mso-font-width:100%;mso-text-raise:12" hidden>&#8202;&#8202;</i><![endif]--></span>`,
       ),
     ).toMatchSnapshot();
   });
--- packages/render/src/shared/utils/pretty.ts
+++ packages/render/src/shared/utils/pretty.ts
@@ -1,28 +1,87 @@
 import type { Options, Plugin } from 'prettier';
 import html from 'prettier/plugins/html';
+import type { builders } from 'prettier/doc';
 import { format } from 'prettier/standalone';
+
+interface HTMLNode {
+  type: 'element' | 'text' | 'ieConditionalComment';
+  name?: string;
+  sourceSpan: {
+    start: { file: unknown[]; offset: number; line: number; col: number };
+    end: { file: unknown[]; offset: number; line: number; col: number };
+    details: null;
+  };
+  parent?: HTMLNode;
+}
+
+function recursivelyMapDoc(
+  doc: builders.Doc,
+  callback: (innerDoc: string | builders.DocCommand) => builders.Doc,
+): builders.Doc {
+  if (Array.isArray(doc)) {
+    return doc.map((innerDoc) => recursivelyMapDoc(innerDoc, callback));
+  }
+
+  if (typeof doc === 'object') {
+    if (doc.type === 'group') {
+      return {
+        ...doc,
+        contents: recursivelyMapDoc(doc.contents, callback),
+        expandedStates: recursivelyMapDoc(
+          doc.expandedStates,
+          callback,
+        ) as builders.Doc[],
+      };
+    }
+
+    if ('contents' in doc) {
+      return {
+        ...doc,
+        contents: recursivelyMapDoc(doc.contents, callback),
+      };
+    }
+
+    if ('parts' in doc) {
+      return {
+        ...doc,
+        parts: recursivelyMapDoc(doc.parts, callback) as builders.Doc[],
+      };
+    }
+
+    if (doc.type === 'if-break') {
+      return {
+        ...doc,
+        breakContents: recursivelyMapDoc(doc.breakContents, callback),
+        flatContents: recursivelyMapDoc(doc.flatContents, callback),
+      };
+    }
+  }
+
+  return callback(doc);
+}
 
 const modifiedHtml = { ...html } as Plugin;
 if (modifiedHtml.printers) {
   // eslint-disable-next-line @typescript-eslint/unbound-method
   const previousPrint = modifiedHtml.printers.html.print;
   modifiedHtml.printers.html.print = (path, options, print, args) => {
-    const node = path.getNode() as {
-      type: string;
-      sourceSpan: {
-        start: { file: unknown[]; offset: number; line: number; col: number };
-        end: { file: unknown[]; offset: number; line: number; col: number };
-        details: null;
-      };
-    };
+    const node = path.getNode() as HTMLNode;
+
+    const rawPrintingResult = previousPrint(path, options, print, args);
 
     if (node.type === 'ieConditionalComment') {
-      return options.originalText.slice(
-        node.sourceSpan.start.offset,
-        node.sourceSpan.end.offset,
-      );
+      const printingResult = recursivelyMapDoc(rawPrintingResult, (doc) => {
+        if (typeof doc === 'object' && doc.type === 'line') {
+          return doc.soft ? '' : ' ';
+        }
+
+        return doc;
+      });
+
+      return printingResult;
     }
-    return previousPrint(path, options, print, args);
+
+    return rawPrintingResult;
   };
 }
 
