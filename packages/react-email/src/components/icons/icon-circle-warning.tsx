a9f3232d2dc08816d6870ca53c6879b5515811ac
Gabriel Miranda <gabrielmfern@outlook.com>
2025-01-27 16:47:09 -0300

    feat(react-email): Link checker for preview server on a toolbar (#1799)
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Bu Kinoshita <6929565+bukinoshita@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Lucas de França <luxon4uta@gmail.com>

---
+++ packages/react-email/src/components/icons/icon-circle-warning.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import type { IconElement, IconProps } from './icon-base';
+import { IconBase } from './icon-base';
+
+export const IconCircleWarning = React.forwardRef<
+  IconElement,
+  Readonly<IconProps>
+>(({ ...props }, forwardedRef) => (
+  <IconBase ref={forwardedRef} {...props}>
+    <path
+      d="M11 15h2v2h-2zm0-8h2v6h-2zm.99-5C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2M12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8s8 3.58 8 8s-3.58 8-8 8"
+      fill="currentColor"
+    />
+  </IconBase>
+));
+
+IconCircleWarning.displayName = 'IconCircleWarning';
--- packages/react-email/src/components/sidebar/file-tree-directory-children.tsx
+++ packages/react-email/src/components/sidebar/file-tree-directory-children.tsx
@@ -6,9 +6,9 @@ import { cn } from '../../utils';
 import { emailsDirectoryAbsolutePath } from '../../utils/emails-directory-absolute-path';
 import type { EmailsDirectory } from '../../utils/get-emails-directory-metadata';
 import { IconFile } from '../icons/icon-file';
-import { SidebarDirectory } from './sidebar-directory';
+import { FileTreeDirectory } from './file-tree-directory';
 
-export const SidebarDirectoryChildren = (props: {
+export const FileTreeDirectoryChildren = (props: {
   emailsDirectoryMetadata: EmailsDirectory;
   currentEmailOpenSlug?: string;
   open: boolean;
@@ -32,22 +32,20 @@ export const SidebarDirectoryChildren = (props: {
             initial={{ opacity: 0, height: 0 }}
           >
             {props.isRoot ? null : (
-              <div className="line absolute left-2.5 w-px h-full bg-slate-6" />
+              <div className="line absolute left-2.5 h-full w-px bg-slate-6" />
             )}
-
             <div className="data-[root=true]:py-2 flex flex-col truncate">
               <LayoutGroup id="sidebar">
                 {props.emailsDirectoryMetadata.subDirectories.map(
                   (subDirectory) => (
-                    <SidebarDirectory
-                      className="pl-4 py-0"
+                    <FileTreeDirectory
+                      className="p-0 data-[state=open]:mb-2"
                       currentEmailOpenSlug={props.currentEmailOpenSlug}
                       emailsDirectoryMetadata={subDirectory}
                       key={subDirectory.absolutePath}
                     />
                   ),
                 )}
-
                 {props.emailsDirectoryMetadata.emailFilenames.map(
                   (emailFilename, index) => {
                     const emailSlug = isBaseEmailsDirectory
@@ -81,7 +79,7 @@ export const SidebarDirectoryChildren = (props: {
                         <motion.span
                           animate={{ x: 0, opacity: 1 }}
                           className={cn(
-                            'text-[14px] flex items-center align-middle pl-3 h-8 max-w-full rounded-md text-slate-11 relative transition-colors',
+                            'relative flex h-8 max-w-full items-center rounded-md pl-3 align-middle text-slate-11 text-sm transition-colors duration-100 ease-[cubic-bezier(.6,.12,.34,.96)]',
                             {
                               'text-cyan-11': isCurrentPage,
                               'hover:text-slate-12':
@@ -97,19 +95,25 @@ export const SidebarDirectoryChildren = (props: {
                           {isCurrentPage ? (
                             <motion.span
                               animate={{ opacity: 1 }}
-                              className="absolute left-0 right-0 top-0 bottom-0 rounded-md bg-cyan-5 opacity-0"
+                              className="absolute inset-0 rounded-md bg-cyan-5 opacity-0 transition-all duration-200 ease-[cubic-bezier(.6,.12,.34,.96)]"
                               exit={{ opacity: 0 }}
                               initial={{ opacity: 0 }}
                             >
-                              {!props.isRoot && (
-                                <div className="bg-cyan-11 w-px absolute top-1 left-1.5 h-6" />
-                              )}
+                              <motion.div
+                                className="absolute top-1 left-[.625rem] h-6 w-px rounded-sm bg-cyan-11"
+                                layoutId="active-file"
+                                transition={{
+                                  type: 'spring',
+                                  bounce: 0.2,
+                                  duration: 0.6,
+                                }}
+                              />
                             </motion.span>
                           ) : null}
                           <IconFile
-                            className="absolute left-4 w-[24px] h-[24px]"
-                            height="24"
-                            width="24"
+                            className="absolute left-4 h-5 w-5"
+                            height="20"
+                            width="20"
                           />
                           <span className="truncate pl-8">{emailFilename}</span>
                         </motion.span>
