e76fb5ad45cc4d5e501dc8dd1528539019dd8a2f
gabriel miranda <gabrielmfern@outlook.com>
2025-02-03 14:15:07 -0300

    write intial working version with zod

--- packages/react-email/src/actions/get-email-controls.ts
+++ packages/react-email/src/actions/get-email-controls.ts
@@ -1,10 +1,23 @@
 'use server';
 
-import type { Controls } from '../package';
+import { ZodFirstPartyTypeKind, type ZodString } from 'zod';
 import { cachedGetEmailComponent } from '../utils/cached-get-email-component';
 import type { ErrorObject } from '../utils/types/error-object';
 
-export type ControlsResult = { error: ErrorObject } | { controls: Controls };
+export type Control =
+  | {
+    key: string;
+    type: 'email' | 'text' | 'checkbox' | 'number';
+  }
+  | {
+    key: string;
+    type: 'select';
+    options: { name: string; value: string }[];
+  };
+
+type ControlsResult =
+  | { error: ErrorObject }
+  | { controls: Control[] | undefined };
 
 export const getEmailControls = async (
   emailPath: string,
@@ -16,6 +29,36 @@ export const getEmailControls = async (
   }
 
   const { emailComponent: Email } = result;
+  if (!Email.PreviewSchema) return { controls: undefined };
 
-  return { controls: Email.controls ?? {} };
+  const controls: Control[] = [];
+
+  const propsShape = Email.PreviewSchema._def.shape();
+  for (const key in propsShape) {
+    const type = propsShape[key];
+    if (type && 'typeName' in type._def) {
+      switch (type._def.typeName) {
+        case ZodFirstPartyTypeKind.ZodString:
+          controls.push({
+            key,
+            type: (type as ZodString).isEmail ? 'email' : 'text',
+          });
+          break;
+        case ZodFirstPartyTypeKind.ZodNumber:
+          controls.push({
+            key,
+            type: 'number',
+          });
+          break;
+        case ZodFirstPartyTypeKind.ZodBoolean:
+          controls.push({
+            key,
+            type: 'checkbox',
+          });
+          break;
+      }
+    }
+  }
+
+  return { controls };
 };
