f20491057a9989592ccea699fda992773728197f
gabriel miranda <gabrielmfern@outlook.com>
2025-02-03 16:57:56 -0300

    use the oficial lottie react library

--- packages/react-email/src/components/button.tsx
+++ packages/react-email/src/components/button.tsx
@@ -1,10 +1,10 @@
 'use client';
 import * as SlotPrimitive from '@radix-ui/react-slot';
-import Lottie from 'lottie-react';
 import type * as React from 'react';
 import animatedLoadIcon from '../animated-icons-data/load.json';
 import { cn } from '../utils/cn';
 import { unreachable } from '../utils/unreachable';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
 type RootProps = React.ComponentProps<'button'>;
 
@@ -49,9 +49,9 @@ export const Button = ({
           loading && 'opacity-100',
         )}
       >
-        <Lottie
-          animationData={animatedLoadIcon}
-          autoPlay={false}
+        <DotLottieReact
+          data={animatedLoadIcon}
+          autoplay={false}
           className="h-5 w-5"
           loop={true}
         />
