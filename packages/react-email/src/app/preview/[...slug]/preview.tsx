9ad8efaa84a0c42f6c99d7fc68ee5560beb396b2
Gabriel Miranda <gabrielmfern@outlook.com>
2025-02-10 07:26:47 -0300

    feat(react-email): Customizable email view size (#1837)
    Co-authored-by: Lucas de França <luxon4uta@gmail.com>

--- packages/react-email/src/app/preview/[...slug]/preview.tsx
+++ packages/react-email/src/app/preview/[...slug]/preview.tsx
@@ -1,12 +1,19 @@
 'use client';
 
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
-import React from 'react';
+import { useState } from 'react';
+import { flushSync } from 'react-dom';
 import { Toaster } from 'sonner';
+import { useDebouncedCallback } from 'use-debounce';
 import type { EmailRenderingResult } from '../../../actions/render-email-by-path';
 import { CodeContainer } from '../../../components/code-container';
+import {
+  ResizableWarpper,
+  makeIframeDocumentBubbleEvents,
+} from '../../../components/resizable-wrapper';
 import { Shell } from '../../../components/shell';
 import { Tooltip } from '../../../components/tooltip';
+import { useClampedState } from '../../../hooks/use-clamped-state';
 import { useEmailRenderingResult } from '../../../hooks/use-email-rendering-result';
 import { useHotreload } from '../../../hooks/use-hot-reload';
 import { useRenderingMetadata } from '../../../hooks/use-rendering-metadata';
@@ -29,7 +36,7 @@ const Preview = ({
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
-  const activeView = searchParams.get('view') ?? 'desktop';
+  const activeView = searchParams.get('view') ?? 'preview';
   const activeLang = searchParams.get('lang') ?? 'jsx';
 
   const renderingResult = useEmailRenderingResult(
@@ -75,36 +82,115 @@ const Preview = ({
 
   const hasNoErrors = typeof renderedEmailMetadata !== 'undefined';
 
+  const [maxWidth, setMaxWidth] = useState(Number.POSITIVE_INFINITY);
+  const [maxHeight, setMaxHeight] = useState(Number.POSITIVE_INFINITY);
+  const minWidth = 350;
+  const minHeight = 600;
+  const storedWidth = searchParams.get('width');
+  const storedHeight = searchParams.get('height');
+  const [width, setWidth] = useClampedState(
+    storedWidth ? Number.parseInt(storedWidth) : 600,
+    350,
+    maxWidth,
+  );
+  const [height, setHeight] = useClampedState(
+    storedHeight ? Number.parseInt(storedHeight) : 1024,
+    600,
+    maxHeight,
+  );
+
+  const handleSaveViewSize = useDebouncedCallback(() => {
+    const params = new URLSearchParams(searchParams);
+    params.set('width', width.toString());
+    params.set('height', height.toString());
+    router.push(`${pathname}?${params.toString()}`);
+  }, 300);
+
   return (
     <Shell
-      activeView={hasNoErrors ? activeView : undefined}
+      activeView={activeView}
       currentEmailOpenSlug={slug}
       markup={renderedEmailMetadata?.markup}
       pathSeparator={pathSeparator}
-      setActiveView={hasNoErrors ? handleViewChange : undefined}
+      setActiveView={handleViewChange}
+      setViewHeight={(height) => {
+        setHeight(height);
+        flushSync(() => {
+          handleSaveViewSize();
+        });
+      }}
+      setViewWidth={(width) => {
+        setWidth(width);
+        flushSync(() => {
+          handleSaveViewSize();
+        });
+      }}
+      viewHeight={height}
+      viewWidth={width}
     >
       {/* This relative is so that when there is any error the user can still switch between emails */}
-      <div className="relative h-full">
+      <div
+        className="relative flex h-full pb-8 bg-gray-200"
+        ref={(element) => {
+          const observer = new ResizeObserver((entry) => {
+            const [elementEntry] = entry;
+            if (elementEntry) {
+              setMaxWidth(elementEntry.contentRect.width - 80);
+              setMaxHeight(elementEntry.contentRect.height - 80);
+            }
+          });
+
+          if (element) {
+            observer.observe(element);
+          }
+
+          return () => {
+            observer.disconnect();
+          };
+        }}
+      >
         {'error' in renderingResult ? (
           <RenderingError error={renderingResult.error} />
         ) : null}
 
         {hasNoErrors ? (
           <>
-            {activeView === 'desktop' && (
-              <iframe
-                className="h-full w-full bg-white"
-                srcDoc={renderedEmailMetadata.markup}
-                title={slug}
-              />
-            )}
-
-            {activeView === 'mobile' && (
-              <iframe
-                className="mx-auto h-full w-[360px] bg-white"
-                srcDoc={renderedEmailMetadata.markup}
-                title={slug}
-              />
+            {activeView === 'preview' && (
+              <ResizableWarpper
+                minHeight={minHeight}
+                minWidth={minWidth}
+                maxHeight={maxHeight}
+                maxWidth={maxWidth}
+                height={height}
+                onResizeEnd={() => {
+                  handleSaveViewSize();
+                }}
+                onResize={(value, direction) => {
+                  const isHorizontal =
+                    direction === 'east' || direction === 'west';
+                  if (isHorizontal) {
+                    setWidth(value);
+                  } else {
+                    setHeight(value);
+                  }
+                }}
+                width={width}
+              >
+                <iframe
+                  className="solid max-h-full rounded-lg bg-white"
+                  ref={(iframe) => {
+                    if (iframe) {
+                      return makeIframeDocumentBubbleEvents(iframe);
+                    }
+                  }}
+                  srcDoc={renderedEmailMetadata.markup}
+                  style={{
+                    width: `${width}px`,
+                    height: `${height}px`,
+                  }}
+                  title={slug}
+                />
+              </ResizableWarpper>
             )}
 
             {activeView === 'source' && (
