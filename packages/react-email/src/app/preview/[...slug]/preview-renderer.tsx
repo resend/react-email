1b843f0fe24111fda87e7503ee5130bb515f2490
gabriel miranda <gabrielmfern@outlook.com>
2025-02-17 12:25:52 -0300

    use portals, SO MUCH SIMPLER

--- packages/react-email/src/app/preview/[...slug]/preview-renderer.tsx
+++ packages/react-email/src/app/preview/[...slug]/preview-renderer.tsx
@@ -1,83 +1,32 @@
-import {
-  type ComponentProps,
-  useEffect,
-  useRef,
-  useTransition,
-  startTransition,
-  useState,
-} from 'react';
-import { type Root, createRoot } from 'react-dom/client';
+import { type ComponentProps, useState } from 'react';
+import ReactDOM from 'react-dom';
 import { mergeRefs } from '../../../utils/merge-refs';
 
 type ShadowRootRendererProps = {
   children: React.ReactElement;
 } & ComponentProps<'iframe'>;
 
-interface EmailWrapperProps {
-  onMount?: () => void;
-  children: React.ReactNode;
-}
-
-const EmailWrapper = ({ onMount, children }: EmailWrapperProps) => {
-  useEffect(() => {
-    onMount?.();
-  }, []);
-
-  return <>{children}</>;
-};
-
 export const PreviewRenderer = ({
   children,
   ...rest
 }: ShadowRootRendererProps) => {
-  const rootRef = useRef<Root>(null);
-
-  type RenderingState =
-    | {
-        isRendering: false;
-      }
-    | {
-        isRendering: true;
-        onMountCallbacks: (() => unknown)[];
-      };
-
-  const renderingState = useRef<RenderingState>({
-    isRendering: false,
-  });
-
-  const iframeContent = (
-    <EmailWrapper
-      onMount={() => {
-        if (!renderingState.current.isRendering) return;
-        renderingState.current = {
-          isRendering: false,
-        };
-      }}
-    >
-      {children}
-    </EmailWrapper>
-  );
-
-  useEffect(() => {
-    rootRef.current?.render(children);
-  }, [children]);
+  const [iframeDocument, setIframeDocument] = useState<Document>();
 
   return (
-    <iframe
-      {...rest}
-      srcDoc={''}
-      ref={mergeRefs((iframe) => {
-        (async () => {
-          if (iframe?.contentDocument && !rootRef.current) {
-            rootRef.current = createRoot(iframe.contentDocument);
-            rootRef.current.render(children);
-          }
-
-          return () => {
-            rootRef.current?.unmount();
-          };
-        })();
-      }, rest.ref)}
-    />
+    <>
+      <iframe
+        {...rest}
+        onLoad={(event) => {
+          setIframeDocument(event.currentTarget.contentDocument ?? undefined);
+        }}
+        ref={mergeRefs(rest.ref, (iframe) => {
+          setIframeDocument(iframe?.contentDocument ?? undefined);
+        })}
+        id="preview-renderer-iframe"
+      />
+      {iframeDocument
+        ? ReactDOM.createPortal(children, iframeDocument as unknown as Element)
+        : null}
+    </>
   );
 };
