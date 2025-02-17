b740a528e69493c706aee4ef7d605379d07d8dfc
Kaylee <65376239+KayleeWilliams@users.noreply.github.com>
2025-02-17 18:39:15 +0000

    feat(react-email): added a theme switcher to the dev preview (#1749)
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: gabriel miranda <gabrielmfern@outlook.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Bu Kinoshita <6929565+bukinoshita@users.noreply.github.com>

---
+++ packages/react-email/src/components/icons/icon-sun.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import type { IconElement, IconProps } from './icon-base';
+import { IconBase } from './icon-base';
+
+export const IconSun = React.forwardRef<IconElement, Readonly<IconProps>>(
+  ({ ...props }, forwardedRef) => (
+    <IconBase ref={forwardedRef} {...props}>
+      <path
+        fill="currentColor"
+        d="m3.55 19.09l1.41 1.41l1.8-1.79l-1.42-1.42M12 6c-3.31 0-6 2.69-6 6s2.69 6 6 6s6-2.69 6-6c0-3.32-2.69-6-6-6m8 7h3v-2h-3m-2.76 7.71l1.8 1.79l1.41-1.41l-1.79-1.8M20.45 5l-1.41-1.4l-1.8 1.79l1.42 1.42M13 1h-2v3h2M6.76 5.39L4.96 3.6L3.55 5l1.79 1.81zM1 13h3v-2H1m12 9h-2v3h2"
+      />
+    </IconBase>
+  ),
+);
+
+IconSun.displayName = 'IconSun';
