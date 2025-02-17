5d84ee7ab6d94ee9998883e65d5806117a01e06a
gabriel miranda <gabrielmfern@outlook.com>
2025-02-17 12:25:52 -0300

    use portals, SO MUCH SIMPLER

--- packages/react-email/src/utils/merge-refs.ts
+++ packages/react-email/src/utils/merge-refs.ts
@@ -2,12 +2,20 @@ export const mergeRefs = <T>(
   ...refs: (React.Ref<T> | undefined)[]
 ): React.RefCallback<T> => {
   return (value) => {
+    const cleanups: (() => void)[] = [];
     refs.forEach((ref) => {
       if (typeof ref === 'function') {
-        ref(value);
+        const cleanup = ref(value);
+        if (cleanup) {
+          cleanups.push(cleanup);
+        }
       } else if (ref) {
         ref.current = value;
       }
     });
+
+    return () => {
+      cleanups.forEach((callback) => callback());
+    };
   };
 };
