bbd84f6d46b3a57bf7cc6c632adac9bc86495cd9
student <student@W-N-AppDev23.local>
2025-02-22 20:34:43 -0800

    test: update body component snapshots

--- packages/body/src/body.tsx
+++ packages/body/src/body.tsx
@@ -5,7 +5,7 @@ export type BodyProps = Readonly<React.HtmlHTMLAttributes<HTMLBodyElement>>;
 export const Body = React.forwardRef<HTMLBodyElement, BodyProps>(
   ({ children, style, ...props }, ref) => {
     return (
-      <body className='p-0' {...props} ref={ref} style={style}>
+      <body className="p-0" {...props} ref={ref} style={style}>
         {children}
       </body>
     );
