afbcc5aa96ae06d8c8246f835ba2f18c892bf661
gabriel miranda <gabrielmfern@outlook.com>
2025-02-26 17:37:13 -0300

    chore(render): Remove duplicate code because of renderAsync

--- packages/render/src/browser/index.ts
+++ packages/render/src/browser/index.ts
@@ -1,5 +1,14 @@
+import { render } from './render';
+import type { Options } from '../shared/options';
+
+/**
+  * @deprecated use {@link render}
+  */
+export const renderAsync = (element: React.ReactElement, options?: Options) => {
+  return render(element, options);
+}
+
 export * from './render';
-export * from './render-async';
 
 export * from '../shared/options';
 export * from '../shared/plain-text-selectors';
