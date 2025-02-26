43106a84b900442e8632ae20b713c44080bd5072
gabriel miranda <gabrielmfern@outlook.com>
2025-02-26 18:08:23 -0300

    chore(tailwind): Improve code for running Tailwind integration test

---
+++ packages/tailwind/integrations/integrations.spec.ts
@@ -0,0 +1,36 @@
+import path from 'node:path';
+import shell from 'shelljs';
+
+const $ = (command: string, cwd: string = path.resolve(__dirname, '..')) => {
+  const executionResult = shell.exec(command, { cwd, fatal: true });
+  if (executionResult.code !== 0) {
+    process.stdout.write(executionResult.stderr);
+    process.stderr.write(executionResult.stderr);
+  }
+  expect(
+    executionResult.code,
+    `Expected command "${command}" to work properly but it returned a non-zero exit code`,
+  ).toBe(0);
+};
+
+describe('integrations', () => {
+  beforeAll(() => {
+    const packageLocation = path.resolve(__dirname, '../');
+    $('yalc installations clean @react-email/tailwind', packageLocation);
+    $('yalc publish', packageLocation);
+  });
+
+  const integrationsLocation = __dirname;
+
+  test.sequential("Tailwind works on the Next App's build process", () => {
+    const nextAppLocation = path.resolve(integrationsLocation, 'nextjs');
+    $('npm install', nextAppLocation);
+    $('npm run build', nextAppLocation);
+  });
+
+  test.sequential("Tailwind works on the Vite App's build process", () => {
+    const viteAppLocation = path.resolve(integrationsLocation, 'vite');
+    $('npm install', viteAppLocation);
+    $('npm run build', viteAppLocation);
+  });
+});
