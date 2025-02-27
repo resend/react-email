133c2e4b6f490378953a6125a2c6dac226b0deeb
gabriel miranda <gabrielmfern@outlook.com>
2025-02-27 10:24:13 -0300

    fix import for Tailwind being from components

--- packages/tailwind/integrations/nextjs/emails/vercel-invite-user.tsx
+++ packages/tailwind/integrations/nextjs/emails/vercel-invite-user.tsx
@@ -13,8 +13,8 @@ import {
   Row,
   Section,
   Text,
-  Tailwind,
 } from '@react-email/components';
+import { Tailwind } from '@react-email/tailwind';
 import * as React from 'react';
 
 interface VercelInviteUserEmailProps {
