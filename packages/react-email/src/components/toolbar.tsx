21f8ab6ddbcb23d01454dd1544211f9f00b7b60a
gabriel miranda <gabrielmfern@outlook.com>
2025-03-11 11:51:07 -0300

    add animation for active tab indicator

--- packages/react-email/src/components/toolbar.tsx
+++ packages/react-email/src/components/toolbar.tsx
@@ -9,6 +9,7 @@ import { IconReload } from './icons/icon-reload';
 import { IconArrowDown } from './icons/icon-arrow-down';
 import { IconScanner } from './icons/icon-scanner';
 import { IconScissors } from './icons/icon-scissors';
+import { LayoutGroup, motion } from 'framer-motion';
 
 type ToolbarProps = React.ComponentProps<'div'> & {
   emailSlug: string;
@@ -20,9 +21,15 @@ type ActivePanelValue = 'linter' | 'spam-assassin';
 
 interface ToolbarButton extends React.ComponentProps<'button'> {
   children: React.ReactNode;
+  active?: boolean;
 }
 
-const ToolbarButton = ({ children, className, ...props }: ToolbarButton) => {
+const ToolbarButton = ({
+  children,
+  className,
+  active,
+  ...props
+}: ToolbarButton) => {
   return (
     <button
       type="button"
@@ -30,12 +37,22 @@ const ToolbarButton = ({ children, className, ...props }: ToolbarButton) => {
       className={cn(
         'h-full w-fit font-medium flex text-sm text-slate-10 items-center align-middle justify-center px-1 py-2 gap-1 relative',
         'hover:text-slate-12 transition-colors',
-        'data-[state=active]:text-cyan-11 group/toolbar-button',
+        active && 'data-[state=active]:text-cyan-11',
         className,
       )}
     >
       {children}
-      <span className="hidden group-data-[state=active]/toolbar-button:inline -bottom-px absolute left-0 w-full bg-cyan-11 h-px" />
+      {active ? (
+        <motion.span
+          className="-bottom-px absolute rounded-sm left-0 w-full bg-cyan-11 h-px"
+          layoutId="active-toolbar-button"
+          transition={{
+            type: 'spring',
+            bounce: 0.2,
+            duration: 0.6,
+          }}
+        />
+      ) : null}
     </button>
   );
 };
@@ -81,18 +98,20 @@ export const Toolbar = ({
       >
         <div className="flex flex-col h-full">
           <Tabs.List className="flex gap-4 px-2 border-b border-solid border-slate-6 h-7 w-full">
-            <Tabs.Trigger asChild value="spam-assassin">
-              <ToolbarButton>
-                <IconScissors />
-                Spam Assassin
-              </ToolbarButton>
-            </Tabs.Trigger>
-            <Tabs.Trigger asChild value="linter">
-              <ToolbarButton>
-                <IconScanner />
-                Linter
-              </ToolbarButton>
-            </Tabs.Trigger>
+            <LayoutGroup id="toolbar">
+              <Tabs.Trigger asChild value="spam-assassin">
+                <ToolbarButton active={activePanelValue === 'spam-assassin'}>
+                  <IconScissors />
+                  Spam Assassin
+                </ToolbarButton>
+              </Tabs.Trigger>
+              <Tabs.Trigger asChild value="linter">
+                <ToolbarButton active={activePanelValue === 'linter'}>
+                  <IconScanner />
+                  Linter
+                </ToolbarButton>
+              </Tabs.Trigger>
+            </LayoutGroup>
             <div className="flex gap-1 ml-auto">
               <ToolbarButton>
                 <IconReload />
