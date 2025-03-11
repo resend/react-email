4519e85e4b2b8936e7f537abfc5dbde20dda4543
gabriel miranda <gabrielmfern@outlook.com>
2025-03-11 16:54:30 -0300

    add tooltips to buttons on the right

--- packages/react-email/src/components/toolbar.tsx
+++ packages/react-email/src/components/toolbar.tsx
@@ -11,6 +11,7 @@ import { Linter, useLinter } from './toolbar/linter';
 // import { ImageChecker } from './image-checker';
 // import { LinkChecker } from './link-checker';
 import { SpamAssassin, useSpamAssassin } from './toolbar/spam-assassin';
+import { Tooltip } from './tooltip';
 
 type ToolbarProps = React.ComponentProps<'div'> & {
   emailSlug: string;
@@ -23,38 +24,47 @@ type ActivePanelValue = 'linter' | 'spam-assassin';
 interface ToolbarButton extends React.ComponentProps<'button'> {
   children: React.ReactNode;
   active?: boolean;
+  tooltip?: React.ReactNode;
 }
 
 const ToolbarButton = ({
   children,
   className,
   active,
+  tooltip,
   ...props
 }: ToolbarButton) => {
   return (
-    <button
-      type="button"
-      {...props}
-      className={cn(
-        'h-full w-fit font-medium flex text-sm text-slate-10 items-center align-middle justify-center px-1 py-2 gap-1 relative',
-        'hover:text-slate-12 transition-colors',
-        active && 'data-[state=active]:text-cyan-11',
-        className,
-      )}
-    >
-      {children}
-      {active ? (
-        <motion.span
-          className="-bottom-px absolute rounded-sm left-0 w-full bg-cyan-11 h-px"
-          layoutId="active-toolbar-button"
-          transition={{
-            type: 'spring',
-            bounce: 0.2,
-            duration: 0.6,
-          }}
-        />
-      ) : null}
-    </button>
+    <Tooltip.Provider>
+      <Tooltip>
+        <Tooltip.Trigger asChild>
+          <button
+            type="button"
+            {...props}
+            className={cn(
+              'h-full w-fit font-medium flex text-sm text-slate-10 items-center align-middle justify-center px-1 py-2 gap-1 relative',
+              'hover:text-slate-12 transition-colors',
+              active && 'data-[state=active]:text-cyan-11',
+              className,
+            )}
+          >
+            {children}
+            {active ? (
+              <motion.span
+                className="-bottom-px absolute rounded-sm left-0 w-full bg-cyan-11 h-px"
+                layoutId="active-toolbar-button"
+                transition={{
+                  type: 'spring',
+                  bounce: 0.2,
+                  duration: 0.6,
+                }}
+              />
+            ) : null}
+          </button>
+        </Tooltip.Trigger>
+        {tooltip ? <Tooltip.Content>{tooltip}</Tooltip.Content> : null}
+      </Tooltip>
+    </Tooltip.Provider>
   );
 };
 
@@ -132,6 +142,7 @@ export const Toolbar = ({
             </LayoutGroup>
             <div className="flex gap-1 ml-auto">
               <ToolbarButton
+                tooltip="Reload"
                 onClick={() => {
                   if (activePanelValue === 'spam-assassin') {
                     void loadSpamChecking();
@@ -143,6 +154,7 @@ export const Toolbar = ({
                 <IconReload />
               </ToolbarButton>
               <ToolbarButton
+                tooltip="Toggle toolbar"
                 onClick={() => {
                   setToggled((v) => !v);
                 }}
