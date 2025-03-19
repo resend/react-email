df5154b79780dce8359f5130e9b72005ac7be1df
gabriel miranda <gabrielmfern@outlook.com>
2025-03-19 16:54:46 -0300

    disable spinner when doing preview development

--- packages/react-email/src/cli/utils/preview/get-env-variables-for-preview-app.ts
+++ packages/react-email/src/cli/utils/preview/get-env-variables-for-preview-app.ts
@@ -1,4 +1,5 @@
 import path from 'node:path';
+import { isDev } from './start-dev-server';
 
 export const getEnvVariablesForPreviewApp = (
   relativePathToEmailsDirectory: string,
@@ -8,5 +9,6 @@ export const getEnvVariablesForPreviewApp = (
     EMAILS_DIR_RELATIVE_PATH: relativePathToEmailsDirectory,
     EMAILS_DIR_ABSOLUTE_PATH: path.resolve(cwd, relativePathToEmailsDirectory),
     USER_PROJECT_LOCATION: cwd,
+    NEXT_PUBLIC_IS_PREVIEW_DEVELOPMENT: isDev ? 'true': 'false',
   } as const;
 };
