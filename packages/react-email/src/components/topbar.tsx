a6dc3414616f3a70674eaa3d57c8a8e21ec6d22f
Zeh Fernandes <ozehfernandes@gmail.com>
2025-03-19 22:30:15 -0300

    Update packages/react-email/src/components/topbar.tsx
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>

--- packages/react-email/src/components/topbar.tsx
+++ packages/react-email/src/components/topbar.tsx
@@ -16,7 +16,7 @@ export const Topbar = ({ emailTitle, children }: TopbarProps) => {
 
   return (
     <Tooltip.Provider>
-      <header className=" px-4 py-2 relative flex h-[3.3125rem] items-center justify-between gap-3 border-slate-6 border-b px-3">
+      <header className="relative flex h-[3.3125rem] items-center justify-between gap-3 border-slate-6 border-b px-3 py-2">
         <div className="relative flex w-fit items-center gap-3">
           <Tooltip>
             <Tooltip.Trigger asChild>
