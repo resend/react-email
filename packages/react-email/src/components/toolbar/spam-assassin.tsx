191c6f720955790cf26bae04eac5a57a3b78a02b
gabriel miranda <gabrielmfern@outlook.com>
2025-03-26 15:21:42 -0300

    chore: Lint

--- packages/react-email/src/components/toolbar/spam-assassin.tsx
+++ packages/react-email/src/components/toolbar/spam-assassin.tsx
@@ -103,12 +103,10 @@ export const SpamAssassin = ({ result }: SpamAssassinProps) => {
                 ? 'Congratulations! Younr email is clean of abuse indicators.'
                 : 'Higher scores are better'}
             </Results.Column>
-            <Results.Column
-              className="text-right tracking-tighter font-bold"
-            >
+            <Results.Column className="text-right tracking-tighter font-bold">
               <span
                 className={cn(
-                  "text-3xl",
+                  'text-3xl',
                   result.points === 0 ? 'text-green-400' : null,
                   result.points > 0 && result.points <= 1.5 ? null : null,
                   result.points > 1.5 ? 'text-yellow-200' : null,
