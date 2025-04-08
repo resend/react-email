9eda3466a3211aeb2c513da11656a9c162fd0514
gabriel miranda <gabrielmfern@outlook.com>
2025-04-08 18:02:16 -0300

    fix discrepancies in simple-rating-survey

--- apps/web/components/simple-rating-survey/tailwind.tsx
+++ apps/web/components/simple-rating-survey/tailwind.tsx
@@ -22,36 +22,36 @@ export const component = (
       claim?
     </Preview>
     <Body>
-      <Container className="mx-auto max-w-[600px] rounded-lg bg-white px-[42px] py-[24px]">
-        <Heading className="mb-4 text-2xl leading-8">
+      <Container className="mx-auto max-w-[600px] rounded-[8px] bg-white px-[42px] py-[24px]">
+        <Heading className="mb-[16px] text-[24px] leading-[32px]">
           How satisfied were you overall with the initial conversation about
           your claim?
         </Heading>
-        <Text className="mb-[42px] text-gray-500 text-sm leading-6">
+        <Text className="mb-[42px] text-gray-500 text-[14px] leading-[24px]">
           Your feedback is important to us and it will be used to better serve
           our customers.
         </Text>
         <Section className="max-w-[300px]">
           <Row>
             <Column className="w-[100px] text-center">
-              <Text className="ml-3 text-left text-[#444444] text-xs leading-none">
+              <Text className="ml-[12px] text-left text-[rgb(68,68,68)] text-[12px] leading-none">
                 Dissatisfied
               </Text>
             </Column>
             <Column className="w-[100px] text-center">
-              <Text className="mr-3 text-right text-[#444444] text-xs leading-none">
+              <Text className="mr-[12px] text-right text-[rgb(68,68,68)] text-[12px] leading-none">
                 Satisfied
               </Text>
             </Column>
           </Row>
         </Section>
         <Section className="mt-[12px] mb-[24px]">
-          <Row className="w-full max-w-[300px] table-fixed border-separate border-spacing-[12px]">
+          <Row className="w-full max-w-[300px] table-fixed border-separate [border-spacing:12px]">
             {Array.from({ length: 5 }).map((_, i) => (
-              <Column key={i} className="rounded-md bg-indigo-600">
+              <Column key={i} className="rounded-[6px] bg-indigo-600">
                 <Button
                   href={`?rating=${i + 1}`}
-                  className="flex w-full items-center justify-center p-3 text-center font-semibold text-base text-white leading-none"
+                  className="flex box-border w-full items-center justify-center p-[12px] m-0 text-center font-semibold text-[16px] text-white leading-none"
                 >
                   {i + 1}
                 </Button>
@@ -61,7 +61,7 @@ export const component = (
         </Section>
         <Section>
           <Hr />
-          <Text className="mt-[30px] text-center font-medium text-gray-500 text-xs">
+          <Text className="mt-[30px] text-center font-medium text-gray-500 text-[12px] leading-[16px]">
             Customer Experience Research Team
           </Text>
         </Section>
