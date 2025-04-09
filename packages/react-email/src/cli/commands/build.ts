087300b8fc03569def14501ae53d884e5cfb490f
gabriel miranda <gabrielmfern@outlook.com>
2025-04-09 09:54:31 -0300

    fix(react-email): Use of `--silent` for deno instead of `--quiet`

--- packages/react-email/src/cli/commands/build.ts
+++ packages/react-email/src/cli/commands/build.ts
@@ -194,7 +194,11 @@ const npmInstall = async (
   return new Promise<void>((resolve, reject) => {
     const childProc = spawn(
       packageManager,
-      ['install', '--silent', '--include=dev'],
+      [
+        'install',
+        packageManager === 'deno' ? '--quiet' : '--silent',
+        '--include=dev',
+      ],
       {
         cwd: builtPreviewAppPath,
         shell: true,
