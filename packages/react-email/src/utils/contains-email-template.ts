2cfe46855fd0328be3f48b945767d4b5317e2721
Gabriel Miranda <gabrielmfern@outlook.com>
2025-04-14 16:41:44 -0300

    fix(react-email): Dependents of dependents of email templates not causing hot reloads  (#2083)

---
+++ packages/react-email/src/utils/contains-email-template.ts
@@ -0,0 +1,23 @@
+import type { EmailsDirectory } from './get-emails-directory-metadata';
+
+export const containsEmailTemplate = (
+  relativeEmailPath: string,
+  directory: EmailsDirectory,
+) => {
+  const remainingSegments = relativeEmailPath
+    .replace(directory.relativePath, '')
+    .split('/')
+    .filter(Boolean);
+  if (remainingSegments.length === 1) {
+    const emailFilename = remainingSegments[0]!;
+    return directory.emailFilenames.includes(emailFilename);
+  }
+  const subDirectory = directory.subDirectories.find(
+    (sub) => sub.relativePath === remainingSegments[0],
+  );
+  if (subDirectory === undefined) {
+    return false;
+  }
+
+  return containsEmailTemplate(relativeEmailPath, subDirectory);
+};
