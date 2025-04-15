c9533a1bfe0c0b010f60883dc86c9815421f4133
thena-seer <thena@aidkit.cloud>
2025-04-15 08:09:52 -0700

    feat(render): Separate pretty from render (#1906)
    Co-authored-by: gabriel miranda <gabrielmfern@outlook.com>

---
+++ .changeset/strong-peas-kneel.md
@@ -0,0 +1,5 @@
+---
+"@react-email/render": minor
+---
+
+Deprecate the `pretty` option for `render` in favor of standalone `pretty` function
--- packages/render/src/shared/options.ts
+++ packages/render/src/shared/options.ts
@@ -1,6 +1,11 @@
 import type { HtmlToTextOptions } from 'html-to-text';
+// biome-ignore lint/correctness/noUnusedImports: this is used in the deprecated doc
+import type { pretty } from './utils/pretty';
 
 export type Options = {
+  /**
+   * @deprecated use {@link pretty} instead
+   */
   pretty?: boolean;
 } & (
   | {
