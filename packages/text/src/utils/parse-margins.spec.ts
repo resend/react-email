659e2b95229ebbb664b2da9e72050e8d8224d58e
gabriel miranda <gabrielmfern@outlook.com>
2025-04-17 14:26:52 -0300

    lint

--- packages/text/src/utils/parse-margins.spec.ts
+++ packages/text/src/utils/parse-margins.spec.ts
@@ -1,15 +1,17 @@
-import { parseMargin } from "./parse-margins";
+import { parseMargin } from './parse-margins';
 
 describe('parseMargins()', () => {
   it('should give priority for constituent properties', () => {
-    expect(parseMargin({
-      margin: 0,
-      marginBottom: '1rem'
-    })).toEqual({
+    expect(
+      parseMargin({
+        margin: 0,
+        marginBottom: '1rem',
+      }),
+    ).toEqual({
       mt: 0,
       mr: 0,
       ml: 0,
-      mb: '1rem'
+      mb: '1rem',
     });
   });
 });
