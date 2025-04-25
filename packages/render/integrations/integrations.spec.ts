f5dc6955e3cec8bdc8b6678aaa0b651a99fa4ee5
gabriel miranda <gabrielmfern@outlook.com>
2025-04-25 08:13:09 -0300

    add toString

--- packages/render/integrations/integrations.spec.ts
+++ packages/render/integrations/integrations.spec.ts
@@ -61,10 +61,10 @@ const startWebServer = async (command: string, url: string, cwd: string) => {
   });
 
   child.stdout.on('data', (data) => {
-    console.log(data);
+    console.log(data.toString());
   });
   child.stderr.on('data', (data) => {
-    console.error(data);
+    console.error(data.toString());
   });
 
   process.on('exit', () => {
