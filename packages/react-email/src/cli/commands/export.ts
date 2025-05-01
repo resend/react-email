e42c26764ad3c6fbebc3604bc7f9c627686a9827
gabriel miranda <gabrielmfern@outlook.com>
2025-05-01 14:29:48 -0300

    chore(react-email): Remove debugging log

--- packages/react-email/src/cli/commands/export.ts
+++ packages/react-email/src/cli/commands/export.ts
@@ -130,7 +130,6 @@ export const exportTemplates = async (
         ) => Promise<string>;
         reactEmailCreateReactElement: typeof React.createElement;
       };
-      console.log(emailModule);
       const rendered = await emailModule.render(
         emailModule.reactEmailCreateReactElement(emailModule.default, {}),
         options,
