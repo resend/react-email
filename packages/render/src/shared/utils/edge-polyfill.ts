25bbe98d55f127c2c7e7f0554920b12d7d629262
Mendy Landa <landamendy@gmail.com>
2025-05-06 21:40:46 +0300

    feat(render): edge & cf worker support

---
+++ packages/render/src/shared/utils/edge-polyfill.ts
@@ -0,0 +1,37 @@
+/**
+ * MessageChannel is not supported in Edge, but it's not needed to successfully render.
+ * This polyfill is used to avoid errors when importing the package in an vercel edge and cf worker runtimes.
+ *
+ * @see https://github.com/resend/react-email/issues/1630#issuecomment-2773421899
+ *
+ * We can remove this once MessageChannel is supported on all runtimes.
+ */
+
+if (typeof MessageChannel === "undefined") {
+  class MockMessagePort {
+    onmessage: ((ev: MessageEvent) => void) | undefined;
+    onmessageerror: ((ev: MessageEvent) => void) | undefined;
+
+    close() {}
+    postMessage(_message: unknown, _transfer: Transferable[] = []) {}
+    start() {}
+    addEventListener() {}
+    removeEventListener() {}
+    dispatchEvent(_event: Event): boolean {
+      return false;
+    }
+  }
+
+  class MockMessageChannel {
+    port1: MockMessagePort;
+    port2: MockMessagePort;
+
+    constructor() {
+      this.port1 = new MockMessagePort();
+      this.port2 = new MockMessagePort();
+    }
+  }
+
+  globalThis.MessageChannel =
+    MockMessageChannel as unknown as typeof MessageChannel;
+}
