ded7ed5024f853a3bad3f8225b5a420d4b12c833
gabriel miranda <gabrielmfern@outlook.com>
2025-05-12 17:15:25 -0300

    write script that generates emails directory with 15 directories and 10 files each

---
+++ packages/preview-server/scripts/prepare-development-server.mjs
@@ -0,0 +1,72 @@
+import { existsSync, promises as fs } from 'node:fs';
+import path from 'node:path';
+import url from 'node:url';
+
+const filename = url.fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+await fs.writeFile(
+  path.resolve(dirname, '../.env.local'),
+  `EMAILS_DIR_RELATIVE_PATH=./emails
+EMAILS_DIR_ABSOLUTE_PATH=${path.resolve(dirname, '../emails')}
+USER_PROJECT_LOCATION=${path.resolve(dirname, '..')}
+NEXT_PUBLIC_IS_PREVIEW_DEVELOPMENT=true`,
+  'utf8',
+);
+
+const emptyEmail = `import {
+  Body,
+  Container,
+  Head,
+  Heading,
+  Html,
+  Preview,
+  Tailwind,
+  Text,
+} from '@react-email/components';
+
+interface EmptyEmailProps {
+  username: string;
+}
+
+export default function EmptyEmail({ username }: EmptyEmailProps) {
+  return <Html>
+    <Head/>
+    <Tailwind>
+      <Body>
+        <Preview>Empty email for testing the preview server's UI</Preview>
+        <Container className="bg-[#eaeaea]">
+          <Text>Hello, {username}</Text>
+          <Heading className="text-[36px]">
+            This is an empty email meant for testing the preview server's UI.
+          </Heading>
+        </Container>
+      </Body>
+    </Tailwind>
+  </Html>;
+}
+
+EmptyEmail.PreviewProps = {
+  username: 'developer'
+} satisfies EmptyEmailProps;
+
+`;
+
+const emailsDirectoryPath = path.resolve(dirname, '../emails');
+
+const directoryCount = 15;
+const emailsPerDirectoryCount = 10;
+if (existsSync(emailsDirectoryPath)) {
+  await fs.rm(emailsDirectoryPath, {
+    recursive: true,
+  });
+}
+await fs.mkdir(emailsDirectoryPath);
+for (let i = 0; i < directoryCount; i++) {
+  const directoryPath = path.join(emailsDirectoryPath, `directory-${i}`);
+  await fs.mkdir(directoryPath);
+  for (let j = 0; j < emailsPerDirectoryCount; j++) {
+    await fs.writeFile(path.join(directoryPath, `file-${i}-${j}.tsx`), emptyEmail, 'utf8');
+  }
+}
+
