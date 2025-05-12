b1a2080dd840221737a45f8f4cd4bdcacbb944c9
gabriel miranda <gabrielmfern@outlook.com>
2025-05-12 10:21:48 -0300

    add data-skip-in-text property

--- packages/render/src/shared/plain-text-selectors.ts
+++ packages/render/src/shared/plain-text-selectors.ts
@@ -2,7 +2,7 @@ import type { SelectorDefinition } from 'html-to-text';
 
 export const plainTextSelectors: SelectorDefinition[] = [
   { selector: 'img', format: 'skip' },
-  { selector: '#__react-email-preview', format: 'skip' },
+  { selector: '[data-skip-in-text=true]', format: 'skip' },
   {
     selector: 'a',
     options: { linkBrackets: false },
