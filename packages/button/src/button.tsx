268baabff818f4989e17ad3d8f094837dcdc8a52
Gabriel Miranda <gabrielmfern@outlook.com>
2025-05-12 14:30:05 -0300

    feat(button): respect the user's order of padding properties (#2234)

--- packages/button/src/button.tsx
+++ packages/button/src/button.tsx
@@ -11,7 +11,7 @@ const maxFontWidth = 5;
  * when applied, end up being as close to `expectedWidth` as possible.
  */
 function computeFontWidthAndSpaceCount(expectedWidth: number) {
-  if (expectedWidth === 0) return [0, 0];
+  if (expectedWidth === 0) return [0, 0] as const;
 
   let smallestSpaceCount = 0;
 
@@ -27,30 +27,47 @@ function computeFontWidthAndSpaceCount(expectedWidth: number) {
     smallestSpaceCount++;
   }
 
-  return [computeRequiredFontWidth(), smallestSpaceCount];
+  return [computeRequiredFontWidth(), smallestSpaceCount] as const;
+}
+
+declare module 'react' {
+  interface CSSProperties {
+    msoPaddingAlt?: string | number | undefined;
+    msoTextRaise?: string | number | undefined;
+  }
 }
 
 export const Button = React.forwardRef<HTMLAnchorElement, ButtonProps>(
   ({ children, style, target = '_blank', ...props }, ref) => {
-    const { pt, pr, pb, pl } = parsePadding({
-      padding: style?.padding,
-      paddingLeft: style?.paddingLeft ?? style?.paddingInline,
-      paddingRight: style?.paddingRight ?? style?.paddingInline,
-      paddingTop: style?.paddingTop ?? style?.paddingBlock,
-      paddingBottom: style?.paddingBottom ?? style?.paddingBlock,
-    });
+    const { paddingTop, paddingRight, paddingBottom, paddingLeft } =
+      parsePadding(style ?? {});
 
-    const y = pt + pb;
+    const y = (paddingTop ?? 0) + (paddingBottom ?? 0);
     const textRaise = pxToPt(y);
 
-    const [plFontWidth, plSpaceCount] = computeFontWidthAndSpaceCount(pl);
-    const [prFontWidth, prSpaceCount] = computeFontWidthAndSpaceCount(pr);
+    const [plFontWidth, plSpaceCount] = computeFontWidthAndSpaceCount(
+      paddingLeft ?? 0,
+    );
+    const [prFontWidth, prSpaceCount] = computeFontWidthAndSpaceCount(
+      paddingRight ?? 0,
+    );
 
     return (
       <a
         {...props}
         ref={ref}
-        style={buttonStyle({ ...style, pt, pr, pb, pl })}
+        style={{
+          lineHeight: '100%',
+          textDecoration: 'none',
+          display: 'inline-block',
+          maxWidth: '100%',
+          msoPaddingAlt: '0px',
+          ...style,
+          paddingTop,
+          paddingRight,
+          paddingBottom,
+          paddingLeft,
+        }}
         target={target}
       >
         <span
@@ -67,7 +84,17 @@ export const Button = React.forwardRef<HTMLAnchorElement, ButtonProps>(
             )}</i><![endif]-->`,
           }}
         />
-        <span style={buttonTextStyle(pb)}>{children}</span>
+        <span
+          style={{
+            maxWidth: '100%',
+            display: 'inline-block',
+            lineHeight: '120%',
+            msoPaddingAlt: '0px',
+            msoTextRaise: pxToPt(paddingBottom),
+          }}
+        >
+          {children}
+        </span>
         <span
           dangerouslySetInnerHTML={{
             __html: `<!--[if mso]><i style="mso-font-width:${
@@ -83,34 +110,3 @@ export const Button = React.forwardRef<HTMLAnchorElement, ButtonProps>(
 );
 
 Button.displayName = 'Button';
-
-const buttonStyle = (
-  style?: React.CSSProperties & {
-    pt: number;
-    pr: number;
-    pb: number;
-    pl: number;
-  },
-) => {
-  const { pt, pr, pb, pl, ...rest } = style || {};
-
-  return {
-    lineHeight: '100%',
-    textDecoration: 'none',
-    display: 'inline-block',
-    maxWidth: '100%',
-    msoPaddingAlt: '0px',
-    ...rest,
-    padding: `${pt}px ${pr}px ${pb}px ${pl}px`,
-  };
-};
-
-const buttonTextStyle = (pb?: number) => {
-  return {
-    maxWidth: '100%',
-    display: 'inline-block',
-    lineHeight: '120%',
-    msoPaddingAlt: '0px',
-    msoTextRaise: pxToPt(pb || 0),
-  };
-};
