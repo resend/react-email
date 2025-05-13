7689206afc91cf7428749a61d3530652c1ca2f40
gabriel miranda <gabrielmfern@outlook.com>
2025-05-13 07:58:01 -0300

    actually run next dev in the mjs script to delete the local env once finished

--- packages/preview-server/scripts/start-development-server.mjs
+++ packages/preview-server/scripts/start-development-server.mjs
@@ -1,15 +1,19 @@
 import { existsSync, promises as fs } from 'node:fs';
 import path from 'node:path';
 import url from 'node:url';
+import child_process from 'node:child_process';
 
 const filename = url.fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
 
+const previewServerRoot = path.resolve(dirname, '..');
+const emailsDirectoryPath = path.join(previewServerRoot, 'emails');
+
 await fs.writeFile(
-  path.resolve(dirname, '../.env.local'),
+  path.join(previewServerRoot, '.env.local'),
   `EMAILS_DIR_RELATIVE_PATH=./emails
-EMAILS_DIR_ABSOLUTE_PATH=${path.resolve(dirname, '../emails')}
-USER_PROJECT_LOCATION=${path.resolve(dirname, '..')}
+EMAILS_DIR_ABSOLUTE_PATH=${emailsDirectoryPath}
+USER_PROJECT_LOCATION=${previewServerRoot}
 NEXT_PUBLIC_IS_PREVIEW_DEVELOPMENT=true`,
   'utf8',
 );
@@ -52,8 +56,6 @@ EmptyEmail.PreviewProps = {
 
 `;
 
-const emailsDirectoryPath = path.resolve(dirname, '../emails');
-
 const directoryCount = 15;
 const emailsPerDirectoryCount = 10;
 if (existsSync(emailsDirectoryPath)) {
@@ -66,7 +68,36 @@ for (let i = 0; i < directoryCount; i++) {
   const directoryPath = path.join(emailsDirectoryPath, `directory-${i}`);
   await fs.mkdir(directoryPath);
   for (let j = 0; j < emailsPerDirectoryCount; j++) {
-    await fs.writeFile(path.join(directoryPath, `file-${i}-${j}.tsx`), emptyEmail, 'utf8');
+    await fs.writeFile(
+      path.join(directoryPath, `file-${i}-${j}.tsx`),
+      emptyEmail,
+      'utf8',
+    );
   }
 }
 
+const webServerProcess = child_process.spawn('next', ['dev'], {
+  cwd: previewServerRoot,
+  shell: true,
+  stdio: 'inherit',
+});
+
+webServerProcess.on('exit', async () => {
+  await fs.rm(path.join(previewServerRoot, '.env.local'));
+});
+
+process.on('SIGINT', () => {
+  webServerProcess.kill('SIGINT');
+});
+process.on('SIGUSR1', () => {
+  webServerProcess.kill('SIGUSR1');
+});
+process.on('SIGUSR2', () => {
+  webServerProcess.kill('SIGUSR2');
+});
+process.on('uncaughtExceptionMonitor', () => {
+  webServerProcess.kill();
+});
+process.on('exit', () => {
+  webServerProcess.kill();
+});
