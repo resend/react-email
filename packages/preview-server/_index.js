ba5844e38582cac5e74fea176c23e4c85063b90d
Gabriel Miranda <gabrielmfern@outlook.com>
2025-05-20 12:36:54 -0300

    feat: Separate preview server from `react-email` (#2182)
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Kaylee <65376239+KayleeWilliams@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Bu Kinoshita <6929565+bukinoshita@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ packages/preview-server/_index.js
@@ -0,0 +1,4 @@
+/**
+ * this file is just a placeholder file so that import.meta.resolve and require.resolve can properly
+ * find out the path to this module. This file does not do anything nor does it need to export anything of value.
+ */
