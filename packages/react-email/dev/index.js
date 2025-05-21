8d9be6136f69f6accc623a020a012c413edbfa49
gabriel miranda <gabrielmfern@outlook.com>
2025-05-21 10:27:04 -0300

    add email-dev package for linking globally

---
+++ packages/react-email/dev/index.js
@@ -0,0 +1,40 @@
+import child_process from 'node:child_process';
+import path from 'node:path';
+import url from 'node:url';
+
+const filename = url.fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+const tsx = child_process.spawn(
+  'tsx',
+  ['../src/index.ts', ...process.argv.slice(2)],
+  {
+    shell: true,
+    cwd: dirname,
+    stdio: 'inherit',
+  },
+);
+
+process.on('uncaughtExceptionMonitor', () => {
+  tsx.kill();
+});
+
+process.on('exit', (code) => {
+  tsx.kill(code);
+});
+
+process.on('SIGINT', () => {
+  tsx.kill('SIGINT');
+});
+
+process.on('SIGTERM', () => {
+  tsx.kill('SIGTERM');
+});
+
+process.on('SIGUSR1', () => {
+  tsx.kill('SIGUSR1');
+});
+
+process.on('SIGUSR2', () => {
+  tsx.kill('SIGUSR2');
+});
