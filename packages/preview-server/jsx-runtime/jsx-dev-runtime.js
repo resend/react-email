43ed48c24ce59a18d2fecaa218aa0a9290d7a393
gabriel miranda <gabrielmfern@outlook.com>
2025-05-26 15:41:16 -0300

    lint

--- packages/preview-server/jsx-runtime/jsx-dev-runtime.js
+++ packages/preview-server/jsx-runtime/jsx-dev-runtime.js
@@ -1,11 +1,11 @@
-import * as ReactJsxDevRuntime from "react/jsx-dev-runtime";
+import * as ReactJsxDevRuntime from 'react/jsx-dev-runtime';
 
 export function jsxDEV(type, props, key, isStaticChildren, source, self) {
   const newProps = { ...props };
 
   if (source) {
-    newProps["data-source-file"] = source.fileName;
-    newProps["data-source-line"] = source.lineNumber;
+    newProps['data-source-file'] = source.fileName;
+    newProps['data-source-line'] = source.lineNumber;
   }
 
   return ReactJsxDevRuntime.jsxDEV(
--- packages/preview-server/src/app/preview/[...slug]/error-overlay.tsx
+++ packages/preview-server/src/app/preview/[...slug]/error-overlay.tsx
@@ -1,17 +1,19 @@
-"use client";
-import type { ErrorObject } from "../../../utils/types/error-object";
+'use client';
+import type { ErrorObject } from '../../../utils/types/error-object';
 
 export const ErrorOverlay = (props: { error: ErrorObject }) => {
   return (
     <>
       <div className="absolute inset-0 z-50 bg-black/80" />
-      <div className="
+      <div
+        className="
         min-h-[50vh] w-full max-w-lg sm:rounded-lg md:max-w-[568px] lg:max-w-[920px]
         absolute left-[50%] top-[50%] z-50  translate-x-[-50%] translate-y-[-50%] 
         rounded-t-sm overflow-hidden bg-white selection:text-black text-black shadow-lg duration-200
         flex flex-col
-      ">
-        <div className="bg-red-500 h-3"/>
+      "
+      >
+        <div className="bg-red-500 h-3" />
         <div className="flex flex-grow p-6 min-w-0 max-w-full flex-col space-y-1.5">
           <h2 className="flex-shrink pb-2 text-xl leading-none tracking-tight">
             <b>{props.error.name}</b>: {props.error.message}
