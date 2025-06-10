43304c9800d151536f440695f7832ba7730e3d2e
Gabriel Miranda <gabrielmfern@outlook.com>
2025-06-10 11:44:58 -0300

    Revert "fix(text): marginTop, marginBottom losing their priority defined by o…"
    This reverts commit 72138ae02ec7b70b77920f99e44e42c699854986.

--- packages/text/src/text.tsx
+++ packages/text/src/text.tsx
@@ -5,30 +5,15 @@ export type TextProps = Readonly<React.ComponentPropsWithoutRef<'p'>>;
 
 export const Text = React.forwardRef<HTMLParagraphElement, TextProps>(
   ({ style, ...props }, ref) => {
-    /**
-     * we do this clunky way of spreading these default margins because
-     * if we were to simply spread, the ordering of the margins would be lost
-     *
-     * ex:
-     * ```js
-     * { ...{ marginTop: '16px', marginBottom: '16px' }, ...{ marginTop: '24px' } }
-     * // would result in
-     * { marginTop: '24px', marginBottom: '16px' }
-     * // not the expected
-     * { marginBottom: '16px', marginTop: '24px' }
-     * ```
-     */
-    const defaultMargins: React.CSSProperties = {};
-    if (style?.marginTop === undefined) {
-      defaultMargins.marginTop = '16px';
+    let modifiedStyle: React.CSSProperties = {};
+    if (modifiedStyle.marginBottom === undefined) {
+      modifiedStyle.marginBottom = '16px';
     }
-    if (style?.marginBottom === undefined) {
-      defaultMargins.marginBottom = '16px';
+    if (modifiedStyle.marginTop === undefined) {
+      modifiedStyle.marginTop = '16px';
     }
-    const margins = computeMargins({
-      ...defaultMargins,
-      ...style,
-    });
+    modifiedStyle = { ...modifiedStyle, ...style };
+    const margins = computeMargins(modifiedStyle);
 
     return (
       <p
