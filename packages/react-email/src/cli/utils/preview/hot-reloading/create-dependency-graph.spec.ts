5575fc81b9b7df7179606769bb0a7f7c5bd13b7a
gabriel miranda <gabrielmfern@outlook.com>
2025-06-24 16:19:03 -0300

    remove outdated describe

--- packages/react-email/src/cli/utils/preview/hot-reloading/create-dependency-graph.spec.ts
+++ packages/react-email/src/cli/utils/preview/hot-reloading/create-dependency-graph.spec.ts
@@ -16,111 +16,109 @@ vi.mock('@babel/traverse', async () => {
 });
 
 describe('createDependencyGraph()', async () => {
-  describe('in the directory of this file', async () => {
-    if (existsSync(pathToTemporaryFile)) {
-      await fs.rm(pathToTemporaryFile);
-    }
+  if (existsSync(pathToTemporaryFile)) {
+    await fs.rm(pathToTemporaryFile);
+  }
 
-    const [dependencyGraph, updateDependencyGraph, { resolveDependentsOf }] =
-      await createDependencyGraph(__dirname);
+  const [dependencyGraph, updateDependencyGraph, { resolveDependentsOf }] =
+    await createDependencyGraph(__dirname);
 
-    const toAbsolute = (relativePath: string) => {
-      return path.resolve(__dirname, './create-dependency-graph-test/inner', relativePath);
-    };
+  const toAbsolute = (relativePath: string) => {
+    return path.resolve(__dirname, './create-dependency-graph-test/inner', relativePath);
+  };
 
-    it('should resolve dependents when there are circular dependencies', async () => {
-      expect(resolveDependentsOf(toAbsolute('file-a.ts'))).toEqual([
-        toAbsolute('file-b.ts'),
-        toAbsolute('general-importing-file.ts'),
-      ]);
-    });
+  it.sequential('should resolve dependents when there are circular dependencies', async () => {
+    expect(resolveDependentsOf(toAbsolute('file-a.ts'))).toEqual([
+      toAbsolute('file-b.ts'),
+      toAbsolute('general-importing-file.ts'),
+    ]);
+  });
 
-    it.sequential('should have the right initial value for the dependency graph', () => {
-      expect(dependencyGraph).toMatchSnapshot();
-    });
+  it.sequential('should have the right initial value for the dependency graph', () => {
+    expect(dependencyGraph).toMatchSnapshot();
+  });
 
-    it.sequential('should work when adding a new file', async () => {
-      await fs.writeFile(
-        pathToTemporaryFile,
-        `
+  it.sequential('should work when adding a new file', async () => {
+    await fs.writeFile(
+      pathToTemporaryFile,
+      `
 import {} from './file-a';
 import {} from './file-b';
 import {} from './general-importing-file';
 `,
-        'utf8',
-      );
-      await updateDependencyGraph('add', pathToTemporaryFile);
-      expect(dependencyGraph[pathToTemporaryFile]).toEqual({
-        path: pathToTemporaryFile,
-        dependentPaths: [],
-        dependencyPaths: [
-          toAbsolute('file-a.ts'),
-          toAbsolute('file-b.ts'),
-          toAbsolute('general-importing-file.ts'),
-        ],
-        moduleDependencies: [],
-      } satisfies DependencyGraph[number]);
-      expect(
-        dependencyGraph[toAbsolute('file-a.ts')]?.dependentPaths,
-      ).toContain(pathToTemporaryFile);
-      expect(
-        dependencyGraph[toAbsolute('file-b.ts')]?.dependentPaths,
-      ).toContain(pathToTemporaryFile);
-      expect(
-        dependencyGraph[toAbsolute('general-importing-file.ts')]?.dependentPaths,
-      ).toContain(pathToTemporaryFile);
-    });
+      'utf8',
+    );
+    await updateDependencyGraph('add', pathToTemporaryFile);
+    expect(dependencyGraph[pathToTemporaryFile]).toEqual({
+      path: pathToTemporaryFile,
+      dependentPaths: [],
+      dependencyPaths: [
+        toAbsolute('file-a.ts'),
+        toAbsolute('file-b.ts'),
+        toAbsolute('general-importing-file.ts'),
+      ],
+      moduleDependencies: [],
+    } satisfies DependencyGraph[number]);
+    expect(
+      dependencyGraph[toAbsolute('file-a.ts')]?.dependentPaths,
+    ).toContain(pathToTemporaryFile);
+    expect(
+      dependencyGraph[toAbsolute('file-b.ts')]?.dependentPaths,
+    ).toContain(pathToTemporaryFile);
+    expect(
+      dependencyGraph[toAbsolute('general-importing-file.ts')]?.dependentPaths,
+    ).toContain(pathToTemporaryFile);
+  });
 
-    it.sequential('should working when updating a file', async () => {
-      await fs.writeFile(
-        pathToTemporaryFile,
-        `
+  it.sequential('should working when updating a file', async () => {
+    await fs.writeFile(
+      pathToTemporaryFile,
+      `
 import {} from './file-a';
 import {} from './file-b';
 `,
-        'utf8',
-      );
-      await updateDependencyGraph('change', pathToTemporaryFile);
-      expect(
-        dependencyGraph[pathToTemporaryFile],
-        'changed file to have updated dependencyPaths',
-      ).toEqual({
-        path: pathToTemporaryFile,
-        dependentPaths: [],
-        dependencyPaths: [
-          toAbsolute('file-a.ts'),
-          toAbsolute('file-b.ts'),
-        ],
-        moduleDependencies: [],
-      } satisfies DependencyGraph[number]);
-      expect(
-        dependencyGraph[toAbsolute('file-a.ts')]?.dependentPaths,
-      ).toContain(pathToTemporaryFile);
-      expect(
-        dependencyGraph[toAbsolute('file-b.ts')]?.dependentPaths,
-      ).toContain(pathToTemporaryFile);
-      expect(
-        dependencyGraph[toAbsolute('general-importing-file.ts')]?.dependentPaths,
-        'when removing dependency on a file, the dependency should have its dependents updated to not have the testing file again',
-      ).not.toContain(pathToTemporaryFile);
-    });
+      'utf8',
+    );
+    await updateDependencyGraph('change', pathToTemporaryFile);
+    expect(
+      dependencyGraph[pathToTemporaryFile],
+      'changed file to have updated dependencyPaths',
+    ).toEqual({
+      path: pathToTemporaryFile,
+      dependentPaths: [],
+      dependencyPaths: [
+        toAbsolute('file-a.ts'),
+        toAbsolute('file-b.ts'),
+      ],
+      moduleDependencies: [],
+    } satisfies DependencyGraph[number]);
+    expect(
+      dependencyGraph[toAbsolute('file-a.ts')]?.dependentPaths,
+    ).toContain(pathToTemporaryFile);
+    expect(
+      dependencyGraph[toAbsolute('file-b.ts')]?.dependentPaths,
+    ).toContain(pathToTemporaryFile);
+    expect(
+      dependencyGraph[toAbsolute('general-importing-file.ts')]?.dependentPaths,
+      'when removing dependency on a file, the dependency should have its dependents updated to not have the testing file again',
+    ).not.toContain(pathToTemporaryFile);
+  });
 
-    it.sequential('should work when unlinking a file', async () => {
-      await fs.rm(pathToTemporaryFile);
-      await updateDependencyGraph('unlink', pathToTemporaryFile);
-      expect(dependencyGraph[pathToTemporaryFile]).toBeUndefined();
-      expect(
-        dependencyGraph[toAbsolute('file-a.ts')]?.dependentPaths,
-        "should remove itself from dependents once it's unlinked",
-      ).not.toContain(pathToTemporaryFile);
-      expect(
-        dependencyGraph[toAbsolute('file-b.ts')]?.dependentPaths,
-        "should remove itself from dependents once it's unlinked",
-      ).not.toContain(pathToTemporaryFile);
-      expect(
-        dependencyGraph[toAbsolute('general-importing-file.ts')]?.dependentPaths,
-        "should remove itself from dependents once it's unlinked",
-      ).not.toContain(pathToTemporaryFile);
-    });
+  it.sequential('should work when unlinking a file', async () => {
+    await fs.rm(pathToTemporaryFile);
+    await updateDependencyGraph('unlink', pathToTemporaryFile);
+    expect(dependencyGraph[pathToTemporaryFile]).toBeUndefined();
+    expect(
+      dependencyGraph[toAbsolute('file-a.ts')]?.dependentPaths,
+      "should remove itself from dependents once it's unlinked",
+    ).not.toContain(pathToTemporaryFile);
+    expect(
+      dependencyGraph[toAbsolute('file-b.ts')]?.dependentPaths,
+      "should remove itself from dependents once it's unlinked",
+    ).not.toContain(pathToTemporaryFile);
+    expect(
+      dependencyGraph[toAbsolute('general-importing-file.ts')]?.dependentPaths,
+      "should remove itself from dependents once it's unlinked",
+    ).not.toContain(pathToTemporaryFile);
   });
 });
