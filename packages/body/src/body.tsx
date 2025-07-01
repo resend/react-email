ef6d9ff91fb8784e76776a3324bccb74f572aac4
gabriel miranda <gabrielmfern@outlook.com>
2025-07-01 13:13:28 -0300

    fix(body): Yahoo and AOL not keeping background colors for body

--- packages/body/src/body.tsx
+++ packages/body/src/body.tsx
@@ -3,13 +3,30 @@ import * as React from 'react';
 export type BodyProps = Readonly<React.HtmlHTMLAttributes<HTMLBodyElement>>;
 
 export const Body = React.forwardRef<HTMLBodyElement, BodyProps>(
-  ({ children, style, ...props }, ref) => {
-    return (
-      <body {...props} ref={ref} style={style}>
-        {children}
-      </body>
-    );
-  },
+  ({ children, style, ...props }, ref) => (
+    <body {...props} ref={ref}>
+      <table
+        style={{ width: '100%', height: '100%' }}
+        border={0}
+        cellPadding="0"
+        cellSpacing="0"
+        role="presentation"
+        align="center"
+      >
+        <tbody>
+          <tr>
+            {/* 
+              Yahoo and AOL simply do not keep the styles applied to the `body`, 
+              so we need to apply it to the body and to a table cell inside. 
+
+              See https://github.com/resend/react-email/issues/662.
+            */}
+            <td style={style}>{children}</td>
+          </tr>
+        </tbody>
+      </table>
+    </body>
+  ),
 );
 
 Body.displayName = 'Body';
