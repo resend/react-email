ff1e9fc66c6e80ccd4f2d9abca24ac534fbbd1f9
gabriel miranda <gabrielmfern@outlook.com>
2025-07-11 13:52:57 -0300

    add tests to ensure it works for both inlined configs, as well as imported ones

---
+++ packages/preview-server/src/utils/caniemail/tailwind/get-tailwind-config.spec.ts
@@ -0,0 +1,44 @@
+import { promises as fs } from 'node:fs';
+import path from 'node:path';
+import { parse } from '@babel/parser';
+import { pixelBasedPreset } from '@react-email/components';
+import { getTailwindConfig } from './get-tailwind-config';
+
+describe('getTailwindConfig()', () => {
+  it("should work on the demo's Vercel Invite template", async () => {
+    const sourcePath = path.resolve(
+      __dirname,
+      '../../../../../../apps/demo/emails/notifications/vercel-invite-user.tsx',
+    );
+    const sourceCode = await fs.readFile(sourcePath, 'utf8');
+    const ast = parse(sourceCode, {
+      strictMode: false,
+      errorRecovery: true,
+      sourceType: 'unambiguous',
+      plugins: ['jsx', 'typescript', 'decorators'],
+    });
+
+    expect(await getTailwindConfig(sourceCode, ast, sourcePath)).toEqual({
+      presets: [pixelBasedPreset],
+    });
+  });
+
+  it('should work with email templates that import the tailwind config', async () => {
+    const sourcePath = path.resolve(
+      __dirname,
+      './tests/dummy-email-template.tsx',
+    );
+    const sourceCode = await fs.readFile(sourcePath, 'utf8');
+    const ast = parse(sourceCode, {
+      strictMode: false,
+      errorRecovery: true,
+      sourceType: 'unambiguous',
+      plugins: ['jsx', 'typescript', 'decorators'],
+    });
+
+    expect(await getTailwindConfig(sourceCode, ast, sourcePath)).toEqual({
+      theme: {},
+      presets: [pixelBasedPreset],
+    });
+  });
+});
