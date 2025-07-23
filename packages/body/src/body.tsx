fb641b905ebe342f1534a3ea1a8628761737072a
Gabriel Miranda <gabrielmfern@outlook.com>
2025-07-23 16:27:59 -0300

    only inherit properties that can be duplicated without negative impact

--- packages/body/src/body.tsx
+++ packages/body/src/body.tsx
@@ -1,40 +1,47 @@
 import * as React from 'react';
 
-export type BodyProps = Readonly<
-  React.HtmlHTMLAttributes<HTMLTableCellElement>
->;
+export type BodyProps = Readonly<React.HtmlHTMLAttributes<HTMLBodyElement>>;
 
 export const Body = React.forwardRef<HTMLBodyElement, BodyProps>(
-  ({ children, ...props }, ref) => (
-    <body ref={ref} style={{ margin: 0, padding: 0 }}>
-      <table
-        border={0}
-        width="100%"
-        style={{
-          width: '100vw',
-          height: '100vh',
-        }}
-        cellPadding="0"
-        cellSpacing="0"
-        role="presentation"
-        align="center"
-      >
-        <tbody>
-          <tr>
-            {/*
-              Yahoo and AOL simply do not keep the styles applied to the `body`,
-              so we need to apply it to a table cell inside.
+  ({ children, style, ...props }, ref) => {
+    const innerCellStyle: Record<string, string | number | undefined> = {};
+    if (style) {
+      for (const key of Object.keys(style)) {
+        if (
+          key === 'backgroundColor' ||
+          key === 'background' ||
+          key === 'color' ||
+          key === 'fontFamily'
+        ) {
+          innerCellStyle[key] = style[key];
+        }
+      }
+    }
+    return (
+      <body {...props} style={style} ref={ref}>
+        <table
+          border={0}
+          width="100%"
+          cellPadding="0"
+          cellSpacing="0"
+          role="presentation"
+          align="center"
+        >
+          <tbody>
+            <tr>
+              {/*
+                Yahoo and AOL don't keep the background color of the body, 
+                so we need to apply it to an inner cell.
 
-              See https://github.com/resend/react-email/issues/662.
-            */}
-            <td {...props} style={{ textAlign: 'left', verticalAlign: 'top' }}>
-              {children}
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </body>
-  ),
+                See https://github.com/resend/react-email/issues/662.
+              */}
+              <td style={innerCellStyle}>{children}</td>
+            </tr>
+          </tbody>
+        </table>
+      </body>
+    );
+  },
 );
 
 Body.displayName = 'Body';
