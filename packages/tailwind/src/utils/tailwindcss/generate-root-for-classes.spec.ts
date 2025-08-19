f76feb30cd1e64e4f556aab88a1c60ef0e7dcf29
Gabriel Miranda <gabrielmfern@outlook.com>
2025-08-19 11:33:00 -0300

    new standalone generateRootForClasses with tailwindcss  v4's compile function

---
+++ packages/tailwind/src/utils/tailwindcss/generate-root-for-classes.spec.ts
@@ -0,0 +1,10 @@
+import { generateRootForClasses } from './generate-root-for-classes';
+
+test("tailwind's generateRootForClasses()", async () => {
+  expect(
+    (await generateRootForClasses(
+      ['text-red-500', 'sm:bg-blue-300', 'bg-slate-900'],
+      {},
+    )).toString(),
+  ).toMatchSnapshot();
+});
---
+++ packages/tailwind/src/utils/tailwindcss/generate-root-for-classes.ts
@@ -0,0 +1,72 @@
+import { parse } from 'postcss';
+import { type Config, compile } from 'tailwindcss';
+import { resolveAllCSSVariables } from '../css/resolve-all-css-variables';
+import indexCss from './tailwind-stylesheets/index';
+import preflightCss from './tailwind-stylesheets/preflight';
+import themeCss from './tailwind-stylesheets/theme';
+import utilitiesCss from './tailwind-stylesheets/utilities';
+
+const baseCss = `@import "tailwindcss";`;
+
+export async function generateRootForClasses(
+  classes: string[],
+  config: Config,
+) {
+  const compiler = await compile(baseCss, {
+    async loadModule(id, base, resourceHint) {
+      if (resourceHint === 'config') {
+        return {
+          path: id,
+          base: base,
+          module: config,
+        };
+      }
+
+      throw new Error(
+        `NO-OP: should we implement support for ${resourceHint}?`,
+      );
+    },
+    async loadStylesheet(id, base) {
+      if (id === 'tailwindcss') {
+        return {
+          base,
+          path: 'tailwindcss/index.css',
+          content: indexCss,
+        };
+      }
+
+      if (id === 'tailwindcss/preflight') {
+        return {
+          base,
+          path: 'tailwindcss/preflight.css',
+          content: preflightCss,
+        };
+      }
+
+      if (id === 'tailwindcss/theme') {
+        return {
+          base,
+          path: 'tailwindcss/theme.css',
+          content: themeCss,
+        };
+      }
+
+      if (id === 'tailwindcss/utilities') {
+        return {
+          base,
+          path: 'tailwindcss/utilities.css',
+          content: utilitiesCss,
+        };
+      }
+
+      throw new Error(
+        'stylesheet not supported, you can only import tailwindcss',
+      );
+    },
+  });
+  const css = compiler.build(classes);
+  const root = parse(css);
+  resolveAllCSSVariables(root);
+
+  return root;
+}
