de69ad170888039691a3a3cd383efcde1449c40d
Gabriel Miranda <gabrielmfern@outlook.com>
2025-08-19 14:37:26 -0300

    fix exports

--- packages/render/src/edge/import-react-dom.tsx
+++ packages/render/src/edge/import-react-dom.tsx
@@ -2,7 +2,7 @@ export const importReactDOM = async () => {
   try {
     return await import('react-dom/server.edge');
   } catch (_exception) {
-    // This ensures that we still have compatibiltity with React 18, which doesn't ahve the `edge` conditional export.
+    // This ensures that we still have compatibiltity with React 18, which doesn't have the `edge` conditional export.
     return await import('react-dom/server');
   }
 };
