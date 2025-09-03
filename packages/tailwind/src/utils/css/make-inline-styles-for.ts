0b898eb70de88b832c51deaddded32322dbdba86
Gabriel Miranda <gabrielmfern@outlook.com>
2025-09-03 11:06:13 -0300

    use css-tree for makeInlineStylesFor

--- packages/tailwind/src/utils/css/make-inline-styles-for.ts
+++ packages/tailwind/src/utils/css/make-inline-styles-for.ts
@@ -1,36 +1,43 @@
-import type { Root } from 'postcss';
-import selectorParser from 'postcss-selector-parser';
+import { type CssNode, generate, walk } from 'css-tree';
 import { convertCssPropertyToReactProperty } from '../compatibility/convert-css-property-to-react-property';
 import { unescapeClass } from '../compatibility/unescape-class';
 import { isRuleInlinable } from './is-rule-inlinable';
 
 export function makeInlineStylesFor(
   className: string,
-  tailwindStylesRoot: Root,
+  tailwindStyles: CssNode,
 ) {
   const classes = className.split(' ');
 
   let residualClasses = [...classes];
   const styles: Record<string, string> = {};
 
-  tailwindStylesRoot.walkRules((rule) => {
-    if (isRuleInlinable(rule)) {
-      const classesOnSelector: string[] = [];
-      selectorParser((selector) => {
-        selector.walkClasses((v) => {
-          classesOnSelector.push(unescapeClass(v.value));
+  walk(tailwindStyles, {
+    visit: 'Rule',
+    enter(rule) {
+      if (isRuleInlinable(rule)) {
+        const classesOnSelector: string[] = [];
+        walk(rule.prelude, {
+          visit: 'ClassSelector',
+          enter(classNode) {
+            classesOnSelector.push(unescapeClass(classNode.name));
+          },
         });
-      }).processSync(rule.selector);
 
-      residualClasses = residualClasses.filter((singleClass) => {
-        return !classesOnSelector.includes(singleClass);
-      });
+        residualClasses = residualClasses.filter((singleClass) => {
+          return !classesOnSelector.includes(singleClass);
+        });
 
-      rule.walkDecls((declaration) => {
-        styles[convertCssPropertyToReactProperty(declaration.prop)] =
-          declaration.value + (declaration.important ? '!important' : '');
-      });
-    }
+        walk(rule, {
+          visit: 'Declaration',
+          enter(declaration) {
+            styles[convertCssPropertyToReactProperty(declaration.property)] =
+              generate(declaration.value) +
+              (declaration.important ? '!important' : '');
+          },
+        });
+      }
+    },
   });
 
   return {
