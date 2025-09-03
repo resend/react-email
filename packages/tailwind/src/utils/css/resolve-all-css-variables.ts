82c724c1769684ad2a24dc097e7358714c69ee7c
Gabriel Miranda <gabrielmfern@outlook.com>
2025-09-03 18:14:41 -0300

    wip: resolving css variables with css tree 🤩

--- packages/tailwind/src/utils/css/resolve-all-css-variables.ts
+++ packages/tailwind/src/utils/css/resolve-all-css-variables.ts
@@ -1,5 +1,11 @@
-import { AtRule, type Declaration, type Root, Rule } from 'postcss';
-import { removeIfEmptyRecursively } from './remove-if-empty-recursively';
+import {
+  type CssNode,
+  type Declaration,
+  generate,
+  walk,
+  parse,
+  lexer,
+} from 'css-tree';
 
 interface VariableUse {
   declaration: Declaration;
@@ -12,6 +18,8 @@ interface VariableDefinition {
   declaration: Declaration;
   variableName: string;
   definition: string;
+
+  remove(): void;
 }
 
 const doSelectorsIntersect = (first: string, second: string): boolean => {
@@ -30,87 +38,114 @@ const doSelectorsIntersect = (first: string, second: string): boolean => {
   return false;
 };
 
-export const resolveAllCSSVariables = (root: Root) => {
+export const resolveAllCSSVariables = (node: CssNode) => {
   const variableDefinitions = new Set<VariableDefinition>();
   const variableUses = new Set<VariableUse>();
 
-  root.walkDecls((declaration) => {
-    if (/--[^\s]+/.test(declaration.prop)) {
-      variableDefinitions.add({
-        declaration,
-        variableName: `${declaration.prop}`,
-        definition: declaration.value,
-      });
-    } else {
-      for (const [match, variableName, fallback] of declaration.value.matchAll(
-        /var\((--[^\s)]+)(?:,\s*([^)]+))?\)/gm,
-      )) {
-        variableUses.add({
+  walk(node, {
+    visit: 'Declaration',
+    enter(declaration, item, list) {
+      if (/--[\S]+/.test(declaration.property)) {
+        variableDefinitions.add({
           declaration,
-          fallback,
-          variableName,
-          raw: match,
+          variableName: `${declaration.property}`,
+          definition: generate(declaration.value),
+          remove() {
+            list.remove(item);
+          },
         });
+      } else {
+        function parseVariableUsesFrom(node: CssNode) {
+          walk(node, {
+            visit: 'Function',
+            enter(funcNode) {
+              if (funcNode.name === 'var') {
+                const children = funcNode.children.toArray();
+                const name = generate(children[0]);
+                const fallback =
+                  // The second argument should be an "," Operator Node,
+                  // such that the actual fallback is only in the third argument
+                  children[2] ? generate(children[2]) : undefined;
+
+                variableUses.add({
+                  declaration,
+                  fallback,
+                  variableName: name,
+                  raw: generate(node),
+                });
+
+                if (fallback?.includes('var(')) {
+                  const parsedFallback = parse(fallback, {
+                    context: 'value',
+                  });
+
+                  parseVariableUsesFrom(parsedFallback);
+                }
+              }
+            },
+          });
+        }
+
+        parseVariableUsesFrom(declaration.value);
       }
-    }
+    },
   });
 
-  for (const use of variableUses) {
-    let hasReplaced = false;
-    for (const definition of variableDefinitions) {
-      if (use.variableName !== definition.variableName) {
-        continue;
-      }
+  console.log(variableUses);
 
-      if (
-        use.declaration.parent instanceof AtRule &&
-        use.declaration.parent.parent instanceof Rule &&
-        definition.declaration.parent instanceof Rule &&
-        doSelectorsIntersect(
-          use.declaration.parent.parent.selector,
-          definition.declaration.parent.selector,
-        )
-      ) {
-        use.declaration.value = use.declaration.value.replaceAll(
-          use.raw,
-          definition.definition,
-        );
-        hasReplaced = true;
-        break;
-      }
+  /// YOU ARE HERE: just finished implementing the code to find the variable uses with css tree that is much more robust that using regex, now you were about to do the proper handling for resolving the variable uses
 
-      if (
-        use.declaration.parent instanceof Rule &&
-        definition.declaration.parent instanceof Rule &&
-        doSelectorsIntersect(
-          use.declaration.parent.selector,
-          definition.declaration.parent.selector,
-        )
-      ) {
-        use.declaration.value = use.declaration.value.replaceAll(
-          use.raw,
-          definition.definition,
-        );
-        hasReplaced = true;
-        break;
-      }
-    }
-
-    if (!hasReplaced && use.fallback) {
-      use.declaration.value = use.declaration.value.replaceAll(
-        use.raw,
-        use.fallback,
-      );
-    }
-  }
+  // for (const use of variableUses) {
+  //   for (const definition of variableDefinitions) {
+  //     if (use.variableName !== definition.variableName) {
+  //       continue;
+  //     }
+  //
+  //     if (
+  //       use.declaration.parent instanceof AtRule &&
+  //       use.declaration.parent.parent instanceof Rule &&
+  //       definition.declaration.parent instanceof Rule &&
+  //       doSelectorsIntersect(
+  //         use.declaration.parent.parent.selector,
+  //         definition.declaration.parent.selector,
+  //       )
+  //     ) {
+  //       use.declaration.value = use.declaration.value.replaceAll(
+  //         use.raw,
+  //         definition.definition,
+  //       );
+  //       hasReplaced = true;
+  //       break;
+  //     }
+  //
+  //     if (
+  //       use.declaration.parent instanceof Rule &&
+  //       definition.declaration.parent instanceof Rule &&
+  //       doSelectorsIntersect(
+  //         use.declaration.parent.selector,
+  //         definition.declaration.parent.selector,
+  //       )
+  //     ) {
+  //       use.declaration.value = use.declaration.value.replaceAll(
+  //         use.raw,
+  //         definition.definition,
+  //       );
+  //       hasReplaced = true;
+  //       break;
+  //     }
+  //   }
+  //
+  //   if (!hasReplaced && use.fallback) {
+  //     use.declaration.value = use.declaration.value.replaceAll(
+  //       use.raw,
+  //       use.fallback,
+  //     );
+  //   }
+  // }
 
   for (const definition of variableDefinitions) {
-    const parent = definition.declaration.parent;
-    definition.declaration.remove();
-    if (parent) {
-      removeIfEmptyRecursively(parent);
-    }
+    definition.remove();
   }
 
-  return root;
+  return node;
 };
