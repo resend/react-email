291f6c8713903226aa8abd010e782094a8bc1df5
Gabriel Miranda <gabrielmfern@outlook.com>
2025-09-04 12:10:33 -0300

    update test

--- packages/tailwind/src/utils/tailwindcss/generate-root-for-classes.spec.ts
+++ packages/tailwind/src/utils/tailwindcss/generate-root-for-classes.spec.ts
@@ -1,12 +1,13 @@
+import { generate } from 'css-tree';
 import { generateRootForClasses } from './generate-root-for-classes';
 
 test("tailwind's generateRootForClasses()", async () => {
   expect(
-    (
+    generate(
       await generateRootForClasses(
         ['text-red-500', 'sm:bg-blue-300', 'bg-slate-900'],
         {},
-      )
-    ).toString(),
+      ),
+    ),
   ).toMatchSnapshot();
 });
