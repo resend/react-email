ef8202cce81f7fb0c1446883bf6cb0494999ac48
Gabriel Miranda <gabrielmfern@outlook.com>
2025-09-05 09:29:54 -0300

    fix(render): bundling issues for esbuild when using react-dom@18

--- packages/render/src/edge/import-react-dom.tsx
+++ packages/render/src/edge/import-react-dom.tsx
@@ -1,8 +1,11 @@
-export const importReactDOM = async () => {
-  try {
-    return await import('react-dom/server.edge');
-  } catch (_exception) {
-    // This ensures that we still have compatibiltity with React 18, which doesn't have the `edge` conditional export.
-    return await import('react-dom/server');
-  }
+export const importReactDOM = () => {
+  // We don't use async here because tsup converts it to a generator syntax 
+  // that esbuild doesn't understand as dealing with the import failing during
+  // bundling: https://github.com/evanw/esbuild/issues/3216#issuecomment-1628913722
+  return import('react-dom/server.edge').catch(
+    () =>
+      // This ensures that we still have compatibility with React 18, 
+      // which doesn't have the `.edge` export.
+      import('react-dom/server'),
+  );
 };
