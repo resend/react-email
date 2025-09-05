29b3a652e0933e48557123dd8e50a5cb7ecd6a04
Gabriel Miranda <gabrielmfern@outlook.com>
2025-09-05 09:53:11 -0300

    lint

--- packages/render/src/edge/import-react-dom.tsx
+++ packages/render/src/edge/import-react-dom.tsx
@@ -1,10 +1,10 @@
 export const importReactDom = () => {
-  // We don't use async here because tsup converts it to a generator syntax 
+  // We don't use async here because tsup converts it to a generator syntax
   // that esbuild doesn't understand as dealing with the import failing during
   // bundling: https://github.com/evanw/esbuild/issues/3216#issuecomment-1628913722
   return import('react-dom/server.edge').catch(
     () =>
-      // This ensures that we still have compatibility with React 18, 
+      // This ensures that we still have compatibility with React 18,
       // which doesn't have the `.edge` export.
       import('react-dom/server'),
   );
