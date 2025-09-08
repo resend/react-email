b9e4a4b11193e38b11cfaa8054ed20b90a4335f5
Gabriel Miranda <gabrielmfern@outlook.com>
2025-09-08 16:42:35 -0300

    update remaining failing test snapshots, everything passes!

--- packages/tailwind/src/utils/css/make-inline-styles-for.spec.ts
+++ packages/tailwind/src/utils/css/make-inline-styles-for.spec.ts
@@ -6,8 +6,5 @@ test('makeInlineStylesFor()', async () => {
     'bg-red-500 sm:bg-blue-300 w-full md:max-w-[400px] my-custom-class';
   const tailwindStyles = await generateRootForClasses(className.split(' '), {});
 
-  expect(makeInlineStylesFor(className, tailwindStyles)).toEqual({
-    styles: { backgroundColor: 'rgb(239 68 68 / 1)', width: '100%' },
-    residualClassName: 'sm:bg-blue-300 md:max-w-[400px] my-custom-class',
-  });
+  expect(makeInlineStylesFor(className, tailwindStyles)).toMatchSnapshot();
 });
