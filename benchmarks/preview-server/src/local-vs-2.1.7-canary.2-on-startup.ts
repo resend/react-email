79fdf6aa290e1984248229929151d431f77cde45
gabriel miranda <gabrielmfern@outlook.com>
2025-09-17 17:51:11 -0300

    try avoinding race conditions of sub processes not being closed

--- benchmarks/preview-server/src/local-vs-2.1.7-canary.2-on-startup.ts
+++ benchmarks/preview-server/src/local-vs-2.1.7-canary.2-on-startup.ts
@@ -21,14 +21,22 @@ const bench = new Bench({
 
 bench
   .add('startup on local', async () => {
-    const server = await runServer(pathToLocalCliScript);
-    await fetch(`${server.url}/preview/magic-links/notion-magic-link`);
-    server.subprocess.kill();
+    try {
+      const server = await runServer(pathToLocalCliScript);
+      await fetch(`${server.url}/preview/magic-links/notion-magic-link`);
+      if (!server.subprocess.kill()) {
+        throw new Error('could not close sub process for preview server');
+      }
+    } catch (err) {
+      console.error('Error starting local server:', err);
+    }
   })
   .add('startup on 2.1.7-canary.2', async () => {
     const server = await runServer(pathToCanaryCliScript);
     await fetch(`${server.url}/preview/magic-links/notion-magic-link`);
-    server.subprocess.kill();
+    if (!server.subprocess.kill()) {
+      throw new Error('could not close sub process for preview server');
+    }
   });
 
 await bench.run();
