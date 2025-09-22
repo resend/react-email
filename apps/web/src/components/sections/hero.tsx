e08e1c3c760cead3edd26a13fc737f2e859900f5
guhrodrrigues <gumartinsr1201@gmail.com>
2025-09-21 21:50:19 -0300

    feat: add new home page

---
+++ apps/web/src/components/sections/hero.tsx
@@ -0,0 +1,58 @@
+import { ArrowRightIcon } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Button } from '@/components/button';
+import { Code } from '@/components/code';
+import { Heading } from '@/components/heading';
+import { Text } from '@/components/text';
+
+const HeroSection = () => {
+  return (
+    <>
+      <section className="relative mx-auto flex max-w-3xl flex-col justify-center h-[calc(100vh-11rem)]">
+        <Image
+          alt=""
+          className="pointer-events-none absolute inset-0 -top-40 z-[3] scale-150 select-none mix-blend-lighten"
+          fill
+          priority
+          src="/static/bg.png"
+        />
+        <div className="max-w-full text-center md:max-w-[45rem] md:mx-auto">
+          <div className="mb-8 flex items-center justify-center">
+            <Image
+              alt="React Email Logo"
+              height="120"
+              src="/brand/logo.png"
+              width="120"
+            />
+          </div>
+          <Heading
+            className="!text-white/80 relative mb-8 before:absolute before:top-0 before:left-0 before:w-full before:animate-[shine_2s_ease-in-out] before:bg-[length:200%] before:bg-shine before:bg-clip-text before:text-transparent before:content-['The_next_generation_of_writing_emails']"
+            size="10"
+          >
+            The next generation of writing emails
+          </Heading>
+          <div className="sm:px-20">
+            <Text size="5">
+              A collection of high-quality, unstyled components for creating
+              beautiful emails using React and TypeScript.
+            </Text>
+          </div>
+        </div>
+        <div className="mt-10 flex items-center justify-center gap-4">
+          <Button asChild size="4">
+            <Link href="/components">
+              Explore components
+              <ArrowRightIcon size={16} />
+            </Link>
+          </Button>
+          <Code className="md:!inline-flex hidden max-w-max">
+            npx create-email@latest
+          </Code>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default HeroSection;
---
+++ apps/web/src/components/sections/playground/index.tsx
@@ -0,0 +1,99 @@
+'use client';
+
+import * as Tabs from '@radix-ui/react-tabs';
+import classNames from 'classnames';
+import { AnimatePresence, motion } from 'framer-motion';
+import Image from 'next/image';
+import { useState } from 'react';
+import { Heading } from '@/components/heading';
+import { Text } from '@/components/text';
+import { CodePreview } from './code-preview';
+import { cssCode, tailwindCSSCode } from './utils';
+
+const tabs = [
+  {
+    label: 'Tailwind CSS',
+    value: 'tailwind',
+    code: tailwindCSSCode,
+  },
+  {
+    label: 'CSS',
+    value: 'css',
+    code: cssCode,
+  },
+];
+
+const PlaygroundSection = () => {
+  const [activeTab, setActiveTab] = useState(tabs[0].value);
+
+  return (
+    <section className="relative min-h-screen my-24 text-center flex flex-col items-center justify-center">
+      <div className="space-y-8">
+        <div className="max-w-full text-center md:max-w-[40rem] md:mx-auto space-y-6">
+          <Heading size="9" weight="medium" className="text-white/80">
+            Modern styling using your favorite tools
+          </Heading>
+          <div className="sm:px-20">
+            <Text size="5">
+              Style your emails with your favorite tools, including support for
+              inline CSS or Tailwind CSS.
+            </Text>
+          </div>
+        </div>
+        <Tabs.Root
+          className="w-full max-w-6xl space-y-16"
+          defaultValue={tabs[0].value}
+          onValueChange={setActiveTab}
+        >
+          <div className="flex justify-center">
+            <Tabs.List className="flex items-center p-1 rounded-2xl shadow-[0px_32px_64px_-16px_transparent,0px_16px_32px_-8px_transparent,0px_8px_16px_-4px_transparent,0px_4px_8px_-2px_transparent,0px_-8px_16px_-1px_transparent,0px_2px_4px_-1px_transparent,0px_0px_0px_1px_transparent,inset_0px_0px_0px_1px_rgba(255,255,255,0.1),inset_0px_1px_0px_rgb(255,255,255,0.15)]">
+              {tabs.map((tab) => (
+                <Tabs.Trigger
+                  key={tab.value}
+                  value={tab.value}
+                  className="group relative px-4 py-2"
+                >
+                  <span
+                    className={classNames(
+                      'relative z-10 text-lg',
+                      activeTab === tab.value
+                        ? 'text-slate-12'
+                        : 'text-slate-11 transition-colors group-hover:text-slate-12',
+                    )}
+                  >
+                    {tab.label}
+                  </span>
+                  <AnimatePresence mode="wait" initial={false}>
+                    {activeTab === tab.value && (
+                      <motion.div
+                        className="absolute inset-0 size-full rounded-xl bg-gradient-to-b from-zinc-800 to-zinc-950 border border-zinc-800 z-0"
+                        layoutId="activeTab"
+                        transition={{
+                          type: 'spring',
+                          duration: 0.3,
+                          bounce: 0,
+                        }}
+                      />
+                    )}
+                  </AnimatePresence>
+                </Tabs.Trigger>
+              ))}
+            </Tabs.List>
+          </div>
+
+          <CodePreview tabs={tabs} activeTab={activeTab} />
+        </Tabs.Root>
+      </div>
+
+      <Image
+        alt=""
+        className="pointer-events-none absolute inset-0 -top-40 z-[3] select-none mix-blend-lighten"
+        fill
+        priority
+        src="/static/bg.png"
+      />
+    </section>
+  );
+};
+
+export default PlaygroundSection;
