66b8c65f0c6c19b066d0b9242ecf1a1f55247da9
gabriel miranda <gabrielmfern@outlook.com>
2025-09-23 10:53:55 -0300

    fix broken css tests for calc expressions

--- packages/tailwind/src/utils/css/resolve-calc-expressions.spec.ts
+++ packages/tailwind/src/utils/css/resolve-calc-expressions.spec.ts
@@ -5,7 +5,7 @@ describe('resolveCalcExpressions()', async () => {
   it('should resolve spacing calc expressions from tailwind v4', () => {
     const root = parse(`
 .px-3{padding-inline:calc(0.25rem*3)}
-.py-2{padding-block:calc(0.25rem*2)}}
+.py-2{padding-block:calc(0.25rem*2)}
   `);
     resolveCalcExpressions(root);
     expect(generate(root)).toMatchSnapshot();
@@ -13,8 +13,8 @@ describe('resolveCalcExpressions()', async () => {
 
   it('should not do anything to complex calc expressions', () => {
     const root = parse(`
-.px-3{padding-inline:calc(0.25rem*(3 + 1px)}
-.py-2{padding-block:calc(0.25rem*2 + 1px)}}
+.px-3{padding-inline:calc(0.25rem*(3 + 1px))}
+.py-2{padding-block:calc(0.25rem*(2 + 1px))}
   `);
     resolveCalcExpressions(root);
     expect(generate(root)).toMatchSnapshot();
