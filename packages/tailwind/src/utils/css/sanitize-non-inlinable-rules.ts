9603452bc479c34492b11366d5c64f4d1d17143a
gabriel miranda <gabrielmfern@outlook.com>
2025-09-23 11:13:31 -0300

    improve grammar

--- packages/tailwind/src/utils/css/sanitize-non-inlinable-rules.ts
+++ packages/tailwind/src/utils/css/sanitize-non-inlinable-rules.ts
@@ -4,12 +4,12 @@ import { isRuleInlinable } from './is-rule-inlinable';
 
 /**
  * This function goes through a few steps to ensure the best email client support and
- * to ensure that the media queries and pseudo classes are going to applied correctly alongside
+ * to ensure that media queries and pseudo classes are applied correctly alongside
  * the inline styles.
  *
- * What it does is:
- * 1. Converts all declarations in all rules into being important ones
- * 2. Sanitizes all the selectors of all non-inlinable rules
+ * What it does:
+ * 1. Converts all declarations in all rules into important ones
+ * 2. Sanitizes class selectors of all non-inlinable rules
  */
 export function sanitizeNonInlinableRules(node: CssNode) {
   walk(node, {
