c1631a85e920c5ce192388dd2e974ee2d74bdfa2
gabriel miranda <gabrielmfern@outlook.com>
2025-10-01 12:59:33 -0300

    change some things

--- apps/demo/emails/magic-links/notion-magic-link.tsx
+++ apps/demo/emails/magic-links/notion-magic-link.tsx
@@ -29,27 +29,28 @@ export const NotionMagicLinkEmail = ({
       <Body className="bg-white">
         <Preview>Log in with this magic link</Preview>
         <Container className="px-3 mx-auto">
-          <Heading className="font-sans text-2xl font-bold text-[#333] my-10 p-0">
+          <Heading className="text-[#333] font-sans text-2xl my-10 mx-0 p-0">
             Login
           </Heading>
           <Link
             href="https://notion.so"
             target="_blank"
-            className="text-[#2754C5] font-sans text-sm underline block mb-4"
+            className="text-[#2754C5] font-sans text-sm underline mb-4 block"
           >
             Click here to log in with this magic link
           </Link>
           <Text className="text-[#333] font-sans text-sm my-6 mb-3.5">
             Or, copy and paste this temporary login code:
           </Text>
-          <code className="inline-block py-4 px-[4.5%] w-[90.5%] bg-[#f4f4f4] rounded-md border border-[#eee] text-[#333]">
+          <code className="inline-block py-4 px-[4.5%] w-9/10">
             {loginCode}
           </code>
-
-          <Text className="text-[#ababab] mt-3.5 mb-4 font-sans text-sm">
+          <Text className="text-[#ababab] font-sans text-sm mt-3.5 mb-4">
             If you didn&apos;t try to login, you can safely ignore this email.
           </Text>
-          <Text className="text-[#ababab] mt-3 mb-[38px] font-sans text-sm">
+          <Text
+            className="text-[#ababab] font-sans text-sm mt-3.5 mb-9.5"
+          >
             Hint: You can set a permanent password in Settings & members → My
             account.
           </Text>
@@ -59,11 +60,11 @@ export const NotionMagicLinkEmail = ({
             height="32"
             alt="Notion's Logo"
           />
-          <Text className="text-[#898989] mt-3 mb-[22px] font-sans text-sm">
+          <Text className="text-[#898989] font-sans text-xs mt-3 mb-6">
             <Link
-              className="text-[#898989] underline font-sans text-sm"
               href="https://notion.so"
               target="_blank"
+              className="text-[#898989] font-sans text-sm underline"
             >
               Notion.so
             </Link>
@@ -82,3 +83,4 @@ NotionMagicLinkEmail.PreviewProps = {
 } as NotionMagicLinkEmailProps;
 
 export default NotionMagicLinkEmail;
+
