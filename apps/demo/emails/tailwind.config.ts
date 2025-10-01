12db6cf986e40932aaed53e733c97a8e04dc3c61
gabriel miranda <gabrielmfern@outlook.com>
2025-10-01 17:17:54 -0300

    use tailwind v4

--- apps/demo/emails/tailwind.config.ts
+++ apps/demo/emails/tailwind.config.ts
@@ -5,6 +5,8 @@ export default {
   theme: {
     fontFamily: {
       amazon: ['Ember', 'Helvetica', 'Arial', 'sans-serif'],
+      linear:
+        '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif',
     },
   },
 } satisfies TailwindConfig;
