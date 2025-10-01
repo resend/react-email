918ede1606a25a59d9ebf52c521f8170546d9aad
gabriel miranda <gabrielmfern@outlook.com>
2025-10-01 15:31:10 -0300

    convert aws-verify-email.tsx

--- apps/demo/emails/magic-links/aws-verify-email.tsx
+++ apps/demo/emails/magic-links/aws-verify-email.tsx
@@ -9,9 +9,10 @@ import {
   Link,
   Preview,
   Section,
+  Tailwind,
   Text,
 } from '@react-email/components';
-import { Tailwind } from '@react-email/tailwind';
+import tailwindConfig from '../tailwind.config';
 
 interface AWSVerifyEmailProps {
   verificationCode?: string;
@@ -27,59 +28,76 @@ export default function AWSVerifyEmail({
   return (
     <Html>
       <Head />
-      <Body style={main}>
-        <Preview>AWS Email Verification</Preview>
-        <Container style={container}>
-          <Section style={coverSection}>
-            <Section style={imageSection}>
-              <Img
-                src={`${baseUrl}/static/aws-logo.png`}
-                width="75"
-                height="45"
-                alt="AWS's Logo"
-              />
-            </Section>
-            <Section style={upperSection}>
-              <Heading style={h1}>Verify your email address</Heading>
-              <Text style={mainText}>
-                Thanks for starting the new AWS account creation process. We
-                want to make sure it's really you. Please enter the following
-                verification code when prompted. If you don&apos;t want to
-                create an account, you can ignore this message.
-              </Text>
-              <Section style={verificationSection}>
-                <Text style={verifyText}>Verification code</Text>
+      <Tailwind config={tailwindConfig}>
+        <Body className="bg-white font-aws text-[#212121]">
+          <Preview>AWS Email Verification</Preview>
+          <Container className="p-5 mx-auto bg-[#eee]">
+            <Section className="bg-white">
+              <Section className="bg-[#252f3d] flex p-5 items-center justify-center">
+                <Img
+                  src={`${baseUrl}/static/aws-logo.png`}
+                  width="75"
+                  height="45"
+                  alt="AWS's Logo"
+                />
+              </Section>
+              <Section className="py-6 p-9">
+                <Heading className="text-[#333] text-xl font-bold mb-6">
+                  Verify your email address
+                </Heading>
+                <Text className="mb-3.5 text-[#333] text-sm my-6 mx-0">
+                  Thanks for starting the new AWS account creation process. We
+                  want to make sure it's really you. Please enter the following
+                  verification code when prompted. If you don&apos;t want to
+                  create an account, you can ignore this message.
+                </Text>
+                <Section className="flex items-center justify-center">
+                  <Text className="text-[#333] m-0 font-bold text-center text-sm">
+                    Verification code
+                  </Text>
 
-                <Text style={codeText}>{verificationCode}</Text>
-                <Text style={validityText}>
-                  (This code is valid for 10 minutes)
+                  <Text className="text-[#333] text-4xl my-2.5 mx-0 font-bold text-center">
+                    {verificationCode}
+                  </Text>
+                  <Text className="text-[#333] text-sm m-0 text-center">
+                    (This code is valid for 10 minutes)
+                  </Text>
+                </Section>
+              </Section>
+              <Hr />
+              <Section className="py-6 px-9">
+                <Text className="text-[#333] text-sm m-0">
+                  Amazon Web Services will never email you and ask you to
+                  disclose or verify your password, credit card, or banking
+                  account number.
                 </Text>
               </Section>
             </Section>
-            <Hr />
-            <Section style={lowerSection}>
-              <Text style={cautionText}>
-                Amazon Web Services will never email you and ask you to disclose
-                or verify your password, credit card, or banking account number.
-              </Text>
-            </Section>
-          </Section>
-          <Text style={footerText}>
-            This message was produced and distributed by Amazon Web Services,
-            Inc., 410 Terry Ave. North, Seattle, WA 98109. © 2022, Amazon Web
-            Services, Inc.. All rights reserved. AWS is a registered trademark
-            of{' '}
-            <Link href="https://amazon.com" target="_blank" style={link}>
-              Amazon.com
-            </Link>
-            , Inc. View our{' '}
-            <Link href="https://amazon.com" target="_blank" style={link}>
-              privacy policy
-            </Link>
-            .
-          </Text>
-        </Container>
-      </Body>
+            <Text className="text-[#333] text-xs my-6 mx-0 px-5 py-0">
+              This message was produced and distributed by Amazon Web Services,
+              Inc., 410 Terry Ave. North, Seattle, WA 98109. © 2022, Amazon Web
+              Services, Inc.. All rights reserved. AWS is a registered trademark
+              of{' '}
+              <Link
+                href="https://amazon.com"
+                target="_blank"
+                className="text-[#2754C5] underline text-sm"
+              >
+                Amazon.com
+              </Link>
+              , Inc. View our{' '}
+              <Link
+                href="https://amazon.com"
+                target="_blank"
+                className="text-[#2754C5] underline text-sm"
+              >
+                privacy policy
+              </Link>
+              .
+            </Text>
+          </Container>
+        </Body>
+      </Tailwind>
     </Html>
   );
 }
@@ -88,89 +106,3 @@ AWSVerifyEmail.PreviewProps = {
   verificationCode: '596853',
 } satisfies AWSVerifyEmailProps;
 
-const main = {
-  backgroundColor: '#fff',
-  color: '#212121',
-};
-
-const container = {
-  padding: '20px',
-  margin: '0 auto',
-  backgroundColor: '#eee',
-};
-
-const h1 = {
-  color: '#333',
-  fontFamily:
-    "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
-  fontSize: '20px',
-  fontWeight: 'bold',
-  marginBottom: '15px',
-};
-
-const link = {
-  color: '#2754C5',
-  fontFamily:
-    "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
-  fontSize: '14px',
-  textDecoration: 'underline',
-};
-
-const text = {
-  color: '#333',
-  fontFamily:
-    "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
-  fontSize: '14px',
-  margin: '24px 0',
-};
-
-const imageSection = {
-  backgroundColor: '#252f3d',
-  display: 'flex',
-  padding: '20px 0',
-  alignItems: 'center',
-  justifyContent: 'center',
-};
-
-const coverSection = { backgroundColor: '#fff' };
-
-const upperSection = { padding: '25px 35px' };
-
-const lowerSection = { padding: '25px 35px' };
-
-const footerText = {
-  ...text,
-  fontSize: '12px',
-  padding: '0 20px',
-};
-
-const verifyText = {
-  ...text,
-  margin: 0,
-  fontWeight: 'bold',
-  textAlign: 'center' as const,
-};
-
-const codeText = {
-  ...text,
-  fontWeight: 'bold',
-  fontSize: '36px',
-  margin: '10px 0',
-  textAlign: 'center' as const,
-};
-
-const validityText = {
-  ...text,
-  margin: '0px',
-  textAlign: 'center' as const,
-};
-
-const verificationSection = {
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-};
-
-const mainText = { ...text, marginBottom: '14px' };
-
-const cautionText = { ...text, margin: '0px' };
