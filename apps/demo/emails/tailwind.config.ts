ee72046f488bd185f2c72933b555f14b4f856e80
gabriel miranda <gabrielmfern@outlook.com>
2025-10-02 16:17:59 -0300

    migrate google play policy to tailwind v4

--- apps/demo/emails/tailwind.config.ts
+++ apps/demo/emails/tailwind.config.ts
@@ -1,10 +1,12 @@
-import { pixelBasedPreset, type TailwindConfig } from '@react-email/components';
+import { pixelBasedPreset, type TailwindConfig } from "@react-email/components";
 
 export default {
   presets: [pixelBasedPreset],
   theme: {
     fontFamily: {
       amazon: ['Ember', 'Helvetica', 'Arial', 'sans-serif'],
+      google:
+        '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif',
     },
   },
 } satisfies TailwindConfig;
