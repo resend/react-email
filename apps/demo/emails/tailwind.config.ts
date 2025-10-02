38341df03e1ce6cdfb9bb8735f4c0b6881c49597
gabriel miranda <gabrielmfern@outlook.com>
2025-10-02 11:45:27 -0300

    migrate raycast template to using tailwind v4

--- apps/demo/emails/tailwind.config.ts
+++ apps/demo/emails/tailwind.config.ts
@@ -5,6 +5,7 @@ export default {
   theme: {
     fontFamily: {
       amazon: ['Ember', 'Helvetica', 'Arial', 'sans-serif'],
-    },
-  },
+      raycast: '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif'
+    }
+  }
 } satisfies TailwindConfig;
