a66137b522ad119e0055818a2695a2220badc690
gabriel miranda <gabrielmfern@outlook.com>
2025-10-02 12:04:15 -0300

    migrate slack-confirm.tsx to using tailwind v4

--- apps/demo/emails/tailwind.config.ts
+++ apps/demo/emails/tailwind.config.ts
@@ -2,4 +2,10 @@ import { pixelBasedPreset, type TailwindConfig } from '@react-email/components';
 
 export default {
   presets: [pixelBasedPreset],
+  theme: {
+    fontFamily: {
+      slack:
+        "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
+    },
+  },
 } satisfies TailwindConfig;
