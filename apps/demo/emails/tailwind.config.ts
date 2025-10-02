9da68772f1ba8359fefbfb875465facca68ac39b
gabriel miranda <gabrielmfern@outlook.com>
2025-10-02 11:45:27 -0300

    migrate raycast template to using tailwind v4

--- apps/demo/emails/tailwind.config.ts
+++ apps/demo/emails/tailwind.config.ts
@@ -2,4 +2,9 @@ import { pixelBasedPreset, type TailwindConfig } from '@react-email/components';
 
 export default {
   presets: [pixelBasedPreset],
+  theme: {
+    fontFamily: {
+      raycast: '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif'
+    }
+  }
 } satisfies TailwindConfig;
