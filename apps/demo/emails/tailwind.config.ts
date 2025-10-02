7c64e46bc61dc13e2b9fa87ac19ad6246bfb6d8e
gabriel miranda <gabrielmfern@outlook.com>
2025-10-02 12:57:37 -0300

    use tailwind v4 on codepen email template

--- apps/demo/emails/tailwind.config.ts
+++ apps/demo/emails/tailwind.config.ts
@@ -2,4 +2,9 @@ import { pixelBasedPreset, type TailwindConfig } from '@react-email/components';
 
 export default {
   presets: [pixelBasedPreset],
+  theme: {
+    fontFamily: {
+      codepen: '"Google Sans",Roboto,RobotoDraft,Helvetica,Arial,sans-serif',
+    },
+  },
 } satisfies TailwindConfig;
