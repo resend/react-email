47d0495e51bef4e72b6c8073a42e8b9d3ac5fb1d
gabriel miranda <gabrielmfern@outlook.com>
2025-10-03 09:49:50 -0300

    use tailwind

--- apps/demo/emails/tailwind.config.ts
+++ apps/demo/emails/tailwind.config.ts
@@ -2,4 +2,17 @@ import { pixelBasedPreset, type TailwindConfig } from '@react-email/components';
 
 export default {
   presets: [pixelBasedPreset],
+  theme: {
+    fontFamily: {
+      stripe: [
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Ubuntu',
+        'sans-serif',
+      ],
+    },
+  },
 } satisfies TailwindConfig;
