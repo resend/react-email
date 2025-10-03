f18c1c249c43cf51dbe79c3fb70f2cf518f698c5
gabriel miranda <gabrielmfern@outlook.com>
2025-10-03 13:32:44 -0300

    add failing test

--- packages/tailwind/src/utils/css/resolve-calc-expressions.spec.ts
+++ packages/tailwind/src/utils/css/resolve-calc-expressions.spec.ts
@@ -11,6 +11,14 @@ describe('resolveCalcExpressions()', async () => {
     expect(generate(root)).toMatchSnapshot();
   });
 
+  it('resolves calc expressions repeating decimals', () => {
+    const root = parse(`
+      .w-1\/3 { width: calc(0.3333333333333333*100%); }
+    `);
+    resolveCalcExpressions(root);
+    expect(generate(root)).toBe('');
+  });
+
   it('should not do anything to complex calc expressions', () => {
     const root = parse(`
 .px-3{padding-inline:calc(0.25rem*(3 + 1px))}
