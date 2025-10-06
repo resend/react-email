e4e0b03843c84d8a58d08bc681583732523e5461
gabriel miranda <gabrielmfern@outlook.com>
2025-10-06 15:19:25 -0300

    equate line height to the original one

--- apps/demo/emails/magic-links/aws-verify-email.tsx
+++ apps/demo/emails/magic-links/aws-verify-email.tsx
@@ -42,38 +42,38 @@ export default function AWSVerifyEmail({
                 />
               </Section>
               <Section className="py-6 px-9">
-                <Heading className="text-[#333] text-xl font-bold mb-6">
+                <Heading className="text-[#333] text-[20px] font-bold mb-6">
                   Verify your email address
                 </Heading>
-                <Text className="mb-3.5 text-[#333] text-sm my-6 mx-0">
+                <Text className="text-[#333] text-[14px] mt-6 mb-3.5 mx-0">
                   Thanks for starting the new AWS account creation process. We
                   want to make sure it's really you. Please enter the following
                   verification code when prompted. If you don&apos;t want to
                   create an account, you can ignore this message.
                 </Text>
                 <Section className="flex items-center justify-center">
-                  <Text className="text-[#333] m-0 font-bold text-center text-sm">
+                  <Text className="text-[#333] m-0 font-bold text-center text-[14px]">
                     Verification code
                   </Text>
 
-                  <Text className="text-[#333] text-4xl my-2.5 mx-0 font-bold text-center">
+                  <Text className="text-[#333] text-[36px] my-2.5 mx-0 font-bold text-center">
                     {verificationCode}
                   </Text>
-                  <Text className="text-[#333] text-sm m-0 text-center">
+                  <Text className="text-[#333] text-[14px] m-0 text-center">
                     (This code is valid for 10 minutes)
                   </Text>
                 </Section>
               </Section>
               <Hr />
               <Section className="py-6 px-9">
-                <Text className="text-[#333] text-sm m-0">
+                <Text className="text-[#333] text-[14px] m-0">
                   Amazon Web Services will never email you and ask you to
                   disclose or verify your password, credit card, or banking
                   account number.
                 </Text>
               </Section>
             </Section>
-            <Text className="text-[#333] text-xs my-6 mx-0 px-5 py-0">
+            <Text className="text-[#333] text-[12px] my-6 mx-0 px-5 py-0">
               This message was produced and distributed by Amazon Web Services,
               Inc., 410 Terry Ave. North, Seattle, WA 98109. © 2022, Amazon Web
               Services, Inc.. All rights reserved. AWS is a registered trademark
@@ -81,7 +81,7 @@ export default function AWSVerifyEmail({
               <Link
                 href="https://amazon.com"
                 target="_blank"
-                className="text-[#2754C5] underline text-sm"
+                className="text-[#2754C5] underline text-[12px]"
               >
                 Amazon.com
               </Link>
@@ -89,7 +89,7 @@ export default function AWSVerifyEmail({
               <Link
                 href="https://amazon.com"
                 target="_blank"
-                className="text-[#2754C5] underline text-sm"
+                className="text-[#2754C5] underline text-[12px]"
               >
                 privacy policy
               </Link>
