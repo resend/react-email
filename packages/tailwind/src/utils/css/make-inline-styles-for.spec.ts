4206bd83bdb7a6d7a14d9da34038237cfcd7545b
gabriel miranda <gabrielmfern@outlook.com>
2025-10-06 11:08:48 -0300

    add variable resolution to make-inline-styles

--- packages/tailwind/src/utils/css/make-inline-styles-for.spec.ts
+++ packages/tailwind/src/utils/css/make-inline-styles-for.spec.ts
@@ -1,13 +1,32 @@
 import { parse, type StyleSheet } from 'css-tree';
 import { makeInlineStylesFor } from './make-inline-styles-for';
 
-test('makeInlineStylesFor()', async () => {
-  const tailwindStyles = parse(`
-    .bg-red-500 { background-color: #f56565; }
-    .w-full { width: 100%; }
-  `) as StyleSheet;
+describe('makeInlineStylesFor()', async () => {
+  it('works in simple use case', () => {
+    const tailwindStyles = parse(`
+      .bg-red-500 { background-color: #f56565; }
+      .w-full { width: 100%; }
+    `) as StyleSheet;
 
-  expect(
-    makeInlineStylesFor(tailwindStyles.children.toArray()),
-  ).toMatchSnapshot();
+    expect(
+      makeInlineStylesFor(tailwindStyles.children.toArray()),
+    ).toMatchSnapshot();
+  });
+
+  it('does basic local variable resolution', () => {
+    const tailwindStyles = parse(`
+      .btn {
+        --btn-bg: #3490dc;
+        --btn-text: #fff;
+        background-color: var(--btn-bg);
+        color: var(--btn-text);
+        padding: 0.5rem 1rem;
+        border-radius: 0.25rem;
+      }
+    `) as StyleSheet;
+
+    expect(
+      makeInlineStylesFor(tailwindStyles.children.toArray()),
+    ).toMatchSnapshot();
+  });
 });
