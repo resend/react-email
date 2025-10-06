908101a08d9325564bbbee807339eac37f1ccba2
cpenned <66193019+cpenned@users.noreply.github.com>
2025-10-06 16:33:21 -0600

    feat: change example email (#2540)

--- apps/web/src/components/sections/playground/code-example.tsx
+++ apps/web/src/components/sections/playground/code-example.tsx
@@ -13,8 +13,8 @@ import {
 } from '@react-email/components';
 
 const WelcomeEmail = ({
-  username = 'Steve',
-  company = 'Vercel',
+  username = 'Nicole',
+  company = 'Helix',
 }: WelcomeEmailProps) => {
   const previewText = `Welcome to ${company}, ${username}!`;
 
@@ -27,9 +27,9 @@ const WelcomeEmail = ({
           <Container className="mb-10 mx-auto p-5 w-[465px]">
             <Section className="mt-10">
               <Img
-                src={`${baseUrl}/brand/vercel.png`}
+                src={`${baseUrl}/brand/example-logo.png`}
                 width="60"
-                height="52"
+                height="60"
                 alt="Logo Example"
                 className="my-0 mx-auto"
               />
