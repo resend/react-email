0486c658d62a85844ffb25ebc429a474a216827e
gabriel miranda <gabrielmfern@outlook.com>
2025-10-06 11:08:48 -0300

    add variable resolution to make-inline-styles

--- packages/tailwind/src/utils/css/make-inline-styles-for.ts
+++ packages/tailwind/src/utils/css/make-inline-styles-for.ts
@@ -1,16 +1,41 @@
 import { type CssNode, generate, walk } from 'css-tree';
-import { convertCssPropertyToReactProperty } from '../compatibility/convert-css-property-to-react-property';
+import { getReactProperty } from '../compatibility/get-react-property';
+import type { VariableDefinition } from './resolve-all-css-variables';
 
 export function makeInlineStylesFor(inlinableRules: CssNode[]) {
   const styles: Record<string, string> = {};
 
   for (const rule of inlinableRules) {
+    const localVariableDefinitions = new Set<VariableDefinition>();
     walk(rule, {
       visit: 'Declaration',
       enter(declaration) {
-        styles[convertCssPropertyToReactProperty(declaration.property)] =
+        if (declaration.property.startsWith('--')) {
+          localVariableDefinitions.add({
+            declaration,
+            definition: generate(declaration.value).trim(),
+            variableName: declaration.property.trim(),
+          });
+        }
+      },
+    });
+
+    walk(rule, {
+      visit: 'Declaration',
+      enter(declaration) {
+        if (declaration.property.startsWith('--')) {
+          return;
+        }
+        let value =
           generate(declaration.value) +
           (declaration.important ? '!important' : '');
+        for (const localVarDef of localVariableDefinitions) {
+          const varUsage = `var(${localVarDef.variableName})`;
+          if (value.includes(varUsage)) {
+            value = value.replaceAll(varUsage, localVarDef.definition);
+          }
+        }
+        styles[getReactProperty(declaration.property)] = value;
       },
     });
   }
