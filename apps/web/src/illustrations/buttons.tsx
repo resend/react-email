54a7ae75404c2d7148b6c5cf9f9b39c7769ee75b
Gustavo <79873612+guhrodrrigues@users.noreply.github.com>
2025-10-07 05:45:28 -0300

    fix: add improvements to mobile screen (#2544)
    Co-authored-by: Danilo Woznica <danilowoz@gmail.com>

--- apps/web/src/illustrations/buttons.tsx
+++ apps/web/src/illustrations/buttons.tsx
@@ -1,7 +1,7 @@
 import { MousePointer2Icon } from 'lucide-react';
 
 const IllustrationButtons: React.FC = () => (
-  <div className="relative flex h-6 w-[24%] items-center justify-center rounded-md border border-[#2EBDC9] bg-[#25AEBA] p-1 shadow-sm transition-transform duration-150 ease-[cubic-bezier(.42,0,.58,1.8)] group-hover:rotate-3">
+  <div className="relative flex h-6 w-[24%] items-center justify-center rounded-md border border-[#2EBDC9] bg-[#25AEBA] p-1 shadow-sm transition-transform duration-150 ease-[cubic-bezier(.42,0,.58,1.8)] md:group-hover:rotate-3">
     <div className="h-2 w-[80%] rounded-sm bg-black/30" />
     <MousePointer2Icon
       className="-bottom-4 group-hover:-rotate-12 absolute left-[80%] transition-transform duration-150 ease-[cubic-bezier(.42,0,.58,1.8)]"
