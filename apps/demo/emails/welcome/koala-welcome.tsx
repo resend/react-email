fe81830dcc3fe60e6625ce4ed3972cce79f6c03c
gabriel miranda <gabrielmfern@outlook.com>
2025-10-07 17:28:43 -0300

    fix minor differences

--- apps/demo/emails/welcome/koala-welcome.tsx
+++ apps/demo/emails/welcome/koala-welcome.tsx
@@ -40,26 +40,26 @@ export const KoalaWelcomeEmail = ({
             alt="Koala"
             className="mx-auto"
           />
-          <Text className="text-base leading-7">Hi {userFirstname},</Text>
-          <Text className="text-base leading-7">
+          <Text className="text-[16px] leading-[26px]">Hi {userFirstname},</Text>
+          <Text className="text-[16px] leading-[26px]">
             Welcome to Koala, the sales intelligence platform that helps you
             uncover qualified leads and close deals faster.
           </Text>
           <Section className="text-center">
             <Button
-              className="bg-[#5F51E8] rounded text-white text-base no-underline text-center block p-3"
+              className="bg-[#5F51E8] rounded-[3px] text-white text-[16px] no-underline text-center block p-3"
               href="https://getkoala.com"
             >
               Get started
             </Button>
           </Section>
-          <Text className="text-base leading-7">
+          <Text className="text-[16px] leading-[26px]">
             Best,
             <br />
             The Koala team
           </Text>
           <Hr className="border-[#cccccc] my-5" />
-          <Text className="text-[#8898aa] text-xs">
+          <Text className="text-[#8898aa] text-[12px]">
             470 Noor Ave STE B #1148, South San Francisco, CA 94080
           </Text>
         </Container>
