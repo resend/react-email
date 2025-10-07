e9880cf45f3f3b9b28e54b9a81356add731b3d51
gabriel miranda <gabrielmfern@outlook.com>
2025-10-07 10:00:42 -0300

    fix pixel differences

--- apps/demo/emails/magic-links/plaid-verify-identity.tsx
+++ apps/demo/emails/magic-links/plaid-verify-identity.tsx
@@ -27,7 +27,7 @@ export const PlaidVerifyIdentityEmail = ({
     <Head />
     <Tailwind config={tailwindConfig}>
       <Body className="bg-white font-plaid">
-        <Container className="bg-white border border-solid border-[#eee] rounded shadow-[rgba(20,50,70,.2)] shadow-md mt-5 max-w-[360px] mx-auto my-0 pt-17 px-0 pb-33">
+        <Container className="bg-white border border-solid border-[#eee] rounded shadow-[rgba(20,50,70,.2)] shadow-md mt-5 max-w-[360px] mx-auto my-0 pt-[68px] px-0 pb-[130px]">
           <Img
             src={`${baseUrl}/static/plaid-logo.png`}
             width="212"
@@ -35,21 +35,21 @@ export const PlaidVerifyIdentityEmail = ({
             alt="Plaid"
             className="mx-auto my-0"
           />
-          <Text className="text-[#0a85ea] text-xs font-bold h-4 tracking-[0] leading-4 mt-4 mb-2 mx-2 uppercase text-center">
+          <Text className="text-[#0a85ea] text-[11px] font-bold h-4 tracking-[0] leading-[16px] mt-4 mb-2 mx-2 uppercase text-center">
             Verify Your Identity
           </Text>
-          <Heading className="text-black inline-block text-xl my-0 text-center">
+          <Heading className="text-black font-medium inline-block text-[20px] leading-[24px] my-0 text-center">
             Enter the following code to finish linking Venmo.
           </Heading>
-          <Section className="bg-[rgba(0,0,0,.05)] rounded mx-auto mt-4 mb-3.5 align-middle w-[280px]">
-            <Text className="text-black text-3xl font-bold tracking-[6px] leading-10 py-2 mx-auto my-0 block text-center">
+          <Section className="bg-[rgba(0,0,0,.05)] rounded mx-auto font-[HelveticaNeue-Bold] mt-4 mb-3.5 align-middle w-[280px]">
+            <Text className="text-black text-[32px] font-bold tracking-[6px] leading-10 py-2 mx-auto my-0 block text-center">
               {validationCode}
             </Text>
           </Section>
-          <Text className="text-[#444] text-[15px] tracking-[0] py-0 px-10 m-0 text-center">
+          <Text className="text-[#444] text-[15px] leading-[23px] tracking-[0] py-0 px-10 m-0 text-center">
             Not expecting this email?
           </Text>
-          <Text className="text-[#444] text-[15px] tracking-[0] py-0 px-10 m-0 text-center">
+          <Text className="text-[#444] text-[15px] leading-[23px] tracking-[0] py-0 px-10 m-0 text-center">
             Contact{' '}
             <Link
               href="mailto:login@plaid.com"
@@ -60,7 +60,7 @@ export const PlaidVerifyIdentityEmail = ({
             if you did not request this code.
           </Text>
         </Container>
-        <Text className="text-black text-xs font-extrabold tracking-[0] leading-6 m-0 mt-4 text-center uppercase">
+        <Text className="text-black text-xs font-extrabold tracking-[0] leading-[23px] m-0 mt-5 text-center uppercase">
           Securely powered by Plaid.
         </Text>
       </Body>
