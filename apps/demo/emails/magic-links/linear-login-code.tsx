15bbffb564409ae63f5523a2bf3839c47f860404
gabriel miranda <gabrielmfern@outlook.com>
2025-10-07 09:13:43 -0300

    fix small differences

--- apps/demo/emails/magic-links/linear-login-code.tsx
+++ apps/demo/emails/magic-links/linear-login-code.tsx
@@ -37,29 +37,29 @@ export const LinearLoginCodeEmail = ({
             width="42"
             height="42"
             alt="Linear"
-            className="rounded-3xl w-10.5 h-10.5"
+            className="rounded-3xl w-[42px] h-[42px]"
           />
-          <Heading className="text-2xl tracking-tight leading-[1.3] font-normal text-[#484848] pt-4 px-0 pb-0">
+          <Heading className="text-[24px] tracking-[-0.5px] leading-[1.3] font-normal text-[#484848] pt-[17px] px-0 pb-0">
             Your login code for Linear
           </Heading>
-          <Section className="py-7 px-0">
+          <Section className="py-[27px] px-0">
             <Button
-              className="bg-[#5e6ad2] rounded font-semibold text-white text-[15px] no-underline text-center block py-[11px] px-6"
+              className="bg-[#5e6ad2] rounded font-semibold text-white text-[15px] no-underline text-center block py-[11px] px-[23px]"
               href="https://linear.app"
             >
               Login to Linear
             </Button>
           </Section>
-          <Text className="mb-4 mx-0 mt-0 leading-[1.4] text-[14px] text-[#3c4149]">
+          <Text className="mb-[15px] mx-0 mt-0 leading-[1.4] text-[15px] text-[#3c4149]">
             This link and code will only be valid for the next 5 minutes. If the
             link does not work, you can use the login verification code
             directly:
           </Text>
-          <code className="font-mono font-bold px-1 py-px bg-[#dfe1e4] text-[#3c4149] text-xl tracking-tight rounded">
+          <code className="font-mono font-bold px-1 py-px bg-[#dfe1e4] text-[#3c4149] text-[21px] tracking-[-0.3px] rounded">
             {validationCode}
           </code>
-          <Hr className="border-[#dfe1e4] mt-10.5 mb-6.5" />
-          <Link href="https://linear.app" className="text-[#b4becc] text-sm">
+          <Hr className="border-[#dfe1e4] mt-[42px] mb-[26px]" />
+          <Link href="https://linear.app" className="text-[#b4becc] text-[14px]">
             Linear
           </Link>
         </Container>
