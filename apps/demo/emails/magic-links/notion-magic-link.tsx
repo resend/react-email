bfc63a8ebba87c2b8bbe1438bcf14219d6cdb3eb
gabriel miranda <gabrielmfern@outlook.com>
2025-10-07 09:48:50 -0300

    fix pixel differences

--- apps/demo/emails/magic-links/notion-magic-link.tsx
+++ apps/demo/emails/magic-links/notion-magic-link.tsx
@@ -29,26 +29,26 @@ export const NotionMagicLinkEmail = ({
       <Body className="bg-white font-notion">
         <Preview>Log in with this magic link</Preview>
         <Container className="px-3 mx-auto">
-          <Heading className="text-[#333] text-2xl my-10 mx-0 p-0">
+          <Heading className="text-[#333] text-[24px] my-10 mx-0 p-0">
             Login
           </Heading>
           <Link
             href="https://notion.so"
             target="_blank"
-            className="text-[#2754C5] text-sm underline mb-4 block"
+            className="text-[#2754C5] text-[14px] underline mb-4 block"
           >
             Click here to log in with this magic link
           </Link>
-          <Text className="text-[#333] text-sm my-6 mb-3.5">
+          <Text className="text-[#333] text-[14px] my-6 mb-3.5">
             Or, copy and paste this temporary login code:
           </Text>
           <code className="inline-block py-4 px-[4.5%] w-9/10 bg-[#f4f4f4] rounded-md border border-solid border-[#eee] text-[#333]">
             {loginCode}
           </code>
-          <Text className="text-[#ababab] text-sm mt-3.5 mb-4">
+          <Text className="text-[#ababab] text-[14px] mt-3.5 mb-4">
             If you didn&apos;t try to login, you can safely ignore this email.
           </Text>
-          <Text className="text-[#ababab] text-sm mt-3.5 mb-9.5">
+          <Text className="text-[#ababab] text-[14px] mt-3.5 mb-9.5">
             Hint: You can set a permanent password in Settings & members → My
             account.
           </Text>
@@ -58,11 +58,11 @@ export const NotionMagicLinkEmail = ({
             height="32"
             alt="Notion's Logo"
           />
-          <Text className="text-[#898989] text-xs mt-3 mb-6">
+          <Text className="text-[#898989] text-[12px] leading-[22px] mt-3 mb-6">
             <Link
               href="https://notion.so"
               target="_blank"
-              className="text-[#898989] text-sm underline"
+              className="text-[#898989] text-[14px] underline"
             >
               Notion.so
             </Link>
