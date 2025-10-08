8edc152fcceda5e85347a6a7781a95bc55dd8d44
gabriel miranda <gabrielmfern@outlook.com>
2025-10-08 09:19:31 -0300

    check for our components by hand in Tailwind instead of using a property for them

--- packages/tailwind/src/utils/react/is-component.ts
+++ packages/tailwind/src/utils/react/is-component.ts
@@ -1,3 +1,27 @@
+import { Body } from '@react-email/body';
+import { Button } from '@react-email/button';
+import { CodeBlock } from '@react-email/code-block';
+import { CodeInline } from '@react-email/code-inline';
+import { Container } from '@react-email/container';
+import { Heading } from '@react-email/heading';
+import { Hr } from '@react-email/hr';
+import { Img } from '@react-email/img';
+import { Link } from '@react-email/link';
+import { Preview } from '@react-email/preview';
+
+const componentsToTreatAsElements: React.ReactElement['type'][] = [
+  Body,
+  Button,
+  CodeBlock,
+  CodeInline,
+  Container,
+  Heading,
+  Hr,
+  Img,
+  Link,
+  Preview,
+];
+
 export const isComponent = (
   element: React.ReactElement,
 ): element is React.ReactElement<unknown, React.FC<unknown>> => {
@@ -5,7 +29,6 @@ export const isComponent = (
     (typeof element.type === 'function' ||
       // @ts-expect-error - we know this is a component that may have a render function
       element.type.render !== undefined) &&
-    // @ts-expect-error this is set for our components
-    element.type.tailwindTreatAsElement !== true
+    !componentsToTreatAsElements.includes(element.type)
   );
 };
