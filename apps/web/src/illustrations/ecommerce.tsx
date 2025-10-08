06c6e2cea5c9964f87dcbc9decd9febea406e58c
guhrodrrigues <gumartinsr1201@gmail.com>
2025-10-07 21:31:59 -0300

    style: add mobile improvements to home page

--- apps/web/src/illustrations/ecommerce.tsx
+++ apps/web/src/illustrations/ecommerce.tsx
@@ -1,9 +1,9 @@
 import { CircleDollarSign } from 'lucide-react';
 
 const IllustrationEcommerce: React.FC = () => (
-  <div className="relative flex aspect-video w-[42%] items-center gap-2 rounded-md bg-[#0F0F10] bg-gradient-to-b from-transparent via-black/20 to-black/20 px-3 py-2 shadow-sm transition-transform duration-150 ease-[cubic-bezier(.42,0,.58,1.8)] md:md:group-hover:skew-x-2">
+  <div className="relative flex aspect-video w-[42%] items-center gap-2 rounded-md bg-[#0F0F10] bg-gradient-to-b from-transparent via-black/20 to-black/20 px-3 py-2 shadow-sm transition-transform duration-150 ease-[cubic-bezier(.42,0,.58,1.8)] md:group-hover:skew-x-2">
     <div className="flex aspect-square w-[42%] items-center justify-center rounded-sm bg-slate-4">
-      <CircleDollarSign className="opacity-10 transition-opacity group-hover:opacity-20" />
+      <CircleDollarSign className="opacity-10 transition-opacity md:group-hover:opacity-20" />
     </div>
     <div className="flex shrink grow basis-0 flex-col gap-1">
       <div className="h-2 w-[84%] rounded-sm bg-slate-5" />
--- apps/web/src/illustrations/gallery.tsx
+++ apps/web/src/illustrations/gallery.tsx
@@ -7,8 +7,8 @@ const IllustrationGallery: React.FC = () => (
         <div className="h-full w-full rounded-sm bg-slate-3" />
       </div>
       <div className="aspect-square w-[30%] rounded-sm bg-[#0F0F10] bg-gradient-to-b from-transparent via-black/20 to-black/20 p-2 shadow-sm">
-        <div className="flex h-full w-full items-center justify-center rounded-sm bg-slate-3 transition-colors group-hover:bg-slate-4">
-          <ImageIcon className="opacity-5 transition-opacity group-hover:opacity-20" />
+        <div className="flex h-full w-full items-center justify-center rounded-sm bg-slate-3 transition-colors md:group-hover:bg-slate-4">
+          <ImageIcon className="opacity-5 transition-opacity md:group-hover:opacity-20" />
         </div>
       </div>
       <div className="aspect-square w-[30%] rounded-sm bg-[#0F0F10] bg-gradient-to-b from-transparent via-black/20 to-black/20 p-2 shadow-sm">
