e846411dac29256ea97de9511e918155dbc6814b
Gabriel Miranda <gabrielmfern@outlook.com>
2025-10-10 16:29:50 -0300

    feat(demo): use Tailwind v4 in raycast-magic-link.tsx (#2524)
    Co-authored-by: Bu Kinoshita <bukinoshita@gmail.com>
    Co-authored-by: Bu Kinoshita <6929565+bukinoshita@users.noreply.github.com>

--- apps/demo/emails/tailwind.config.ts
+++ apps/demo/emails/tailwind.config.ts
@@ -5,6 +5,8 @@ export default {
   theme: {
     fontFamily: {
       amazon: ['Ember', 'Helvetica', 'Arial', 'sans-serif'],
+      raycast:
+        '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif',
       slack:
         "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
       twitch: ['HelveticaNeue', 'Helvetica', 'Arial', 'sans-serif'],
