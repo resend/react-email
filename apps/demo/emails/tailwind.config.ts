2f2ec080fd3aab5e5db0d5ee3b5fca25a10bb889
gabriel miranda <gabrielmfern@outlook.com>
2025-10-10 11:04:24 -0300

    lint

--- apps/demo/emails/tailwind.config.ts
+++ apps/demo/emails/tailwind.config.ts
@@ -5,7 +5,8 @@ export default {
   theme: {
     fontFamily: {
       amazon: ['Ember', 'Helvetica', 'Arial', 'sans-serif'],
-      raycast: '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif'
-    }
-  }
+      raycast:
+        '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif',
+    },
+  },
 } satisfies TailwindConfig;
