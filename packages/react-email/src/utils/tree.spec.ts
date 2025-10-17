3cb975b3068c234273b9eac72273b05f9ae4382c
gabriel miranda <gabrielmfern@outlook.com>
2025-10-17 11:42:50 -0300

    make tree run in a sub directory where no other tests can cause race conditions

--- packages/react-email/src/utils/tree.spec.ts
+++ packages/react-email/src/utils/tree.spec.ts
@@ -1,5 +1,8 @@
+import path from 'node:path';
 import { tree } from './tree.js';
 
 test('tree(__dirname, 2)', async () => {
-  expect(await tree(__dirname, 2)).toMatchSnapshot();
+  expect(
+    await tree(path.resolve(import.meta.dirname, './preview'), 2),
+  ).toMatchSnapshot();
 });
