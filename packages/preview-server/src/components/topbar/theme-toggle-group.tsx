5bc245dd0894e438590f4265da0f57e54df423ac
gabriel miranda <gabrielmfern@outlook.com>
2025-10-23 17:10:01 -0300

    add aria-labels, improve accessibility

--- packages/preview-server/src/components/topbar/theme-toggle-group.tsx
+++ packages/preview-server/src/components/topbar/theme-toggle-group.tsx
@@ -28,62 +28,62 @@ export const ThemeToggleGroup = ({
       type="single"
       value={active}
     >
-      <ToggleGroup.Item value="light">
-        <Tooltip>
-          <Tooltip.Trigger asChild>
-            <div
-              className={cn(
-                'relative px-3 py-2 transition duration-200 ease-in-out hover:text-slate-12',
-                {
-                  'text-slate-11': active !== 'light',
-                  'text-slate-12': active === 'light',
-                },
-              )}
-            >
-              {active === 'light' && (
-                <motion.span
-                  animate={{ opacity: 1 }}
-                  className="absolute top-0 right-0 bottom-0 left-0 bg-slate-4"
-                  exit={{ opacity: 0 }}
-                  initial={{ opacity: 0 }}
-                  layoutId="topbar-theme-tabs"
-                  transition={tabTransition}
-                />
-              )}
-              <IconSun />
-            </div>
-          </Tooltip.Trigger>
-          <Tooltip.Content>Light</Tooltip.Content>
-        </Tooltip>
-      </ToggleGroup.Item>
-      <ToggleGroup.Item value="dark">
-        <Tooltip>
-          <Tooltip.Trigger asChild>
-            <div
-              className={cn(
-                'relative px-3 py-2 transition duration-200 ease-in-out hover:text-slate-12',
-                {
-                  'text-slate-11': active !== 'dark',
-                  'text-slate-12': active === 'dark',
-                },
-              )}
-            >
-              {active === 'dark' && (
-                <motion.span
-                  animate={{ opacity: 1 }}
-                  className="absolute top-0 right-0 bottom-0 left-0 bg-slate-4"
-                  exit={{ opacity: 0 }}
-                  initial={{ opacity: 0 }}
-                  layoutId="topbar-theme-tabs"
-                  transition={tabTransition}
-                />
-              )}
-              <IconMoon />
-            </div>
-          </Tooltip.Trigger>
-          <Tooltip.Content>Dark</Tooltip.Content>
-        </Tooltip>
-      </ToggleGroup.Item>
+      <Tooltip>
+        <Tooltip.Trigger asChild>
+          <ToggleGroup.Item
+            value="light"
+            aria-label="Enable light mode"
+            className={cn(
+              'relative px-3 py-2 transition duration-200 ease-in-out hover:text-slate-12',
+              {
+                'text-slate-11': active !== 'light',
+                'text-slate-12': active === 'light',
+              },
+            )}
+          >
+            {active === 'light' && (
+              <motion.span
+                animate={{ opacity: 1 }}
+                className="absolute top-0 right-0 bottom-0 left-0 bg-slate-4"
+                exit={{ opacity: 0 }}
+                initial={{ opacity: 0 }}
+                layoutId="topbar-theme-tabs"
+                transition={tabTransition}
+              />
+            )}
+            <IconSun />
+          </ToggleGroup.Item>
+        </Tooltip.Trigger>
+        <Tooltip.Content>System</Tooltip.Content>
+      </Tooltip>
+      <Tooltip>
+        <Tooltip.Trigger asChild>
+          <ToggleGroup.Item
+            aria-label="Enable dark mode"
+            value="dark"
+            className={cn(
+              'relative px-3 py-2 transition duration-200 ease-in-out hover:text-slate-12',
+              {
+                'text-slate-11': active !== 'dark',
+                'text-slate-12': active === 'dark',
+              },
+            )}
+          >
+            {active === 'dark' && (
+              <motion.span
+                animate={{ opacity: 1 }}
+                className="absolute top-0 right-0 bottom-0 left-0 bg-slate-4"
+                exit={{ opacity: 0 }}
+                initial={{ opacity: 0 }}
+                layoutId="topbar-theme-tabs"
+                transition={tabTransition}
+              />
+            )}
+            <IconMoon />
+          </ToggleGroup.Item>
+        </Tooltip.Trigger>
+        <Tooltip.Content>Emulated Dark</Tooltip.Content>
+      </Tooltip>
     </ToggleGroup.Root>
   );
 };
