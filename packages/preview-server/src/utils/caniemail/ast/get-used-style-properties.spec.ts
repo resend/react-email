86aa6343272daea257c923cfe563cad03e953083
Gabriel Miranda <gabrielmfern@outlook.com>
2025-10-28 11:01:59 -0300

    use inline snapshots

--- packages/preview-server/src/utils/caniemail/ast/get-used-style-properties.spec.ts
+++ packages/preview-server/src/utils/caniemail/ast/get-used-style-properties.spec.ts
@@ -20,7 +20,28 @@ const buttonStyle = {
     const objectVariables = getObjectVariables(ast);
     expect(
       await getUsedStyleProperties(ast, reactCode, '', objectVariables),
-    ).toEqual([]);
+    ).toMatchInlineSnapshot(`
+      [
+        {
+          "location": SourceLocation {
+            "end": Position {
+              "column": 21,
+              "index": 91,
+              "line": 5,
+            },
+            "filename": undefined,
+            "identifierName": undefined,
+            "start": Position {
+              "column": 2,
+              "index": 72,
+              "line": 5,
+            },
+          },
+          "name": "borderRadius",
+          "value": "5px",
+        },
+      ]
+    `);
   });
 
   it('handles styles defined inline in the attribute', async () => {
@@ -36,6 +57,63 @@ const buttonStyle = {
     const objectVariables = getObjectVariables(ast);
     expect(
       await getUsedStyleProperties(ast, reactCode, '', objectVariables),
-    ).toEqual([]);
+    ).toMatchInlineSnapshot(`
+      [
+        {
+          "location": SourceLocation {
+            "end": Position {
+              "column": 36,
+              "index": 37,
+              "line": 2,
+            },
+            "filename": undefined,
+            "identifierName": undefined,
+            "start": Position {
+              "column": 17,
+              "index": 18,
+              "line": 2,
+            },
+          },
+          "name": "borderRadius",
+          "value": "5px",
+        },
+        {
+          "location": SourceLocation {
+            "end": Position {
+              "column": 53,
+              "index": 54,
+              "line": 2,
+            },
+            "filename": undefined,
+            "identifierName": undefined,
+            "start": Position {
+              "column": 38,
+              "index": 39,
+              "line": 2,
+            },
+          },
+          "name": "color",
+          "value": "#fff",
+        },
+        {
+          "location": SourceLocation {
+            "end": Position {
+              "column": 66,
+              "index": 67,
+              "line": 2,
+            },
+            "filename": undefined,
+            "identifierName": undefined,
+            "start": Position {
+              "column": 55,
+              "index": 56,
+              "line": 2,
+            },
+          },
+          "name": "padding",
+          "value": "10",
+        },
+      ]
+    `);
   });
 });
