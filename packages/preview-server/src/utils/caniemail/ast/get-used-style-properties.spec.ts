600c9fef62960e3ccef84e66368590419c2e227e
Gabriel Miranda <gabrielmfern@outlook.com>
2025-10-28 11:00:52 -0300

    add tests to ensure getUsedStyleProperties gets inline styles

--- packages/preview-server/src/utils/caniemail/ast/get-used-style-properties.spec.ts
+++ packages/preview-server/src/utils/caniemail/ast/get-used-style-properties.spec.ts
@@ -2,22 +2,40 @@ import { parse } from '@babel/parser';
 import { getObjectVariables } from './get-object-variables';
 import { getUsedStyleProperties } from './get-used-style-properties';
 
-test('getUsedStyleProperties()', async () => {
-  const reactCode = `
+describe('getUsedStyleProperties()', async () => {
+  it('handles styles defined as an object in another variable', async () => {
+    const reactCode = `
 <Button style={buttonStyle}>Click me</Button>
 
 const buttonStyle = {
   borderRadius: '5px',
 };
 `;
-  const ast = parse(reactCode, {
-    strictMode: false,
-    errorRecovery: true,
-    sourceType: 'unambiguous',
-    plugins: ['jsx', 'typescript', 'decorators'],
+    const ast = parse(reactCode, {
+      strictMode: false,
+      errorRecovery: true,
+      sourceType: 'unambiguous',
+      plugins: ['jsx', 'typescript', 'decorators'],
+    });
+    const objectVariables = getObjectVariables(ast);
+    expect(
+      await getUsedStyleProperties(ast, reactCode, '', objectVariables),
+    ).toEqual([]);
   });
-  const objectVariables = getObjectVariables(ast);
-  expect(
-    await getUsedStyleProperties(ast, reactCode, '', objectVariables),
-  ).toMatchSnapshot();
+
+  it('handles styles defined inline in the attribute', async () => {
+    const reactCode = `
+<Button style={{ borderRadius: '5px', "color": "#fff", padding: 10 }}>Click me</Button>
+`;
+    const ast = parse(reactCode, {
+      strictMode: false,
+      errorRecovery: true,
+      sourceType: 'unambiguous',
+      plugins: ['jsx', 'typescript', 'decorators'],
+    });
+    const objectVariables = getObjectVariables(ast);
+    expect(
+      await getUsedStyleProperties(ast, reactCode, '', objectVariables),
+    ).toEqual([]);
+  });
 });
