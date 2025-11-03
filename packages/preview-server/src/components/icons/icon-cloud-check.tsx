7c037afd5235ee3745087a6b2d2f7b38c8fdb85c
João Melo <jopcmelo@gmail.com>
2025-11-03 17:02:54 -0300

    fix: remove forwardRef usage

--- packages/preview-server/src/components/icons/icon-cloud-check.tsx
+++ packages/preview-server/src/components/icons/icon-cloud-check.tsx
@@ -1,13 +1,8 @@
-import * as React from 'react';
-import type { IconElement, IconProps } from './icon-base';
+import type { IconProps } from './icon-base';
 import { IconBase } from './icon-base';
 
-export const IconCloudCheck = React.forwardRef<
-  IconElement,
-  Readonly<IconProps>
->(({ ...props }, forwardedRef) => (
+export const IconCloudCheck = (props: IconProps) => (
   <IconBase
-    ref={forwardedRef}
     {...props}
     stroke="currentColor"
     strokeWidth="2"
@@ -17,6 +12,6 @@ export const IconCloudCheck = React.forwardRef<
     <path d="m17 15-5.5 5.5L9 18" />
     <path d="M5 17.743A7 7 0 1 1 15.71 10h1.79a4.5 4.5 0 0 1 1.5 8.742" />
   </IconBase>
-));
+);
 
 IconCloudCheck.displayName = 'IconCloudCheck';
--- packages/preview-server/src/components/icons/icon-loader.tsx
+++ packages/preview-server/src/components/icons/icon-loader.tsx
@@ -1,11 +1,9 @@
-import { forwardRef } from 'react';
-import type { IconElement, IconProps } from './icon-base';
+import type { IconProps } from './icon-base';
 import { IconBase } from './icon-base';
 
-export const IconLoader = forwardRef<IconElement, IconProps>((props, ref) => (
+export const IconLoader = (props: IconProps) => (
   <IconBase
     {...props}
-    ref={ref}
     stroke="currentColor"
     strokeWidth="2"
     strokeLinecap="round"
@@ -13,6 +11,6 @@ export const IconLoader = forwardRef<IconElement, IconProps>((props, ref) => (
   >
     <path d="M21 12a9 9 0 1 1-6.219-8.56" />
   </IconBase>
-));
+);
 
 IconLoader.displayName = 'IconLoader';
