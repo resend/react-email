eeeb465beb0ab38d0ac77a5e2e3d792a5ed75c72
gabriel miranda <gabrielmfern@outlook.com>
2025-11-05 10:34:33 -0300

    add sizes to images

--- apps/web/components/rounded-image/tailwind.tsx
+++ apps/web/components/rounded-image/tailwind.tsx
@@ -4,7 +4,8 @@ import { Layout } from '../_components/layout';
 export const component = (
   <Img
     alt="Stagg Electric Kettle"
-    className="rounded-[12px] [margin:0_auto]"
+    className="rounded-[12px] my-0 mx-auto"
+    width={250}
     height={250}
     src="/static/stagg-eletric-kettle.jpg"
   />
