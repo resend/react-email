4e64f10e5fbea99ea23ad9b5872ff21e33c74573
Gabriel Miranda <gabrielmfern@outlook.com>
2025-11-14 09:44:07 -0300

    feat(render): use experimental react-markup on react-server export

---
+++ packages/render/src/react-server/index.ts
@@ -0,0 +1,4 @@
+export * from '../shared/options';
+export * from '../shared/utils/pretty';
+export * from '../shared/utils/to-plain-text';
+export * from './render';
---
+++ packages/render/src/react-server/react-markup.d.ts
@@ -0,0 +1,3 @@
+declare module 'react-markup' {
+  function experimental_renderToHTML(react: React.ReactNode): Promise<string>;
+}
