c92f264ae2d5aace39e386c68819e7722b4b41e8
Gabriel Miranda <gabrielmfern@outlook.com>
2025-12-12 16:59:11 -0300

    enable experimental vm modules on all preview server scripts

--- packages/react-email/src/utils/preview/start-dev-server.ts
+++ packages/react-email/src/utils/preview/start-dev-server.ts
@@ -127,6 +127,7 @@ export const startDevServer = async (
     ...(process.env as Omit<NodeJS.ProcessEnv, 'NODE_ENV'> & {
       NODE_ENV?: NodeJS.ProcessEnv['NODE_ENV'];
     }),
+    NODE_OPTIONS: `${process.env.NODE_OPTIONS ?? ''} --experimental-vm-modules`,
     ...getEnvVariablesForPreviewApp(
       // If we don't do normalization here, stuff like https://github.com/resend/react-email/issues/1354 happens.
       path.normalize(emailsDirRelativePath),
@@ -194,21 +195,21 @@ const makeExitHandler =
       | { shouldKillProcess: false }
       | { shouldKillProcess: true; killWithErrorCode: boolean },
   ) =>
-  (codeSignalOrError: number | NodeJS.Signals | Error) => {
-    if (typeof devServer !== 'undefined') {
-      console.log('\nshutting down dev server');
-      devServer.close();
-      devServer = undefined;
-    }
+    (codeSignalOrError: number | NodeJS.Signals | Error) => {
+      if (typeof devServer !== 'undefined') {
+        console.log('\nshutting down dev server');
+        devServer.close();
+        devServer = undefined;
+      }
 
-    if (codeSignalOrError instanceof Error) {
-      console.error(codeSignalOrError);
-    }
+      if (codeSignalOrError instanceof Error) {
+        console.error(codeSignalOrError);
+      }
 
-    if (options?.shouldKillProcess) {
-      process.exit(options.killWithErrorCode ? 1 : 0);
-    }
-  };
+      if (options?.shouldKillProcess) {
+        process.exit(options.killWithErrorCode ? 1 : 0);
+      }
+    };
 
 // do something when app is closing
 process.on('exit', makeExitHandler());
