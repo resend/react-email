e3e719befa5a391221ec5b862783a62022d9b84c
gabriel miranda <gabrielmfern@outlook.com>
2025-12-12 18:09:56 -0300

    remove should from tests

--- packages/preview-server/src/utils/run-bundled-code.spec.ts
+++ packages/preview-server/src/utils/run-bundled-code.spec.ts
@@ -3,7 +3,7 @@ import { err, isOk } from './result';
 import { runBundledCode } from './run-bundled-code';
 
 describe('runBundledCode()', () => {
-  it('should run the bundled code in a VM context', async () => {
+  it('runs the bundled code in a VM context', async () => {
     const result = await runBundledCode('export default 42;', 'test.js');
     expect(isOk(result)).toBe(true);
     if (!isOk(result)) {
@@ -18,7 +18,7 @@ describe('runBundledCode()', () => {
     ).toEqual(42);
   });
 
-  it('should return an error if the code throws', async () => {
+  it('returns an error if the code throws', async () => {
     const result = await runBundledCode(
       'throw new Error("Test error");',
       'test.js',
