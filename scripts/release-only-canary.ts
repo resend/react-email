4f8c4948eec88471127752da4fe57a03f9ee0016
gabriel miranda <gabrielmfern@outlook.com>
2026-01-19 15:09:10 -0300

    dont use top level await

--- scripts/release-only-canary.ts
+++ scripts/release-only-canary.ts
@@ -1,12 +1,14 @@
 import { readPreState } from '@changesets/pre';
 import { runScript } from 'nypm';
 
-const preState = await readPreState(process.cwd());
-if (preState?.mode === 'pre') {
-  console.log('Is in prerelease mode, making automated release');
-  await runScript('release');
-} else {
-  console.log(
-    'Was not in prerelease, skipping automated release. To release this you should rebase onto main',
-  );
-}
+(async () => {
+  const preState = await readPreState(process.cwd());
+  if (preState?.mode === 'pre') {
+    console.log('Is in prerelease mode, making automated release');
+    await runScript('release');
+  } else {
+    console.log(
+      'Was not in prerelease, skipping automated release. To release this you should rebase onto main',
+    );
+  }
+})();
