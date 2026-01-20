aa552962ff8789c0551bba4d37991d1ec944709a
gabriel miranda <gabrielmfern@outlook.com>
2026-01-20 12:36:05 -0300

    chore(ci): add missing changes in workflows to use scripts/release.ts

--- scripts/release-only-canary.ts
+++ scripts/release-only-canary.ts
@@ -1,11 +1,10 @@
 import { readPreState } from '@changesets/pre';
-import { runScript } from 'nypm';
 
 (async () => {
   const preState = await readPreState(process.cwd());
   if (preState?.mode === 'pre') {
     console.log('Is in prerelease mode, making automated release');
-    await runScript('release');
+    await import('./release.js');
   } else {
     console.log(
       'Was not in prerelease, skipping automated release. To release this you should rebase onto main',
