5471f0af963bbe60d3be564fbea26a840b6931d4
gabriel miranda <gabrielmfern@outlook.com>
2026-01-22 17:48:48 -0300

    increase install preview server test timeout to 1 minute

--- packages/react-email/src/utils/get-preview-server-location.spec.ts
+++ packages/react-email/src/utils/get-preview-server-location.spec.ts
@@ -3,7 +3,7 @@ import path from 'node:path';
 import { installPreviewServer } from './get-preview-server-location.js';
 import { packageJson } from './packageJson.js';
 
-test.sequential('installPreviewServer()', { timeout: 30_000 }, async () => {
+test.sequential('installPreviewServer()', { timeout: 60_000 }, async () => {
   const testDirectory = path.join(import.meta.dirname, '.test');
   await installPreviewServer(testDirectory, packageJson.version);
   expect(fs.existsSync(testDirectory)).toBe(true);
