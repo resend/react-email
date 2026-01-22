29adcc7c15221feff661572c25a86ffee402ea76
gabriel miranda <gabrielmfern@outlook.com>
2026-01-22 13:51:40 -0300

    increase timeout for install preview server test

--- packages/react-email/src/utils/get-preview-server-location.spec.ts
+++ packages/react-email/src/utils/get-preview-server-location.spec.ts
@@ -3,12 +3,11 @@ import path from 'node:path';
 import { installPreviewServer } from './get-preview-server-location.js';
 import { packageJson } from './packageJson.js';
 
-test.sequential('installPreviewServer()', { timeout: 10_000 }, async () => {
+test.sequential('installPreviewServer()', { timeout: 30_000 }, async () => {
   const testDirectory = path.join(import.meta.dirname, '.test');
   await installPreviewServer(testDirectory, packageJson.version);
   expect(fs.existsSync(testDirectory)).toBe(true);
 
-  // @ts-expect-error The directory should exist at this point
   const importedModule = await import(path.join(testDirectory, 'index.mjs'));
   expect({ ...importedModule }).toEqual({
     version: packageJson.version,
