1150f8024580bbf8841a38eaaf6f6c8609eeb5aa
Gabriel Miranda <gabrielmfern@outlook.com>
2026-01-26 10:00:45 -0300

    fix(react-email): installPreviewServer test failing with unreleased version (#2892)

--- packages/react-email/src/utils/get-preview-server-location.spec.ts
+++ packages/react-email/src/utils/get-preview-server-location.spec.ts
@@ -1,16 +1,17 @@
 import fs from 'node:fs';
 import path from 'node:path';
 import { installPreviewServer } from './get-preview-server-location.js';
-import { packageJson } from './packageJson.js';
 
 test.sequential('installPreviewServer()', { timeout: 60_000 }, async () => {
   const testDirectory = path.join(import.meta.dirname, '.test');
-  await installPreviewServer(testDirectory, packageJson.version);
+  // Pinned version here because the one in `package.json` might be unreleased
+  const version = '5.2.5';
+  await installPreviewServer(testDirectory, version);
   expect(fs.existsSync(testDirectory)).toBe(true);
 
   const importedModule = await import(path.join(testDirectory, 'index.mjs'));
   expect({ ...importedModule }).toEqual({
-    version: packageJson.version,
+    version,
   });
   await fs.promises.rm(testDirectory, { recursive: true, force: true });
 });
