b8b785adf3a83d07656b8a8d0e6fd27939985a6d
gabriel miranda <gabrielmfern@outlook.com>
2026-01-30 16:10:34 -0300

    export things properly in react-email

--- packages/react-email/tsdown.config.ts
+++ packages/react-email/tsdown.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'tsdown';
 
-export default defineConfig({
-  dts: false,
-  entry: ['./src/cli/index.ts'],
-  format: ['esm'],
-  outDir: 'dist/cli',
-});
+export default defineConfig([
+  {
+    dts: false,
+    entry: ['./src/cli/index.ts'],
+    format: ['esm'],
+    outDir: 'dist/cli',
+  },
+  {
+    dts: true,
+    entry: ['./src/index.ts'],
+    format: ['esm', 'cjs'],
+    outDir: 'dist',
+  },
+]);
