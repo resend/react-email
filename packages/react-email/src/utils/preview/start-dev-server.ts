ff83098c2ba61ace1f8ad6884914ef904bb0d441
gabriel miranda <gabrielmfern@outlook.com>
2026-02-06 11:33:50 -0300

    Revert "start dev server changes"
    This reverts commit 0754ca2af175a2d980f8b80519a82bfcd929ca2b.

--- packages/react-email/src/utils/preview/start-dev-server.ts
+++ packages/react-email/src/utils/preview/start-dev-server.ts
@@ -1,19 +1,38 @@
-import { type ChildProcess, spawn } from 'node:child_process';
+import http from 'node:http';
 import path from 'node:path';
+import url from 'node:url';
+import { createJiti } from 'jiti';
 import logSymbols from 'log-symbols';
 import ora from 'ora';
 import { registerSpinnerAutostopping } from '../../utils/register-spinner-autostopping.js';
 import { conf } from '../conf.js';
 import { getPreviewServerLocation } from '../get-preview-server-location.js';
+import { packageJson } from '../packageJson.js';
+import { styleText } from '../style-text.js';
 import { getEnvVariablesForPreviewApp } from './get-env-variables-for-preview-app.js';
+import { serveStaticFile } from './serve-static-file.js';
 
-let appProcess: ChildProcess | undefined;
+let devServer: http.Server | undefined;
+
+const safeAsyncServerListen = (server: http.Server, port: number) => {
+  return new Promise<{ portAlreadyInUse: boolean }>((resolve) => {
+    server.listen(port, () => {
+      resolve({ portAlreadyInUse: false });
+    });
+
+    server.on('error', (e: NodeJS.ErrnoException) => {
+      if (e.code === 'EADDRINUSE') {
+        resolve({ portAlreadyInUse: true });
+      }
+    });
+  });
+};
 
 export const startDevServer = async (
   emailsDirRelativePath: string,
   staticBaseDirRelativePath: string,
   port: number,
-): Promise<ChildProcess> => {
+): Promise<http.Server> => {
   const [majorNodeVersion] = process.versions.node.split('.');
   if (majorNodeVersion && Number.parseInt(majorNodeVersion, 10) < 20) {
     console.error(
@@ -23,6 +42,75 @@ export const startDevServer = async (
   }
 
   const previewServerLocation = await getPreviewServerLocation();
+  const previewServer = createJiti(previewServerLocation);
+
+  devServer = http.createServer((req, res) => {
+    if (!req.url) {
+      res.end(404);
+      return;
+    }
+
+    const parsedUrl = url.parse(req.url, true);
+
+    // Never cache anything to avoid
+    res.setHeader(
+      'Cache-Control',
+      'no-cache, max-age=0, must-revalidate, no-store',
+    );
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '-1');
+
+    try {
+      if (
+        parsedUrl.path?.includes('static/') &&
+        !parsedUrl.path.includes('_next/static/')
+      ) {
+        void serveStaticFile(res, parsedUrl, staticBaseDirRelativePath);
+      } else if (!isNextReady) {
+        void nextReadyPromise.then(() =>
+          nextHandleRequest?.(req, res, parsedUrl),
+        );
+      } else {
+        void nextHandleRequest?.(req, res, parsedUrl);
+      }
+    } catch (e) {
+      console.error('caught error', e);
+
+      res.writeHead(500);
+      res.end();
+    }
+  });
+
+  const { portAlreadyInUse } = await safeAsyncServerListen(devServer, port);
+
+  if (!portAlreadyInUse) {
+    console.log(
+      styleText('greenBright', `    React Email ${packageJson.version}`),
+    );
+    console.log(`    Running preview at:          http://localhost:${port}\n`);
+  } else {
+    const nextPortToTry = port + 1;
+    console.warn(
+      ` ${logSymbols.warning} Port ${port} is already in use, trying ${nextPortToTry}`,
+    );
+    return startDevServer(
+      emailsDirRelativePath,
+      staticBaseDirRelativePath,
+      nextPortToTry,
+    );
+  }
+
+  devServer.on('close', async () => {
+    await app.close();
+  });
+
+  devServer.on('error', (e: NodeJS.ErrnoException) => {
+    spinner.stopAndPersist({
+      symbol: logSymbols.error,
+      text: `Preview Server had an error: ${e}`,
+    });
+    process.exit(1);
+  });
 
   const spinner = ora({
     text: 'Getting react-email preview server ready...\n',
@@ -32,35 +120,59 @@ export const startDevServer = async (
   registerSpinnerAutostopping(spinner);
   const timeBeforeNextReady = performance.now();
 
-  appProcess = spawn(
-    process.execPath,
-    [path.resolve(previewServerLocation, '.next/standalone/server.js')],
+  // these environment variables are used on the next app
+  // this is the most reliable way of communicating these paths through
+  process.env = {
+    NODE_ENV: 'development',
+    ...(process.env as Omit<NodeJS.ProcessEnv, 'NODE_ENV'> & {
+      NODE_ENV?: NodeJS.ProcessEnv['NODE_ENV'];
+    }),
+    ...getEnvVariablesForPreviewApp(
+      // If we don't do normalization here, stuff like https://github.com/resend/react-email/issues/1354 happens.
+      path.normalize(emailsDirRelativePath),
+      previewServerLocation,
+      process.cwd(),
+      conf.get('resendApiKey'),
+    ),
+  };
+
+  const next = await previewServer.import<typeof import('next')['default']>(
+    'next',
     {
-      shell: true,
-      stdio: 'inherit',
-      env: {
-        NODE_ENV: 'development',
-        PORT: port.toString(),
-        ...(process.env as Omit<NodeJS.ProcessEnv, 'NODE_ENV'> & {
-          NODE_ENV?: NodeJS.ProcessEnv['NODE_ENV'];
-        }),
-        ...getEnvVariablesForPreviewApp(
-          // If we don't do normalization here, stuff like https://github.com/resend/react-email/issues/1354 happens.
-          path.normalize(emailsDirRelativePath),
-          previewServerLocation,
-          process.cwd(),
-          conf.get('resendApiKey'),
-        ),
-      },
+      default: true,
     },
   );
 
-  appProcess.on('error', (err) => {
+  const app = next({
+    dev: false,
+    conf: {
+      // passing in env here does not get the environment variables there
+      images: {
+        // This is to avoid the warning with sharp
+        unoptimized: true,
+      },
+    },
+    hostname: 'localhost',
+    port,
+    dir: previewServerLocation,
+  });
+
+  let isNextReady = false;
+  const nextReadyPromise = app.prepare();
+  try {
+    await nextReadyPromise;
+  } catch (exception) {
     spinner.stopAndPersist({
       symbol: logSymbols.error,
-      text: ` Preview Server had an error: ${err}`,
+      text: ` Preview Server had an error: ${exception}`,
     });
-  });
+    process.exit(1);
+  }
+  isNextReady = true;
+
+  const nextHandleRequest:
+    | ReturnType<typeof app.getRequestHandler>
+    | undefined = app.getRequestHandler();
 
   const secondsToNextReady = (
     (performance.now() - timeBeforeNextReady) /
@@ -72,7 +184,7 @@ export const startDevServer = async (
     symbol: logSymbols.success,
   });
 
-  return appProcess;
+  return devServer;
 };
 
 // based on https://stackoverflow.com/a/14032965
@@ -82,24 +194,21 @@ const makeExitHandler =
       | { shouldKillProcess: false }
       | { shouldKillProcess: true; killWithErrorCode: boolean },
   ) =>
-    (codeSignalOrError: number | NodeJS.Signals | Error) => {
-      const isError = codeSignalOrError instanceof Error;
-      if (typeof appProcess !== 'undefined') {
-        console.log('\nshutting down dev server');
-        if (!isError) {
-          appProcess.kill(codeSignalOrError);
-        }
-        appProcess = undefined;
-      }
+  (codeSignalOrError: number | NodeJS.Signals | Error) => {
+    if (typeof devServer !== 'undefined') {
+      console.log('\nshutting down dev server');
+      devServer.close();
+      devServer = undefined;
+    }
 
-      if (isError) {
-        console.error(codeSignalOrError);
-      }
+    if (codeSignalOrError instanceof Error) {
+      console.error(codeSignalOrError);
+    }
 
-      if (options?.shouldKillProcess) {
-        process.exit(options.killWithErrorCode ? 1 : 0);
-      }
-    };
+    if (options?.shouldKillProcess) {
+      process.exit(options.killWithErrorCode ? 1 : 0);
+    }
+  };
 
 // do something when app is closing
 process.on('exit', makeExitHandler());
