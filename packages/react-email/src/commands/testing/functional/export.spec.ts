6515641c4224fa902132e3c0e711a2f1e74bdee4
Dmitry Romashov <framebassman@gmail.com>
2026-02-10 13:49:38 +0100

    Fix?

--- packages/react-email/src/commands/testing/functional/export.spec.ts
+++ packages/react-email/src/commands/testing/functional/export.spec.ts
@@ -1,5 +1,6 @@
 import fs from 'node:fs';
 import path from 'node:path';
+import { expect, test } from 'vitest';
 import { exportTemplates } from '../../export.js';
 
 test('email export', { retry: 3 }, async () => {
