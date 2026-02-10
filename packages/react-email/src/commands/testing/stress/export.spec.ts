d4cb3fb64929a88631601f8b8f8612c0d4549664
Dmitry Romashov <framebassman@gmail.com>
2026-02-10 13:01:59 +0100

    Test

--- packages/react-email/src/commands/testing/stress/export.spec.ts
+++ packages/react-email/src/commands/testing/stress/export.spec.ts
@@ -1,4 +1,3 @@
-import fsPromises from 'node:fs/promises';
 import fs from 'node:fs';
 import path from 'node:path';
 import { exportTemplates } from '../../export.js';
@@ -7,26 +6,23 @@ test('email export', async () => {
   const emailsQuantity = 10;
   const emailsDir = path.join(__dirname, 'emails');
   const srcDir = path.join(__dirname, 'src');
-  await fsPromises.rm(emailsDir);
-  const templates = (await fsPromises.readdir(srcDir))
-    .filter(file => file.endsWith('.tsx'));
+  const outDir = path.join(__dirname, 'out');
+
+  fs.rmSync(emailsDir, { force: true });
+
+  const templates = fs.readdirSync(srcDir).filter((file) =>
+    file.endsWith('.tsx'),
+  );
+
   for (let i = 0; i < emailsQuantity; i++) {
     const template = String(templates[i % templates.length]);
     const { name } = path.parse(template);
     const source = path.join(srcDir, template);
-    const destination = path.join(
-      emailsDir,
-      `${i}_${name}.tsx`
-    );
-
-    await fsPromises.cp(source, destination);
+    const destination = path.join(emailsDir, `${i}_${name}.tsx`);
+    fs.cpSync(source, destination);
   }
-  const pathToEmailsDirectory = path.resolve(__dirname, './emails');
-  const pathToDumpMarkup = path.resolve(__dirname, './out');
-  await exportTemplates(pathToDumpMarkup, pathToEmailsDirectory, {
-    silent: true,
-    pretty: true,
-  });
 
-  expect(fs.existsSync(pathToDumpMarkup)).toBe(true);
+  await exportTemplates(outDir, emailsDir, { silent: true, pretty: true });
+
+  expect(fs.existsSync(outDir)).toBe(true);
 });
