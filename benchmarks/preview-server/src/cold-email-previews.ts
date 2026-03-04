46da68d42fd531f66babbbf082e675302c50b78b
gabriel miranda <gabrielmfern@outlook.com>
2026-03-04 18:14:55 -0300

    simplify preview server benchs

---
+++ benchmarks/preview-server/src/cold-email-previews.ts
@@ -0,0 +1,30 @@
+import { promises as fs } from 'node:fs';
+import path from 'node:path';
+import { Bench } from 'tinybench';
+import { runServer } from './utils/run-server';
+
+const pathToLocalCliScript = path.resolve(
+  __dirname,
+  '../',
+  './node_modules/react-email/dist/cli/index.js',
+);
+
+(async () => {
+  const bench = new Bench({
+    iterations: 30,
+  });
+
+  bench.add('coldemail previews', async () => {
+    const server = await runServer(pathToLocalCliScript);
+    await fetch(`${server.url}/preview/magic-links/notion-magic-link`);
+    server.subprocess.kill();
+  });
+
+  await bench.run();
+
+  await fs.writeFile(
+    'bench-results-30-iterations.json',
+    JSON.stringify(bench.results),
+    'utf8',
+  );
+})();
