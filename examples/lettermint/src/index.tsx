efb39f9c4eb7da4fdf86c35820ced2d6989250c4
Glennmen <me@glennmen.dev>
2026-03-10 17:13:35 +0100

    feat: add Lettermint integration
    Add docs and example for sending emails with the Lettermint Node.js SDK.

---
+++ examples/lettermint/src/index.tsx
@@ -0,0 +1,14 @@
+import { render } from '@react-email/components';
+import { Lettermint } from 'lettermint';
+import { Email } from './email';
+
+const lettermint = new Lettermint({ apiToken: process.env.LETTERMINT_API_TOKEN || '' });
+
+const emailHtml = await render(<Email url="https://example.com" />);
+
+await lettermint.email
+  .from('you@example.com')
+  .to('user@gmail.com')
+  .subject('hello world')
+  .html(emailHtml)
+  .send();
