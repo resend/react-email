a732a14b9d3b3f047c8bfc3fb075d272616a6def
Gabriel Miranda <gabrielmfern@outlook.com>
2026-03-12 17:58:14 -0300

    improve

--- packages/editor/src/extensions/index.ts
+++ packages/editor/src/extensions/index.ts
@@ -1,5 +1,7 @@
-import { type AnyExtension, Extension } from '@tiptap/core';
-import TipTapStarterKit from '@tiptap/starter-kit';
+import { type AnyExtension, type Extendable, Extension } from '@tiptap/core';
+import TipTapStarterKit, {
+  type StarterKitOptions as TipTapStarterKitOptions,
+} from '@tiptap/starter-kit';
 import { AlignmentAttribute } from './alignment-attribute';
 import { Blockquote } from './blockquote';
 import { Body } from './body';
@@ -58,233 +60,185 @@ export * from './sup';
 export * from './table';
 export * from './uppercase';
 
-type ExtensionOptions<T extends AnyExtension> = T extends {
-  configure: (options?: infer O) => AnyExtension;
-}
-  ? O
-  : never;
+type StarterKitExtensionMap = typeof starterKitExtensions;
 
-type StarterKitExtensionOption<T extends AnyExtension> =
-  | ExtensionOptions<T>
-  | false;
-
-const alignmentTypes = [
-  'heading',
-  'paragraph',
-  'image',
-  'blockquote',
-  'codeBlock',
-  'bulletList',
-  'orderedList',
-  'listItem',
-  'button',
-  'youtube',
-  'twitter',
-  'table',
-  'tableRow',
-  'tableCell',
-  'tableHeader',
-  'columnsColumn',
-] as const;
-
-const styleTypes = [
-  'heading',
-  'paragraph',
-  'image',
-  'blockquote',
-  'codeBlock',
-  'bulletList',
-  'orderedList',
-  'listItem',
-  'button',
-  'youtube',
-  'twitter',
-  'horizontalRule',
-  'footer',
-  'section',
-  'div',
-  'body',
-  'table',
-  'tableRow',
-  'tableCell',
-  'tableHeader',
-  'columnsColumn',
-  'link',
-] as const;
-
-const classTypes = [
-  'heading',
-  'paragraph',
-  'image',
-  'blockquote',
-  'bulletList',
-  'orderedList',
-  'listItem',
-  'button',
-  'youtube',
-  'twitter',
-  'horizontalRule',
-  'footer',
-  'section',
-  'div',
-  'body',
-  'table',
-  'tableRow',
-  'tableCell',
-  'tableHeader',
-  'columnsColumn',
-  'link',
-] as const;
-
-const tipTapStarterKitDefaults: ExtensionOptions<typeof TipTapStarterKit> = {
-  // Collaboration extensions handle history separately.
-  undoRedo: false,
-  heading: false,
-  link: false,
-  underline: false,
-  trailingNode: false,
-  bold: false,
-  italic: false,
-  strike: false,
-  code: false,
-  paragraph: false,
-  bulletList: false,
-  orderedList: false,
-  listItem: false,
-  blockquote: false,
-  hardBreak: false,
-  gapcursor: false,
-  codeBlock: false,
-  horizontalRule: false,
-  dropcursor: {
-    color: '#61a8f8',
-    class: 'rounded-full animate-[fade-in_300ms_ease-in-out] !z-40',
-    width: 4,
-  },
+const starterKitExtensions = {
+  CodeBlockPrism,
+  Code,
+  Paragraph,
+  BulletList,
+  OrderedList,
+  Blockquote,
+  ListItem,
+  HardBreak,
+  Italic,
+  Placeholder,
+  PreviewText,
+  Bold,
+  Strike,
+  Heading,
+  Divider,
+  Link,
+  Sup,
+  Uppercase,
+  PreservedStyle,
+  Table,
+  TableRow,
+  TableCell,
+  TableHeader,
+  Body,
+  Div,
+  Button,
+  Section,
+  GlobalContent,
+  AlignmentAttribute,
+  StyleAttribute,
+  ClassAttribute,
+  MaxNesting,
 };
 
-export interface StarterKitOptions {
-  tiptapStarterKit: StarterKitExtensionOption<typeof TipTapStarterKit>;
-  codeBlock: StarterKitExtensionOption<typeof CodeBlockPrism>;
-  code: StarterKitExtensionOption<typeof Code>;
-  paragraph: StarterKitExtensionOption<typeof Paragraph>;
-  bulletList: StarterKitExtensionOption<typeof BulletList>;
-  orderedList: StarterKitExtensionOption<typeof OrderedList>;
-  blockquote: StarterKitExtensionOption<typeof Blockquote>;
-  listItem: StarterKitExtensionOption<typeof ListItem>;
-  hardBreak: StarterKitExtensionOption<typeof HardBreak>;
-  italic: StarterKitExtensionOption<typeof Italic>;
-  placeholder: StarterKitExtensionOption<typeof Placeholder>;
-  previewText: StarterKitExtensionOption<typeof PreviewText>;
-  bold: StarterKitExtensionOption<typeof Bold>;
-  strike: StarterKitExtensionOption<typeof Strike>;
-  heading: StarterKitExtensionOption<typeof Heading>;
-  divider: StarterKitExtensionOption<typeof Divider>;
-  link: StarterKitExtensionOption<typeof Link>;
-  sup: StarterKitExtensionOption<typeof Sup>;
-  uppercase: StarterKitExtensionOption<typeof Uppercase>;
-  preservedStyle: StarterKitExtensionOption<typeof PreservedStyle>;
-  table: StarterKitExtensionOption<typeof Table>;
-  tableRow: StarterKitExtensionOption<typeof TableRow>;
-  tableCell: StarterKitExtensionOption<typeof TableCell>;
-  tableHeader: StarterKitExtensionOption<typeof TableHeader>;
-  body: StarterKitExtensionOption<typeof Body>;
-  div: StarterKitExtensionOption<typeof Div>;
-  button: StarterKitExtensionOption<typeof Button>;
-  section: StarterKitExtensionOption<typeof Section>;
-  globalContent: StarterKitExtensionOption<typeof GlobalContent>;
-  alignmentAttribute: StarterKitExtensionOption<typeof AlignmentAttribute>;
-  styleAttribute: StarterKitExtensionOption<typeof StyleAttribute>;
-  classAttribute: StarterKitExtensionOption<typeof ClassAttribute>;
-  maxNesting: StarterKitExtensionOption<typeof MaxNesting>;
-}
-
-function pushConfiguredExtension<T extends AnyExtension>(
-  extensions: AnyExtension[],
-  extension: T,
-  options: StarterKitExtensionOption<T>,
-) {
-  if (options === false) {
-    return;
-  }
-  extensions.push(extension.configure(options as ExtensionOptions<T>));
-}
-
-type ConfiguredExtension = readonly [
-  AnyExtension,
-  StarterKitExtensionOption<AnyExtension>,
-];
+export type StarterKitOptions = {
+  [Key in keyof StarterKitExtensionMap]: StarterKitExtensionMap[Key] extends Extendable<
+    infer Options
+  >
+    ? Partial<Options> | false
+    : never;
+} & {
+  TiptapStarterKit: Partial<TipTapStarterKitOptions> | false;
+};
 
 export const StarterKit = Extension.create<StarterKitOptions>({
   name: 'reactEmailStarterKit',
 
   addOptions() {
     return {
-      tiptapStarterKit: tipTapStarterKitDefaults,
-      codeBlock: {
+      TiptapStarterKit: {},
+      CodeBlockPrism: {
         defaultLanguage: 'javascript',
         HTMLAttributes: {
           class: 'prism node-codeBlock',
         },
       },
-      code: {
+      Code: {
         HTMLAttributes: {
           class: 'node-inlineCode',
           spellcheck: 'false',
         },
       },
-      paragraph: {
+      Paragraph: {
         HTMLAttributes: {
           class: 'node-paragraph',
         },
       },
-      bulletList: {
+      BulletList: {
         HTMLAttributes: {
           class: 'node-bulletList',
         },
       },
-      orderedList: {
+      OrderedList: {
         HTMLAttributes: {
           class: 'node-orderedList',
         },
       },
-      blockquote: {
+      Blockquote: {
         HTMLAttributes: {
           class: 'node-blockquote',
         },
       },
-      listItem: {},
-      hardBreak: {},
-      italic: {},
-      placeholder: {},
-      previewText: {},
-      bold: {},
-      strike: {},
-      heading: {},
-      divider: {},
-      link: {},
-      sup: {},
-      uppercase: {},
-      preservedStyle: {},
-      table: {},
-      tableRow: {},
-      tableCell: {},
-      tableHeader: {},
-      body: {},
-      div: {},
-      button: {},
-      section: {},
-      globalContent: {},
-      alignmentAttribute: {
-        types: [...alignmentTypes],
+      ListItem: {},
+      HardBreak: {},
+      Italic: {},
+      Placeholder: {},
+      PreviewText: {},
+      Bold: {},
+      Strike: {},
+      Heading: {},
+      Divider: {},
+      Link: {},
+      Sup: {},
+      Uppercase: {},
+      PreservedStyle: {},
+      Table: {},
+      TableRow: {},
+      TableCell: {},
+      TableHeader: {},
+      Body: {},
+      Div: {},
+      Button: {},
+      Section: {},
+      GlobalContent: {},
+      AlignmentAttribute: {
+        types: [
+          'heading',
+          'paragraph',
+          'image',
+          'blockquote',
+          'codeBlock',
+          'bulletList',
+          'orderedList',
+          'listItem',
+          'button',
+          'youtube',
+          'twitter',
+          'table',
+          'tableRow',
+          'tableCell',
+          'tableHeader',
+          'columnsColumn',
+        ],
       },
-      styleAttribute: {
-        types: [...styleTypes],
+      StyleAttribute: {
+        types: [
+          'heading',
+          'paragraph',
+          'image',
+          'blockquote',
+          'codeBlock',
+          'bulletList',
+          'orderedList',
+          'listItem',
+          'button',
+          'youtube',
+          'twitter',
+          'horizontalRule',
+          'footer',
+          'section',
+          'div',
+          'body',
+          'table',
+          'tableRow',
+          'tableCell',
+          'tableHeader',
+          'columnsColumn',
+          'link',
+        ],
       },
-      classAttribute: {
-        types: [...classTypes],
+      ClassAttribute: {
+        types: [
+          'heading',
+          'paragraph',
+          'image',
+          'blockquote',
+          'bulletList',
+          'orderedList',
+          'listItem',
+          'button',
+          'youtube',
+          'twitter',
+          'horizontalRule',
+          'footer',
+          'section',
+          'div',
+          'body',
+          'table',
+          'tableRow',
+          'tableCell',
+          'tableHeader',
+          'columnsColumn',
+          'link',
+        ],
       },
-      maxNesting: {
+      MaxNesting: {
         maxDepth: 50,
         nodeTypes: ['section', 'bulletList', 'orderedList'],
       },
@@ -294,53 +248,47 @@ export const StarterKit = Extension.create<StarterKitOptions>({
   addExtensions() {
     const extensions: AnyExtension[] = [];
 
-    if (this.options.tiptapStarterKit !== false) {
+    if (this.options.TiptapStarterKit !== false) {
       extensions.push(
         TipTapStarterKit.configure({
-          ...tipTapStarterKitDefaults,
-          ...this.options.tiptapStarterKit,
+          // Collaboration extensions handle history separately.
+          undoRedo: false,
+          heading: false,
+          link: false,
+          underline: false,
+          trailingNode: false,
+          bold: false,
+          italic: false,
+          strike: false,
+          code: false,
+          paragraph: false,
+          bulletList: false,
+          orderedList: false,
+          listItem: false,
+          blockquote: false,
+          hardBreak: false,
+          gapcursor: false,
+          codeBlock: false,
+          horizontalRule: false,
+          dropcursor: {
+            color: '#61a8f8',
+            class: 'rounded-full animate-[fade-in_300ms_ease-in-out] !z-40',
+            width: 4,
+          },
+          ...this.options.TiptapStarterKit,
         }),
       );
     }
 
-    const configuredExtensions: ConfiguredExtension[] = [
-      [CodeBlockPrism, this.options.codeBlock],
-      [Code, this.options.code],
-      [Paragraph, this.options.paragraph],
-      [BulletList, this.options.bulletList],
-      [OrderedList, this.options.orderedList],
-      [Blockquote, this.options.blockquote],
-      [ListItem, this.options.listItem],
-      [HardBreak, this.options.hardBreak],
-      [Italic, this.options.italic],
-      [Placeholder, this.options.placeholder],
-      [PreviewText, this.options.previewText],
-      [Bold, this.options.bold],
-      [Strike, this.options.strike],
-      [Heading, this.options.heading],
-      [Divider, this.options.divider],
-      [Link, this.options.link],
-      [Sup, this.options.sup],
-      [Uppercase, this.options.uppercase],
-      [PreservedStyle, this.options.preservedStyle],
-      [Table, this.options.table],
-      [TableRow, this.options.tableRow],
-      [TableCell, this.options.tableCell],
-      [TableHeader, this.options.tableHeader],
-      [Body, this.options.body],
-      [Div, this.options.div],
-      [Button, this.options.button],
-      [Section, this.options.section],
-      [GlobalContent, this.options.globalContent],
-      [AlignmentAttribute, this.options.alignmentAttribute],
-      [StyleAttribute, this.options.styleAttribute],
-      [ClassAttribute, this.options.classAttribute],
-      [MaxNesting, this.options.maxNesting],
-    ];
-
-    configuredExtensions.forEach(([extension, options]) => {
-      pushConfiguredExtension(extensions, extension, options);
-    });
+    for (const [name, extension] of Object.entries(starterKitExtensions)) {
+      const key = name as keyof StarterKitExtensionMap;
+      const extensionOptions = this.options[key];
+      if (extensionOptions !== false) {
+        extensions.push(
+          (extension as AnyExtension).configure(extensionOptions),
+        );
+      }
+    }
 
     return extensions;
   },
