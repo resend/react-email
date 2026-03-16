7b9981ee476b89a0654568ce6a7c32bbd8826ced
gabriel miranda <gabrielmfern@outlook.com>
2026-03-16 12:46:41 -0300

    fix regression add test to avoid it

--- packages/editor/src/extensions/bold.tsx
+++ packages/editor/src/extensions/bold.tsx
@@ -4,7 +4,26 @@ import { EmailMark } from '../core/serializer/email-mark';
 
 export type BoldOptions = TipTapBoldOptions;
 
+const BoldWithoutFontWeightInference = BoldBase.extend({
+  parseHTML() {
+    return [
+      {
+        tag: 'strong',
+      },
+      {
+        tag: 'b',
+        getAttrs: (node) =>
+          (node as HTMLElement).style.fontWeight !== 'normal' && null,
+      },
+      {
+        style: 'font-weight=400',
+        clearMark: (mark) => mark.type.name === this.name,
+      },
+    ];
+  },
+});
+
 export const Bold: EmailMark<TipTapBoldOptions, any> = EmailMark.from(
-  BoldBase,
+  BoldWithoutFontWeightInference,
   ({ children, style }) => <strong style={style}>{children}</strong>,
 );
