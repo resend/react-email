e71022e043df9d9129edaae1d737884933f7b860
gabriel miranda <gabrielmfern@outlook.com>
2026-03-17 13:14:19 -0300

    simplify Code

--- packages/editor/src/extensions/code.tsx
+++ packages/editor/src/extensions/code.tsx
@@ -1,13 +1,9 @@
-import type { CodeOptions } from '@tiptap/extension-code';
 import CodeBase from '@tiptap/extension-code';
 import { EmailMark } from '../core/serializer/email-mark';
 import { inlineCssToJs } from '../utils/styles';
 
-export const Code: EmailMark<CodeOptions, any> = EmailMark.from(
-  CodeBase,
-  ({ children, node, style }) => (
-    <code style={{ ...style, ...inlineCssToJs(node.attrs?.style) }}>
-      {children}
-    </code>
-  ),
-);
+export const Code = EmailMark.from(CodeBase, ({ children, node, style }) => (
+  <code style={{ ...style, ...inlineCssToJs(node.attrs?.style) }}>
+    {children}
+  </code>
+));
