7609ba23f9dccf57e4813e20aa8eece7a8248d90
gabriel miranda <gabrielmfern@outlook.com>
2026-03-26 11:08:08 -0300

    remove old API

--- packages/editor/src/ui/inspector/index.tsx
+++ packages/editor/src/ui/inspector/index.tsx
@@ -1,17 +1,13 @@
 'use client';
 
 import { InspectorBreadcrumb } from './breadcrumb';
-import { DefaultField } from './default-field';
 import { InspectorDocument } from './document';
-import { InspectorPanel } from './panel';
 import { InspectorProvider } from './provider';
 
 export const Inspector = {
   Provider: InspectorProvider,
   Breadcrumb: InspectorBreadcrumb,
-  Panel: InspectorPanel,
   Document: InspectorDocument,
-  DefaultField,
 };
 
 export type { InspectorDocumentProps } from './document';
