846c568636be47ed346056eb5e6e96a8b5773472
gabriel miranda <gabrielmfern@outlook.com>
2026-03-26 18:04:42 -0300

    add example back into index

--- packages/editor/examples/src/examples/index.ts
+++ packages/editor/examples/src/examples/index.ts
@@ -3,6 +3,7 @@ import { BubbleMenuExample } from './bubble-menu';
 import { ColumnLayouts } from './column-layouts';
 import { CustomBubbleMenu } from './custom-bubble-menu';
 import { CustomExtensions } from './custom-extensions';
+import { DocumentInspector } from './document-inspector';
 import { EmailExport } from './email-export';
 import { EmailThemingExample } from './email-theming';
 import { FullEmailBuilder } from './full-email-builder';
@@ -94,6 +95,11 @@ export const sections: ExampleSection[] = [
         id: 'full-email-builder',
         label: 'Full Email Builder',
         component: FullEmailBuilder,
+      },
+      {
+        id: 'document-inspector',
+        label: 'Document Inspector',
+        component: DocumentInspector,
       },
     ],
   },
