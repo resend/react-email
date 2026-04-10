09deadc88935468eb44d7749127e7a279cda6f4f
gabriel miranda <gabrielmfern@outlook.com>
2026-04-10 15:09:43 -0300

    movemove cli to the cli dir

--- packages/react-email/tsdown.config.ts
+++ packages/react-email/tsdown.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'tsdown';
 
-export default defineConfig({
-  dts: false,
-  entry: ['./src/index.ts'],
-  format: ['esm'],
-  outDir: 'dist',
-});
+export default defineConfig([
+  {
+    dts: false,
+    entry: ['./src/cli/index.ts'],
+    format: ['esm'],
+    outDir: 'dist/cli',
+  },
+]);
