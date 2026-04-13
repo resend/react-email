d349ac009981dff83e4dda0b221eda6d8e8b0771
Gabriel Miranda <gabriel@resend.com>
2026-04-13 12:26:15 -0300

    refactor: colocate @react-email/column source in react-email (#3236)

--- packages/react-email/src/components/index.ts
+++ packages/react-email/src/components/index.ts
@@ -2,3 +2,4 @@ export * from './body/index.js';
 export * from './button/index.js';
 export * from './code-block/index.js';
 export * from './code-inline/index.js';
+export * from './column/index.js';
