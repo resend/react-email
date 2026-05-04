e67008935ef290948ea4b45ff7f72cd0a1ac12b1
dielduarte <magdiel@resend.com>
2026-05-04 14:01:00 -0300

    revert reusing function to parseCode

--- packages/ui/src/utils/caniemail/tailwind/get-tailwind-config.spec.ts
+++ packages/ui/src/utils/caniemail/tailwind/get-tailwind-config.spec.ts
@@ -4,14 +4,6 @@ import { parse } from '@babel/parser';
 import { pixelBasedPreset } from 'react-email';
 import { getTailwindConfig } from './get-tailwind-config';
 
-const parseSource = (sourceCode: string) =>
-  parse(sourceCode, {
-    strictMode: false,
-    errorRecovery: true,
-    sourceType: 'unambiguous',
-    plugins: ['jsx', 'typescript', 'decorators'],
-  });
-
 describe('getTailwindConfig()', () => {
   it('works with email templates that import the tailwind config', async () => {
     const sourcePath = path.resolve(
@@ -19,7 +11,12 @@ describe('getTailwindConfig()', () => {
       './tests/dummy-email-template.tsx',
     );
     const sourceCode = await fs.readFile(sourcePath, 'utf8');
-    const ast = parseSource(sourceCode);
+    const ast = parse(sourceCode, {
+      strictMode: false,
+      errorRecovery: true,
+      sourceType: 'unambiguous',
+      plugins: ['jsx', 'typescript', 'decorators'],
+    });
 
     expect(await getTailwindConfig(sourceCode, ast, sourcePath)).toEqual({
       theme: {},
@@ -33,7 +30,12 @@ describe('getTailwindConfig()', () => {
       './tests/dummy-email-template-inline-config.tsx',
     );
     const sourceCode = await fs.readFile(sourcePath, 'utf8');
-    const ast = parseSource(sourceCode);
+    const ast = parse(sourceCode, {
+      strictMode: false,
+      errorRecovery: true,
+      sourceType: 'unambiguous',
+      plugins: ['jsx', 'typescript', 'decorators'],
+    });
 
     expect(await getTailwindConfig(sourceCode, ast, sourcePath)).toEqual({
       theme: {},
@@ -47,7 +49,12 @@ describe('getTailwindConfig()', () => {
       './tests/dummy-email-template-shadowed-config.tsx',
     );
     const sourceCode = await fs.readFile(sourcePath, 'utf8');
-    const ast = parseSource(sourceCode);
+    const ast = parse(sourceCode, {
+      strictMode: false,
+      errorRecovery: true,
+      sourceType: 'unambiguous',
+      plugins: ['jsx', 'typescript', 'decorators'],
+    });
 
     expect(await getTailwindConfig(sourceCode, ast, sourcePath)).toEqual({
       theme: {},
@@ -61,7 +68,12 @@ describe('getTailwindConfig()', () => {
       './tests/dummy-email-template-empty-config.tsx',
     );
     const sourceCode = await fs.readFile(sourcePath, 'utf8');
-    const ast = parseSource(sourceCode);
+    const ast = parse(sourceCode, {
+      strictMode: false,
+      errorRecovery: true,
+      sourceType: 'unambiguous',
+      plugins: ['jsx', 'typescript', 'decorators'],
+    });
 
     expect(await getTailwindConfig(sourceCode, ast, sourcePath)).toEqual({});
   });
