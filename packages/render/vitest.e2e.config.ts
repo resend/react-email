7fb2e7b818096da3505a6581621ded1a7fe1996b
Gabriel Miranda <gabriel@resend.com>
2026-05-08 18:03:51 -0300

    add test:e2e script
    Signed-off-by: Gabriel Miranda <gabriel@resend.com>

---
+++ packages/render/vitest.e2e.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    include: ['**/e2e/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
+    exclude: ['**/node_modules/**'],
+  },
+});
