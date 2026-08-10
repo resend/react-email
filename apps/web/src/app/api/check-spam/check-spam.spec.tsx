c5deffcf4b3916f9ee85e61c9fdc02ed96c5bd15
Vitor Capretz <capretzvitor@gmail.com>
2026-08-10 12:57:35 -0300

    skip flaky test

--- apps/web/src/app/api/check-spam/check-spam.spec.tsx
+++ apps/web/src/app/api/check-spam/check-spam.spec.tsx
@@ -2,10 +2,10 @@ import { render } from 'react-email';
 import { checkSpam } from './check-spam';
 import { StripeWelcomeEmail } from './testing/stripe-welcome-email';
 
-const host = process.env.SPAM_ASSASSIN_HOST;
-const port = process.env.SPAM_ASSASSIN_PORT;
+// const host = process.env.SPAM_ASSASSIN_HOST;
+// const port = process.env.SPAM_ASSASSIN_PORT;
 
-describe.skipIf(!host || !port)('checkSpam()', { timeout: 10_000 }, () => {
+describe.skip('checkSpam()', { timeout: 10_000 }, () => {
   test('with most spammy email', async () => {
     const template = (
       <html lang="en">
