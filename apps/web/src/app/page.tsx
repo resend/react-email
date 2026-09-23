5327901471961d1facde9b5125232b2eb3216522
Pedro Ivo Hudson <ivo.pedro@gmail.com>
2026-09-23 14:41:33 -0300

    feat(web): add AI section to homepage

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Footer } from '@/components/footer';
+import AISection from '@/components/sections/ai';
 import { EditorHomepage } from '@/components/sections/editor';
 import HeroSection from '@/components/sections/hero';
 import IntegrationSection from '@/components/sections/integration';
@@ -13,6 +14,7 @@ const Home = () => (
     <HeroSection />
     <div className="relative mx-auto flex flex-col justify-between px-2 md:max-w-7xl md:px-4">
       <PlaygroundSection />
+      <AISection />
       <TestimonialSection />
       <PatternsSection />
       <PrimitivesSection />
